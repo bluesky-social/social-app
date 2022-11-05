5d6ab1f5485ab419e2c2155d91a9585bea8ff219
Paul Frazee <pfrazee@gmail.com>
2022-11-05 15:07:28 -0500

    Unify profile follow btn colors with the rest

--- src/view/com/util/UserBanner.tsx
+++ src/view/com/util/UserBanner.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Svg, {Rect, Defs, LinearGradient, Stop} from 'react-native-svg'
-import {getGradient} from './UserAvatar'
+import {getGradient} from '../../lib/asset-gen'
 
 export function UserBanner({handle}: {handle: string}) {
   const gradient = getGradient(handle)
@@ -12,8 +12,8 @@ export function UserBanner({handle}: {handle: string}) {
           <Stop offset="1" stopColor={gradient[1]} stopOpacity="1" />
         </LinearGradient>
         <LinearGradient id="grad2" x1="0" y1="0" x2="0" y2="1">
-          <Stop offset="0" stopColor="#000" stopOpacity="0" />
-          <Stop offset="1" stopColor="#000" stopOpacity="0.1" />
+          <Stop offset="0" stopColor="#fff" stopOpacity="0" />
+          <Stop offset="1" stopColor="#fff" stopOpacity="0.3" />
         </LinearGradient>
       </Defs>
       <Rect x="0" y="0" width="400" height="100" fill="url(#grad)" />
