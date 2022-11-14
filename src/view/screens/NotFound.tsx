7d39cb5f6b6fc679850a2733a1f1f7d4f47f83a9
Paul Frazee <pfrazee@gmail.com>
2022-11-14 16:27:59 -0600

    Improve not found page

--- src/view/screens/NotFound.tsx
+++ src/view/screens/NotFound.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import {Text, Button, View} from 'react-native'
+import {ViewHeader} from '../com/util/ViewHeader'
 import {useStores} from '../../state'
 
 export const NotFound = () => {
   const stores = useStores()
   return (
-    <View style={{justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 20, fontWeight: 'bold'}}>Page not found</Text>
-      <Button title="Home" onPress={() => stores.nav.navigate('/')} />
+    <View>
+      <ViewHeader title="Page not found" />
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingTop: 100,
+        }}>
+        <Text style={{fontSize: 40, fontWeight: 'bold'}}>Page not found</Text>
+        <Button title="Home" onPress={() => stores.nav.navigate('/')} />
+      </View>
     </View>
   )
 }
