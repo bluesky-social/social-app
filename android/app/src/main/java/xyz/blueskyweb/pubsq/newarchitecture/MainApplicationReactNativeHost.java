51c9a51f8c31c78adbebda2520d3a0267fbad1ec
Paul Frazee <pfrazee@gmail.com>
2022-11-14 13:13:08 -0600

    Change appid to xyz.blueskyweb.app

--- android/app/src/main/java/xyz/blueskyweb/pubsq/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/xyz/blueskyweb/pubsq/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package app.bsky.newarchitecture;
+package xyz.blueskyweb.app.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.EmptyReactNativeConfig;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import app.bsky.BuildConfig;
-import app.bsky.newarchitecture.components.MainComponentsRegistry;
-import app.bsky.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import xyz.blueskyweb.app.BuildConfig;
+import xyz.blueskyweb.app.newarchitecture.components.MainComponentsRegistry;
+import xyz.blueskyweb.app.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
