16c06cd997297e64592d09f4825bce29426fb7b2
Paul Frazee <pfrazee@gmail.com>
2022-11-14 13:36:09 -0600

    Turn 'include reason' on posts into a link

--- src/view/com/posts/FeedItem.tsx
+++ src/view/com/posts/FeedItem.tsx
@@ -65,15 +65,21 @@ export const FeedItem = observer(function FeedItem({
   return (
     <Link style={styles.outer} href={itemHref} title={itemTitle}>
       {item.repostedBy && (
-        <View style={styles.includeReason}>
+        <Link
+          style={styles.includeReason}
+          href={`/profile/${item.repostedBy.handle}`}
+          title={item.repostedBy.displayName || item.repostedBy.handle}>
           <FontAwesomeIcon icon="retweet" style={styles.includeReasonIcon} />
           <Text style={[s.gray4, s.bold, s.f13]}>
             Reposted by {item.repostedBy.displayName || item.repostedBy.handle}
           </Text>
-        </View>
+        </Link>
       )}
       {item.trendedBy && (
-        <View style={styles.includeReason}>
+        <Link
+          style={styles.includeReason}
+          href={`/profile/${item.trendedBy.handle}`}
+          title={item.trendedBy.displayName || item.trendedBy.handle}>
           <FontAwesomeIcon
             icon="arrow-trend-up"
             style={styles.includeReasonIcon}
@@ -81,7 +87,7 @@ export const FeedItem = observer(function FeedItem({
           <Text style={[s.gray4, s.bold, s.f13]}>
             Trending with {item.trendedBy.displayName || item.trendedBy.handle}
           </Text>
-        </View>
+        </Link>
       )}
       <View style={styles.layout}>
         <Link
