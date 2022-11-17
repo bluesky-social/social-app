924c76f11b9e03182c500875ffda73978d56fd29
Paul Frazee <pfrazee@gmail.com>
2022-11-17 11:21:04 -0600

    Add tabs explainer and fix 'black screen' during onboard

--- src/view/lib/assets.ts
+++ src/view/lib/assets.ts
@@ -4,3 +4,6 @@ export const DEF_AVATAR: ImageSourcePropType = {uri: '/img/default-avatar.jpg'}
 export const SCENE_EXPLAINER: ImageSourcePropType = {
   uri: '/img/scene-explainer.jpg',
 }
+export const TABS_EXPLAINER: ImageSourcePropType = {
+  uri: '/img/tabs-explainer.jpg',
+}
