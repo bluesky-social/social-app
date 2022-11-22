1df48d4dad0d6b13047185e37db94997ab36bb4b
Paul Frazee <pfrazee@gmail.com>
2022-11-22 14:30:13 -0600

    Fix to tests

---
+++ __mocks__/async-storage.js
@@ -0,0 +1,1 @@
+export default from '@react-native-async-storage/async-storage/jest/async-storage-mock'
---
+++ jest.js
@@ -0,0 +1,3 @@
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+)
