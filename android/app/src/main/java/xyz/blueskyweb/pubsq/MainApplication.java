2e9a858e3d2ef70c87649a14ac9fa4c65bfa2f83
Paul Frazee <pfrazee@gmail.com>
2022-11-23 15:04:44 -0600

    Remove webcrypto polyfill, as it's no longer needed

--- android/app/src/main/java/xyz/blueskyweb/pubsq/MainApplication.java
+++ android/app/src/main/java/xyz/blueskyweb/pubsq/MainApplication.java
@@ -28,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new AppSecureRandomPackage());
           return packages;
         }
 
--- src/App.native.tsx
+++ src/App.native.tsx
@@ -5,7 +5,6 @@ import {RootSiblingParent} from 'react-native-root-siblings'
 import {GestureHandlerRootView} from 'react-native-gesture-handler'
 import SplashScreen from 'react-native-splash-screen'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
-import {whenWebCrypto} from './platform/polyfills.native'
 import * as view from './view/index'
 import {RootStoreModel, setupState, RootStoreProvider} from './state'
 import {MobileShell} from './view/shell/mobile'
@@ -17,23 +16,19 @@ function App() {
 
   // init
   useEffect(() => {
-    whenWebCrypto
-      .then(() => {
-        view.setup()
-        return setupState()
-      })
-      .then(store => {
-        setRootStore(store)
-        SplashScreen.hide()
-        Linking.getInitialURL().then((url: string | null) => {
-          if (url) {
-            store.nav.handleLink(url)
-          }
-        })
-        Linking.addEventListener('url', ({url}) => {
+    view.setup()
+    setupState().then(store => {
+      setRootStore(store)
+      SplashScreen.hide()
+      Linking.getInitialURL().then((url: string | null) => {
+        if (url) {
           store.nav.handleLink(url)
-        })
+        }
       })
+      Linking.addEventListener('url', ({url}) => {
+        store.nav.handleLink(url)
+      })
+    })
   }, [])
 
   // show nothing prior to init
