9e69d399b72f96069039a8c50a67858e1679b506
Paul Frazee <pfrazee@gmail.com>
2022-12-22 09:31:14 -0600

    Remove platform detection on regex

--- src/lib/strings.ts
+++ src/lib/strings.ts
@@ -1,4 +1,3 @@
-import {Platform} from 'react-native'
 import {AtUri} from '../third-party/uri'
 import {AppBskyFeedPost} from '@atproto/api'
 type Entity = AppBskyFeedPost.Entity
@@ -79,10 +78,7 @@ export function extractEntities(
   let ents: Entity[] = []
   {
     // mentions
-    const re =
-      Platform.OS !== 'android'
-        ? /(^|\s|\()(@)([a-zA-Z0-9\.-]+)(\b)/dg
-        : /(^|\s|\()(@)([a-zA-Z0-9\.-]+)(\b)/g
+    const re = /(^|\s|\()(@)([a-zA-Z0-9\.-]+)(\b)/g
     while ((match = re.exec(text))) {
       if (knownHandles && !knownHandles.has(match[3])) {
         continue // not a known handle
@@ -100,9 +96,7 @@ export function extractEntities(
   {
     // links
     const re =
-      Platform.OS !== 'android'
-        ? /(^|\s|\()((https?:\/\/[\S]+)|((?<domain>[a-z][a-z0-9]*(\.[a-z0-9]+)+)[\S]*))/dgm
-        : /(^|\s|\()((https?:\/\/[\S]+)|((?<domain>[a-z][a-z0-9]*(\.[a-z0-9]+)+)[\S]*))/gm
+      /(^|\s|\()((https?:\/\/[\S]+)|((?<domain>[a-z][a-z0-9]*(\.[a-z0-9]+)+)[\S]*))/gm
     while ((match = re.exec(text))) {
       let value = match[2]
       if (!value.startsWith('http')) {
