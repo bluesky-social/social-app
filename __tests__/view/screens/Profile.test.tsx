610d83d8dbbd16b3a77679c27fb1aaa009e76093
João Ferreiro <ferreiro@pinkroom.dev>
2022-12-29 18:10:22 +0000

    profile screen above 80% coverage

--- __tests__/view/screens/Profile.test.tsx
+++ __tests__/view/screens/Profile.test.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import {Profile} from '../../../src/view/screens/Profile'
 import {render} from '../../../jest/test-utils'
 import {
+  mockedFeedModel,
+  mockedMembershipsModel,
+  mockedMembersStore,
+  mockedMeStore,
   mockedProfileStore,
   mockedProfileUiStore,
 } from '../../../__mocks__/state-mock'
+import {Sections} from '../../../src/state/models/profile-ui'
 
 describe('Profile', () => {
   const mockedProps = {
@@ -54,7 +59,165 @@ describe('Profile', () => {
     expect(profileErrorDetails.props.children).toBe('error')
   })
 
-  it('', () => {
+  it('renders loading placeholder when isInitialLoading', async () => {
+    jest.spyOn(React, 'useMemo').mockReturnValue({
+      ...mockedProfileUiStore,
+      isInitialLoading: true,
+    })
+    const {findByTestId} = render(<Profile {...mockedProps} />)
+    const viewSelectorFlatList = await findByTestId('viewSelectorFlatList')
+
+    expect(viewSelectorFlatList).toBeTruthy()
+    expect(viewSelectorFlatList.props.data).toContainEqual({
+      _reactKey: '__loading__',
+    })
+  })
+
+  it('renders non reply feed when posts section is selected', async () => {
     jest.spyOn(React, 'useMemo').mockReturnValue(mockedProfileUiStore)
+    const {findByTestId} = render(<Profile {...mockedProps} />)
+    const viewSelectorFlatList = await findByTestId('viewSelectorFlatList')
+
+    expect(viewSelectorFlatList).toBeTruthy()
+    expect(viewSelectorFlatList.props.data[2]).toBe(
+      mockedFeedModel.nonReplyFeed[0],
+    )
+  })
+
+  it('triggers slice funcion when posts sections is not selected', async () => {
+    jest.spyOn(React, 'useMemo').mockReturnValue({
+      ...mockedProfileUiStore,
+      selectedView: Sections.Trending,
+    })
+    // @ts-expect-error
+    const sliceSpy = jest.spyOn(mockedFeedModel.feed, 'slice')
+
+    render(<Profile {...mockedProps} />)
+
+    expect(sliceSpy).toHaveBeenCalled()
+  })
+
+  it('renders end of feed when it has no more items', async () => {
+    jest.spyOn(React, 'useMemo').mockReturnValue({
+      ...mockedProfileUiStore,
+      feed: {
+        ...mockedFeedModel,
+        hasMore: false,
+      },
+    })
+    const {findByTestId} = render(<Profile {...mockedProps} />)
+
+    const viewSelectorFlatList = await findByTestId('viewSelectorFlatList')
+    expect(viewSelectorFlatList.props.data).toContainEqual({
+      _reactKey: '__end__',
+    })
+
+    const endOfFeed = await findByTestId('endOfFeed')
+    expect(endOfFeed).toBeTruthy()
+  })
+
+  it('renders state when it has no feed (posts)', async () => {
+    jest.spyOn(React, 'useMemo').mockReturnValue({
+      ...mockedProfileUiStore,
+      feed: {
+        ...mockedFeedModel,
+        hasContent: false,
+        isEmpty: true,
+      },
+    })
+    const {findByTestId} = render(<Profile {...mockedProps} />)
+
+    const viewSelectorFlatList = await findByTestId('viewSelectorFlatList')
+    expect(viewSelectorFlatList.props.data).toContainEqual({
+      _reactKey: '__empty__',
+    })
+
+    const emptyStateView = await findByTestId('emptyStateView')
+    expect(emptyStateView).toBeTruthy()
+  })
+
+  it('triggers slice funcion when scenes are selected', async () => {
+    jest.spyOn(React, 'useMemo').mockReturnValue({
+      ...mockedProfileUiStore,
+      selectedView: Sections.Scenes,
+    })
+    // @ts-expect-error
+    const sliceSpy = jest.spyOn(mockedMembershipsModel.memberships, 'slice')
+
+    render(<Profile {...mockedProps} />)
+
+    expect(sliceSpy).toHaveBeenCalled()
+  })
+
+  it('renders state when it has no feed (scenes)', async () => {
+    jest.spyOn(React, 'useMemo').mockReturnValue({
+      ...mockedProfileUiStore,
+      selectedView: Sections.Scenes,
+      memberships: {
+        ...mockedMembershipsModel,
+        hasContent: false,
+        isEmpty: true,
+      },
+    })
+    const {findByTestId} = render(<Profile {...mockedProps} />)
+
+    const viewSelectorFlatList = await findByTestId('viewSelectorFlatList')
+    expect(viewSelectorFlatList.props.data).toContainEqual({
+      _reactKey: '__empty__',
+    })
+
+    const emptyStateView = await findByTestId('emptyStateView')
+    expect(emptyStateView).toBeTruthy()
+  })
+
+  it('triggers slice funcion when members are selected', async () => {
+    jest.spyOn(React, 'useMemo').mockReturnValue({
+      ...mockedProfileUiStore,
+      selectedView: Sections.Members,
+    })
+    // @ts-expect-error
+    const sliceSpy = jest.spyOn(mockedMembersStore.members, 'slice')
+
+    render(<Profile {...mockedProps} />)
+
+    expect(sliceSpy).toHaveBeenCalled()
+  })
+
+  it('renders state when it has no feed (members)', async () => {
+    jest.spyOn(React, 'useMemo').mockReturnValue({
+      ...mockedProfileUiStore,
+      selectedView: Sections.Members,
+      members: {
+        ...mockedMembersStore,
+        hasContent: false,
+        isEmpty: true,
+      },
+    })
+    const {findByTestId} = render(<Profile {...mockedProps} />)
+
+    const viewSelectorFlatList = await findByTestId('viewSelectorFlatList')
+    expect(viewSelectorFlatList.props.data).toContainEqual({
+      _reactKey: '__empty__',
+    })
+
+    const emptyStateView = await findByTestId('emptyStateView')
+    expect(emptyStateView).toBeTruthy()
+  })
+
+  it('renders as scene creator', async () => {
+    jest.spyOn(React, 'useMemo').mockReturnValue({
+      ...mockedProfileUiStore,
+      selectedView: Sections.Members,
+      isScene: true,
+      profile: {
+        ...mockedProfileStore,
+        creator: '123',
+      },
+    })
+
+    const {findByTestId} = render(<Profile {...mockedProps} />)
+    const shouldAdminButton = await findByTestId('shouldAdminButton')
+
+    expect(shouldAdminButton).toBeTruthy()
   })
 })
