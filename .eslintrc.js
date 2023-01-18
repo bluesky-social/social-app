465d9b41f4fc1abf9e23d6705b0932ae9dd09ec0
Aryan Goharzad <arrygoo@gmail.com>
2023-01-18 18:31:37 -0500

    Configuring eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  // plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['**/__mocks__/*.ts'],
   overrides: [
     {
       files: ['*.js', '*.mjs', '*.ts', '*.tsx'],
