8840293c92deff4d4c967e9cee57d81ecb91f260
Paul Frazee <pfrazee@gmail.com>
2023-01-19 17:55:00 -0600

    Fix a case where the 'aborted' error message was getting through

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,4 +1,4 @@
 export function isNetworkError(e: unknown) {
   const str = String(e)
-  return str.includes('Aborted') || str.includes('Network request failed')
+  return str.includes('Abort') || str.includes('Network request failed')
 }
