0536a6afcfce0ef2bebfc7cacf13d3d80ef82229
Aryan Goharzad <arrygoo@gmail.com>
2023-01-19 13:28:52 -0500

    Adding action for linting (#51)
    * Adding action for linting
    
    * Configuring eslint
    
    * Update lint.yml
    
    * Update lint.yml
    
    * Update lint.yml
    
    * adds yarn jest to package.json
    
    * test report
    
    * debugging tests
    
    * trying force exit
    
    * force exit
    
    * test1
    
    * forceexit
    
    * using another reporter
    
    * jest debugging
    
    * separated lints & tests
    
    * test
    
    * Cleanup
    
    * Add  /src/third-party, /ios, and /android to ignore list

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,12 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  // plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['**/__mocks__/*.ts', 'src/third-party', 'ios', 'android'],
   overrides: [
     {
       files: ['*.js', '*.mjs', '*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-shadow': 'off',
-        'no-shadow': 'off',
-        'no-undef': 'off',
         semi: [2, 'never'],
       },
     },
