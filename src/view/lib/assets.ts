7b1a742a5cab581336f7f86e26a404423bc0129f
Paul Frazee <pfrazee@gmail.com>
2023-01-25 14:29:53 -0600

    Update login screen to use branding

--- src/view/lib/assets.ts
+++ src/view/lib/assets.ts
@@ -4,3 +4,4 @@ export const DEF_AVATAR: ImageSourcePropType = {uri: '/img/default-avatar.jpg'}
 export const TABS_EXPLAINER: ImageSourcePropType = {
   uri: '/img/tabs-explainer.jpg',
 }
+export const CLOUD_SPLASH: ImageSourcePropType = {uri: '/img/cloud-splash.png'}
