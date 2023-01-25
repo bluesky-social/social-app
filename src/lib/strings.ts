858a923ebdec16f11a5e523dcc8c5c0a81fe4bfe
Aryan Goharzad <arrygoo@gmail.com>
2023-01-25 15:55:00 -0500

    lint

--- src/lib/strings.ts
+++ src/lib/strings.ts
@@ -1,13 +1,9 @@
-import RNFetchBlob from 'rn-fetch-blob'
-import {Share} from 'react-native'
-import RNFS from 'react-native-fs'
-import TLDs from 'tlds'
 import {AtUri} from '../third-party/uri'
 import {AppBskyFeedPost} from '@atproto/api'
 type Entity = AppBskyFeedPost.Entity
 import {PROD_SERVICE} from '../state'
 import {isNetworkError} from './errors'
-import * as Toast from '../Toast'
+import TLDs from 'tlds'
 
 export const MAX_DISPLAY_NAME = 64
 export const MAX_DESCRIPTION = 256
@@ -268,22 +264,4 @@ export function convertBskyAppUrlIfNeeded(url: string): string {
     }
   }
   return url
-}
-
-export const saveImageModal = async ({uri}: {uri: string}) => {
-  const downloadResponse = await RNFetchBlob.config({
-    fileCache: true,
-  }).fetch('GET', uri)
-
-  const imagePath = downloadResponse.path()
-  const base64Data = await downloadResponse.readFile('base64')
-  const result = await Share.share({
-    url: 'data:image/png;base64,' + base64Data,
-  })
-  if (result.action === Share.sharedAction) {
-    Toast.show('Image saved to gallery')
-  } else if (result.action === Share.dismissedAction) {
-    // dismissed
-  }
-  RNFS.unlink(imagePath)
 }
