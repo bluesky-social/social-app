3fa6e936e34708b367ac0d22979998687e51471d
Aryan Goharzad <arrygoo@gmail.com>
2023-02-07 13:13:11 -0500

    even longer timeout

--- jest/jestSetup.js
+++ jest/jestSetup.js
@@ -2,7 +2,7 @@
 import {configure} from '@testing-library/react-native'
 import 'react-native-gesture-handler/jestSetup'
 
-configure({asyncUtilTimeout: 5000})
+configure({asyncUtilTimeout: 20000})
 
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
