4d5d9b96f6215021ed5e97ae5ef902ca44135352
Aryan Goharzad <arrygoo@gmail.com>
2023-02-07 13:04:07 -0500

    Longer waitfor time in tests

--- jest/jestSetup.js
+++ jest/jestSetup.js
@@ -1,6 +1,9 @@
 /* global jest */
-
+import {configure} from '@testing-library/react-native'
 import 'react-native-gesture-handler/jestSetup'
+
+configure({asyncUtilTimeout: 5000})
+
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
 )
