eee1cb8d29902f497ba4ce3224f61b253875f77e
Paul Frazee <pfrazee@gmail.com>
2023-02-08 14:39:48 -0600

    Search: give better user feedback to UI state and add a cancel button

--- src/view/screens/Search.tsx
+++ src/view/screens/Search.tsx
@@ -5,8 +5,10 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native'
+import {observer} from 'mobx-react-lite'
 import {SuggestedFollows} from '../com/discover/SuggestedFollows'
 import {UserAvatar} from '../com/util/UserAvatar'
 import {Text} from '../com/util/text/Text'
@@ -20,11 +22,12 @@ import {useAnalytics} from '@segment/analytics-react-native'
 
 const MENU_HITSLOP = {left: 10, top: 10, right: 30, bottom: 10}
 
-export const Search = ({navIdx, visible, params}: ScreenParams) => {
+export const Search = observer(({navIdx, visible, params}: ScreenParams) => {
   const pal = usePalette('default')
   const store = useStores()
   const {track} = useAnalytics()
   const textInput = useRef<TextInput>(null)
+  const [isInputFocused, setIsInputFocused] = useState<boolean>(false)
   const [query, setQuery] = useState<string>('')
   const autocompleteView = useMemo<UserAutocompleteViewModel>(
     () => new UserAutocompleteViewModel(store),
@@ -54,77 +57,111 @@ export const Search = ({navIdx, visible, params}: ScreenParams) => {
       autocompleteView.setActive(false)
     }
   }
+  const onPressCancelSearch = () => {
+    setQuery('')
+    autocompleteView.setActive(false)
+  }
   const onSelect = (handle: string) => {
     textInput.current?.blur()
     store.nav.navigate(`/profile/${handle}`)
   }
+  const onPressContainer = () => {
+    textInput.current?.blur()
+  }
 
   return (
-    <View style={[pal.view, styles.container]}>
-      <View style={[pal.view, pal.border, styles.header]}>
-        <TouchableOpacity
-          testID="viewHeaderBackOrMenuBtn"
-          onPress={onPressMenu}
-          hitSlop={MENU_HITSLOP}
-          style={styles.headerMenuBtn}>
-          <UserAvatar
-            size={30}
-            handle={store.me.handle}
-            displayName={store.me.displayName}
-            avatar={store.me.avatar}
-          />
-        </TouchableOpacity>
-        <View
-          style={[
-            {backgroundColor: pal.colors.backgroundLight},
-            styles.headerSearchContainer,
-          ]}>
-          <MagnifyingGlassIcon style={[pal.icon, styles.inputIcon]} size={21} />
-          <TextInput
-            testID="searchTextInput"
-            ref={textInput}
-            placeholder="Search"
-            placeholderTextColor={pal.colors.textLight}
-            selectTextOnFocus
-            returnKeyType="search"
-            style={[pal.text, styles.input]}
-            onChangeText={onChangeQuery}
-          />
+    <TouchableWithoutFeedback onPress={onPressContainer}>
+      <View style={[pal.view, styles.container]}>
+        <View style={[pal.view, pal.border, styles.header]}>
+          <TouchableOpacity
+            testID="viewHeaderBackOrMenuBtn"
+            onPress={onPressMenu}
+            hitSlop={MENU_HITSLOP}
+            style={styles.headerMenuBtn}>
+            <UserAvatar
+              size={30}
+              handle={store.me.handle}
+              displayName={store.me.displayName}
+              avatar={store.me.avatar}
+            />
+          </TouchableOpacity>
+          <View
+            style={[
+              {backgroundColor: pal.colors.backgroundLight},
+              styles.headerSearchContainer,
+            ]}>
+            <MagnifyingGlassIcon
+              style={[pal.icon, styles.headerSearchIcon]}
+              size={21}
+            />
+            <TextInput
+              testID="searchTextInput"
+              ref={textInput}
+              placeholder="Search"
+              placeholderTextColor={pal.colors.textLight}
+              selectTextOnFocus
+              returnKeyType="search"
+              value={query}
+              style={[pal.text, styles.headerSearchInput]}
+              onFocus={() => setIsInputFocused(true)}
+              onBlur={() => setIsInputFocused(false)}
+              onChangeText={onChangeQuery}
+            />
+          </View>
+          {query ? (
+            <View style={styles.headerCancelBtn}>
+              <TouchableOpacity onPress={onPressCancelSearch}>
+                <Text>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          ) : undefined}
+        </View>
+        <View style={styles.outputContainer}>
+          {query && autocompleteView.searchRes.length ? (
+            <ScrollView testID="searchScrollView" onScroll={Keyboard.dismiss}>
+              {autocompleteView.searchRes.map((item, i) => (
+                <TouchableOpacity
+                  key={i}
+                  style={[pal.view, pal.border, styles.searchResult]}
+                  onPress={() => onSelect(item.handle)}>
+                  <UserAvatar
+                    handle={item.handle}
+                    displayName={item.displayName}
+                    avatar={item.avatar}
+                    size={36}
+                  />
+                  <View style={[s.ml10, s.flex1]}>
+                    <Text type="title-sm" style={pal.text} numberOfLines={1}>
+                      {item.displayName || item.handle}
+                    </Text>
+                    <Text style={pal.textLight} numberOfLines={1}>
+                      @{item.handle}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              ))}
+              <View style={s.footerSpacer} />
+            </ScrollView>
+          ) : query && !autocompleteView.searchRes.length ? (
+            <View>
+              <Text style={[pal.textLight, styles.searchPrompt]}>
+                No results found for {autocompleteView.prefix}
+              </Text>
+            </View>
+          ) : isInputFocused ? (
+            <View>
+              <Text style={[pal.textLight, styles.searchPrompt]}>
+                Search for users on the network
+              </Text>
+            </View>
+          ) : (
+            <SuggestedFollows asLinks />
+          )}
         </View>
       </View>
-      <View style={styles.outputContainer}>
-        {query ? (
-          <ScrollView testID="searchScrollView" onScroll={Keyboard.dismiss}>
-            {autocompleteView.searchRes.map((item, i) => (
-              <TouchableOpacity
-                key={i}
-                style={[pal.view, pal.border, styles.searchResult]}
-                onPress={() => onSelect(item.handle)}>
-                <UserAvatar
-                  handle={item.handle}
-                  displayName={item.displayName}
-                  avatar={item.avatar}
-                  size={36}
-                />
-                <View style={[s.ml10, s.flex1]}>
-                  <Text type="title-sm" style={pal.text} numberOfLines={1}>
-                    {item.displayName || item.handle}
-                  </Text>
-                  <Text style={pal.textLight} numberOfLines={1}>
-                    @{item.handle}
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            ))}
-            <View style={s.footerSpacer} />
-          </ScrollView>
-        ) : (
-          <SuggestedFollows asLinks />
-        )}
-      </View>
-    </View>
+    </TouchableWithoutFeedback>
   )
-}
+})
 
 const styles = StyleSheet.create({
   container: {
@@ -151,19 +188,27 @@ const styles = StyleSheet.create({
     paddingHorizontal: 12,
     paddingVertical: 6,
   },
-  inputIcon: {
+  headerSearchIcon: {
     marginRight: 6,
     alignSelf: 'center',
   },
-  input: {
+  headerSearchInput: {
     flex: 1,
     fontSize: 16,
+  },
+  headerCancelBtn: {
+    width: 60,
+    paddingLeft: 10,
+  },
+
+  searchPrompt: {
+    textAlign: 'center',
+    paddingTop: 10,
   },
 
   outputContainer: {
     flex: 1,
   },
-
   searchResult: {
     flexDirection: 'row',
     borderTopWidth: 1,
