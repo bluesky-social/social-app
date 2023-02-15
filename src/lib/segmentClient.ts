4f2ae885b71b9f02340360f3b44ce007a0497c64
Paul Frazee <pfrazee@gmail.com>
2023-02-15 16:37:54 -0600

    Implement our own lifecycle tracking to ensure it never fires while the app is backgrounded (close #193)

--- src/lib/segmentClient.ts
+++
@@ -1,9 +0,0 @@
-import {createClient} from '@segment/analytics-react-native'
-// import {createLogger} from './logger'
-
-export const segmentClient = createClient({
-  writeKey: '8I6DsgfiSLuoONyaunGoiQM7A6y2ybdI',
-  trackAppLifecycleEvents: true,
-  // Uncomment to debug:
-  // logger: createLogger(),
-})
