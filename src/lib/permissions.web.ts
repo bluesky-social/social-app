1465b4454856f1496654dde5c4f4a64a217c9875
Paul Frazee <pfrazee@gmail.com>
2023-02-22 14:11:58 -0600

    Create web split for permissions lib

---
+++ src/lib/permissions.web.ts
@@ -0,0 +1,18 @@
+export const PHOTO_LIBRARY = ''
+export const CAMERA = ''
+
+export async function hasAccess(_perm: any): Promise<boolean> {
+  return true
+}
+
+export async function requestAccessIfNeeded(_perm: any): Promise<boolean> {
+  return true
+}
+
+export async function requestPhotoAccessIfNeeded() {
+  return requestAccessIfNeeded(PHOTO_LIBRARY)
+}
+
+export async function requestCameraAccessIfNeeded() {
+  return requestAccessIfNeeded(CAMERA)
+}
