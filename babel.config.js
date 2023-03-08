18347bdb224b303c6e3683067c7be3e3fbfa5e39
Paul Frazee <pfrazee@gmail.com>
2023-03-08 13:53:00 -0600

    WIP - adding expo 2

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,34 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        envName: 'APP_ENV',
-        moduleName: '@env',
-        path: '.env',
-        blocklist: null,
-        allowlist: null,
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
-      },
-    ],
-    [
-      'module-resolver',
-      {
-        alias: {
-          // This needs to be mirrored in tsconfig.json
-          lib: './src/lib',
-          platform: './src/platform',
-          state: './src/state',
-          view: './src/view',
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
         },
-      },
+      ],
+      [
+        'module-resolver',
+        {
+          alias: {
+            // This needs to be mirrored in tsconfig.json
+            lib: './src/lib',
+            platform: './src/platform',
+            state: './src/state',
+            view: './src/view',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin', // NOTE: this plugin MUST be last
     ],
-    'react-native-reanimated/plugin', // NOTE: this plugin MUST be last
-  ],
+  }
 }
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-/**
- * @format
- */
-
 import 'platform/polyfills'
-import {AppRegistry} from 'react-native'
-import App from './src/App'
-import {name as appName} from './src/app.json'
+import {registerRootComponent} from 'expo'
 
-AppRegistry.registerComponent(appName, () => App)
+import App from './src/App'
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App)
