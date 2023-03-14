8629e167cd668cd1d41bf6a37acf9d94502e5c2b
bnewbold <bnewbold@robocracy.org>
2023-03-14 13:00:44 -0700

    bskyweb: proof-of-concept golang daemon to serve SPA (#275)
    * gitignore: /dist/
    
    * bskyweb: initial work-in-progress
    
    * bskyweb: import icons from bluesky-website
    
    * bskyweb: switch to pongo2 templates; iterate on views
    
    * bskyweb: example.env (and docs)
    
    * bskyweb: go fmt
    
    * bskyweb: remove plan file
    
    * bskyweb: README: tweak formatting
    
    * prettier: ignore /dist/, bskyweb templates
    
    ---------
    
    Co-authored-by: Paul Frazee <pfrazee@gmail.com>

---
+++ bskyweb/cmd/bskyweb/server.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	comatproto "github.com/bluesky-social/indigo/api/atproto"
+	appbsky "github.com/bluesky-social/indigo/api/bsky"
+	cliutil "github.com/bluesky-social/indigo/cmd/gosky/util"
+	"github.com/bluesky-social/indigo/xrpc"
+
+	"github.com/flosch/pongo2/v6"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/urfave/cli/v2"
+)
+
+// TODO: embed templates in executable
+
+type Renderer struct {
+	Debug bool
+}
+
+func (r Renderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	var ctx pongo2.Context
+
+	if data != nil {
+		var ok bool
+		ctx, ok = data.(pongo2.Context)
+
+		if !ok {
+			return errors.New("no pongo2.Context data was passed...")
+		}
+	}
+
+	var t *pongo2.Template
+	var err error
+
+	if r.Debug {
+		t, err = pongo2.FromFile(name)
+	} else {
+		t, err = pongo2.FromCache(name)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return t.ExecuteWriter(ctx, w)
+}
+
+type Server struct {
+	xrpcc *xrpc.Client
+}
+
+func serve(cctx *cli.Context) error {
+
+	// create a new session
+	// TODO: does this work with no auth at all?
+	xrpcc := &xrpc.Client{
+		Client: cliutil.NewHttpClient(),
+		Host:   cctx.String("pds-host"),
+		Auth: &xrpc.AuthInfo{
+			Handle: cctx.String("handle"),
+		},
+	}
+
+	auth, err := comatproto.SessionCreate(context.TODO(), xrpcc, &comatproto.SessionCreate_Input{
+		Identifier: &xrpcc.Auth.Handle,
+		Password:   cctx.String("password"),
+	})
+	if err != nil {
+		return err
+	}
+	xrpcc.Auth.AccessJwt = auth.AccessJwt
+	xrpcc.Auth.RefreshJwt = auth.RefreshJwt
+	xrpcc.Auth.Did = auth.Did
+	xrpcc.Auth.Handle = auth.Handle
+
+	server := Server{xrpcc}
+
+	e := echo.New()
+	e.HideBanner = true
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method} path=${uri} status=${status} latency=${latency_human}\n",
+	}))
+	e.Renderer = Renderer{Debug: true}
+	e.HTTPErrorHandler = customHTTPErrorHandler
+
+	// configure routes
+	e.File("/robots.txt", "static/robots.txt")
+	e.Static("/static", "static")
+
+	e.GET("/", server.WebHome)
+
+	// generic routes
+	e.GET("/contacts", server.WebGeneric)
+	e.GET("/search", server.WebGeneric)
+	e.GET("/notifications", server.WebGeneric)
+	e.GET("/settings", server.WebGeneric)
+	e.GET("/settings", server.WebGeneric)
+
+	// profile endpoints; only first populates info
+	e.GET("/profile/:handle", server.WebProfile)
+	e.GET("/profile/:handle/follows", server.WebGeneric)
+	e.GET("/profile/:handle/following", server.WebGeneric)
+
+	// post endpoints; only first populates info
+	e.GET("/profile/:handle/post/:rkey", server.WebPost)
+	e.GET("/profile/:handle/post/:rkey/upvoted-by", server.WebGeneric)
+	e.GET("/profile/:handle/post/:rkey/downvoted-by", server.WebGeneric)
+	e.GET("/profile/:handle/post/:rkey/reposted-by", server.WebGeneric)
+
+	bind := "localhost:8100"
+	log.Infof("starting server bind=%s", bind)
+	return e.Start(bind)
+}
+
+func customHTTPErrorHandler(err error, c echo.Context) {
+	code := http.StatusInternalServerError
+	if he, ok := err.(*echo.HTTPError); ok {
+		code = he.Code
+	}
+	c.Logger().Error(err)
+	data := pongo2.Context{
+		"statusCode": code,
+	}
+	c.Render(code, "templates/error.html", data)
+}
+
+// handler for endpoint that have no specific server-side handling
+func (srv *Server) WebGeneric(c echo.Context) error {
+	data := pongo2.Context{}
+	return c.Render(http.StatusOK, "templates/base.html", data)
+}
+
+func (srv *Server) WebHome(c echo.Context) error {
+	data := pongo2.Context{}
+	return c.Render(http.StatusOK, "templates/home.html", data)
+}
+
+func (srv *Server) WebPost(c echo.Context) error {
+	data := pongo2.Context{}
+	handle := c.Param("handle")
+	rkey := c.Param("rkey")
+	// sanity check argument
+	if len(handle) > 4 && len(handle) < 128 && len(rkey) > 0 {
+		ctx := context.TODO()
+		// requires two fetches: first fetch profile (!)
+		pv, err := appbsky.ActorGetProfile(ctx, srv.xrpcc, handle)
+		if err != nil {
+			log.Warnf("failed to fetch handle: %s\t%v", handle, err)
+		} else {
+			did := pv.Did
+			data["did"] = did
+
+			// then fetch the post thread (with extra context)
+			uri := fmt.Sprintf("at://%s/app.bsky.feed.post/%s", did, rkey)
+			tpv, err := appbsky.FeedGetPostThread(ctx, srv.xrpcc, 1, uri)
+			if err != nil {
+				log.Warnf("failed to fetch post: %s\t%v", uri, err)
+			} else {
+				data["postView"] = tpv.Thread.FeedGetPostThread_ThreadViewPost.Post
+			}
+		}
+
+	}
+	return c.Render(http.StatusOK, "templates/post.html", data)
+}
+
+func (srv *Server) WebProfile(c echo.Context) error {
+	data := pongo2.Context{}
+	handle := c.Param("handle")
+	// sanity check argument
+	if len(handle) > 4 && len(handle) < 128 {
+		ctx := context.TODO()
+		pv, err := appbsky.ActorGetProfile(ctx, srv.xrpcc, handle)
+		if err != nil {
+			log.Warnf("failed to fetch handle: %s\t%v", handle, err)
+		} else {
+			data["profileView"] = pv
+		}
+	}
+
+	return c.Render(http.StatusOK, "templates/profile.html", data)
+}
---
+++ bskyweb/go.mod
@@ -0,0 +1,72 @@
+module github.com/bluesky-social/social-app/bskyweb
+
+go 1.19
+
+require (
+	github.com/bluesky-social/indigo v0.0.0-20230307000525-294e33e70185
+	github.com/ipfs/go-log v1.0.5
+	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo/v4 v4.10.2
+	github.com/urfave/cli/v2 v2.25.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/flosch/pongo2/v6 v6.0.0 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/ipfs/go-cid v0.3.2 // indirect
+	github.com/ipfs/go-log/v2 v2.5.1 // indirect
+	github.com/ipsn/go-secp256k1 v0.0.0-20180726113642-9d62b9f0bc52 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.2.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.3 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.4 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/multiformats/go-base32 v0.1.0 // indirect
+	github.com/multiformats/go-base36 v0.2.0 // indirect
+	github.com/multiformats/go-multibase v0.1.1 // indirect
+	github.com/multiformats/go-multihash v0.2.1 // indirect
+	github.com/multiformats/go-varint v0.0.7 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/whyrusleeping/cbor-gen v0.0.0-20230126041949-52956bd4c9aa // indirect
+	github.com/whyrusleeping/go-did v0.0.0-20230301193428-2146016fc220 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	go.opentelemetry.io/otel v1.11.2 // indirect
+	go.opentelemetry.io/otel/trace v1.11.2 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	gorm.io/driver/postgres v1.4.6 // indirect
+	gorm.io/driver/sqlite v1.4.4 // indirect
+	gorm.io/gorm v1.24.3 // indirect
+	lukechampine.com/blake3 v1.1.7 // indirect
+)
