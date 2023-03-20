67e4882bb372bc45d178e3eacc409cdbf60f1344
Jake Gold <52801504+Jacob2161@users.noreply.github.com>
2023-03-20 14:41:15 -0700

    bskyweb additions (#296)
    Add some minor bskyweb improvements, Mailmodo endpoint, Dockerfile for bskyweb, container image push

---
+++ Dockerfile
@@ -0,0 +1,75 @@
+FROM golang:1.20-bullseye AS build-env
+
+WORKDIR /usr/src/social-app
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Node
+ENV NODE_VERSION=18
+ENV NVM_DIR=/usr/share/nvm
+
+# Go
+ENV GODEBUG="netdns=go"
+ENV GOOS="linux"
+ENV GOARCH="amd64"
+ENV CGO_ENABLED=1
+
+COPY . .
+
+#
+# Generate the Javascript webpack.
+#
+RUN mkdir --parents $NVM_DIR && \
+  wget \
+    --output-document=/tmp/nvm-install.sh \
+    https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh && \
+  bash /tmp/nvm-install.sh
+
+RUN \. "$NVM_DIR/nvm.sh" && \
+  nvm install $NODE_VERSION && \
+  nvm use $NODE_VERSION && \
+  npm install --global yarn && \
+  yarn && \
+  yarn build-web
+
+# DEBUG
+RUN find ./bskyweb/static && find ./web-build/static
+
+# Copy the bundle js files.
+RUN cp --verbose ./web-build/static/js/*.* ./bskyweb/static/js/
+
+#
+# Generate the bksyweb Go binary.
+#
+RUN cd bskyweb/ && \
+  go mod download && \
+  go mod verify
+
+RUN cd bskyweb/ && \
+  go build \
+    -v  \
+    -trimpath \
+    -tags timetzdata \
+    -o /bskyweb \
+    ./cmd/bskyweb
+
+FROM debian:bullseye-slim
+
+ENV GODEBUG=netdns=go
+ENV TZ=Etc/UTC
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update && apt-get install --yes \
+  dumb-init \
+  ca-certificates
+
+ENTRYPOINT ["dumb-init", "--"]
+
+WORKDIR /bskyweb
+COPY --from=build-env /bskyweb /usr/bin/bskyweb
+
+CMD ["/usr/bin/bskyweb"]
+
+LABEL org.opencontainers.image.source=https://github.com/bluesky-social/social-app
+LABEL org.opencontainers.image.description="bsky.app Web App"
+LABEL org.opencontainers.image.licenses=MIT
---
+++ bskyweb/cmd/bskyweb/mailmodo.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Mailmodo struct {
+	httpClient *http.Client
+	APIKey     string
+	BaseURL    string
+}
+
+func NewMailmodo(apiKey string) *Mailmodo {
+	return &Mailmodo{
+		APIKey:     apiKey,
+		BaseURL:    "https://api.mailmodo.com/api/v1",
+		httpClient: &http.Client{},
+	}
+}
+
+func (m *Mailmodo) request(ctx context.Context, httpMethod string, apiMethod string, data any) error {
+	endpoint := fmt.Sprintf("%s/%s", m.BaseURL, apiMethod)
+	js, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("Mailmodo JSON encoding failed: %w", err)
+	}
+	req, err := http.NewRequestWithContext(ctx, httpMethod, endpoint, bytes.NewBuffer(js))
+	if err != nil {
+		return fmt.Errorf("Mailmodo HTTP creating request %s %s failed: %w", httpMethod, apiMethod, err)
+	}
+	req.Header.Set("mmApiKey", m.APIKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := m.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("Mailmodo HTTP making request %s %s failed: %w", httpMethod, apiMethod, err)
+	}
+	defer res.Body.Close()
+
+	status := struct {
+		Success bool   `json:"success"`
+		Message string `json:"message"`
+	}{}
+	if err := json.NewDecoder(res.Body).Decode(&status); err != nil {
+		return fmt.Errorf("Mailmodo HTTP parsing response %s %s failed: %w", httpMethod, apiMethod, err)
+	}
+	if !status.Success {
+		return fmt.Errorf("Mailmodo API response %s %s failed: %s", httpMethod, apiMethod, status.Message)
+	}
+	return nil
+}
+
+func (m *Mailmodo) AddToList(ctx context.Context, listName, email string) error {
+	return m.request(ctx, "POST", "addToList", map[string]any{
+		"listName": listName,
+		"email":    email,
+		"data": map[string]any{
+			"email_hashed": fmt.Sprintf("%x", sha256.Sum256([]byte(email))),
+		},
+		"created_at": time.Now().UTC().Format(time.RFC3339),
+	})
+}
