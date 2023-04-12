2ac578a4f301ef3c73bd0a8c94a22bad34dbd607
Ansh Nanda <anshnanda10@gmail.com>
2023-04-12 12:37:19 -0700

    fix TS for useColorSchemeStyle

--- src/lib/hooks/useColorSchemeStyle.ts
+++ src/lib/hooks/useColorSchemeStyle.ts
@@ -1,6 +1,6 @@
 import {useTheme} from 'lib/ThemeContext'
 
-export function useColorSchemeStyle(lightStyle: any, darkStyle: any) {
+export function useColorSchemeStyle<T>(lightStyle: T, darkStyle: T) {
   const colorScheme = useTheme().colorScheme
   return colorScheme === 'dark' ? darkStyle : lightStyle
 }
