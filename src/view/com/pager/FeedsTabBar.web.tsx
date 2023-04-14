368cea21ae6eebddf6bbadf0fe89211e70217936
Paul Frazee <pfrazee@gmail.com>
2023-04-14 10:54:55 -0700

    Fix lint

--- src/view/com/pager/FeedsTabBar.web.tsx
+++ src/view/com/pager/FeedsTabBar.web.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Animated, StyleSheet, TouchableOpacity} from 'react-native'
+import {Animated, StyleSheet} from 'react-native'
 import {observer} from 'mobx-react-lite'
 import {TabBar} from 'view/com/pager/TabBar'
 import {RenderTabBarFnProps} from 'view/com/pager/Pager'
