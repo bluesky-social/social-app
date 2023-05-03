0c97d9281cafbf55d23f791e2c7d3208b5bf942c
Paul Frazee <pfrazee@gmail.com>
2023-05-02 22:51:13 -0500

    Add nice date to expanded post view

--- src/lib/strings/time.ts
+++ src/lib/strings/time.ts
@@ -27,3 +27,15 @@ export function ago(date: number | string | Date): string {
     return new Date(ts).toLocaleDateString()
   }
 }
+
+export function niceDate(date: number | string | Date) {
+  const d = new Date(date)
+  return `${d.toLocaleDateString('en-us', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  })} at ${d.toLocaleTimeString(undefined, {
+    hour: 'numeric',
+    minute: '2-digit',
+  })}`
+}
