36fd8bdc88b4adcd3bb06b78aa34ec2ce90ebf81
renahlee <renahlee@outlook.com>
2023-05-08 14:33:01 -0700

    Add avatar to mobile autocomplete and create grapheme hook

---
+++ src/view/com/composer/text-input/hooks/useDisplayName.tsx
@@ -0,0 +1,39 @@
+import Graphemer from 'graphemer'
+import {useCallback, useMemo} from 'react'
+
+export const useGrapheme = () => {
+  const splitter = useMemo(() => new Graphemer(), [])
+
+  const getGraphemeString = useCallback(
+    (name: string, length: number) => {
+      let remainingCharacters = 0
+
+      // Heuristic value based on max display name and handle lengths
+      if (name.length > length) {
+        const graphemes = splitter.splitGraphemes(name)
+
+        if (graphemes.length > length) {
+          if (graphemes.length > length) {
+            remainingCharacters = 0
+            name = `${graphemes.slice(0, length).join('')}...`
+          } else {
+            remainingCharacters = length - graphemes.length
+            name = graphemes.join('')
+          }
+        }
+      } else {
+        remainingCharacters = length - name.length
+      }
+
+      return {
+        name,
+        remainingCharacters,
+      }
+    },
+    [splitter],
+  )
+
+  return {
+    getGraphemeString,
+  }
+}
