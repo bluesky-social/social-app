b2d4a6eefa16edc448f56bad70f0867a937f2cbf
Jacques Favreau <betaorbust@users.noreply.github.com>
2023-05-16 11:57:42 -0700

    fix: Add vendor prefix for web blur.

--- src/view/com/util/BlurView.web.tsx
+++ src/view/com/util/BlurView.web.tsx
@@ -14,7 +14,8 @@ export const BlurView = ({
   ...props
 }: React.PropsWithChildren<BlurViewProps>) => {
   // @ts-ignore using an RNW-specific attribute here -prf
-  style = addStyle(style, {backdropFilter: `blur(${blurAmount || 10}px`})
+  let blur = `blur(${blurAmount || 10}px`
+  style = addStyle(style, {backdropFilter: blur, WebkitBackdropFilter: blur})
   if (blurType === 'dark') {
     style = addStyle(style, styles.dark)
   } else {
