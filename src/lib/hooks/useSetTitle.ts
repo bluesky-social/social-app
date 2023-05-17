7a4629f7f51d56ac931d7a9eb542d94b39bb42f4
L Lllvvuu <git@llllvvuu.dev>
2023-05-16 21:30:38 -0700

    style: remove `useUnreadCountLabel` hack from `50c1841` (#655)
    I just realized how `mobx` works (never used before lol) and now I feel
    dumb.

--- src/lib/hooks/useSetTitle.ts
+++ src/lib/hooks/useSetTitle.ts
@@ -3,11 +3,15 @@ import {useNavigation} from '@react-navigation/native'
 
 import {NavigationProp} from 'lib/routes/types'
 import {bskyTitle} from 'lib/strings/headings'
-import {useUnreadCountLabel} from './useUnreadCountLabel'
+import {useStores} from 'state/index'
 
+/**
+ * Requires consuming component to be wrapped in `observer`:
+ * https://stackoverflow.com/a/71488009
+ */
 export function useSetTitle(title?: string) {
   const navigation = useNavigation<NavigationProp>()
-  const unreadCountLabel = useUnreadCountLabel()
+  const {unreadCountLabel} = useStores().me.notifications
   useEffect(() => {
     if (title) {
       navigation.setOptions({title: bskyTitle(title, unreadCountLabel)})
