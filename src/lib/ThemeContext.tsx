d38b1f6e472a5538d375aad455fa91b877a19622
Reza <rezamahmudy1@gmail.com>
2023-05-31 11:31:51 +0330

    fix ThemeContext.tsx lint issue

--- src/lib/ThemeContext.tsx
+++ src/lib/ThemeContext.tsx
@@ -96,7 +96,7 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({
 
   const value = useMemo(
     () => (colorScheme === 'dark' ? darkTheme : defaultTheme),
-    [colorScheme, theme],
+    [colorScheme],
   )
 
   return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
