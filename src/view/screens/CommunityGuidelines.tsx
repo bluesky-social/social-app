bc2bf20e0803b88113ed89249d9481b40fe14dd6
Paul Frazee <pfrazee@gmail.com>
2023-06-12 17:25:52 -0500

    Update info screen titles to use H1

--- src/view/screens/CommunityGuidelines.tsx
+++ src/view/screens/CommunityGuidelines.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {View} from 'react-native'
 import {useFocusEffect} from '@react-navigation/native'
+import {H1} from '@expo/html-elements'
 import {NativeStackScreenProps, CommonNavigatorParams} from 'lib/routes/types'
 import {ViewHeader} from '../com/util/ViewHeader'
 import {useStores} from 'state/index'
 import {ScrollView} from 'view/com/util/Views'
-import {Text} from 'view/com/util/text/Text'
 import {usePalette} from 'lib/hooks/usePalette'
 import {s} from 'lib/styles'
 import Html from '../../locale/en/community-guidelines'
@@ -29,9 +29,9 @@ export const CommunityGuidelinesScreen = (_props: Props) => {
       <ViewHeader title="Community Guidelines" />
       <ScrollView style={[s.hContentRegion, pal.view]}>
         <View style={[s.p20]}>
-          <Text type="title-xl" style={[pal.text, s.bold, s.pb20]}>
+          <H1 style={[pal.text, s.bold, s.pb20, {marginVertical: 0}]}>
             Community Guidelines
-          </Text>
+          </H1>
           <Html />
         </View>
         <View style={s.footerSpacer} />
--- src/view/screens/CopyrightPolicy.tsx
+++ src/view/screens/CopyrightPolicy.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {View} from 'react-native'
 import {useFocusEffect} from '@react-navigation/native'
+import {H1} from '@expo/html-elements'
 import {NativeStackScreenProps, CommonNavigatorParams} from 'lib/routes/types'
 import {ViewHeader} from '../com/util/ViewHeader'
 import {useStores} from 'state/index'
 import {ScrollView} from 'view/com/util/Views'
-import {Text} from 'view/com/util/text/Text'
 import {usePalette} from 'lib/hooks/usePalette'
 import {s} from 'lib/styles'
 import Html from '../../locale/en/copyright-policy'
@@ -26,9 +26,9 @@ export const CopyrightPolicyScreen = (_props: Props) => {
       <ViewHeader title="Copyright Policy" />
       <ScrollView style={[s.hContentRegion, pal.view]}>
         <View style={[s.p20]}>
-          <Text type="title-xl" style={[pal.text, s.bold, s.pb20]}>
+          <H1 style={[pal.text, s.bold, s.pb20, {marginVertical: 0}]}>
             Copyright Policy
-          </Text>
+          </H1>
           <Html />
         </View>
         <View style={s.footerSpacer} />
