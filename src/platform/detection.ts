8ab195af0c317e0cccccba209f86bfe80e4dda16
Ansh Nanda <anshnanda10@gmail.com>
2023-06-13 13:18:54 -0700

    fix desktop window size

--- src/platform/detection.ts
+++ src/platform/detection.ts
@@ -7,5 +7,5 @@ export const isWeb = !isNative
 export const isMobileWeb =
   isWeb &&
   // @ts-ignore we know window exists -prf
-  global.window.matchMedia('only screen and (max-width: 1000px)')?.matches
+  global.window.matchMedia('only screen and (max-width: 1230px)')?.matches
 export const isDesktopWeb = isWeb && !isMobileWeb
