c1309033b8a2338f9a2772ad19600e35a47c67cb
Ansh Nanda <anshnanda10@gmail.com>
2023-06-26 15:43:26 -0700

    refactor post-thread-item to use post-feed-item model under the hood

--- src/state/models/content/post-thread-item.ts
+++ src/state/models/content/post-thread-item.ts
@@ -5,21 +5,16 @@ import {
   RichText,
 } from '@atproto/api'
 import {RootStoreModel} from '../root-store'
-import {updateDataOptimistically} from 'lib/async/revertible'
 import {PostLabelInfo, PostModeration} from 'lib/labeling/types'
-import {
-  getEmbedLabels,
-  getEmbedMuted,
-  getEmbedMutedByList,
-  getEmbedBlocking,
-  getEmbedBlockedBy,
-  filterAccountLabels,
-  filterProfileLabels,
-  getPostModeration,
-} from 'lib/labeling/helpers'
+import {PostsFeedItemModel} from '../feeds/post'
 
 type PostView = AppBskyFeedDefs.PostView
 
+// NOTE: this model uses the same data as PostsFeedItemModel, but is used for
+// rendering a single post in a thread view, and has additional state
+// for rendering the thread view, but calls the same data methods
+// as PostsFeedItemModel
+// TODO: refactor as an extension or subclass of PostsFeedItemModel
 export class PostThreadItemModel {
   // ui state
   _reactKey: string = ''
@@ -30,38 +25,25 @@ export class PostThreadItemModel {
   _hasMore = false
 
   // data
+  data: PostsFeedItemModel
   post: PostView
   postRecord?: FeedPost.Record
+  richText?: RichText
   parent?:
     | PostThreadItemModel
     | AppBskyFeedDefs.NotFoundPost
     | AppBskyFeedDefs.BlockedPost
   replies?: (PostThreadItemModel | AppBskyFeedDefs.NotFoundPost)[]
-  richText?: RichText
 
   constructor(
     public rootStore: RootStoreModel,
     v: AppBskyFeedDefs.ThreadViewPost,
   ) {
     this._reactKey = `thread-${v.post.uri}`
-    this.post = v.post
-    if (FeedPost.isRecord(this.post.record)) {
-      const valid = FeedPost.validateRecord(this.post.record)
-      if (valid.success) {
-        this.postRecord = this.post.record
-        this.richText = new RichText(this.postRecord, {cleanNewlines: true})
-      } else {
-        rootStore.log.warn(
-          'Received an invalid app.bsky.feed.post record',
-          valid.error,
-        )
-      }
-    } else {
-      rootStore.log.warn(
-        'app.bsky.feed.getPostThread served an unexpected record type',
-        this.post.record,
-      )
-    }
+    this.data = new PostsFeedItemModel(rootStore, this._reactKey, v)
+    this.post = this.data.post
+    this.postRecord = this.data.postRecord
+    this.richText = this.data.richText
     // replies and parent are handled via assignTreeModels
     makeAutoObservable(this, {rootStore: false})
   }
@@ -69,7 +51,6 @@ export class PostThreadItemModel {
   get uri() {
     return this.post.uri
   }
-
   get parentUri() {
     return this.postRecord?.reply?.parent.uri
   }
@@ -78,40 +59,18 @@ export class PostThreadItemModel {
     if (this.postRecord?.reply?.root.uri) {
       return this.postRecord.reply.root.uri
     }
-    return this.uri
+    return this.post.uri
   }
-
   get isThreadMuted() {
     return this.rootStore.mutedThreads.uris.has(this.rootUri)
   }
 
   get labelInfo(): PostLabelInfo {
-    return {
-      postLabels: (this.post.labels || []).concat(
-        getEmbedLabels(this.post.embed),
-      ),
-      accountLabels: filterAccountLabels(this.post.author.labels),
-      profileLabels: filterProfileLabels(this.post.author.labels),
-      isMuted:
-        this.post.author.viewer?.muted ||
-        getEmbedMuted(this.post.embed) ||
-        false,
-      mutedByList:
-        this.post.author.viewer?.mutedByList ||
-        getEmbedMutedByList(this.post.embed),
-      isBlocking:
-        !!this.post.author.viewer?.blocking ||
-        getEmbedBlocking(this.post.embed) ||
-        false,
-      isBlockedBy:
-        !!this.post.author.viewer?.blockedBy ||
-        getEmbedBlockedBy(this.post.embed) ||
-        false,
-    }
+    return this.data.labelInfo
   }
 
   get moderation(): PostModeration {
-    return getPostModeration(this.rootStore, this.labelInfo)
+    return this.data.moderation
   }
 
   assignTreeModels(
@@ -165,69 +124,18 @@ export class PostThreadItemModel {
   }
 
   async toggleLike() {
-    this.post.viewer = this.post.viewer || {}
-    if (this.post.viewer.like) {
-      const url = this.post.viewer.like
-      await updateDataOptimistically(
-        this.post,
-        () => {
-          this.post.likeCount = (this.post.likeCount || 0) - 1
-          this.post.viewer!.like = undefined
-        },
-        () => this.rootStore.agent.deleteLike(url),
-      )
-    } else {
-      await updateDataOptimistically(
-        this.post,
-        () => {
-          this.post.likeCount = (this.post.likeCount || 0) + 1
-          this.post.viewer!.like = 'pending'
-        },
-        () => this.rootStore.agent.like(this.post.uri, this.post.cid),
-        res => {
-          this.post.viewer!.like = res.uri
-        },
-      )
-    }
+    this.data.toggleLike()
   }
 
   async toggleRepost() {
-    this.post.viewer = this.post.viewer || {}
-    if (this.post.viewer?.repost) {
-      const url = this.post.viewer.repost
-      await updateDataOptimistically(
-        this.post,
-        () => {
-          this.post.repostCount = (this.post.repostCount || 0) - 1
-          this.post.viewer!.repost = undefined
-        },
-        () => this.rootStore.agent.deleteRepost(url),
-      )
-    } else {
-      await updateDataOptimistically(
-        this.post,
-        () => {
-          this.post.repostCount = (this.post.repostCount || 0) + 1
-          this.post.viewer!.repost = 'pending'
-        },
-        () => this.rootStore.agent.repost(this.post.uri, this.post.cid),
-        res => {
-          this.post.viewer!.repost = res.uri
-        },
-      )
-    }
+    this.data.toggleRepost()
   }
 
   async toggleThreadMute() {
-    if (this.isThreadMuted) {
-      this.rootStore.mutedThreads.uris.delete(this.rootUri)
-    } else {
-      this.rootStore.mutedThreads.uris.add(this.rootUri)
-    }
+    this.data.toggleThreadMute()
   }
 
   async delete() {
-    await this.rootStore.agent.deletePost(this.post.uri)
-    this.rootStore.emitPostDeleted(this.post.uri)
+    this.data.delete()
   }
 }
