4630e44abdcb45cdbbfdc5e51abd79eba934ae8d
Paul Frazee <pfrazee@gmail.com>
2023-06-27 10:04:32 -0500

    Update accessibility setting to match styles

--- src/view/screens/Settings.tsx
+++ src/view/screens/Settings.tsx
@@ -330,15 +330,20 @@ export const SettingsScreen = withAuthRequired(
           </TouchableOpacity>
 
           <View style={styles.spacer20} />
+
           <Text type="xl-bold" style={[pal.text, styles.heading]}>
             Accessibility
           </Text>
-          <ToggleButton
-            type="default-light"
-            label="Require alt text on images"
-            isSelected={store.preferences.requireAltTextEnabled}
-            onPress={store.preferences.toggleRequireAltTextEnabled}
-          />
+          <View style={[pal.view, styles.toggleCard]}>
+            <ToggleButton
+              type="default-light"
+              label="Require alt text on images"
+              labelType="lg"
+              isSelected={store.preferences.requireAltTextEnabled}
+              onPress={store.preferences.toggleRequireAltTextEnabled}
+            />
+          </View>
+
           <View style={styles.spacer20} />
 
           <Text type="xl-bold" style={[pal.text, styles.heading]}>
@@ -642,6 +647,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingVertical: 20,
     paddingHorizontal: 18,
+    marginBottom: 1,
+  },
+  toggleCard: {
+    paddingVertical: 8,
+    paddingHorizontal: 6,
     marginBottom: 1,
   },
   avi: {
