bb99a234e18e8582f391f6f2e93c9efa10f79cc3
Ansh <anshnanda10@gmail.com>
2023-07-21 15:39:06 -0700

    [APP-720] Paginate custom feeds (#1030)
    * paginate custom feeds
    
    * Fix loading state bug
    
    * DRY code up

--- src/view/screens/DiscoverFeeds.tsx
+++ src/view/screens/DiscoverFeeds.tsx
@@ -85,6 +85,7 @@ export const DiscoverFeedsScreen = withAuthRequired(
           renderItem={renderItem}
           initialNumToRender={10}
           ListEmptyComponent={renderListEmptyComponent}
+          onEndReached={() => feeds.loadMore()}
           extraData={feeds.isLoading}
         />
       </CenteredView>
