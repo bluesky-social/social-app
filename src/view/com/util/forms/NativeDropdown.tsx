bfc92c072caabf9b33df9ceae0f9d32d79f1e696
Ansh Nanda <anshnanda10@gmail.com>
2023-07-24 13:39:12 -0700

    refined native dropdown component

--- src/view/com/util/forms/NativeDropdown.tsx
+++ src/view/com/util/forms/NativeDropdown.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome'
 
 import * as DropdownMenu from 'zeego/dropdown-menu'
-import {View} from 'react-native'
+import {StyleSheet, View} from 'react-native'
+import {IconProp} from '@fortawesome/fontawesome-svg-core'
+import {MenuItemCommonProps} from 'zeego/lib/typescript/menu'
 export const DropdownMenuRoot = DropdownMenu.Root
 export const DropdownMenuTrigger = DropdownMenu.Trigger
 export const DropdownMenuContent = DropdownMenu.Content
@@ -24,16 +26,25 @@ export const DropdownMenuItemIcon = DropdownMenu.create((props: IconProps) => {
 type SeparatorProps = React.ComponentProps<(typeof DropdownMenu)['Separator']>
 export const DropdownMenuSeparator = DropdownMenu.create(
   (props: SeparatorProps) => {
-    return <DropdownMenu.Separator {...props} />
+    return (
+      <DropdownMenu.Separator
+        {...props}
+        style={[props.style, styles.separator]}
+      />
+    )
   },
   'Separator',
 )
 
 export type DropdownItem = {
-  label: string
-  onPress: () => void
+  label: string | 'separator'
+  onPress?: () => void
   testId?: string
-  icon?: string
+  icon?: {
+    ios: MenuItemCommonProps['ios']
+    android: string
+    web: IconProp
+  }
 }
 type Props = {
   items: DropdownItem[]
@@ -42,19 +53,28 @@ type Props = {
 export function NativeDropdown({items}: Props) {
   return (
     <DropdownMenuRoot>
-      <DropdownMenuTrigger style={{}}>
-        <View hitSlop={{top: 10, bottom: 10, left: 10, right: 10}}>
+      <DropdownMenuTrigger style={{}} action="press">
+        <View hitSlop={{top: 20, bottom: 20, left: 20, right: 20}}>
           <FontAwesomeIcon icon="ellipsis" size={20} />
         </View>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        {items.map((item, index) => (
-          <DropdownMenuItem
-            key={item.testId ? item.testId : `${item.label}_${index}`}
-            onSelect={item.onPress}>
-            <DropdownMenuItemTitle>{item.label}</DropdownMenuItemTitle>
-          </DropdownMenuItem>
-        ))}
+        {items.map((item, index) => {
+          return (
+            <DropdownMenuItem
+              key={item.testId ? item.testId : `${item.label}_${index}`}
+              onSelect={item.onPress}>
+              <DropdownMenuItemTitle>{item.label}</DropdownMenuItemTitle>
+              {item.icon && (
+                <DropdownMenuItemIcon
+                  ios={item.icon.ios}
+                  androidIconName={item.icon.android}>
+                  <FontAwesomeIcon icon={item.icon.web} size={20} />
+                </DropdownMenuItemIcon>
+              )}
+            </DropdownMenuItem>
+          )
+        })}
       </DropdownMenuContent>
     </DropdownMenuRoot>
   )
@@ -72,3 +92,11 @@ export const ExampleDropdown = () => (
     ]}
   />
 )
+
+const styles = StyleSheet.create({
+  separator: {
+    backgroundColor: 'rgb(215, 207, 249)',
+    height: 1,
+    margin: 6,
+  },
+})
