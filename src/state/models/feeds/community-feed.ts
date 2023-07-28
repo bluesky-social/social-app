46a48f70ddaab80fe9117342a632c39b3756cdac
viksit <viksit@usedispatch.xyz>
2023-07-28 16:55:24 -0700

    wip

--- src/state/models/feeds/community-feed.ts
+++ src/state/models/feeds/community-feed.ts
@@ -2,6 +2,7 @@ import { makeAutoObservable, runInAction } from "mobx";
 
 import { AppBskyFeedDefs } from "@atproto/api";
 import { RootStoreModel } from "state/models/root-store";
+import { SOLARPLEX_FEED_API } from "lib/constants";
 import { SolarplexCommunity } from "lib/splx-types";
 import { bundleAsync } from "lib/async/bundle";
 import { sanitizeDisplayName } from "lib/strings/display-names";
@@ -12,13 +13,21 @@ export class CommunityFeedModel {
   // data
   _reactKey: string;
   data: SolarplexCommunity;
+  isRefreshing = false;
+  hasLoaded = false;
+  error = "";
 
   constructor(
     public rootStore: RootStoreModel,
-    view: SolarplexCommunity,
+    id: string,
+    view?: SolarplexCommunity,
   ) {
-    this._reactKey = view.id;
-    this.data = view;
+    this._reactKey = id;
+    if (view) {
+      this.data = view;
+    } else {
+      // fetch via id
+    }
     makeAutoObservable(
       this,
       {
@@ -33,6 +42,18 @@ export class CommunityFeedModel {
 
   get id() {
     return this.data.id;
+  }
+
+  get hasContent() {
+    return this.data.id !== "";
+  }
+
+  get hasError() {
+    return this.error !== "";
+  }
+
+  get isEmpty() {
+    return this.hasLoaded && !this.hasContent;
   }
 
   get displayName() {
@@ -79,6 +100,26 @@ export class CommunityFeedModel {
     //   this.isOnline = res.data.isOnline;
     //   this.isValid = res.data.isValid;
     // });
+  }
+
+  async get(id: string): Promise<CommunityFeedModel> {
+    const response = await fetch(
+      `${SOLARPLEX_FEED_API}/splx/get_community/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+          "Access-Control-Allow-Origin": "no-cors",
+        },
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch community with ID ${id}`);
+    }
+
+    const communityData = await response.json();
+    return new CommunityFeedModel(this.rootStore, communityData);
   }
 
   serialize() {
--- src/view/screens/CommunityFeedScreen.tsx
+++ src/view/screens/CommunityFeedScreen.tsx
@@ -6,11 +6,15 @@ import {
 } from "view/com/util/forms/DropdownButton";
 import { FlatList, StyleSheet, View } from "react-native";
 import { HeartIcon, HeartIconSolid } from "lib/icons";
-import React, { useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { SOLARPLEX_DID, SOLARPLEX_FEED_API } from "lib/constants";
 import { colors, s } from "lib/styles";
 
 import { Button } from "view/com/util/forms/Button";
+import { CenteredView } from "view/com/util/Views";
 import { CommonNavigatorParams } from "lib/routes/types";
+import { CommunityFeedModel } from "state/models/feeds/community-feed";
+import { CommunityHeader } from "view/com/profile/CommunityHeader";
 import { ComposeIcon2 } from "lib/icons";
 import { EmptyState } from "view/com/util/EmptyState";
 import { FAB } from "../com/util/fab/FAB";
@@ -20,7 +24,7 @@ import { Haptics } from "lib/haptics";
 import { LoadLatestBtn } from "view/com/util/load-latest/LoadLatestBtn";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { PostsFeedModel } from "state/models/feeds/posts";
-import { SOLARPLEX_DID } from "lib/constants";
+import { SolarplexCommunity } from "lib/splx-types";
 import { Text } from "view/com/util/text/Text";
 import { TextLink } from "view/com/util/Link";
 import { UserAvatar } from "view/com/util/UserAvatar";
@@ -45,16 +49,40 @@ export const CommunityFeedScreen = withAuthRequired(
     const store = useStores();
     const pal = usePalette("default");
     const { track } = useAnalytics();
-    /**
-    navigation.push("CommunityFeed", {
-            name: item.data.name,
-            rkey: item.id,
-    }); 
-    */
     // name is community name
     // rkey is community id
     // all community feeds are by solarplex DID for now
     const { rkey, name } = route.params;
+    const [communityData, setCommunityData] = useState(null);
+    const [loading, setLoading] = React.useState(true);
+    const fetchCommunityData = async () => {
+      const response = await fetch(
+        `${SOLARPLEX_FEED_API}/splx/get_community/${rkey}`,
+        {
+          method: "GET",
+          headers: {
+            "content-type": "application/json",
+            "Access-Control-Allow-Origin": "no-cors",
+          },
+        },
+      );
+
+      const data = await response.json();
+
+      // Store the fetched data in state
+      setCommunityData(data);
+      setLoading(false);
+    };
+
+    useEffect(() => {
+      fetchCommunityData();
+    }, []);
+
+    const uiState = React.useMemo(() => {
+      if (communityData) {
+        return new CommunityFeedModel(store, communityData);
+      }
+    }, [route.params.rkey, communityData, store]);
 
     const uri = useMemo(
       () => makeRecordUri(SOLARPLEX_DID, "app.bsky.feed.generator", rkey),
@@ -62,6 +90,7 @@ export const CommunityFeedScreen = withAuthRequired(
     );
     const scrollElRef = useRef<FlatList>(null);
     const currentFeed = useCustomFeed(uri);
+
     const algoFeed: PostsFeedModel = useMemo(() => {
       const feed = new PostsFeedModel(store, "custom", {
         feed: uri,
@@ -69,50 +98,11 @@ export const CommunityFeedScreen = withAuthRequired(
       feed.setup();
       return feed;
     }, [store, uri]);
-    const isPinned = store.me.savedFeeds.isPinned(uri);
+
     const [onMainScroll, isScrolledDown, resetMainScroll] =
       useOnMainScroll(store);
+
     useSetTitle(currentFeed?.displayName);
-
-    const onToggleSaved = React.useCallback(async () => {
-      try {
-        Haptics.default();
-        if (currentFeed?.isSaved) {
-          await currentFeed?.unsave();
-        } else {
-          await currentFeed?.save();
-        }
-      } catch (err) {
-        Toast.show(
-          "There was an an issue updating your feeds, please check your internet connection and try again.",
-        );
-        store.log.error("Failed up update communities", { err });
-      }
-    }, [store, currentFeed]);
-
-    const onToggleLiked = React.useCallback(async () => {
-      Haptics.default();
-      try {
-        if (currentFeed?.isLiked) {
-          await currentFeed?.unlike();
-        } else {
-          await currentFeed?.like();
-        }
-      } catch (err) {
-        Toast.show(
-          "There was an an issue contacting the server, please check your internet connection and try again.",
-        );
-        store.log.error("Failed up toggle like", { err });
-      }
-    }, [store, currentFeed]);
-
-    const onTogglePinned = React.useCallback(async () => {
-      Haptics.default();
-      store.me.savedFeeds.togglePinnedFeed(currentFeed!).catch((e) => {
-        Toast.show("There was an issue contacting the server");
-        store.log.error("Failed to toggle pinned feed", { e });
-      });
-    }, [store, currentFeed]);
 
     const onPressShare = React.useCallback(() => {
       const url = toShareUrl(`/community/${rkey}`);
@@ -131,11 +121,6 @@ export const CommunityFeedScreen = withAuthRequired(
 
     const dropdownItems: DropdownItem[] = React.useMemo(() => {
       let items: DropdownItem[] = [
-        // {
-        //   testID: "feedHeaderDropdownRemoveBtn",
-        //   label: "Remove from my communities",
-        //   onPress: onToggleSaved,
-        // },
         {
           testID: "feedHeaderDropdownShareBtn",
           label: "Share link",
@@ -143,81 +128,11 @@ export const CommunityFeedScreen = withAuthRequired(
         },
       ];
       return items;
-    }, [onToggleSaved, onPressShare]);
+    }, [onPressShare]);
 
     const renderHeaderBtns = React.useCallback(() => {
-      return (
-        <></>
-        // <View style={styles.headerBtns}>
-        //   <Button
-        //     type="default-light"
-        //     testID="toggleLikeBtn"
-        //     accessibilityLabel="Like this feed"
-        //     accessibilityHint=""
-        //     onPress={onToggleLiked}
-        //   >
-        //     {currentFeed?.isLiked ? (
-        //       <HeartIconSolid size={19} style={styles.liked} />
-        //     ) : (
-        //       <HeartIcon strokeWidth={3} size={19} style={pal.textLight} />
-        //     )}
-        //   </Button>
-        //   {currentFeed?.isSaved ? (
-        //     <Button
-        //       type="default-light"
-        //       accessibilityLabel={
-        //         isPinned ? "Unpin this feed" : "Pin this feed"
-        //       }
-        //       accessibilityHint=""
-        //       onPress={onTogglePinned}
-        //     >
-        //       <FontAwesomeIcon
-        //         icon="thumb-tack"
-        //         size={17}
-        //         color={isPinned ? colors.blue3 : pal.colors.textLight}
-        //         style={styles.top1}
-        //       />
-        //     </Button>
-        //   ) : undefined}
-        //   {currentFeed?.isSaved ? (
-        //     <DropdownButton
-        //       testID="feedHeaderDropdownBtn"
-        //       type="default-light"
-        //       items={dropdownItems}
-        //       menuWidth={250}
-        //     >
-        //       <FontAwesomeIcon
-        //         icon="ellipsis"
-        //         color={pal.colors.textLight}
-        //         size={18}
-        //       />
-        //     </DropdownButton>
-        //   ) : (
-        //     <Button
-        //       type="default-light"
-        //       onPress={onToggleSaved}
-        //       accessibilityLabel="Add to my feeds"
-        //       accessibilityHint=""
-        //       style={styles.headerAddBtn}
-        //     >
-        //       <FontAwesomeIcon icon="plus" color={pal.colors.link} size={19} />
-        //       <Text type="xl-medium" style={pal.link}>
-        //         Add to My Feeds
-        //       </Text>
-        //     </Button>
-        //   )}
-        // </View>
-      );
-    }, [
-      pal,
-      currentFeed?.isSaved,
-      currentFeed?.isLiked,
-      isPinned,
-      onToggleSaved,
-      onTogglePinned,
-      onToggleLiked,
-      dropdownItems,
-    ]);
+      return <></>;
+    }, [pal, currentFeed?.isSaved, currentFeed?.isLiked, dropdownItems]);
 
     const renderListHeaderComponent = React.useCallback(() => {
       return (
@@ -247,48 +162,6 @@ export const CommunityFeedScreen = withAuthRequired(
               )}
               {isDesktopWeb && !store.session.isSolarplexSession && (
                 <View style={[styles.headerBtns, styles.headerBtnsDesktop]}>
-                  {/* <Button
-                    type={currentFeed?.isSaved ? "default" : "inverted"}
-                    onPress={onToggleSaved}
-                    accessibilityLabel={
-                      currentFeed?.isSaved
-                        ? "Unsave this feed"
-                        : "Save this feed"
-                    }
-                    accessibilityHint=""
-                    label={
-                      currentFeed?.isSaved
-                        ? "Remove from My Feeds"
-                        : "Add to My Feeds"
-                    }
-                  />
-                  <Button
-                    type="default"
-                    accessibilityLabel={
-                      isPinned ? "Unpin this feed" : "Pin this feed"
-                    }
-                    accessibilityHint=""
-                    onPress={onTogglePinned}
-                  >
-                    <FontAwesomeIcon
-                      icon="thumb-tack"
-                      size={15}
-                      color={isPinned ? colors.blue3 : pal.colors.icon}
-                      style={styles.top2}
-                    />
-                  </Button>
-                  <Button
-                    type="default"
-                    accessibilityLabel="Like this feed"
-                    accessibilityHint=""
-                    onPress={onToggleLiked}
-                  >
-                    {currentFeed?.isLiked ? (
-                      <HeartIconSolid size={18} style={styles.liked} />
-                    ) : (
-                      <HeartIcon strokeWidth={3} size={18} style={pal.icon} />
-                    )}
-                  </Button> */}
                   <Button
                     type="default"
                     accessibilityLabel="Share this feed"
@@ -318,19 +191,7 @@ export const CommunityFeedScreen = withAuthRequired(
                 {currentFeed.data.description}
               </Text>
             ) : null}
-            <View style={styles.headerDetailsFooter}>
-              {/* {currentFeed ? (
-                <TextLink
-                  type="md-medium"
-                  style={pal.textLight}
-                  href={`/profile/${name}/feed/${rkey}/liked-by`}
-                  text={`Liked by ${currentFeed.data.likeCount} ${pluralize(
-                    currentFeed?.data.likeCount || 0,
-                    "user",
-                  )}`}
-                />
-              ) : null} */}
-            </View>
+            <View style={styles.headerDetailsFooter}></View>
           </View>
           <View style={[styles.fakeSelector, pal.border]}>
             <View
@@ -350,13 +211,9 @@ export const CommunityFeedScreen = withAuthRequired(
       pal,
       currentFeed,
       store.me.did,
-      onToggleSaved,
-      onToggleLiked,
       onPressShare,
       name,
       rkey,
-      isPinned,
-      onTogglePinned,
       store.session.isSolarplexSession,
     ]);
 
@@ -364,11 +221,37 @@ export const CommunityFeedScreen = withAuthRequired(
       return <EmptyState icon="feed" message="This list is empty!" />;
     }, []);
 
+    const onRefresh = React.useCallback(() => {
+      // uiState
+      //   .refresh()
+      //   .catch((err: any) =>
+      //     store.log.error("Failed to refresh user profile", err),
+      //   );
+    }, [
+      //uiState,
+      store,
+    ]);
+
+    // TODO(viksit): downstream needs isPinned otherwise it prompts an error
+    const isPinned = false;
     return (
       <View style={s.hContentRegion}>
-        {!store.session.isSolarplexSession && (
-          <ViewHeader title="" renderButton={currentFeed && renderHeaderBtns} />
+        {!store.session.isSolarplexSession && uiState && (
+          <>
+            <ViewHeader
+              title=""
+              renderButton={currentFeed && renderHeaderBtns}
+            />
+            <CenteredView>
+              <CommunityHeader
+                view={uiState}
+                onRefreshAll={onRefresh}
+                hideBackButton={true}
+              />
+            </CenteredView>
+          </>
         )}
+
         <Feed
           scrollElRef={scrollElRef}
           feed={algoFeed}
