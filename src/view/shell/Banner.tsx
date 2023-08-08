6e18fd6f5fca29c2e17a76623d27cf15410eef57
0xPratik <sariapratik@gmail.com>
2023-08-09 01:02:49 +0530

    left aligned the logo

--- src/view/shell/Banner.tsx
+++ src/view/shell/Banner.tsx
@@ -1,7 +1,7 @@
-import {Image, StyleSheet, View} from 'react-native'
+import { Image, StyleSheet, View } from "react-native";
 
-import React from 'react'
-import {SolarplexLogo} from 'lib/icons'
+import React from "react";
+import { SolarplexLogo } from "lib/icons";
 
 export const Banner = () => {
   return (
@@ -14,24 +14,25 @@ export const Banner = () => {
       /> */}
       <SolarplexLogo />
     </View>
-  )
-}
+  );
+};
 
 const bannerStyles = StyleSheet.create({
   container: {
     marginBottom: 10,
     flex: 1,
-    position: 'relative',
-    width: '100%',
-    padding: 20,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    position: "relative",
+    width: "100%",
+    paddingLeft: 12,
+    // padding: 20,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
   },
   backgroundImage: {
-    position: 'absolute',
+    position: "absolute",
     zIndex: -1,
-    width: '100%',
+    width: "100%",
     height: 80,
     top: 0,
     left: 0,
@@ -43,4 +44,4 @@ const bannerStyles = StyleSheet.create({
     height: 200,
     marginLeft: 20,
   },
-})
+});
