49c08867a36a31ad85faf711982a981bccc425e3
Ansh Nanda <anshnanda10@gmail.com>
2023-08-09 13:19:26 -0700

    clean up notification handler

--- src/lib/notifications/notifications.ts
+++ src/lib/notifications/notifications.ts
@@ -1,11 +1,14 @@
 import * as Notifications from 'expo-notifications'
 import {RootStoreModel} from '../../state'
 import {resetToTab} from '../../Navigation'
-import {devicePlatform} from 'platform/detection'
-// import {NotificationsFeedItemModel} from '../../state/models/feeds/notifications'
-// import {sanitizeDisplayName} from '../strings/display-names'
-// import {AppBskyEmbedImages, AtUri} from '@atproto/api'
-// import {enforceLen} from '../strings/helpers'
+import {devicePlatform, isIOS} from 'platform/detection'
+// type NotificationReason =
+//   | 'like'
+//   | 'repost'
+//   | 'follow'
+//   | 'mention'
+//   | 'reply'
+//   | 'quote'
 
 export function init(store: RootStoreModel) {
   store.onUnreadNotifications(count => Notifications.setBadgeCountAsync(count))
@@ -40,19 +43,19 @@ export function init(store: RootStoreModel) {
     }
 
     // listens for new changes to the push token
-    Notifications.addPushTokenListener(async ({data: token, type}) => {
-      store.log.debug('Notifications: Push token changed', {token, type})
+    Notifications.addPushTokenListener(async ({data: t, type}) => {
+      store.log.debug('Notifications: Push token changed', {t, type})
       if (token) {
         try {
           await store.agent.api.app.bsky.unspecced.registerPushNotificationEndpoint(
             {
               platform: devicePlatform,
-              token: token,
-              endpoint: 'xyz.blueskyweb.app',
+              token: t,
+              endpoint: 'app.bsky.unspecced.putNotificationPushToken',
               appId: 'xyz.blueskyweb',
             },
           )
-          console.log('SENT PUSH TOKEN')
+          store.log.debug('Notifications: Sent push token', t + type)
         } catch (error) {
           store.log.error('Notifications: Failed to set push token', error)
         }
@@ -61,19 +64,39 @@ export function init(store: RootStoreModel) {
   })
   // handle notifications that are tapped on, regardless of whether the app is in the foreground or background
   Notifications.addNotificationReceivedListener(event => {
-    console.log('Notifications: Recevied one notification', event) // TODO: delete this
     store.log.debug('Notifications: received', event)
-  })
-  const sub = Notifications.addNotificationResponseReceivedListener(event => {
-    store.log.debug('Notification received', event)
-    console.log('Notification received', event) // TODO: delete this
-    if (event.actionIdentifier === Notifications.DEFAULT_ACTION_IDENTIFIER) {
-      store.log.debug('User pressed a notifee, opening notifications')
-      resetToTab('NotificationsTab')
+    if (event.request.trigger.type === 'push') {
+      let payload
+      if (isIOS) {
+        payload = event.request.trigger.payload
+      } else {
+        // TODO: handle android payload
+        // payload = event.request.trigger.remoteMessage?.data
+      }
+      if (payload) {
+        store.log.debug('Notifications: received payload', payload)
+        // TODO: deeplink notif here
+        // const reason = payload.reason as NotificationReason
+      }
     }
   })
 
-  // cleanup function returned
+  const sub = Notifications.addNotificationResponseReceivedListener(
+    response => {
+      console.log('NOTIFICATION RESPONSE LISTENER')
+      store.log.debug(
+        'Notifications: response received',
+        response.actionIdentifier,
+      )
+      if (
+        response.actionIdentifier === Notifications.DEFAULT_ACTION_IDENTIFIER
+      ) {
+        store.log.debug('User pressed a notifee, opening notifications')
+        resetToTab('NotificationsTab')
+      }
+    },
+  )
+
   return () => {
     sub.remove()
   }
@@ -82,62 +105,3 @@ export function init(store: RootStoreModel) {
 export function getPushToken() {
   return Notifications.getDevicePushTokenAsync()
 }
-
-// export function displayNotification(
-//   title: string,
-//   body?: string,
-//   image?: string,
-// ) {
-//   const opts: {title: string; body?: string; ios?: any} = {title}
-//   if (body) {
-//     opts.body = enforceLen(body, 70, true)
-//   }
-//   if (image) {
-//     opts.ios = {
-//       attachments: [{url: image}],
-//     }
-//   }
-//   // TODO: display notification
-//   // return notifee.displayNotification(opts)
-// }
-
-// export function displayNotificationFromModel(
-//   notification: NotificationsFeedItemModel,
-// ) {
-//   let author = sanitizeDisplayName(
-//     notification.author.displayName || notification.author.handle,
-//   )
-//   let title: string
-//   let body: string = ''
-//   if (notification.isLike) {
-//     title = `${author} liked your post`
-//     body = notification.additionalPost?.thread?.postRecord?.text || ''
-//   } else if (notification.isRepost) {
-//     title = `${author} reposted your post`
-//     body = notification.additionalPost?.thread?.postRecord?.text || ''
-//   } else if (notification.isMention) {
-//     title = `${author} mentioned you`
-//     body = notification.additionalPost?.thread?.postRecord?.text || ''
-//   } else if (notification.isReply) {
-//     title = `${author} replied to your post`
-//     body = notification.additionalPost?.thread?.postRecord?.text || ''
-//   } else if (notification.isFollow) {
-//     title = 'New follower!'
-//     body = `${author} has followed you`
-//   } else if (notification.isCustomFeedLike) {
-//     title = `${author} liked your custom feed`
-//     body = `${new AtUri(notification.subjectUri).rkey}`
-//   } else {
-//     return
-//   }
-//   let image
-//   if (
-//     AppBskyEmbedImages.isView(
-//       notification.additionalPost?.thread?.post.embed,
-//     ) &&
-//     notification.additionalPost?.thread?.post.embed.images[0]?.thumb
-//   ) {
-//     image = notification.additionalPost.thread.post.embed.images[0].thumb
-//   }
-//   return displayNotification(title, body, image)
-// }
