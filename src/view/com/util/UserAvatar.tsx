885f65917083c48ea0ef2827736ef92a1c9352bb
viksit <viksit@usedispatch.xyz>
2023-08-16 17:21:21 -0700

    better colors for all avatars

--- src/view/com/util/UserAvatar.tsx
+++ src/view/com/util/UserAvatar.tsx
@@ -1,29 +1,29 @@
-import React, {useMemo} from 'react'
-import {StyleSheet, View} from 'react-native'
-import Svg, {Circle, Path, Rect} from 'react-native-svg'
-import {isAndroid, isWeb} from 'platform/detection'
-import {openCamera, openCropper, openPicker} from '../../../lib/media/picker'
+import React, { useMemo } from "react";
+import { StyleSheet, View } from "react-native";
+import Svg, { Circle, Path, Rect } from "react-native-svg";
+import { isAndroid, isWeb } from "platform/detection";
+import { openCamera, openCropper, openPicker } from "../../../lib/media/picker";
 import {
   useCameraPermission,
   usePhotoLibraryPermission,
-} from 'lib/hooks/usePermissions'
+} from "lib/hooks/usePermissions";
 
-import {AvatarModeration} from 'lib/labeling/types'
-import {DropdownButton} from './forms/DropdownButton'
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome'
-import {HighPriorityImage} from 'view/com/util/images/Image'
-import {IconProp} from '@fortawesome/fontawesome-svg-core'
-import {Image as RNImage} from 'react-native-image-crop-picker'
-import {colors} from 'lib/styles'
-import {usePalette} from 'lib/hooks/usePalette'
-import {useStores} from 'state/index'
+import { AvatarModeration } from "lib/labeling/types";
+import { DropdownButton } from "./forms/DropdownButton";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { HighPriorityImage } from "view/com/util/images/Image";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { Image as RNImage } from "react-native-image-crop-picker";
+import { colors } from "lib/styles";
+import { usePalette } from "lib/hooks/usePalette";
+import { useStores } from "state/index";
 
-type Type = 'user' | 'algo' | 'list'
+type Type = "user" | "algo" | "list";
 
-const BLUR_AMOUNT = isWeb ? 5 : 100
+const BLUR_AMOUNT = isWeb ? 5 : 100;
 
-export function DefaultAvatar({type, size}: {type: Type; size: number}) {
-  if (type === 'algo') {
+export function DefaultAvatar({ type, size }: { type: Type; size: number }) {
+  if (type === "algo") {
     // Font Awesome Pro 6.4.0 by @fontawesome -https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.
     return (
       <Svg
@@ -32,16 +32,17 @@ export function DefaultAvatar({type, size}: {type: Type; size: number}) {
         height={size}
         viewBox="0 0 32 32"
         fill="none"
-        stroke="none">
-        <Rect width="32" height="32" rx="4" fill="#0070FF" />
+        stroke="none"
+      >
+        <Rect width="32" height="32" rx="4" fill="#6E59B1" />
         <Path
           d="M13.5 7.25C13.5 6.55859 14.0586 6 14.75 6C20.9648 6 26 11.0352 26 17.25C26 17.9414 25.4414 18.5 24.75 18.5C24.0586 18.5 23.5 17.9414 23.5 17.25C23.5 12.418 19.582 8.5 14.75 8.5C14.0586 8.5 13.5 7.94141 13.5 7.25ZM8.36719 14.6172L12.4336 18.6836L13.543 17.5742C13.5156 17.4727 13.5 17.3633 13.5 17.25C13.5 16.5586 14.0586 16 14.75 16C15.4414 16 16 16.5586 16 17.25C16 17.9414 15.4414 18.5 14.75 18.5C14.6367 18.5 14.5312 18.4844 14.4258 18.457L13.3164 19.5664L17.3828 23.6328C17.9492 24.1992 17.8438 25.1484 17.0977 25.4414C16.1758 25.8008 15.1758 26 14.125 26C9.63672 26 6 22.3633 6 17.875C6 16.8242 6.19922 15.8242 6.5625 14.9023C6.85547 14.1602 7.80469 14.0508 8.37109 14.6172H8.36719ZM14.75 9.75C18.8906 9.75 22.25 13.1094 22.25 17.25C22.25 17.9414 21.6914 18.5 21 18.5C20.3086 18.5 19.75 17.9414 19.75 17.25C19.75 14.4883 17.5117 12.25 14.75 12.25C14.0586 12.25 13.5 11.6914 13.5 11C13.5 10.3086 14.0586 9.75 14.75 9.75Z"
           fill="white"
         />
       </Svg>
-    )
+    );
   }
-  if (type === 'list') {
+  if (type === "list") {
     // Font Awesome Pro 6.4.0 by @fontawesome -https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.
     return (
       <Svg
@@ -50,7 +51,8 @@ export function DefaultAvatar({type, size}: {type: Type; size: number}) {
         height={size}
         viewBox="0 0 32 32"
         fill="none"
-        stroke="none">
+        stroke="none"
+      >
         <Path
           d="M28 0H4C1.79086 0 0 1.79086 0 4V28C0 30.2091 1.79086 32 4 32H28C30.2091 32 32 30.2091 32 28V4C32 1.79086 30.2091 0 28 0Z"
           fill="#0070FF"
@@ -64,7 +66,7 @@ export function DefaultAvatar({type, size}: {type: Type; size: number}) {
           fill="white"
         />
       </Svg>
-    )
+    );
   }
   return (
     <Svg
@@ -73,8 +75,9 @@ export function DefaultAvatar({type, size}: {type: Type; size: number}) {
       height={size}
       viewBox="0 0 24 24"
       fill="none"
-      stroke="none">
-      <Circle cx="12" cy="12" r="12" fill="#0070ff" />
+      stroke="none"
+    >
+      <Circle cx="12" cy="12" r="12" fill="#6E59B1" />
       <Circle cx="12" cy="9.5" r="3.5" fill="#fff" />
       <Path
         strokeLinecap="round"
@@ -83,51 +86,51 @@ export function DefaultAvatar({type, size}: {type: Type; size: number}) {
         d="M 12.058 22.784 C 9.422 22.784 7.007 21.836 5.137 20.262 C 5.667 17.988 8.534 16.25 11.99 16.25 C 15.494 16.25 18.391 18.036 18.864 20.357 C 17.01 21.874 14.64 22.784 12.058 22.784 Z"
       />
     </Svg>
-  )
+  );
 }
 
 export function UserAvatar({
-  type = 'user',
+  type = "user",
   size,
   avatar,
   moderation,
   onSelectNewAvatar,
 }: {
-  type?: Type
-  size: number
-  avatar?: string | null
-  moderation?: AvatarModeration
-  onSelectNewAvatar?: (img: RNImage | null) => void
+  type?: Type;
+  size: number;
+  avatar?: string | null;
+  moderation?: AvatarModeration;
+  onSelectNewAvatar?: (img: RNImage | null) => void;
 }) {
-  const store = useStores()
-  const pal = usePalette('default')
-  const {requestCameraAccessIfNeeded} = useCameraPermission()
-  const {requestPhotoAccessIfNeeded} = usePhotoLibraryPermission()
+  const store = useStores();
+  const pal = usePalette("default");
+  const { requestCameraAccessIfNeeded } = useCameraPermission();
+  const { requestPhotoAccessIfNeeded } = usePhotoLibraryPermission();
 
   const aviStyle = useMemo(() => {
-    if (type === 'algo' || type === 'list') {
+    if (type === "algo" || type === "list") {
       return {
         width: size,
         height: size,
         borderRadius: 8,
-      }
+      };
     }
     return {
       width: size,
       height: size,
       borderRadius: Math.floor(size / 2),
-    }
-  }, [type, size])
+    };
+  }, [type, size]);
 
   const dropdownItems = useMemo(
     () => [
       !isWeb && {
-        testID: 'changeAvatarCameraBtn',
-        label: 'Camera',
-        icon: 'camera' as IconProp,
+        testID: "changeAvatarCameraBtn",
+        label: "Camera",
+        icon: "camera" as IconProp,
         onPress: async () => {
           if (!(await requestCameraAccessIfNeeded())) {
-            return
+            return;
           }
 
           onSelectNewAvatar?.(
@@ -136,40 +139,40 @@ export function UserAvatar({
               height: 1000,
               cropperCircleOverlay: true,
             }),
-          )
+          );
         },
       },
       {
-        testID: 'changeAvatarLibraryBtn',
-        label: 'Library',
-        icon: 'image' as IconProp,
+        testID: "changeAvatarLibraryBtn",
+        label: "Library",
+        icon: "image" as IconProp,
         onPress: async () => {
           if (!(await requestPhotoAccessIfNeeded())) {
-            return
+            return;
           }
 
           const items = await openPicker({
             aspect: [1, 1],
-          })
-          const item = items[0]
+          });
+          const item = items[0];
 
           const croppedImage = await openCropper(store, {
-            mediaType: 'photo',
+            mediaType: "photo",
             cropperCircleOverlay: true,
             height: item.height,
             width: item.width,
             path: item.path,
-          })
+          });
 
-          onSelectNewAvatar?.(croppedImage)
+          onSelectNewAvatar?.(croppedImage);
         },
       },
       !!avatar && {
-        testID: 'changeAvatarRemoveBtn',
-        label: 'Remove',
-        icon: ['far', 'trash-can'] as IconProp,
+        testID: "changeAvatarRemoveBtn",
+        label: "Remove",
+        icon: ["far", "trash-can"] as IconProp,
         onPress: async () => {
-          onSelectNewAvatar?.(null)
+          onSelectNewAvatar?.(null);
         },
       },
     ],
@@ -180,11 +183,11 @@ export function UserAvatar({
       requestPhotoAccessIfNeeded,
       store,
     ],
-  )
+  );
 
   const warning = useMemo(() => {
     if (!moderation?.warn) {
-      return null
+      return null;
     }
     return (
       <View style={[styles.warningIconContainer, pal.view]}>
@@ -194,8 +197,8 @@ export function UserAvatar({
           size={Math.floor(size / 3)}
         />
       </View>
-    )
-  }, [moderation?.warn, size, pal])
+    );
+  }, [moderation?.warn, size, pal]);
 
   // onSelectNewAvatar is only passed as prop on the EditProfile component
   return onSelectNewAvatar ? (
@@ -206,12 +209,13 @@ export function UserAvatar({
       openToRight
       rightOffset={-10}
       bottomOffset={-10}
-      menuWidth={170}>
+      menuWidth={170}
+    >
       {avatar ? (
         <HighPriorityImage
           testID="userAvatarImage"
           style={aviStyle}
-          source={{uri: avatar}}
+          source={{ uri: avatar }}
           accessibilityRole="image"
         />
       ) : (
@@ -227,38 +231,38 @@ export function UserAvatar({
     </DropdownButton>
   ) : avatar &&
     !((moderation?.blur && isAndroid) /* android crashes with blur */) ? (
-    <View style={{width: size, height: size}}>
+    <View style={{ width: size, height: size }}>
       <HighPriorityImage
         testID="userAvatarImage"
         style={aviStyle}
         contentFit="cover"
-        source={{uri: avatar}}
+        source={{ uri: avatar }}
         blurRadius={moderation?.blur ? BLUR_AMOUNT : 0}
       />
       {warning}
     </View>
   ) : (
-    <View style={{width: size, height: size}}>
+    <View style={{ width: size, height: size }}>
       <DefaultAvatar type={type} size={size} />
       {warning}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   editButtonContainer: {
-    position: 'absolute',
+    position: "absolute",
     width: 24,
     height: 24,
     bottom: 0,
     right: 0,
     borderRadius: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: colors.gray5,
   },
   warningIconContainer: {
-    position: 'absolute',
+    position: "absolute",
     right: 0,
     bottom: 0,
     borderRadius: 100,
@@ -266,4 +270,4 @@ const styles = StyleSheet.create({
   warningIcon: {
     color: colors.red3,
   },
-})
+});
--- src/view/com/util/UserBanner.tsx
+++ src/view/com/util/UserBanner.tsx
@@ -1,81 +1,82 @@
-import React from 'react'
-import {StyleSheet, View} from 'react-native'
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome'
-import {IconProp} from '@fortawesome/fontawesome-svg-core'
-import {Image} from 'expo-image'
-import {colors} from 'lib/styles'
-import {openCamera, openCropper, openPicker} from '../../../lib/media/picker'
-import {useStores} from 'state/index'
+import { StyleSheet, View } from "react-native";
+import { isAndroid, isWeb } from "platform/detection";
+import { openCamera, openCropper, openPicker } from "../../../lib/media/picker";
 import {
-  usePhotoLibraryPermission,
   useCameraPermission,
-} from 'lib/hooks/usePermissions'
-import {DropdownButton} from './forms/DropdownButton'
-import {usePalette} from 'lib/hooks/usePalette'
-import {AvatarModeration} from 'lib/labeling/types'
-import {isWeb, isAndroid} from 'platform/detection'
-import {Image as RNImage} from 'react-native-image-crop-picker'
+  usePhotoLibraryPermission,
+} from "lib/hooks/usePermissions";
+
+import { AvatarModeration } from "lib/labeling/types";
+import { DropdownButton } from "./forms/DropdownButton";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { Image } from "expo-image";
+import { Image as RNImage } from "react-native-image-crop-picker";
+import React from "react";
+import { colors } from "lib/styles";
+import { usePalette } from "lib/hooks/usePalette";
+import { useStores } from "state/index";
 
 export function UserBanner({
   banner,
   moderation,
   onSelectNewBanner,
 }: {
-  banner?: string | null
-  moderation?: AvatarModeration
-  onSelectNewBanner?: (img: RNImage | null) => void
+  banner?: string | null;
+  moderation?: AvatarModeration;
+  onSelectNewBanner?: (img: RNImage | null) => void;
 }) {
-  const store = useStores()
-  const pal = usePalette('default')
-  const {requestCameraAccessIfNeeded} = useCameraPermission()
-  const {requestPhotoAccessIfNeeded} = usePhotoLibraryPermission()
+  const store = useStores();
+  const pal = usePalette("default");
+  const { requestCameraAccessIfNeeded } = useCameraPermission();
+  const { requestPhotoAccessIfNeeded } = usePhotoLibraryPermission();
 
   const dropdownItems = [
     !isWeb && {
-      testID: 'changeBannerCameraBtn',
-      label: 'Camera',
-      icon: 'camera' as IconProp,
+      testID: "changeBannerCameraBtn",
+      label: "Camera",
+      icon: "camera" as IconProp,
       onPress: async () => {
         if (!(await requestCameraAccessIfNeeded())) {
-          return
+          return;
         }
         onSelectNewBanner?.(
           await openCamera(store, {
             width: 3000,
             height: 1000,
           }),
-        )
+        );
       },
     },
     {
-      testID: 'changeBannerLibraryBtn',
-      label: 'Library',
-      icon: 'image' as IconProp,
+      testID: "changeBannerLibraryBtn",
+      label: "Library",
+      icon: "image" as IconProp,
       onPress: async () => {
         if (!(await requestPhotoAccessIfNeeded())) {
-          return
+          return;
         }
-        const items = await openPicker()
+        const items = await openPicker();
 
         onSelectNewBanner?.(
           await openCropper(store, {
-            mediaType: 'photo',
+            mediaType: "photo",
             path: items[0].path,
             width: 3000,
             height: 1000,
           }),
-        )
+        );
       },
     },
     !!banner && {
-      testID: 'changeBannerRemoveBtn',
-      label: 'Remove',
-      icon: ['far', 'trash-can'] as IconProp,
+      testID: "changeBannerRemoveBtn",
+      label: "Remove",
+      icon: ["far", "trash-can"] as IconProp,
       onPress: () => {
-        onSelectNewBanner?.(null)
+        onSelectNewBanner?.(null);
       },
     },
-  ]
+  ];
 
   // setUserBanner is only passed as prop on the EditProfile component
   return onSelectNewBanner ? (
@@ -86,12 +87,13 @@ export function UserBanner({
       openToRight
       rightOffset={-200}
       bottomOffset={-10}
-      menuWidth={170}>
+      menuWidth={170}
+    >
       {banner ? (
         <Image
           testID="userBannerImage"
           style={styles.bannerImage}
-          source={{uri: banner}}
+          source={{ uri: banner }}
           accessible={true}
           accessibilityIgnoresInvertColors
         />
@@ -105,7 +107,7 @@ export function UserBanner({
         <FontAwesomeIcon
           icon="camera"
           size={12}
-          style={{color: colors.white}}
+          style={{ color: colors.white }}
           color={pal.text.color as string}
         />
       </View>
@@ -116,7 +118,7 @@ export function UserBanner({
       testID="userBannerImage"
       style={styles.bannerImage}
       resizeMode="cover"
-      source={{uri: banner}}
+      source={{ uri: banner }}
       blurRadius={moderation?.blur ? 100 : 0}
       accessible={true}
       accessibilityIgnoresInvertColors
@@ -126,26 +128,26 @@ export function UserBanner({
       testID="userBannerFallback"
       style={[styles.bannerImage, styles.defaultBanner]}
     />
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   editButtonContainer: {
-    position: 'absolute',
+    position: "absolute",
     width: 24,
     height: 24,
     bottom: 8,
     right: 24,
     borderRadius: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: colors.gray5,
   },
   bannerImage: {
-    width: '100%',
+    width: "100%",
     height: 150,
   },
   defaultBanner: {
-    backgroundColor: '#0070ff',
+    backgroundColor: "#6E59B1",
   },
-})
+});
