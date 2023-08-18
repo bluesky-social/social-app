a503766f2ee31c7fe440ddb07a1e559f21bd8a15
Paul Frazee <pfrazee@gmail.com>
2023-08-17 18:51:05 -0700

    Fix to post meta overflow in highlighted post

--- src/view/com/post-thread/PostThreadItem.tsx
+++ src/view/com/post-thread/PostThreadItem.tsx
@@ -226,7 +226,6 @@ export const PostThreadItem = observer(function PostThreadItem({
                 </Link>
               </View>
             </View>
-            <View style={s.flex1} />
             <PostDropdownBtn
               testID="postDropdownBtn"
               itemUri={itemUri}
@@ -239,7 +238,12 @@ export const PostThreadItem = observer(function PostThreadItem({
               onOpenTranslate={onOpenTranslate}
               onToggleThreadMute={onToggleThreadMute}
               onDeletePost={onDeletePost}
-              style={{paddingVertical: 6, paddingHorizontal: 10}}
+              style={{
+                paddingVertical: 6,
+                paddingHorizontal: 10,
+                marginLeft: 'auto',
+                width: 40,
+              }}
             />
           </View>
           <View style={[s.pl10, s.pr10, s.pb10]}>
@@ -556,7 +560,7 @@ const styles = StyleSheet.create({
   },
   metaItem: {
     paddingRight: 5,
-    maxWidth: 240,
+    maxWidth: 220,
   },
   alert: {
     marginBottom: 6,
