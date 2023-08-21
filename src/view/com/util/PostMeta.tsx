d1b450aae6ae413e796558326a8a7399fc715ff9
Eric Bailey <git@esb.lol>
2023-08-21 17:20:45 -0500

    use css tooltips

--- src/view/com/util/PostMeta.tsx
+++ src/view/com/util/PostMeta.tsx
@@ -79,6 +79,7 @@ export const PostMeta = observer(function (opts: PostMetaOpts) {
             lineHeight={1.2}
             text={timeElapsed}
             accessibilityLabel={niceDate(opts.timestamp)}
+            title={niceDate(opts.timestamp)}
             accessibilityHint=""
             href={opts.postHref}
           />
