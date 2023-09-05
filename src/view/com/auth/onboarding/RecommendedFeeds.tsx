52f3810e81e6c96ef72876f3394f5a8eaf49a1b3
Paul Frazee <pfrazee@gmail.com>
2023-09-05 14:03:39 -0700

    Update tests

--- src/view/com/auth/onboarding/RecommendedFeeds.tsx
+++ src/view/com/auth/onboarding/RecommendedFeeds.tsx
@@ -79,7 +79,7 @@ export const RecommendedFeeds = observer(({next}: Props) => {
     <>
       <TabletOrDesktop>
         <TitleColumnLayout
-          testID="recommendedFeedsScreen"
+          testID="recommendedFeedsOnboarding"
           title={title}
           horizontal
           titleStyle={isTabletOrMobile ? undefined : {minWidth: 470}}
@@ -93,7 +93,7 @@ export const RecommendedFeeds = observer(({next}: Props) => {
         </TitleColumnLayout>
       </TabletOrDesktop>
       <Mobile>
-        <View style={[mStyles.container]} testID="recommendedFeedsScreen">
+        <View style={[mStyles.container]} testID="recommendedFeedsOnboarding">
           <ViewHeader
             title="Recommended Feeds"
             showBackButton={false}
