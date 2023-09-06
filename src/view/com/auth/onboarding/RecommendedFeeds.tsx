72f04f700997972718de20a8ec4673526d1a2f26
Eric Bailey <git@esb.lol>
2023-09-06 16:15:58 -0500

    oops fix logic

--- src/view/com/auth/onboarding/RecommendedFeeds.tsx
+++ src/view/com/auth/onboarding/RecommendedFeeds.tsx
@@ -43,7 +43,7 @@ export const RecommendedFeeds = observer(({next}: Props) => {
     },
   })
 
-  const hasFeeds = recommendedFeeds && !recommendedFeeds.length
+  const hasFeeds = recommendedFeeds && recommendedFeeds.length
 
   const title = (
     <>
