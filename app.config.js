6db711310823027b4f794b510efb46c84d165cfe
Paul Frazee <pfrazee@gmail.com>
2023-09-19 21:45:03 -0700

    Bump ios build number

--- app.config.js
+++ app.config.js
@@ -19,7 +19,7 @@ module.exports = function () {
         backgroundColor: '#ffffff',
       },
       ios: {
-        buildNumber: '1',
+        buildNumber: '2',
         supportsTablet: false,
         bundleIdentifier: 'xyz.blueskyweb.app',
         config: {
