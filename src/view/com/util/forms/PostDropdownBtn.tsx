b0873cae97b100ba36c7c4b876665d7904c49291
Foysal Ahamed <foysal@blueskyweb.xyz>
2023-09-26 19:48:07 +0200

    :lipstick: Remove report option on my own post (#1537)

--- src/view/com/util/forms/PostDropdownBtn.tsx
+++ src/view/com/util/forms/PostDropdownBtn.tsx
@@ -105,7 +105,7 @@ export function PostDropdownBtn({
     {
       label: 'separator',
     },
-    {
+    !isAuthor && {
       label: 'Report post',
       onPress() {
         store.shell.openModal({
