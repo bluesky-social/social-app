71eb817c853c74a89daf72af9db05c455dc2cd56
Samuel Newman <mozzius@protonmail.com>
2023-09-27 10:29:43 +0100

    don't mention when using did urls

--- src/view/shell/desktop/LeftNav.tsx
+++ src/view/shell/desktop/LeftNav.tsx
@@ -191,7 +191,10 @@ function ComposeBtn() {
     if (currentRoute.name === 'Profile') {
       const {name: handle} =
         currentRoute.params as CommonNavigatorParams['Profile']
-      if (handle === store.me.handle || handle === 'handle.invalid')
+      // what if a user has a valid handle, but the URL uses the DID?
+      // the DID should not be used, but consider accessing the actual
+      // profile data instead of purely looking at the URL -sfn
+      if (handle === store.me.handle || handle.startsWith('did:'))
         return undefined
       return handle
     }
