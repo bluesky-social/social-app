3a184b9dd8fdb1bad0ac6288bdd7d95987412f48
Paul Frazee <pfrazee@gmail.com>
2023-10-02 18:07:37 -0700

    Fix: only use scroll-positioning control on thread when looking at replies

--- src/view/com/post-thread/PostThread.tsx
+++ src/view/com/post-thread/PostThread.tsx
@@ -367,7 +367,7 @@ export const PostThread = observer(function PostThread({
       data={posts}
       initialNumToRender={posts.length}
       maintainVisibleContentPosition={
-        isNative && view.isFromCache
+        isNative && view.isFromCache && view.isCachedPostAReply
           ? MAINTAIN_VISIBLE_CONTENT_POSITION
           : undefined
       }
