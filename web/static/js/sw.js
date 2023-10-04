e6ab4a3634737599c0ed163fa7072c880b48293f
Bryan Lee <38807139+liby@users.noreply.github.com>
2023-10-05 00:13:30 +0800

    Add manifest.json and Service Worker files

---
+++ web/static/js/sw.js
@@ -0,0 +1,8 @@
+/*global importScripts */
+
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js',
+)
+
+console.clear()
+console.log('Successful registered service worker.')
