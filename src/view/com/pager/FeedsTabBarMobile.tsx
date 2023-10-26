cb9954da06efbf9c0785e5645775fb49d4480f62
Paul Frazee <pfrazee@gmail.com>
2023-10-26 10:46:54 -0700

    Remove unused import

--- src/view/com/pager/FeedsTabBarMobile.tsx
+++ src/view/com/pager/FeedsTabBarMobile.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react'
+import React from 'react'
 import {StyleSheet, TouchableOpacity, View} from 'react-native'
 import {observer} from 'mobx-react-lite'
 import {TabBar} from 'view/com/pager/TabBar'
