a9c76e890738b5bce9c6910904225219ee2c60a1
Dan Abramov <dan.abramov@gmail.com>
2023-10-26 18:55:34 +0100

    Enable inline requires

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,15 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const {getDefaultConfig} = require('expo/metro-config')
 const cfg = getDefaultConfig(__dirname)
+
 cfg.resolver.sourceExts = process.env.RN_SRC_EXT
   ? process.env.RN_SRC_EXT.split(',').concat(cfg.resolver.sourceExts)
   : cfg.resolver.sourceExts
+
+cfg.transformer.getTransformOptions = async () => ({
+  transform: {
+    inlineRequires: true,
+  },
+})
+
 module.exports = cfg
