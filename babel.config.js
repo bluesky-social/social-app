46f3265936d9f0289ce047b1c6d6ca58b1a48d32
dan <dan.abramov@gmail.com>
2023-10-27 01:54:15 +0100

    Enable inline requires (#1756)

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,14 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          lazyImports: true,
+        },
+      ],
+    ],
     plugins: [
       [
         'module:react-native-dotenv',
