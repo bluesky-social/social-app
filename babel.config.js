84ee64025f683ec846827bf98c26a0bf8c54c080
dan <dan.abramov@gmail.com>
2023-10-30 21:54:23 +0000

    Let Metro handle import/export instead of Babel (#1772)
    * Let Metro handle import/export instead of Babel
    
    * Keep using Babel for ESM->CJS in tests

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,17 @@
 module.exports = function (api) {
   api.cache(true)
+  const isTestEnv = process.env.NODE_ENV === 'test'
   return {
     presets: [
       [
         'babel-preset-expo',
         {
           lazyImports: true,
+          native: {
+            // Disable ESM -> CJS compilation because Metro takes care of it.
+            // However, we need it in Jest tests since those run without Metro.
+            disableImportExportTransform: !isTestEnv,
+          },
         },
       ],
     ],
