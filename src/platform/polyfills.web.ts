ecfc257f91bc7490054109771451cd89cf3eeaa9
Dan Abramov <dan.abramov@gmail.com>
2023-11-01 17:38:13 +0000

    Simplify loading emoji data

--- src/platform/polyfills.web.ts
+++ src/platform/polyfills.web.ts
@@ -6,12 +6,3 @@ findLast.shim()
 
 // @ts-ignore whatever typescript wants to complain about here, I dont care about -prf
 window.setImmediate = (cb: () => void) => setTimeout(cb, 0)
-
-// @ts-ignore not on the TS signature due to bad support -prf
-if (!globalThis.Intl?.Segmenter) {
-  // loading emoji mart data
-  // TODO: This condition doesn't make sense; emojimart has nothing to do with Intl.
-  const emojiMartScript = document.createElement('script')
-  emojiMartScript.setAttribute('src', '/static/js/emoji-mart-data.js')
-  document.head.appendChild(emojiMartScript)
-}
