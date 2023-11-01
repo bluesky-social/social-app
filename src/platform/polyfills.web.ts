91c0723b9984d302947bf67ab120b7a99dfadb32
Dan Abramov <dan.abramov@gmail.com>
2023-11-01 16:43:54 +0000

    Remove unused Intl.Segmenter polyfill

--- src/platform/polyfills.web.ts
+++ src/platform/polyfills.web.ts
@@ -9,12 +9,8 @@ window.setImmediate = (cb: () => void) => setTimeout(cb, 0)
 
 // @ts-ignore not on the TS signature due to bad support -prf
 if (!globalThis.Intl?.Segmenter) {
-  // NOTE loading as a separate script to reduce main bundle size, as this is only needed in FF -prf
-  const script = document.createElement('script')
-  script.setAttribute('src', '/static/js/intl-segmenter-polyfill.min.js')
-  document.head.appendChild(script)
-
   // loading emoji mart data
+  // TODO: This condition doesn't make sense; emojimart has nothing to do with Intl.
   const emojiMartScript = document.createElement('script')
   emojiMartScript.setAttribute('src', '/static/js/emoji-mart-data.js')
   document.head.appendChild(emojiMartScript)
