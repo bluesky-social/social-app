2570fb82fa435405e0aedb36da6253fb839f26f7
Eric Bailey <git@esb.lol>
2023-11-06 11:11:09 -0600

    Comment out unused schema for now

--- src/storage/schema.ts
+++ src/storage/schema.ts
@@ -1,4 +1,4 @@
-import {LabelPreference} from '@atproto/api'
+// import {LabelPreference} from '@atproto/api'
 
 /**
  * The shape of the object we store in local storage
@@ -7,57 +7,57 @@ export type Schema = {
   shell: {
     colorMode: 'system' | 'light' | 'dark'
   }
-  session: {
-    data: {
-      service: string
-      did: `did:plc:${string}`
-    }
-    accounts: {
-      service: string
-      did: `did:plc:${string}`
-      refreshJwt: string
-      accessJwt: string
-      handle: string
-      email: string
-      displayName: string
-      aviUrl: string
-      emailConfirmed: boolean
-    }[]
-  }
-  me: {
-    did: `did:plc:${string}`
-    handle: string
-    displayName: string
-    description: string
-    avatar: string
-  }
-  onboarding: {
-    step: string
-  }
-  preferences: {
-    primaryLanguage: string
-    contentLanguages: string[]
-    postLanguage: string
-    postLanguageHistory: string[]
-    contentLabels: {
-      nsfw: LabelPreference
-      nudity: LabelPreference
-      suggestive: LabelPreference
-      gore: LabelPreference
-      hate: LabelPreference
-      spam: LabelPreference
-      impersonation: LabelPreference
-    }
-    savedFeeds: string[]
-    pinnedFeeds: string[]
-    requireAltTextEnabled: boolean
-  }
-  invitedUsers: {
-    seenDids: string[]
-    copiedInvites: string[]
-  }
-  mutedThreads: {uris: string[]}
-  reminders: {lastEmailConfirm: string}
+  // session: {
+  //   data: {
+  //     service: string
+  //     did: `did:plc:${string}`
+  //   }
+  //   accounts: {
+  //     service: string
+  //     did: `did:plc:${string}`
+  //     refreshJwt: string
+  //     accessJwt: string
+  //     handle: string
+  //     email: string
+  //     displayName: string
+  //     aviUrl: string
+  //     emailConfirmed: boolean
+  //   }[]
+  // }
+  // me: {
+  //   did: `did:plc:${string}`
+  //   handle: string
+  //   displayName: string
+  //   description: string
+  //   avatar: string
+  // }
+  // onboarding: {
+  //   step: string
+  // }
+  // preferences: {
+  //   primaryLanguage: string
+  //   contentLanguages: string[]
+  //   postLanguage: string
+  //   postLanguageHistory: string[]
+  //   contentLabels: {
+  //     nsfw: LabelPreference
+  //     nudity: LabelPreference
+  //     suggestive: LabelPreference
+  //     gore: LabelPreference
+  //     hate: LabelPreference
+  //     spam: LabelPreference
+  //     impersonation: LabelPreference
+  //   }
+  //   savedFeeds: string[]
+  //   pinnedFeeds: string[]
+  //   requireAltTextEnabled: boolean
+  // }
+  // invitedUsers: {
+  //   seenDids: string[]
+  //   copiedInvites: string[]
+  // }
+  // mutedThreads: {uris: string[]}
+  // reminders: {lastEmailConfirm: string}
 }
 
 /**
