2c489104c57f700afa3eeefa8a46d17fa30622f1
Paul Frazee <pfrazee@gmail.com>
2023-11-13 14:21:03 -0800

    Add feedgens tab to profile

---
+++ src/state/queries/profile-feedgens.ts
@@ -0,0 +1,31 @@
+import {AppBskyFeedGetActorFeeds} from '@atproto/api'
+import {useInfiniteQuery, InfiniteData, QueryKey} from '@tanstack/react-query'
+import {useSession} from '../session'
+
+const PAGE_SIZE = 30
+type RQPageParam = string | undefined
+
+export const RQKEY = (did: string) => ['profile-feedgens', did]
+
+export function useProfileFeedgensQuery(did: string) {
+  const {agent} = useSession()
+  return useInfiniteQuery<
+    AppBskyFeedGetActorFeeds.OutputSchema,
+    Error,
+    InfiniteData<AppBskyFeedGetActorFeeds.OutputSchema>,
+    QueryKey,
+    RQPageParam
+  >({
+    queryKey: RQKEY(did),
+    async queryFn({pageParam}: {pageParam: RQPageParam}) {
+      const res = await agent.app.bsky.feed.getActorFeeds({
+        actor: did,
+        limit: PAGE_SIZE,
+        cursor: pageParam,
+      })
+      return res.data
+    },
+    initialPageParam: undefined,
+    getNextPageParam: lastPage => lastPage.cursor,
+  })
+}
