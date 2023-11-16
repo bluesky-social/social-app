1952705dedd53041c1bcd39a3a04059ed5236ed6
Paul Frazee <pfrazee@gmail.com>
2023-11-16 15:42:37 -0800

    Disable refetchOnWindowFocus on react-query (#1936)

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,3 +1,13 @@
 import {QueryClient} from '@tanstack/react-query'
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // NOTE
+      // refetchOnWindowFocus breaks some UIs (like feeds)
+      // so we NEVER want to enable this
+      // -prf
+      refetchOnWindowFocus: false,
+    },
+  },
+})
