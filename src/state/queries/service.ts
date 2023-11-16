4d886142e438f5983b7c94c1f2d4a08a4d8f5352
Eric Bailey <git@esb.lol>
2023-11-16 10:07:10 -0600

    Remove caching

--- src/state/queries/service.ts
+++ src/state/queries/service.ts
@@ -7,7 +7,6 @@ export const RQKEY = (serviceUrl: string) => ['service', serviceUrl]
 export function useServiceQuery() {
   const {agent} = useSession()
   return useQuery({
-    staleTime: 1000 * 60 * 60 * 24, // one day
     queryKey: RQKEY(agent.service.toString()),
     queryFn: async () => {
       const res = await agent.com.atproto.server.describeServer()
