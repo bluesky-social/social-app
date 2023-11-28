ab05426f8bd11d15a91a2847e4744da58a3c54e7
Eric Bailey <git@esb.lol>
2023-11-28 14:46:17 -0600

    Clarify

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -9,9 +9,9 @@ import * as info from 'expo-updates'
 import {init} from 'sentry-expo'
 
 /**
- * Matches the build profiles from `eas.json`
+ * Matches the build profile `channel` props in `eas.json`
  */
-const buildProfile = (info.channel || 'development') as
+const buildChannel = (info.channel || 'development') as
   | 'development'
   | 'preview'
   | 'production'
@@ -27,6 +27,6 @@ init({
   dsn: 'https://05bc3789bf994b81bd7ce20c86ccd3ae@o4505071687041024.ingest.sentry.io/4505071690514432',
   debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
   enableInExpoDevelopment: true,
-  environment: buildProfile,
+  environment: buildChannel,
   dist: appVersion,
 })
