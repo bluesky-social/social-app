a963af11ca9d693e01401de039971271e6c649ba
Dan Abramov <dan.abramov@gmail.com>
2023-11-29 02:51:00 +0000

    Disable RQ structural sharing

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -8,6 +8,10 @@ export const queryClient = new QueryClient({
       // so we NEVER want to enable this
       // -prf
       refetchOnWindowFocus: false,
+      // Structural sharing between responses makes it impossible to rely on
+      // "first seen" timestamps on objects to determine if they're fresh.
+      // Disable this optimization so that we can rely on "first seen" timestamps.
+      structuralSharing: false,
     },
   },
 })
