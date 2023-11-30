3fef00b93f0c433bb68b21523d4a28bb086f6be9
Dan Abramov <dan.abramov@gmail.com>
2023-11-30 14:06:48 +0000

    Associate shadows with the cache

--- src/state/cache/post-shadow.ts
+++ src/state/cache/post-shadow.ts
@@ -3,6 +3,7 @@ import EventEmitter from 'eventemitter3'
 import {AppBskyFeedDefs} from '@atproto/api'
 import {batchedUpdates} from '#/lib/batchedUpdates'
 import {Shadow, castAsShadow} from './types'
+import {queryClient} from 'lib/react-query'
 export type {Shadow} from './types'
 
 const emitter = new EventEmitter()
@@ -17,49 +18,100 @@ export interface PostShadow {
 
 export const POST_TOMBSTONE = Symbol('PostTombstone')
 
-interface CacheEntry {
-  ts: number
-  value: PostShadow
-}
+let shadows = new WeakMap()
 
 export function usePostShadow(
   post: AppBskyFeedDefs.PostView,
 ): Shadow<AppBskyFeedDefs.PostView> | typeof POST_TOMBSTONE {
-  // TODO
-  return post
+  const [shadow, setShadow] = useState(() => shadows.get(post))
+  const [prevPost, setPrevPost] = useState(post)
+  if (post !== prevPost) {
+    setPrevPost(post)
+    setShadow(shadows.get(post))
+  }
+
+  useEffect(() => {
+    function onUpdate() {
+      setShadow(shadows.get(post))
+    }
+    emitter.addListener(post.uri, onUpdate)
+    return () => {
+      emitter.removeListener(post.uri, onUpdate)
+    }
+  }, [post])
+
+  return useMemo(() => {
+    if (shadow) {
+      return mergeShadow(post, shadow)
+    } else {
+      return castAsShadow(post)
+    }
+  }, [post, shadow])
+}
+
+function findPosts(uri) {
+  const found = []
+  for (let [key, data] of queryClient.getQueriesData()) {
+    if (!data) {
+      continue
+    }
+    switch (key[0]) {
+      case 'post-feed': {
+        for (let page of data.pages) {
+          for (let slice of page.slices) {
+            for (let item of slice.items) {
+              const post = item.post
+              if (post.uri === uri) {
+                found.push(post)
+              }
+            }
+          }
+        }
+        break
+      }
+      case 'post-thread': {
+        if (data.post.uri === uri) {
+          found.push(data.post)
+        }
+        for (let reply of data.replies) {
+          if (reply.post.uri === uri) {
+            found.push(reply.post)
+          }
+        }
+        break
+      }
+    }
+  }
+  return found
 }
 
 export function updatePostShadow(uri: string, value: Partial<PostShadow>) {
-  batchedUpdates(() => {
-    // TODO
-  })
-}
-
-function fromPost(post: AppBskyFeedDefs.PostView): PostShadow {
-  return {
-    likeUri: post.viewer?.like,
-    likeCount: post.likeCount,
-    repostUri: post.viewer?.repost,
-    repostCount: post.repostCount,
-    isDeleted: false,
+  const matches = findPosts(uri)
+  for (let post of matches) {
+    shadows.set(post, {...shadows.get(post), ...value})
   }
+
+  batchedUpdates(() => {
+    emitter.emit(uri)
+  })
 }
 
 function mergeShadow(
   post: AppBskyFeedDefs.PostView,
-  shadow: PostShadow,
+  shadow: Partial<PostShadow>,
 ): Shadow<AppBskyFeedDefs.PostView> | typeof POST_TOMBSTONE {
   if (shadow.isDeleted) {
     return POST_TOMBSTONE
   }
   return castAsShadow({
     ...post,
-    likeCount: shadow.likeCount,
-    repostCount: shadow.repostCount,
+    likeCount: 'likeCount' in shadow ? shadow.likeCount : post.likeCount,
+    repostCount:
+      'repostCount' in shadow ? shadow.repostCount : post.repostCount,
     viewer: {
       ...(post.viewer || {}),
-      like: shadow.likeUri,
-      repost: shadow.repostUri,
+      like: 'likeUri' in shadow ? shadow.likeUri : post.viewer?.like,
+      repost: 'repostUri' in shadow ? shadow.repostUri : post.viewer?.repost,
     },
   })
 }
