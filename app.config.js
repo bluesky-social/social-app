c3e97fee1dd11bbcfd677dba8282e577df143bb9
Paul Frazee <pfrazee@gmail.com>
2023-11-29 20:12:55 -0800

    Bump ios build number

--- app.config.js
+++ app.config.js
@@ -9,7 +9,7 @@ module.exports = function () {
   /**
    * iOS build number. Must be incremented for each TestFlight version.
    */
-  const IOS_BUILD_NUMBER = '4'
+  const IOS_BUILD_NUMBER = '5'
 
   /**
    * Android build number. Must be incremented for each release.
