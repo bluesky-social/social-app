005fc7cc386add96c044e27ae9af7a1725f730a0
Eric Bailey <git@esb.lol>
2023-11-29 19:50:14 -0600

    Disable retrying globally (#2038)

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -12,6 +12,11 @@ export const queryClient = new QueryClient({
       // "first seen" timestamps on objects to determine if they're fresh.
       // Disable this optimization so that we can rely on "first seen" timestamps.
       structuralSharing: false,
+      // We don't want to retry queries by default, because in most cases we
+      // want to fail early and show a response to the user. There are
+      // exceptions, and those can be made on a per-query basis. For others, we
+      // should give users controls to retry.
+      retry: false,
     },
   },
 })
