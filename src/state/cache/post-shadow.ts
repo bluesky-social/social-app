286e86eebf4aea9da965cb34d3c66869f26d9e86
Paul Frazee <pfrazee@gmail.com>
2023-11-30 13:30:17 -0800

    Pass setShadow into the effect deps

--- src/state/cache/post-shadow.ts
+++ src/state/cache/post-shadow.ts
@@ -43,7 +43,7 @@ export function usePostShadow(
     return () => {
       emitter.removeListener(post.uri, onUpdate)
     }
-  }, [post])
+  }, [post, setShadow])
 
   return useMemo(() => {
     if (shadow) {
