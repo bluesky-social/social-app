c1887f510f2e1610e9eaa46068cac45b755b7e93
Eric Bailey <git@esb.lol>
2023-12-04 13:14:20 -0600

    Fix log handling via env vars in expo

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import 'react-native-gesture-handler' // must be first
-
 import {LogBox} from 'react-native'
-LogBox.ignoreLogs(['Require cycle:']) // suppress require-cycle warnings, it's fine
 
 import '#/platform/polyfills'
+import {IS_TEST} from '#/env'
 import {registerRootComponent} from 'expo'
 import {doPolyfill} from '#/lib/api/api-polyfill'
-doPolyfill()
 
 import App from '#/App'
+
+doPolyfill()
+
+if (IS_TEST) {
+  LogBox.ignoreAllLogs() // suppress all logs in tests
+} else {
+  LogBox.ignoreLogs(['Require cycle:']) // suppress require-cycle warnings, it's fine
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
