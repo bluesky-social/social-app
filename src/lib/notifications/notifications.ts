09db4a41b4e2bd2d6f127d137d7b1dd27db2e422
Dan Abramov <dan.abramov@gmail.com>
2023-12-05 23:59:13 +0000

    Extract per-session listener to a function

--- src/lib/notifications/notifications.ts
+++ src/lib/notifications/notifications.ts
@@ -1,3 +1,4 @@
+import {BskyAgent} from '@atproto/api'
 import * as Notifications from 'expo-notifications'
 import {QueryClient} from '@tanstack/react-query'
 import {resetToTab} from '../../Navigation'
@@ -7,35 +8,64 @@ import {logger} from '#/logger'
 import {RQKEY as RQKEY_NOTIFS} from '#/state/queries/notifications/feed'
 import {truncateAndInvalidate} from '#/state/queries/util'
 import {listenSessionLoaded} from '#/state/events'
+import {SessionAccount} from '#/state/session'
 
 const SERVICE_DID = (serviceUrl?: string) =>
   serviceUrl?.includes('staging')
     ? 'did:web:api.staging.bsky.dev'
     : 'did:web:api.bsky.app'
 
-export function init(queryClient: QueryClient) {
-  listenSessionLoaded(async (account, agent) => {
-    // request notifications permission once the user has logged in
-    const perms = await Notifications.getPermissionsAsync()
-    if (!perms.granted) {
-      await Notifications.requestPermissionsAsync()
-    }
+async function onSessionLoaded(account: SessionAccount, agent: BskyAgent) {
+  // request notifications permission once the user has logged in
+  const perms = await Notifications.getPermissionsAsync()
+  if (!perms.granted) {
+    await Notifications.requestPermissionsAsync()
+  }
 
-    // register the push token with the server
-    const token = await Notifications.getDevicePushTokenAsync()
-    if (token) {
+  // register the push token with the server
+  const token = await Notifications.getDevicePushTokenAsync()
+  if (token) {
+    try {
+      await agent.api.app.bsky.notification.registerPush({
+        serviceDid: SERVICE_DID(account.service),
+        platform: devicePlatform,
+        token: token.data,
+        appId: 'xyz.blueskyweb.app',
+      })
+      logger.debug(
+        'Notifications: Sent push token (init)',
+        {
+          tokenType: token.type,
+          token: token.data,
+        },
+        logger.DebugContext.notifications,
+      )
+    } catch (error) {
+      logger.error('Notifications: Failed to set push token', {error})
+    }
+  }
+
+  // listens for new changes to the push token
+  // In rare situations, a push token may be changed by the push notification service while the app is running. When a token is rolled, the old one becomes invalid and sending notifications to it will fail. A push token listener will let you handle this situation gracefully by registering the new token with your backend right away.
+  Notifications.addPushTokenListener(async ({data: t, type}) => {
+    logger.debug(
+      'Notifications: Push token changed',
+      {t, tokenType: type},
+      logger.DebugContext.notifications,
+    )
+    if (t) {
       try {
         await agent.api.app.bsky.notification.registerPush({
           serviceDid: SERVICE_DID(account.service),
           platform: devicePlatform,
-          token: token.data,
+          token: t,
           appId: 'xyz.blueskyweb.app',
         })
         logger.debug(
-          'Notifications: Sent push token (init)',
+          'Notifications: Sent push token (event)',
           {
-            tokenType: token.type,
-            token: token.data,
+            tokenType: type,
+            token: t,
           },
           logger.DebugContext.notifications,
         )
@@ -43,37 +73,11 @@ export function init(queryClient: QueryClient) {
         logger.error('Notifications: Failed to set push token', {error})
       }
     }
-
-    // listens for new changes to the push token
-    // In rare situations, a push token may be changed by the push notification service while the app is running. When a token is rolled, the old one becomes invalid and sending notifications to it will fail. A push token listener will let you handle this situation gracefully by registering the new token with your backend right away.
-    Notifications.addPushTokenListener(async ({data: t, type}) => {
-      logger.debug(
-        'Notifications: Push token changed',
-        {t, tokenType: type},
-        logger.DebugContext.notifications,
-      )
-      if (t) {
-        try {
-          await agent.api.app.bsky.notification.registerPush({
-            serviceDid: SERVICE_DID(account.service),
-            platform: devicePlatform,
-            token: t,
-            appId: 'xyz.blueskyweb.app',
-          })
-          logger.debug(
-            'Notifications: Sent push token (event)',
-            {
-              tokenType: type,
-              token: t,
-            },
-            logger.DebugContext.notifications,
-          )
-        } catch (error) {
-          logger.error('Notifications: Failed to set push token', {error})
-        }
-      }
-    })
   })
+}
+
+export function init(queryClient: QueryClient) {
+  listenSessionLoaded(onSessionLoaded)
 
   // handle notifications that are received, both in the foreground or background
   Notifications.addNotificationReceivedListener(event => {
