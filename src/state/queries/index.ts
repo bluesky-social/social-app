9a49040766be118b5b5db4b1920123f004d51b40
Paul Frazee <pfrazee@gmail.com>
2023-12-06 12:14:17 -0800

    Update pwi endpoint

--- src/state/queries/index.ts
+++ src/state/queries/index.ts
@@ -1,7 +1,7 @@
 import {BskyAgent} from '@atproto/api'
 
 export const PUBLIC_BSKY_AGENT = new BskyAgent({
-  service: 'https://api.bsky.app',
+  service: 'https://public.api.bsky.app',
 })
 
 export const STALE = {
