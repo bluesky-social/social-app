99fd287bd7e5eeaec18b5e04dc267e62620009b5
Eric Bailey <git@esb.lol>
2023-12-06 16:33:32 -0600

    Add legacy transform test

--- src/state/persisted/__tests__/schema.test.ts
+++ src/state/persisted/__tests__/schema.test.ts
@@ -2,6 +2,7 @@ import {expect, test} from '@jest/globals'
 
 import {transform} from '#/state/persisted/legacy'
 import {defaults, schema} from '#/state/persisted/schema'
+import * as fixtures from '#/state/persisted/__tests__/fixtures'
 
 test('defaults', () => {
   expect(() => schema.parse(defaults)).not.toThrow()
@@ -10,4 +11,11 @@ test('defaults', () => {
 test('transform', () => {
   const data = transform({})
   expect(() => schema.parse(data)).not.toThrow()
+})
+
+test('transform: legacy fixture', () => {
+  const data = transform(fixtures.LEGACY_DATA_DUMP)
+  expect(() => schema.parse(data)).not.toThrow()
+  expect(data.session.currentAccount?.did).toEqual(fixtures.ALICE_DID)
+  expect(data.session.accounts.length).toEqual(2)
 })
