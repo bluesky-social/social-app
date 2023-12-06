792577a4bd976aa1575b4db1b6ac862a81b9f757
Paul Frazee <pfrazee@gmail.com>
2023-12-05 17:50:02 -0800

    Perf: Include quote posts in the post-thread placeholder

--- src/state/queries/util.ts
+++ src/state/queries/util.ts
@@ -1,4 +1,10 @@
 import {QueryClient, QueryKey, InfiniteData} from '@tanstack/react-query'
+import {
+  AppBskyEmbedRecord,
+  AppBskyEmbedRecordWithMedia,
+  AppBskyFeedDefs,
+  AppBskyFeedPost,
+} from '@atproto/api'
 
 export function truncateAndInvalidate<T = any>(
   queryClient: QueryClient,
@@ -14,4 +20,46 @@ export function truncateAndInvalidate<T = any>(
     return data
   })
   queryClient.invalidateQueries({queryKey})
+}
+
+export function getEmbeddedPost(
+  v: unknown,
+): AppBskyEmbedRecord.ViewRecord | undefined {
+  if (
+    AppBskyEmbedRecord.isView(v) &&
+    AppBskyEmbedRecord.validateView(v).success
+  ) {
+    if (
+      AppBskyEmbedRecord.isViewRecord(v.record) &&
+      AppBskyFeedPost.isRecord(v.record.value) &&
+      AppBskyFeedPost.validateRecord(v.record.value).success
+    ) {
+      return v.record
+    }
+  }
+  if (
+    AppBskyEmbedRecordWithMedia.isView(v) &&
+    AppBskyEmbedRecordWithMedia.validateView(v).success
+  ) {
+    if (
+      AppBskyEmbedRecord.isViewRecord(v.record.record) &&
+      AppBskyFeedPost.isRecord(v.record.record.value) &&
+      AppBskyFeedPost.validateRecord(v.record.record.value).success
+    ) {
+      return v.record.record
+    }
+  }
+}
+
+export function embedViewRecordToPostView(
+  v: AppBskyEmbedRecord.ViewRecord,
+): AppBskyFeedDefs.PostView {
+  return {
+    uri: v.uri,
+    cid: v.cid,
+    author: v.author,
+    record: v.value,
+    indexedAt: v.indexedAt,
+    labels: v.labels,
+  }
 }
