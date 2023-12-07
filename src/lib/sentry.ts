d76716f3ef9bb40e5d50159f7ee1a6c45ec21c54
Eric Bailey <git@esb.lol>
2023-12-07 09:11:56 -0600

    Disable sentry in dev

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -40,7 +40,7 @@ init({
   autoSessionTracking: false,
   dsn: 'https://05bc3789bf994b81bd7ce20c86ccd3ae@o4505071687041024.ingest.sentry.io/4505071690514432',
   debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
-  enableInExpoDevelopment: true,
+  enableInExpoDevelopment: false, // enable this to test in dev
   environment: buildChannel,
   dist,
   release,
