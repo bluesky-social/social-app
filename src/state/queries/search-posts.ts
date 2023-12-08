6989f9c29150a0b07fba9c4cbeb91d9730515806
Paul Frazee <pfrazee@gmail.com>
2023-12-07 17:07:09 -0800

    Add posts-search query to shadow cache search

--- src/state/queries/search-posts.ts
+++ src/state/queries/search-posts.ts
@@ -1,7 +1,13 @@
-import {AppBskyFeedSearchPosts} from '@atproto/api'
-import {useInfiniteQuery, InfiniteData, QueryKey} from '@tanstack/react-query'
+import {AppBskyFeedDefs, AppBskyFeedSearchPosts} from '@atproto/api'
+import {
+  useInfiniteQuery,
+  InfiniteData,
+  QueryKey,
+  QueryClient,
+} from '@tanstack/react-query'
 
 import {getAgent} from '#/state/session'
+import {embedViewRecordToPostView, getEmbeddedPost} from './util'
 
 const searchPostsQueryKey = ({query}: {query: string}) => [
   'search-posts',
@@ -27,4 +33,31 @@ export function useSearchPostsQuery({query}: {query: string}) {
     initialPageParam: undefined,
     getNextPageParam: lastPage => lastPage.cursor,
   })
+}
+
+export function* findAllPostsInQueryData(
+  queryClient: QueryClient,
+  uri: string,
+): Generator<AppBskyFeedDefs.PostView, undefined> {
+  const queryDatas = queryClient.getQueriesData<
+    InfiniteData<AppBskyFeedSearchPosts.OutputSchema>
+  >({
+    queryKey: ['search-posts'],
+  })
+  for (const [_queryKey, queryData] of queryDatas) {
+    if (!queryData?.pages) {
+      continue
+    }
+    for (const page of queryData?.pages) {
+      for (const post of page.posts) {
+        if (post.uri === uri) {
+          yield post
+        }
+        const quotedPost = getEmbeddedPost(post.embed)
+        if (quotedPost?.uri === uri) {
+          yield embedViewRecordToPostView(quotedPost)
+        }
+      }
+    }
+  }
 }
