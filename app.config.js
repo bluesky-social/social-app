d82b1a104794eb237b898336fae0918dc1ec6753
Paul Frazee <pfrazee@gmail.com>
2023-12-11 15:52:56 -0800

    1.58

--- app.config.js
+++ app.config.js
@@ -9,12 +9,12 @@ module.exports = function () {
   /**
    * iOS build number. Must be incremented for each TestFlight version.
    */
-  const IOS_BUILD_NUMBER = '15'
+  const IOS_BUILD_NUMBER = '1'
 
   /**
    * Android build number. Must be incremented for each release.
    */
-  const ANDROID_VERSION_CODE = 47
+  const ANDROID_VERSION_CODE = 48
 
   /**
    * Uses built-in Expo env vars
