70a5850e0153e294d2f8cd6e10ccb12b272b45a9
Hailey <me@haileyok.com>
2023-12-12 21:50:19 -0800

    fix: correct the logic

--- src/lib/hooks/usePrefetchListImages.ts
+++ src/lib/hooks/usePrefetchListImages.ts
@@ -40,8 +40,7 @@ export const usePrefetchListImages = (): UsePrefetchListImages => {
    */
   const onViewableItemsChanged = React.useRef(
     ({viewableItems}: {viewableItems: ViewToken[]}) => {
-      // Get the first and last index of the viewable items
-      const firstIndex = viewableItems?.[0]?.index
+      // Get the last index of the viewable items
       const lastIndex = viewableItems?.[viewableItems.length - 1]?.index
 
       // If there is no last index, do nothing. Really this is just for ts.
@@ -58,7 +57,6 @@ export const usePrefetchListImages = (): UsePrefetchListImages => {
        */
       if (
         lastIndex % 5 !== 0 && // If the last index is not a multiple of 5, return
-        firstIndex !== 0 && // If the first index is not 0, return (initial load, we want to prefetch)
         !hasNewItems // If there are no new items, return (prefetch if the length changed)
       ) {
         return
@@ -69,15 +67,14 @@ export const usePrefetchListImages = (): UsePrefetchListImages => {
 
       // Determine where we want to slice from and get the posts
 
-      // If the first index is 0 or there are new items, we want to start with the next item. Otherwise, we want to
-      // skip ahead 5 items
+      // If this is the first load, we can prefetch the first 10 items. Otherwise, we want to prefetch the next 5
       const sliceFrom =
-        firstIndex === 0 || hasNewItems ? lastIndex : lastIndex + 5
+        lastIndex === 0 || hasNewItems ? lastIndex : lastIndex + 5
 
       // We always slice to 10 items ahead of the last index. We need to check though that sliceTo is not greater
       // than the length of the items
       let sliceTo = lastIndex + 10
-      if (sliceTo - 1 > feedItems.current.length) {
+      if (sliceTo > feedItems.current.length - 1) {
         sliceTo = feedItems.current.length - 1
       }
 
@@ -85,7 +82,7 @@ export const usePrefetchListImages = (): UsePrefetchListImages => {
       if (sliceTo <= highestPrefetchedIndex.current) return
 
       // Get the next items
-      const nextItems = feedItems.current.slice(sliceFrom, sliceFrom + 5)
+      const nextItems = feedItems.current.slice(sliceFrom, sliceTo)
 
       const imagesToPrefetch: string[] = []
 
@@ -133,30 +130,21 @@ export const usePrefetchListImages = (): UsePrefetchListImages => {
 
       for (const item of nextItems) {
         // If there's an items array, we know this is on the post screen
-        if (item.items) {
+        if (item.items != null) {
           const items = item.items as FeedPostSliceItem[]
           // Get each sub item
           for (const subItem of items) {
             addImages(subItem.post)
           }
         } else if (item.type === 'post') {
-          const post = item.post as PostView
-
-          addImages(post)
-
-          if (AppBskyEmbedImages.isView(post.embed)) {
-            imagesToPrefetch.push(...post.embed.images.map(i => i.thumb))
-          } else if (
-            AppBskyEmbedExternal.isView(post.embed) &&
-            post.embed.external.thumb != null
-          ) {
-            imagesToPrefetch.push(post.embed.external.thumb)
-          }
+          addImages(item.post as PostView)
         }
       }
 
       // Update the highest prefetched index
       highestPrefetchedIndex.current = sliceTo
+
+      // Run the prefetch
       Image.prefetch(imagesToPrefetch)
     },
   ).current
