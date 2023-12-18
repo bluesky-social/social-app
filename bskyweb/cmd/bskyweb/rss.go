108ee0129c318ecd9d48e1a80e017620a7cb4fea
bryan newbold <bnewbold@robocracy.org>
2023-12-18 19:04:20 +0400

    re-work RSS feed to use DID in URL, not handle
    Shouldn't have RSS feeds break when folks change handle.

--- bskyweb/cmd/bskyweb/rss.go
+++ bskyweb/cmd/bskyweb/rss.go
@@ -30,27 +30,26 @@ type rss struct {
 func (srv *Server) WebProfileRSS(c echo.Context) error {
 	ctx := c.Request().Context()
 
-	handleParam := c.Param("handle")
-	handle, err := syntax.ParseHandle(handleParam)
+	didParam := c.Param("did")
+	did, err := syntax.ParseDID(didParam)
 	if err != nil {
-		return echo.NewHTTPError(400, fmt.Sprintf("not a valid handle: %s", handleParam))
+		return echo.NewHTTPError(400, fmt.Sprintf("not a valid DID: %s", didParam))
 	}
-	handle = handle.Normalize()
 
 	// check that public view is Ok
-	pv, err := appbsky.ActorGetProfile(ctx, srv.xrpcc, handle.String())
+	pv, err := appbsky.ActorGetProfile(ctx, srv.xrpcc, did.String())
 	if err != nil {
-		return echo.NewHTTPError(404, fmt.Sprintf("account not found: %s", handle))
+		return echo.NewHTTPError(404, fmt.Sprintf("account not found: %s", did))
 	}
 	for _, label := range pv.Labels {
 		if label.Src == pv.Did && label.Val == "!no-unauthenticated" {
-			return echo.NewHTTPError(403, fmt.Sprintf("account does not allow public views: %s", handle))
+			return echo.NewHTTPError(403, fmt.Sprintf("account does not allow public views: %s", did))
 		}
 	}
 
-	af, err := appbsky.FeedGetAuthorFeed(ctx, srv.xrpcc, handle.String(), "", "", 30)
+	af, err := appbsky.FeedGetAuthorFeed(ctx, srv.xrpcc, did.String(), "", "", 30)
 	if err != nil {
-		log.Warn("failed to fetch author feed", "handle", handle, "err", err)
+		log.Warn("failed to fetch author feed", "did", did, "err", err)
 		return err
 	}
 
@@ -70,15 +69,15 @@ func (srv *Server) WebProfileRSS(c echo.Context) error {
 			continue
 		}
 		posts = append(posts, Item{
-			Title:       "@" + handle.String() + " post",
-			Link:        fmt.Sprintf("https://bsky.app/profile/%s/post/%s", handle, aturi.RecordKey().String()),
+			Title:       "@" + pv.Handle + " post",
+			Link:        fmt.Sprintf("https://bsky.app/profile/%s/post/%s", pv.Handle, aturi.RecordKey().String()),
 			Description: rec.Text,
 			PubDate:     rec.CreatedAt,
 			GUID:        aturi.String(),
 		})
 	}
 
-	title := "@" + handle.String()
+	title := "@" + pv.Handle
 	if pv.DisplayName != nil {
 		title = title + " - " + *pv.DisplayName
 	}
@@ -89,7 +88,7 @@ func (srv *Server) WebProfileRSS(c echo.Context) error {
 	feed := &rss{
 		Version:     "2.0",
 		Description: desc,
-		Link:        fmt.Sprintf("https://bsky.app/profile/%s", handle.String()),
+		Link:        fmt.Sprintf("https://bsky.app/profile/%s", pv.Handle),
 		Title:       title,
 		Item:        posts,
 	}
