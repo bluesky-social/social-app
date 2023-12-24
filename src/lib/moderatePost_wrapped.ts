51506efca64d6a7aa12a8299eed4014d47d08c25
Paul Frazee <pfrazee@gmail.com>
2023-12-24 15:19:37 -0800

    Apply post-hide to quote posts (#2302)

--- src/lib/moderatePost_wrapped.ts
+++ src/lib/moderatePost_wrapped.ts
@@ -1,4 +1,8 @@
-import {moderatePost} from '@atproto/api'
+import {
+  AppBskyEmbedRecord,
+  AppBskyEmbedRecordWithMedia,
+  moderatePost,
+} from '@atproto/api'
 
 type ModeratePost = typeof moderatePost
 type Options = Parameters<ModeratePost>[1] & {
@@ -21,6 +25,31 @@ export function moderatePost_wrapped(
         type: 'post-hidden',
         source: {type: 'user'},
         priority: 1,
+      }
+    }
+  }
+
+  if (subject.embed) {
+    let embedHidden = false
+    if (AppBskyEmbedRecord.isViewRecord(subject.embed.record)) {
+      embedHidden = hiddenPosts.includes(subject.embed.record.uri)
+    }
+    if (
+      AppBskyEmbedRecordWithMedia.isView(subject.embed) &&
+      AppBskyEmbedRecord.isViewRecord(subject.embed.record.record)
+    ) {
+      embedHidden = hiddenPosts.includes(subject.embed.record.record.uri)
+    }
+    if (embedHidden) {
+      moderations.embed.filter = true
+      moderations.embed.blur = true
+      if (!moderations.embed.cause) {
+        moderations.embed.cause = {
+          // @ts-ignore Temporary extension to the moderation system -prf
+          type: 'post-hidden',
+          source: {type: 'user'},
+          priority: 1,
+        }
       }
     }
   }
