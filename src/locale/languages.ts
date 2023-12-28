01a0af738ce8bfc72333dd31d64d94059c3d34ab
Paul Frazee <pfrazee@gmail.com>
2023-12-28 12:10:18 -0800

    missed a line

--- src/locale/languages.ts
+++ src/locale/languages.ts
@@ -27,7 +27,8 @@ export const APP_LANGUAGES: AppLanguageConfig[] = [
   {code2: AppLanguage.fr, name: 'Français'},
   // DISABLED until this translation is fixed -prf
   // {code2: AppLanguage.de, name: 'Deutsch'},
-  {code2: AppLanguage.es, name: 'Español'},
+  // DISABLED until this translation is more thoroughly reviewed -prf
+  // {code2: AppLanguage.es, name: 'Español'},
 ]
 
 export const LANGUAGES: Language[] = [
