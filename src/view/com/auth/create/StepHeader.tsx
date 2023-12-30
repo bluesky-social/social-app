9d3b282880e0bd179404a03a743167d39859cc95
Minseo Lee <itoupluk427@gmail.com>
2023-12-30 11:24:28 +0900

    Update `StepHeader.tsx`

--- src/view/com/auth/create/StepHeader.tsx
+++ src/view/com/auth/create/StepHeader.tsx
@@ -2,17 +2,15 @@ import React from 'react'
 import {StyleSheet, View} from 'react-native'
 import {Text} from 'view/com/util/text/Text'
 import {usePalette} from 'lib/hooks/usePalette'
-import {useLingui} from '@lingui/react'
-import {Trans, msg} from '@lingui/macro'
+import {Trans} from '@lingui/macro'
 
 export function StepHeader({step, title}: {step: string; title: string}) {
   const pal = usePalette('default')
-  const {_} = useLingui()
 
   return (
     <View style={styles.container}>
       <Text type="lg" style={[pal.textLight]}>
-        {step === '3' ? _(msg`Last step!`) : <Trans>Step {step} of 3</Trans>}
+        {step === '3' ? <Trans>Last step!</Trans> : <Trans>Step {step} of 3</Trans>}
       </Text>
       <Text style={[pal.text]} type="title-xl">
         {title}
