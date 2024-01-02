9c51b0c65d250421eb81c6b7837c64339c575bc5
Minseo Lee <itoupluk427@gmail.com>
2024-01-02 13:57:11 +0900

    Add Korean localization

--- src/locale/i18n.ts
+++ src/locale/i18n.ts
@@ -3,13 +3,14 @@ import {i18n} from '@lingui/core'
 
 import {useLanguagePrefs} from '#/state/preferences'
 import {messages as messagesEn} from '#/locale/locales/en/messages'
-import {messages as messagesHi} from '#/locale/locales/hi/messages'
-import {messages as messagesJa} from '#/locale/locales/ja/messages'
-import {messages as messagesFr} from '#/locale/locales/fr/messages'
 // DISABLED until this translation is fixed -prf
 // import {messages as messagesDe} from '#/locale/locales/de/messages'
 // DISABLED until this translation is more thoroughly reviewed -prf
 // import {messages as messagesEs} from '#/locale/locales/es/messages'
+import {messages as messagesFr} from '#/locale/locales/fr/messages'
+import {messages as messagesHi} from '#/locale/locales/hi/messages'
+import {messages as messagesJa} from '#/locale/locales/ja/messages'
+import {messages as messagesKo} from '#/locale/locales/ko/messages'
 
 import {sanitizeAppLanguageSetting} from '#/locale/helpers'
 import {AppLanguage} from '#/locale/languages'
@@ -19,18 +20,6 @@ import {AppLanguage} from '#/locale/languages'
  */
 export async function dynamicActivate(locale: AppLanguage) {
   switch (locale) {
-    case AppLanguage.hi: {
-      i18n.loadAndActivate({locale, messages: messagesHi})
-      break
-    }
-    case AppLanguage.ja: {
-      i18n.loadAndActivate({locale, messages: messagesJa})
-      break
-    }
-    case AppLanguage.fr: {
-      i18n.loadAndActivate({locale, messages: messagesFr})
-      break
-    }
     // DISABLED until this translation is fixed -prf
     // case AppLanguage.de: {
     //   i18n.loadAndActivate({locale, messages: messagesDe})
@@ -41,6 +30,22 @@ export async function dynamicActivate(locale: AppLanguage) {
     //   i18n.loadAndActivate({locale, messages: messagesEs})
     //   break
     // }
+    case AppLanguage.fr: {
+      i18n.loadAndActivate({locale, messages: messagesFr})
+      break
+    }
+    case AppLanguage.hi: {
+      i18n.loadAndActivate({locale, messages: messagesHi})
+      break
+    }
+    case AppLanguage.ja: {
+      i18n.loadAndActivate({locale, messages: messagesJa})
+      break
+    }
+    case AppLanguage.ko: {
+      i18n.loadAndActivate({locale, messages: messagesKo})
+      break
+    }
     default: {
       i18n.loadAndActivate({locale, messages: messagesEn})
       break
