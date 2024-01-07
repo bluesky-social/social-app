9531bb82c8eb9e79700bd50d9c85f4a79ab0446d
Hailey <me@haileyok.com>
2024-01-06 22:22:24 -0800

    organize imports

--- src/view/com/composer/ComposerReplyTo.tsx
+++ src/view/com/composer/ComposerReplyTo.tsx
@@ -1,21 +1,21 @@
-import {ComposerOptsPostRef} from 'state/shell/composer'
-import {usePalette} from 'lib/hooks/usePalette'
-import {useLingui} from '@lingui/react'
 import React from 'react'
 import {LayoutAnimation, Pressable, StyleSheet, View} from 'react-native'
+import {Image} from 'expo-image'
+import {useLingui} from '@lingui/react'
+import {msg} from '@lingui/macro'
 import {
   AppBskyEmbedImages,
   AppBskyEmbedRecord,
   AppBskyEmbedRecordWithMedia,
   AppBskyFeedPost,
 } from '@atproto/api'
-import {msg} from '@lingui/macro'
-import {UserAvatar} from 'view/com/util/UserAvatar'
-import {Text} from 'view/com/util/text/Text'
+import {ComposerOptsPostRef} from 'state/shell/composer'
+import {usePalette} from 'lib/hooks/usePalette'
 import {sanitizeDisplayName} from 'lib/strings/display-names'
 import {sanitizeHandle} from 'lib/strings/handles'
+import {UserAvatar} from 'view/com/util/UserAvatar'
+import {Text} from 'view/com/util/text/Text'
 import QuoteEmbed from 'view/com/util/post-embeds/QuoteEmbed'
-import {Image} from 'expo-image'
 
 export function ComposerReplyTo({replyTo}: {replyTo: ComposerOptsPostRef}) {
   const pal = usePalette('default')
