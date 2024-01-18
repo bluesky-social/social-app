755a6bf1bc4e80a02b48d9316002f5aba0e29dee
Paul Frazee <pfrazee@gmail.com>
2024-01-18 10:38:39 -0800

    Add optional sms verification

--- src/view/com/auth/create/CreateAccount.tsx
+++ src/view/com/auth/create/CreateAccount.tsx
@@ -117,7 +117,7 @@ export function CreateAccount({onPressBack}: {onPressBack: () => void}) {
 
   return (
     <LoggedOutLayout
-      leadin={`Step ${uiState.step}`}
+      leadin=""
       title={_(msg`Create Account`)}
       description={_(msg`We're so excited to have you join us!`)}>
       <ScrollView testID="createAccount" style={pal.view}>
