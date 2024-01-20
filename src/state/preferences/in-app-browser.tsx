55dce03f5612c2cfb22b8ecf15cff00db5be03cb
Hailey <me@haileyok.com>
2024-01-19 16:18:13 -0800

    use color scheme in in-app browser

--- src/state/preferences/in-app-browser.tsx
+++ src/state/preferences/in-app-browser.tsx
@@ -4,6 +4,7 @@ import {Linking} from 'react-native'
 import * as WebBrowser from 'expo-web-browser'
 import {isNative} from '#/platform/detection'
 import {useModalControls} from '../modals'
+import {usePalette} from 'lib/hooks/usePalette'
 
 type StateContext = persisted.Schema['useInAppBrowser']
 type SetContext = (v: persisted.Schema['useInAppBrowser']) => void
@@ -52,6 +53,7 @@ export function useSetInAppBrowser() {
 export function useOpenLink() {
   const {openModal} = useModalControls()
   const enabled = useInAppBrowser()
+  const pal = usePalette('default')
 
   const openLink = React.useCallback(
     (url: string, override?: boolean) => {
@@ -66,13 +68,14 @@ export function useOpenLink() {
           WebBrowser.openBrowserAsync(url, {
             presentationStyle:
               WebBrowser.WebBrowserPresentationStyle.FULL_SCREEN,
+            toolbarColor: pal.colors.backgroundLight,
           })
           return
         }
       }
       Linking.openURL(url)
     },
-    [enabled, openModal],
+    [enabled, openModal, pal.colors.backgroundLight],
   )
 
   return openLink
