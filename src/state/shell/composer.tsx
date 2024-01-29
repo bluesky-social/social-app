9ae039a456e91ac1009429d1e15ff200ebb717d7
Hailey <me@haileyok.com>
2024-01-28 17:04:20 -0800

    moderate avatars and embeds in composer reply to

--- src/state/shell/composer.tsx
+++ src/state/shell/composer.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import {AppBskyEmbedRecord, AppBskyRichtextFacet} from '@atproto/api'
+import {
+  AppBskyEmbedRecord,
+  AppBskyRichtextFacet,
+  PostModeration,
+} from '@atproto/api'
 import {useNonReactiveCallback} from '#/lib/hooks/useNonReactiveCallback'
 
 export interface ComposerOptsPostRef {
@@ -12,6 +16,7 @@ export interface ComposerOptsPostRef {
     avatar?: string
   }
   embed?: AppBskyEmbedRecord.ViewRecord['embed']
+  moderation?: PostModeration
 }
 export interface ComposerOptsQuote {
   uri: string
@@ -26,6 +31,7 @@ export interface ComposerOptsQuote {
     avatar?: string
   }
   embeds?: AppBskyEmbedRecord.ViewRecord['embeds']
+  moderation?: PostModeration
 }
 export interface ComposerOpts {
   replyTo?: ComposerOptsPostRef
--- src/view/com/post-thread/PostThreadItem.tsx
+++ src/view/com/post-thread/PostThreadItem.tsx
@@ -216,10 +216,11 @@ let PostThreadItemLoaded = ({
           avatar: post.author.avatar,
         },
         embed: post.embed,
+        moderation,
       },
       onPost: onPostReply,
     })
-  }, [openComposer, post, record, onPostReply])
+  }, [openComposer, post, record, onPostReply, moderation])
 
   const onPressShowMore = React.useCallback(() => {
     setLimitLines(false)
