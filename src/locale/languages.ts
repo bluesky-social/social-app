677c20e366ad780e744e723a3a116e6ed97d0482
Frudrax Cheng <i@cynosura.one>
2024-01-29 14:39:02 +0800

    Chinese localization

--- src/locale/languages.ts
+++ src/locale/languages.ts
@@ -16,6 +16,7 @@ export enum AppLanguage {
   pt_BR = 'pt-BR',
   uk = 'uk',
   ca = 'ca',
+  zh_CN = 'zh-CN',
 }
 
 interface AppLanguageConfig {
@@ -35,6 +36,7 @@ export const APP_LANGUAGES: AppLanguageConfig[] = [
   {code2: AppLanguage.pt_BR, name: 'Português (BR) – Portuguese (BR)'},
   {code2: AppLanguage.uk, name: 'Українська – Ukrainian'},
   {code2: AppLanguage.ca, name: 'Català – Catalan'},
+  {code2: AppLanguage.ja, name: '简体中文（中国） – Chinese (Simplified)'},
 ]
 
 export const LANGUAGES: Language[] = [
