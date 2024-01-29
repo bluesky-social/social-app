9277e603ea4aff2ddd47ef47f76b65cc6fe54750
Frudrax Cheng <i@cynosura.one>
2024-01-29 22:54:45 +0800

    fixed languages.ts typo

--- src/locale/languages.ts
+++ src/locale/languages.ts
@@ -36,7 +36,7 @@ export const APP_LANGUAGES: AppLanguageConfig[] = [
   {code2: AppLanguage.pt_BR, name: 'Português (BR) – Portuguese (BR)'},
   {code2: AppLanguage.uk, name: 'Українська – Ukrainian'},
   {code2: AppLanguage.ca, name: 'Català – Catalan'},
-  {code2: AppLanguage.ja, name: '简体中文（中国） – Chinese (Simplified)'},
+  {code2: AppLanguage.zh_CN, name: '简体中文（中国） – Chinese (Simplified)'},
 ]
 
 export const LANGUAGES: Language[] = [
