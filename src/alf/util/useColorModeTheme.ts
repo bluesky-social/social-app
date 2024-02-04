b78e3850943a0fddf04a968e967ac7b258c2a61d
Hailey <me@haileyok.com>
2024-02-04 02:09:07 -0800

    set system background color

--- src/alf/util/useColorModeTheme.ts
+++ src/alf/util/useColorModeTheme.ts
@@ -3,7 +3,8 @@ import {useColorScheme} from 'react-native'
 
 import {useThemePrefs} from 'state/shell'
 import {isWeb} from 'platform/detection'
-import {ThemeName} from '#/alf/themes'
+import {ThemeName, light, dark, dim} from '#/alf/themes'
+import * as SystemUI from 'expo-system-ui'
 
 export function useColorModeTheme(): ThemeName {
   const colorScheme = useColorScheme()
@@ -15,10 +16,12 @@ export function useColorModeTheme(): ThemeName {
       colorMode === 'light'
     ) {
       updateDocument('light')
+      updateSystemBackground('light')
       return 'light'
     } else {
       const themeName = darkTheme ?? 'dim'
       updateDocument(themeName)
+      updateSystemBackground(themeName)
       return themeName
     }
   }, [colorMode, darkTheme, colorScheme])
@@ -31,5 +34,19 @@ function updateDocument(theme: ThemeName) {
     html.className = html.className.replace(/(theme)--\w+/g, '')
 
     html.classList.add(`theme--${theme}`)
+  }
+}
+
+function updateSystemBackground(theme: ThemeName) {
+  switch (theme) {
+    case 'light':
+      SystemUI.setBackgroundColorAsync(light.atoms.bg.backgroundColor)
+      break
+    case 'dark':
+      SystemUI.setBackgroundColorAsync(dark.atoms.bg.backgroundColor)
+      break
+    case 'dim':
+      SystemUI.setBackgroundColorAsync(dim.atoms.bg.backgroundColor)
+      break
   }
 }
