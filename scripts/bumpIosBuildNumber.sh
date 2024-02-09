31d654db8acbd29933390c549862ad2382accf93
Hailey <me@haileyok.com>
2024-02-09 15:50:04 -0800

    remove bak

--- scripts/bumpIosBuildNumber.sh
+++ scripts/bumpIosBuildNumber.sh
@@ -4,5 +4,7 @@ line=$(sed "24q;d" ./app.config.js)
 currentBuildNumber=$(echo "$line" | grep -oE '[0-9]+([.][0-9]+)?')
 newBuildNumber=$((currentBuildNumber+1))
 newBuildVariable="const IOS_BUILD_NUMBER = '$newBuildNumber'"
-sed -i -e "24s/.*/  $newBuildVariable/" ./app.config.js
-echo $newBuildNumber
+sed -i.bak "24s/.*/  $newBuildVariable/" ./app.config.js
+rm -rf ./app.config.js.bak
+
+echo "iOS build number bumped to $newBuildNumber"
