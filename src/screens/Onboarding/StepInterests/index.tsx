a8bfd16bde9e5f9c790ff5fcda31939b179356fa
Minseo Lee <itoupluk427@gmail.com>
2024-02-09 17:00:33 +0900

    Update index.tsx

--- src/screens/Onboarding/StepInterests/index.tsx
+++ src/screens/Onboarding/StepInterests/index.tsx
@@ -176,17 +176,19 @@ export function StepInterests() {
               },
             ]}>
             <Text style={[a.text_md]}>
-              <Text
-                style={[
-                  a.text_md,
-                  a.font_bold,
-                  {
-                    color: t.palette.negative_900,
-                  },
-                ]}>
-                <Trans>Error: </Trans>
-              </Text>
-              {error?.message || 'an unknown error occurred'}
+              <Trans>
+                <Text
+                  style={[
+                    a.text_md,
+                    a.font_bold,
+                    {
+                      color: t.palette.negative_900,
+                    },
+                  ]}>
+                  Error:{' '}
+                </Text>
+              </Trans>
+              {error?.message || _(msg`an unknown error occurred`)}
             </Text>
           </View>
         ) : (
