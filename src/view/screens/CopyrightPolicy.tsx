070ae0cb251df71206ed6855671a9bfe28594b9f
Max Walters <mdwalters.pm@proton.me>
2024-02-12 00:37:24 -0500

    fix: change copyright policy url to new one

--- src/view/screens/CopyrightPolicy.tsx
+++ src/view/screens/CopyrightPolicy.tsx
@@ -34,8 +34,8 @@ export const CopyrightPolicyScreen = (_props: Props) => {
               The Copyright Policy has been moved to{' '}
               <TextLink
                 style={pal.link}
-                href="https://blueskyweb.xyz/support/community-guidelines"
-                text="blueskyweb.xyz/support/community-guidelines"
+                href="https://bsky.social/about/support/community-guidelines"
+                text="bsky.social/about/support/community-guidelines"
               />
             </Trans>
           </Text>
