0ef39d412416014c54fe4ba24d085d1ee3c07583
Hailey <me@haileyok.com>
2024-02-13 16:25:22 -0800

    export path strings for emoji

--- src/components/icons/Bubbles.tsx
+++ src/components/icons/Bubbles.tsx
@@ -1,5 +1,7 @@
 import {createSinglePathSVG} from './TEMPLATE'
 
+export const path =
+  'M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8ZM2 8a6 6 0 1 1 12 0A6 6 0 0 1 2 8Zm16.5 0a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3ZM15 9.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0Zm-.5 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5ZM10 17.5a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0Z'
 export const Bubbles_Stroke2_Corner0_Rounded = createSinglePathSVG({
-  path: 'M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8ZM2 8a6 6 0 1 1 12 0A6 6 0 0 1 2 8Zm16.5 0a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3ZM15 9.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0Zm-.5 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5ZM10 17.5a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0Z',
+  path,
 })
