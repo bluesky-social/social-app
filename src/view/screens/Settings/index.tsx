4e1b4b7f4ce2d657d4629c3ab2de96363381d26a
Minseo Lee <itoupluk427@gmail.com>
2024-02-14 10:46:15 +0900

    Update index.tsx

--- src/view/screens/Settings/index.tsx
+++ src/view/screens/Settings/index.tsx
@@ -756,7 +756,7 @@ export function SettingsScreen({}: Props) {
           accessibilityRole="button"
           accessibilityLabel={_(msg`Export my data`)}
           accessibilityHint={_(
-            msg`Downloads Bluesky account data (repository)`,
+            msg`Opens modal for download Bluesky account data (repository)`,
           )}>
           <View style={[styles.iconContainer, pal.btn]}>
             <FontAwesomeIcon
