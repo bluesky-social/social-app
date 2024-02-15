bf9e367a299035749ff3e2a316d129328dbf9f53
Mary <git@mary.my.id>
2024-02-15 15:17:08 +0700

    fix: normalize relative date

--- src/lib/strings/time.ts
+++ src/lib/strings/time.ts
@@ -4,16 +4,12 @@ const HOUR = MINUTE * 60
 const DAY = HOUR * 24
 const MONTH = DAY * 28
 const YEAR = DAY * 365
-export function ago(date: number | string | Date): string {
-  let ts: number
-  if (typeof date === 'string') {
-    ts = Number(new Date(date))
-  } else if (date instanceof Date) {
-    ts = Number(date)
-  } else {
-    ts = date
-  }
-  const diffSeconds = Math.floor((Date.now() - ts) / 1e3)
+export function ago(time: number | string | Date): string {
+  const base = new Date()
+  const date = time instanceof Date ? time : new Date(time)
+
+  let diffSeconds = Math.floor((base.getTime() - date.getTime()) / 1e3)
+
   if (diffSeconds < NOW) {
     return `now`
   } else if (diffSeconds < MINUTE) {
@@ -23,11 +19,16 @@ export function ago(date: number | string | Date): string {
   } else if (diffSeconds < DAY) {
     return `${Math.floor(diffSeconds / HOUR)}h`
   } else if (diffSeconds < MONTH) {
-    return `${Math.floor(diffSeconds / DAY)}d`
+    date.setHours(0, 0, 0, 0)
+    base.setHours(0, 0, 0, 0)
+
+    const normalizedDiff = Math.floor((base.getTime() - date.getTime()) / 1e3)
+
+    return `${Math.floor(normalizedDiff / DAY)}d`
   } else if (diffSeconds < YEAR) {
     return `${Math.floor(diffSeconds / MONTH)}mo`
   } else {
-    return new Date(ts).toLocaleDateString()
+    return date.toLocaleDateString()
   }
 }
 
