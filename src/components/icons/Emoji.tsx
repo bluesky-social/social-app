b88a4e14b745e1b31c5ca6453790565c767c9fc8
Hailey <me@haileyok.com>
2024-02-14 19:23:31 -0800

    remove unused smiles

--- src/components/icons/Emoji.tsx
+++ src/components/icons/Emoji.tsx
@@ -7,12 +7,6 @@ export const EmojiSad_Stroke2_Corner0_Rounded = createSinglePathSVG({
   path: emojiSadPath,
 })
 
-export const emojiAltPath =
-  'M12 3a1 1 0 0 1 1 1v7h7a1 1 0 1 1 0 2h-7v7a1 1 0 1 1-2 0v-7H4a1 1 0 1 1 0-2h7V4a1 1 0 0 1 1-1Z'
-export const EmojiAlt_Stroke2_Corner0_Rounded = createSinglePathSVG({
-  path: emojiAltPath,
-})
-
 export const emojiArcPath =
   'M12 4a8 8 0 1 0 0 16 8 8 0 0 0 0-16ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm8-5a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0V8a1 1 0 0 1 1-1Zm4 0a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0V8a1 1 0 0 1 1-1Zm-5.894 7.803a1 1 0 0 1 1.341-.447c1.719.859 3.387.859 5.106 0a1 1 0 1 1 .894 1.788c-2.281 1.141-4.613 1.141-6.894 0a1 1 0 0 1-.447-1.341Z'
 export const EmojiArc_Stroke2_Corner0_Rounded = createSinglePathSVG({
@@ -23,10 +17,4 @@ export const emojiHeartEyesPath =
   'M4 12a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm8-10C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2ZM9.351 12.13c1.898-1.507 2.176-2.95 1.613-3.83a1.524 1.524 0 0 0-1.225-.707 1.562 1.562 0 0 0-1.218.53 1.561 1.561 0 0 0-1.326-.082c-.456.186-.8.588-.91 1.083-.227 1.02.527 2.282 2.826 3.048a.256.256 0 0 0 .24-.043Zm5.538.042c2.299-.766 3.053-2.027 2.826-3.048a1.524 1.524 0 0 0-.91-1.082 1.561 1.561 0 0 0-1.326.081 1.562 1.562 0 0 0-1.217-.53 1.524 1.524 0 0 0-1.226.706c-.563.881-.285 2.325 1.613 3.83.068.054.158.07.24.043Zm1.072 2.38a4 4 0 0 1-7.924 0c-.04-.293.218-.525.514-.499 2.309.206 4.587.206 6.896 0 .296-.026.555.206.514.5Z'
 export const EmojiHeartEyes_Stroke2_Corner0_Rounded = createSinglePathSVG({
   path: emojiHeartEyesPath,
-})
-
-export const emojiSmilePath =
-  'M12 3a1 1 0 0 1 1 1v7h7a1 1 0 1 1 0 2h-7v7a1 1 0 1 1-2 0v-7H4a1 1 0 1 1 0-2h7V4a1 1 0 0 1 1-1Z'
-export const EmojiSmile_Stroke2_Corner0_Rounded = createSinglePathSVG({
-  path: emojiSmilePath,
 })
