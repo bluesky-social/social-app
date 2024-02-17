e6dc717fa62630ebdea2e5ee4d6bb28e71496b33
Minseo Lee <itoupluk427@gmail.com>
2024-02-17 13:45:24 +0900

    Update FeedPage.tsx

--- src/view/com/feeds/FeedPage.tsx
+++ src/view/com/feeds/FeedPage.tsx
@@ -190,7 +190,7 @@ export function FeedPage({
         <FAB
           testID="composeFAB"
           onPress={onPressCompose}
-          icon={<ComposeIcon2 strokeWidth={1.5} size={29} style={s.white} />}
+          icon={<ComposeIcon2 strokeWidth={1.5} size={30} style={s.white} />}
           accessibilityRole="button"
           accessibilityLabel={_(msg({message: `New post`, context: 'action'}))}
           accessibilityHint=""
