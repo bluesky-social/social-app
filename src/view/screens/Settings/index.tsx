84ed10a5e255caa9f5d27c8e74385e8898efbb83
surfdude29 <149612116+surfdude29@users.noreply.github.com>
2024-02-18 23:51:26 +0000

    Clarify accessibilityHint in Settings

--- src/view/screens/Settings/index.tsx
+++ src/view/screens/Settings/index.tsx
@@ -694,7 +694,7 @@ export function SettingsScreen({}: Props) {
           onPress={isSwitchingAccounts ? undefined : onPressChangeHandle}
           accessibilityRole="button"
           accessibilityLabel={_(msg`Change handle`)}
-          accessibilityHint={_(msg`Choose a new Bluesky username or create`)}>
+          accessibilityHint={_(msg`Opens modal to choose a new Bluesky handle`)}>
           <View style={[styles.iconContainer, pal.btn]}>
             <FontAwesomeIcon
               icon="at"
