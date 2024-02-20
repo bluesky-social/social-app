7d416678a170348aeaae61bbc8c4044f6d81fdef
Eric Bailey <git@esb.lol>
2024-02-19 19:46:28 -0600

    Some cleanup

--- src/components/TagMenu/index.tsx
+++ src/components/TagMenu/index.tsx
@@ -6,6 +6,8 @@ import * as Dialog from '#/components/Dialog'
 import {Text} from '#/components/Typography'
 import {Button, ButtonIcon, ButtonText} from '#/components/Button'
 import {MagnifyingGlass2_Stroke2_Corner0_Rounded as Search} from '#/components/icons/MagnifyingGlass2'
+import {Clipboard_Stroke2_Corner2_Rounded as Clipboard} from '#/components/icons/Clipboard'
+import {Group3_Stroke2_Corner0_Rounded as Group3} from '#/components/icons/Group3'
 
 export function useTagMenuControl() {
   return Dialog.useDialogControl()
@@ -19,6 +21,7 @@ export function TagMenu({
   tag: string
 }) {
   const t = useTheme()
+
   return (
     <Dialog.Outer control={control}>
       <Dialog.Handle />
@@ -32,17 +35,17 @@ export function TagMenu({
           <Button
             label="tag"
             size="large"
-            variant="solid"
+            variant="outline"
             color="secondary"
             style={[a.flex_1]}>
             <ButtonText>Copy</ButtonText>
-            <ButtonIcon icon={Search} position="right" />
+            <ButtonIcon icon={Clipboard} position="right" />
           </Button>
           <Button
             label="tag"
             size="large"
-            variant="solid"
-            color="secondary"
+            variant="outline"
+            color="primary"
             style={[a.flex_1]}>
             <ButtonText>Search</ButtonText>
             <ButtonIcon icon={Search} position="right" />
@@ -51,7 +54,7 @@ export function TagMenu({
 
         <Button label="tag" size="large" variant="solid" color="secondary">
           <ButtonText>Search posts by this user</ButtonText>
-          <ButtonIcon icon={Search} position="right" />
+          <ButtonIcon icon={Group3} position="right" />
         </Button>
       </Dialog.Inner>
     </Dialog.Outer>
