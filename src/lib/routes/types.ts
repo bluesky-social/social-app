894f00d687355ad3b35e49fda1bb3d17b20ad51c
dan <dan.abramov@gmail.com>
2024-02-22 16:51:11 +0000

    Rename Home Feed Prefs to Following Feed Prefs (#2965)

--- src/lib/routes/types.ts
+++ src/lib/routes/types.ts
@@ -30,7 +30,7 @@ export type CommonNavigatorParams = {
   CopyrightPolicy: undefined
   AppPasswords: undefined
   SavedFeeds: undefined
-  PreferencesHomeFeed: undefined
+  PreferencesFollowingFeed: undefined
   PreferencesThreads: undefined
   PreferencesExternalEmbeds: undefined
 }
