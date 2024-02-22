4fe4bfad157e9127804a28093fc9c6373fdb5386
Eric Bailey <git@esb.lol>
2024-02-22 10:01:31 -0600

    Targets

--- src/screens/Settings/MutedWords/index.tsx
+++ src/screens/Settings/MutedWords/index.tsx
@@ -17,11 +17,12 @@ import {
 import {atoms as a, useTheme, useBreakpoints} from '#/alf'
 import {Text} from '#/components/Typography'
 import * as TextField from '#/components/forms/TextField'
-import {Button, ButtonIcon} from '#/components/Button'
+import {Button, ButtonIcon, ButtonText} from '#/components/Button'
 import {PlusLarge_Stroke2_Corner0_Rounded as Plus} from '#/components/icons/Plus'
 import {TimesLarge_Stroke2_Corner0_Rounded as X} from '#/components/icons/Times'
 import {Divider} from '#/components/Divider'
 import {Loader} from '#/components/Loader'
+import * as Toggle from '#/components/forms/Toggle'
 
 export function MutedWords() {
   const t = useTheme()
@@ -35,13 +36,23 @@ export function MutedWords() {
   const {mutateAsync: addMutedWord} = useUpsertMutedWordsMutation()
   const {mutateAsync: removeMutedWord} = useRemoveMutedWordMutation()
   const [field, setField] = React.useState('')
+  const [targetContentEnabled, setTargetContentEnabled] = React.useState(true)
+  const [targetTagEnabled, setTargetTagEnabled] = React.useState(true)
 
   const add = React.useCallback(() => {
     if (field.trim()) {
-      addMutedWord([{value: field, targets: ['content']}])
+      addMutedWord([
+        {
+          value: field,
+          targets: [
+            targetContentEnabled && 'content',
+            targetTagEnabled && 'tag',
+          ].filter(Boolean) as AppBskyActorDefs.MutedWord['targets'],
+        },
+      ])
       setField('')
     }
-  }, [field, addMutedWord])
+  }, [field, addMutedWord, targetContentEnabled, targetTagEnabled])
 
   const remove = React.useCallback(
     (word: AppBskyActorDefs.MutedWord) => {
@@ -62,26 +73,77 @@ export function MutedWords() {
       <ViewHeader title={_(msg`Muted words`)} showOnDesktop />
 
       <View style={[a.p_xl]}>
-        <Text style={[a.font_bold, t.atoms.text_contrast_medium]}>
-          Add muted words and tags
+        <Text
+          style={[a.text_md, a.font_bold, a.pb_sm, t.atoms.text_contrast_high]}>
+          Add muted words
+        </Text>
+        <Text style={[a.pb_lg, t.atoms.text_contrast_medium]}>
+          Muted words can target a post's content, the posts's tags, or both.
         </Text>
 
-        <View style={[a.mt_md, a.mb_xl, a.flex_row, a.align_center, a.gap_md]}>
-          <TextField.Input
-            label="word"
-            placeholder="Enter word or tag"
-            value={field}
-            onChangeText={setField}
-            onSubmitEditing={add}
+        <TextField.Input
+          label="word"
+          placeholder="Enter word or tag"
+          value={field}
+          onChangeText={setField}
+          onSubmitEditing={add}
+        />
+
+        <View
+          style={[
+            a.mt_lg,
+            a.mb_xl,
+            a.flex_row,
+            a.align_center,
+            a.justify_end,
+            a.gap_md,
+          ]}>
+          <Toggle.Item
+            label="Enable on content"
+            name="content"
+            value={targetContentEnabled}
+            onChange={setTargetContentEnabled}>
+            <Toggle.Label>Content</Toggle.Label>
+            <Toggle.Switch />
+          </Toggle.Item>
+
+          <View
+            style={[
+              {
+                width: 1,
+                height: 30,
+                backgroundColor: t.atoms.border_contrast_low.borderColor,
+              },
+            ]}
           />
+
+          <Toggle.Item
+            label="Enable on content"
+            name="tags"
+            value={targetTagEnabled}
+            onChange={setTargetTagEnabled}>
+            <Toggle.Label>Tags</Toggle.Label>
+            <Toggle.Switch />
+          </Toggle.Item>
+
+          <View
+            style={[
+              {
+                width: 1,
+                height: 30,
+                backgroundColor: t.atoms.border_contrast_low.borderColor,
+              },
+            ]}
+          />
+
           <Button
             label="Add"
             size="small"
             color="primary"
             variant="solid"
-            shape="round"
             style={[a.flex_shrink]}
             onPress={add}>
+            <ButtonText>Add</ButtonText>
             <ButtonIcon icon={Plus} />
           </Button>
         </View>
