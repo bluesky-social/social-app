f291c21b942e2a64e587362db0c1c4cd66482f64
Hailey <me@haileyok.com>
2024-02-27 11:43:57 -0800

    update ios extension

--- modules/Share-with-Bluesky/ShareViewController.swift
+++ modules/Share-with-Bluesky/ShareViewController.swift
@@ -1,13 +1,14 @@
 import UIKit
 
 class ShareViewController: UIViewController {
+  // This allows other forks to use this extension while also changing their
+  // scheme.
   let appScheme = Bundle.main.object(forInfoDictionaryKey: "MainAppScheme") as? String ?? "bluesky"
 
-  // Don't freeze
+  //
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
 
-    // If there are not any items, we are not going to do anything here
     guard let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem,
           let attachments = extensionItem.attachments,
           let firstAttachment = extensionItem.attachments?.first
@@ -17,7 +18,6 @@ class ShareViewController: UIViewController {
     }
 
     Task {
-      // Check the items and see what the type is
       if firstAttachment.hasItemConformingToTypeIdentifier("public.text") {
         await self.handleText(item: firstAttachment)
       } else if firstAttachment.hasItemConformingToTypeIdentifier("public.url") {
@@ -31,14 +31,11 @@ class ShareViewController: UIViewController {
   }
 
   private func handleText(item: NSItemProvider) async -> Void {
-    // Load the text
     do {
       if let data = try await item.loadItem(forTypeIdentifier: "public.text") as? String {
-        // Try to get the text
         if let encoded = data.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
-           let url = URL(string: "\(self.appScheme)://?compose=true&text=\(encoded)")
+           let url = URL(string: "\(self.appScheme)://intent/compose?text=\(encoded)")
         {
-          // Open bluesky
           _ = self.openURL(url)
         }
       }
@@ -51,11 +48,9 @@ class ShareViewController: UIViewController {
   private func handleUrl(item: NSItemProvider) async -> Void {
     do {
       if let data = try await item.loadItem(forTypeIdentifier: "public.url") as? URL {
-        // Get the url and open bluesky
         if let encoded = data.absoluteString.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
-           let url = URL(string: "\(self.appScheme)://?compose=true&text=\(encoded)")
+           let url = URL(string: "\(self.appScheme)://intent/compose?text=\(encoded)")
         {
-          // open bluesky
           _ = self.openURL(url)
         }
       }
@@ -74,7 +69,6 @@ class ShareViewController: UIViewController {
     }
 
     var valid = true
-    // Build a string
     var imageUris = ""
 
     for (index, item) in firstFourItems.enumerated() {
@@ -82,7 +76,7 @@ class ShareViewController: UIViewController {
 
       do {
         if let dataUri = try await item.loadItem(forTypeIdentifier: "public.image") as? URL {
-          // Now we need to duplicate this image, since we don't have access to the outgoing temp directory
+          // We need to duplicate this image, since we don't have access to the outgoing temp directory
           // We also will get the image dimensions here, sinze RN makes it difficult to get those dimensions for local files
           let data = try Data(contentsOf: dataUri)
           let image = UIImage(data: data)
@@ -94,7 +88,6 @@ class ShareViewController: UIViewController {
         valid = false
       }
 
-      // If we got data back, add it on
       if let imageUriInfo = imageUriInfo {
         imageUris.append(imageUriInfo)
         if index < items.count - 1 {
@@ -107,7 +100,7 @@ class ShareViewController: UIViewController {
 
     if valid,
        let encoded = imageUris.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
-       let url = URL(string: "\(self.appScheme)://?compose=true&imageUris=\(encoded)")
+       let url = URL(string: "\(self.appScheme)://intent/compose?imageUris=\(encoded)")
     {
       _ = openURL(url)
     }
@@ -121,28 +114,26 @@ class ShareViewController: UIViewController {
     }
 
     do {
-      // Try to get the app group directory
+      // Saving this file to the bundle group's directory lets us access it from
+      // inside of the app. Otherwise, we wouldn't have access even though the
+      // extension does.
       if let dir = FileManager()
         .containerURL(
           forSecurityApplicationGroupIdentifier: "group.\(Bundle.main.bundleIdentifier?.replacingOccurrences(of: ".Share-with-Bluesky", with: "") ?? "")")
       {
-        // Create the filepath
         let filePath = "\(dir.absoluteString)\(ProcessInfo.processInfo.globallyUniqueString).jpeg"
 
-        // Try to create a new URI and create the JPEG data
         if let newUri = URL(string: filePath),
            let jpegData = image.jpegData(compressionQuality: 1)
         {
-          // Write the data and return the info
           try jpegData.write(to: newUri)
           return "\(newUri.absoluteString)|\(image.size.width)|\(image.size.height)"
         }
       }
+      return nil
     } catch {
       return nil
     }
-
-    return nil
   }
 
   private func completeRequest() -> Void {
@@ -150,13 +141,13 @@ class ShareViewController: UIViewController {
   }
 
   @objc func openURL(_ url: URL) -> Bool {
-      var responder: UIResponder? = self
-      while responder != nil {
-          if let application = responder as? UIApplication {
-              return application.perform(#selector(openURL(_:)), with: url) != nil
-          }
-          responder = responder?.next
+    var responder: UIResponder? = self
+    while responder != nil {
+      if let application = responder as? UIApplication {
+          return application.perform(#selector(openURL(_:)), with: url) != nil
       }
-      return false
+      responder = responder?.next
+    }
+    return false
   }
 }
