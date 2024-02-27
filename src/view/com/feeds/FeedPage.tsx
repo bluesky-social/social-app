9557b76506ac3d0a8f383a186b036b112c18b5b8
Dan Abramov <dan.abramov@gmail.com>
2024-02-27 02:10:40 +0000

    Remove dead code

--- src/view/com/feeds/FeedPage.tsx
+++ src/view/com/feeds/FeedPage.tsx
@@ -200,21 +200,13 @@ export function FeedPage({
 function useHeaderOffset() {
   const {isDesktop, isTablet} = useWebMediaQueries()
   const {fontScale} = useWindowDimensions()
-  const {hasSession} = useSession()
   if (isDesktop || isTablet) {
     return 0
   }
-  if (hasSession) {
-    const navBarPad = 16
-    const navBarText = 21 * fontScale
-    const tabBarPad = 20 + 3 // nav bar padding + border
-    const tabBarText = 16 * fontScale
-    const magic = 7 * fontScale
-    return navBarPad + navBarText + tabBarPad + tabBarText + magic
-  } else {
-    const navBarPad = 16
-    const navBarText = 21 * fontScale
-    const magic = 4 * fontScale
-    return navBarPad + navBarText + magic
-  }
+  const navBarPad = 16
+  const navBarText = 21 * fontScale
+  const tabBarPad = 20 + 3 // nav bar padding + border
+  const tabBarText = 16 * fontScale
+  const magic = 7 * fontScale
+  return navBarPad + navBarText + tabBarPad + tabBarText + magic
 }
