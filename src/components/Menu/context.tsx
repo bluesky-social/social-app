aaae7c07174fc40f888fe108d6d8d67815d9f1c0
Eric Bailey <git@esb.lol>
2024-03-04 17:11:24 -0600

    Remove ignore

--- src/components/Menu/context.tsx
+++ src/components/Menu/context.tsx
@@ -3,6 +3,5 @@ import React from 'react'
 import type {ContextType} from '#/components/Menu/types'
 
 export const Context = React.createContext<ContextType>({
-  // @ts-ignore
   control: null,
 })
