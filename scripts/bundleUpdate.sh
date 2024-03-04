5dbbd9031b71f2c96b985fe7f9d047720d73ee36
Hailey <me@haileyok.com>
2024-03-03 22:05:52 -0800

    oops

--- scripts/bundleUpdate.sh
+++ scripts/bundleUpdate.sh
@@ -24,7 +24,7 @@ tar czvf bundle.tar.gz *
 curl \
 -H "deploy-secret: SUPER_SECRET_PASSWORD_YOU_WONT_GUESS_IT" \
 -H "runtime-version: 1.71" \
--F 'data=@./bundle.tar.gz' http://updates-test.haileyok.com/deploy \
+-F 'data=@./bundle.tar.gz' http://updates-test.haileyok.com:3333/deploy \
 /
 
 cd ..
