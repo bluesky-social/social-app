baba077e37f4cffeb95763062320a51f99b98c1b
Eric Bailey <git@esb.lol>
2024-03-05 12:39:44 -0600

    Ignore uninitialized context

--- src/components/Menu/context.tsx
+++ src/components/Menu/context.tsx
@@ -3,5 +3,6 @@ import React from 'react'
 import type {ContextType} from '#/components/Menu/types'
 
 export const Context = React.createContext<ContextType>({
+  // @ts-ignore
   control: null,
 })
