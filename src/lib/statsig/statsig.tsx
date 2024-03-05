630931efe00346317db786f4149cb74c0e918cfe
Dan Abramov <dan.abramov@gmail.com>
2024-03-05 19:48:11 +0000

    Disable local storage for now

--- src/lib/statsig/statsig.tsx
+++ src/lib/statsig/statsig.tsx
@@ -19,6 +19,7 @@ export function Provider({children}: {children: React.ReactNode}) {
       user={statsigUser}
       waitForInitialization={true}
       options={{
+        disableLocalStorage: true,
         environment: {
           tier:
             process.env.NODE_ENV === 'development'
