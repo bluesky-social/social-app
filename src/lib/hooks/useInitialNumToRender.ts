4da048e97bb4b2c5968075e81c65d678a658fc0c
Hailey <me@haileyok.com>
2024-03-06 15:30:19 -0800

    don't react to screen height changes

--- src/lib/hooks/useInitialNumToRender.ts
+++ src/lib/hooks/useInitialNumToRender.ts
@@ -1,13 +1,11 @@
 import React from 'react'
-import {useWindowDimensions} from 'react-native'
+import {Dimensions} from 'react-native'
 
 const MIN_POST_HEIGHT = 100
 
 export function useInitialNumToRender(minItemHeight: number = MIN_POST_HEIGHT) {
-  const {height: screenHeight} = useWindowDimensions()
-
-  return React.useMemo(
-    () => Math.ceil(screenHeight / minItemHeight) + 1,
-    [screenHeight, minItemHeight],
-  )
+  return React.useMemo(() => {
+    const screenHeight = Dimensions.get('window').height
+    return Math.ceil(screenHeight / minItemHeight) + 1
+  }, [minItemHeight])
 }
