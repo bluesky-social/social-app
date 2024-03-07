07d1a7f5beca8bce7fbb19fd7ffd298cb378b2ba
Hailey <me@haileyok.com>
2024-03-07 12:28:28 -0800

    add example

---
+++ src/components/dialogs/DialogExamples.tsx
@@ -0,0 +1,20 @@
+/**
+ * # CONFIRM
+ *
+ *       <Prompt.Outer control={hidePromptControl}>
+ *         <Prompt.Title>
+ *           <Trans>Hide this post?</Trans>
+ *         </Prompt.Title>
+ *         <Prompt.Description>
+ *           <Trans>This post will be hidden from your feeds.</Trans>
+ *         </Prompt.Description>
+ *         <Prompt.Actions>
+ *           <Prompt.Cancel>Cancel</Prompt.Cancel>
+ *           <Prompt.Action onPress={onHidePost}>
+ *             <ButtonText>
+ *               <Trans>Hide</Trans>
+ *             </ButtonText>
+ *           </Prompt.Action>
+ *         </Prompt.Actions>
+ *       </Prompt.Outer>
+ */
