168139efc79f5e6446e7cd31c60c17c6c5906f63
cdfzo <cdfzo@pm.me>
2024-03-12 10:49:53 +0100

    Fix typo in tagMenuSearchByUser

--- src/components/TagMenu/index.web.tsx
+++ src/components/TagMenu/index.web.tsx
@@ -87,7 +87,7 @@ export function TagMenu({
               author: authorHandle,
             })
           },
-          testID: 'tagMenuSeachByUser',
+          testID: 'tagMenuSearchByUser',
           icon: {
             ios: {
               name: 'magnifyingglass',
