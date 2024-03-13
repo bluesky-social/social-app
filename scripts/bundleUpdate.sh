13cf963e280a2d3abc896115778528141b7d12e0
Hailey <me@haileyok.com>
2024-03-13 12:45:28 -0700

    oops

--- scripts/bundleUpdate.sh
+++ scripts/bundleUpdate.sh
@@ -12,12 +12,11 @@ RUNTIME_VERSION=$(cat metadata.json | jq -r '.runtimeVersion')
 BUNDLE_VERSION=$(date +%s)
 DEPLOYMENT_URL="https://updates.bsky.app/v1/upload?runtime-version=$RUNTIME_VERSION&bundle-version=$BUNDLE_VERSION"
 
-
 tar czvf bundle.tar.gz ./*
 
 echo "Deploying to $DEPLOYMENT_URL..."
 
-curl -o - --form 'bundle=@./bundle.tar.gz' \
+curl -o - --form "bundle=@./bundle.tar.gz" \
 --user "bsky:$DENNIS_API_KEY" \
 --basic "$DEPLOYMENT_URL" \
 /
