9e38057f79b1380dee52c0c417f96d71dfb3c5ac
Hailey <me@haileyok.com>
2024-03-13 09:57:39 -0700

    update script

--- scripts/bundleUpdate.sh
+++ scripts/bundleUpdate.sh
@@ -5,15 +5,16 @@ rm -rf bundle.tar.gz
 
 node scripts/bundleUpdate.js
 
-cd bundleTempDir
+cd bundleTempDir || exit
 
-tar czvf bundle.tar.gz *
+RUNTIME_VERSION=$(cat metadata.json | jq -r '.runtimeVersion')
 
-#curl \
-#-H "deploy-secret: SUPER_SECRET_PASSWORD_YOU_WONT_GUESS_IT" \
-#-H "runtime-version: 1.71" \
-#-F 'data=@./bundle.tar.gz' http://updates-test.haileyok.com:3333/deploy \
-#/
+tar czvf bundle.tar.gz ./*
+
+curl --form 'bundle=@./bundle.tar.gz' \
+--user bsky:abc \
+--basic "http://localhost:12345/v1/upload?runtime-version=$RUNTIME_VERSION&bundle-version=12345678" \
+/
 
 cd ..
 
