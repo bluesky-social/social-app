5d931470570281cfbab91503811e95febcda1802
Hailey <me@haileyok.com>
2024-03-13 09:59:42 -0700

    update script

--- scripts/bundleUpdate.sh
+++ scripts/bundleUpdate.sh
@@ -3,18 +3,23 @@
 rm -rf bundleTempDir
 rm -rf bundle.tar.gz
 
+echo "Creating tarball..."
 node scripts/bundleUpdate.js
 
 cd bundleTempDir || exit
 
 RUNTIME_VERSION=$(cat metadata.json | jq -r '.runtimeVersion')
+DEPLOYMENT_URL="http://localhost:12345/v1/upload?runtime-version=$RUNTIME_VERSION&bundle-version=12345"
+
 
 tar czvf bundle.tar.gz ./*
 
-curl --form 'bundle=@./bundle.tar.gz' \
---user bsky:abc \
---basic "http://localhost:12345/v1/upload?runtime-version=$RUNTIME_VERSION&bundle-version=12345678" \
-/
+echo "Deploying to $DEPLOYMENT_URL..."
+
+#curl --form 'bundle=@./bundle.tar.gz' \
+#--user bsky:abc \
+#--basic "$DEPLOYMENT_URL" \
+#/
 
 cd ..
 
