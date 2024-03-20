4cd7dd0dbaa2341b638395ca43672051e91eabda
Hailey <me@haileyok.com>
2024-03-20 14:22:34 -0700

    remove android specific code

--- src/components/hooks/useOnKeyboard.ts
+++ src/components/hooks/useOnKeyboard.ts
@@ -1,17 +1,9 @@
 import React from 'react'
 import {Keyboard} from 'react-native'
-import {isAndroid} from 'platform/detection'
 
 export function useOnKeyboardDidShow(cb: () => unknown) {
   React.useEffect(() => {
-    const subscription = Keyboard.addListener('keyboardDidShow', () => {
-      // Because this happens immediately on Android, we need to delay it
-      if (isAndroid) {
-        setTimeout(cb, 100)
-      } else {
-        cb()
-      }
-    })
+    const subscription = Keyboard.addListener('keyboardDidShow', cb)
 
     return () => {
       subscription.remove()
