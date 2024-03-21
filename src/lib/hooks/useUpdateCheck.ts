018da939dcd4a007a69e08dd675fd0658fe01b84
Hailey <me@haileyok.com>
2024-03-20 18:03:53 -0700

    adjustments
    version bump
    
    adjust
    
    fixes
    
    test

--- src/lib/hooks/useUpdateCheck.ts
+++ src/lib/hooks/useUpdateCheck.ts
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Alert, AppState, AppStateStatus} from 'react-native'
+import app from 'react-native-version-number'
 import * as Updates from 'expo-updates'
 import {useUpdates} from 'expo-updates'
+
 import {logger} from '#/logger'
+import {isIOS} from 'platform/detection'
 import {IS_TESTFLIGHT} from '#/env'
-import app from 'react-native-version-number'
 
 export function useUpdateCheck() {
   const appState = React.useRef<AppStateStatus>('active')
@@ -17,8 +19,8 @@ export function useUpdateCheck() {
     timeout.current = setTimeout(async () => {
       try {
         await Updates.setExtraParamAsync(
-          'buildNumber',
-          app.buildVersion.toString(),
+          isIOS ? 'build-number' : 'android-build-number',
+          app.buildVersion,
         )
         await Updates.setExtraParamAsync(
           'channel',
@@ -42,21 +44,17 @@ export function useUpdateCheck() {
     }, 15e3)
   }, [])
 
-  React.useEffect(() => {
-    // For Testflight users, we can prompt the user to update immediately whenever there's an available update. This
-    // is suspect however with the Apple App Store guidelines, so we don't want to prompt production users to update
-    // immediately.
-    if (IS_TESTFLIGHT) {
-      ;(async () => {
-        await Updates.setExtraParamAsync(
-          'buildNumber',
-          app.buildVersion.toString(),
-        )
-        await Updates.setExtraParamAsync(
-          'channel',
-          IS_TESTFLIGHT ? 'testflight' : 'production',
-        )
+  const onIsTestFlight = React.useCallback(() => {
+    setTimeout(async () => {
+      await Updates.setExtraParamAsync('build-number', null)
+      await Updates.setExtraParamAsync(
+        isIOS ? 'ios-build-number' : 'android-build-number',
+        app.buildVersion,
+      )
+      await Updates.setExtraParamAsync('channel', 'testflight')
+      await Updates.getExtraParamsAsync()
 
+      try {
         const res = await Updates.checkForUpdateAsync()
         if (res.isAvailable) {
           await Updates.fetchUpdateAsync()
@@ -84,16 +82,27 @@ export function useUpdateCheck() {
             'No update available at this time.',
           )
         }
-      })()
+      } catch (e: any) {
+        Alert.alert('error', e.toString())
+      }
+    }, 3000)
+  }, [])
+
+  React.useEffect(() => {
+    // For Testflight users, we can prompt the user to update immediately whenever there's an available update. This
+    // is suspect however with the Apple App Store guidelines, so we don't want to prompt production users to update
+    // immediately.
+
+    if (IS_TESTFLIGHT) {
+      onIsTestFlight()
       return
-    }
-    if (__DEV__ || ranInitialCheck.current) {
+    } else if (__DEV__ || ranInitialCheck.current) {
       return
     }
 
     setCheckTimeout()
     ranInitialCheck.current = true
-  }, [setCheckTimeout])
+  }, [onIsTestFlight, setCheckTimeout])
 
   // After the app has been minimized for 30 minutes, we want to either A. install an update if one has become available
   // or B check for an update again.
