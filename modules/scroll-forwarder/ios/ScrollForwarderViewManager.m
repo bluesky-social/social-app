0d97c07307e66cbdb78a2328a72f7eda4ac7ac17
Hailey <me@haileyok.com>
2024-04-01 17:12:16 -0700

    remove rn module

--- modules/scroll-forwarder/ios/ScrollForwarderViewManager.m
+++
@@ -1,9 +0,0 @@
-#import <React/RCTViewManager.h>
-
-@interface RCT_EXTERN_MODULE(ScrollForwarderViewManager, RCTViewManager)
-
-RCT_EXPORT_VIEW_PROPERTY(scrollViewTag, NSString)
-RCT_EXPORT_VIEW_PROPERTY(onScrollViewRefresh, RCTBubblingEventBlock)
-RCT_EXPORT_VIEW_PROPERTY(scrollViewRefreshing, BOOL)
-
-@end
