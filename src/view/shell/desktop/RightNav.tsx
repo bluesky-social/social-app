33500d0effb7cfe315a8b22e3c2b715fa8af6bb5
Samuel Newman <mozzius@protonmail.com>
2024-04-03 13:58:34 +0100

    re-disable right nav on tablet

--- src/view/shell/desktop/RightNav.tsx
+++ src/view/shell/desktop/RightNav.tsx
@@ -7,6 +7,7 @@ import {isWeb} from '#/platform/detection'
 import {useSession} from '#/state/session'
 import {FEEDBACK_FORM_URL, HELP_DESK_URL} from 'lib/constants'
 import {usePalette} from 'lib/hooks/usePalette'
+import {useWebMediaQueries} from 'lib/hooks/useWebMediaQueries'
 import {s} from 'lib/styles'
 import {TextLink} from 'view/com/util/Link'
 import {Text} from 'view/com/util/text/Text'
@@ -17,6 +18,11 @@ export function DesktopRightNav({routeName}: {routeName: string}) {
   const pal = usePalette('default')
   const {_} = useLingui()
   const {hasSession, currentAccount} = useSession()
+
+  const {isTablet} = useWebMediaQueries()
+  if (isTablet) {
+    return null
+  }
 
   return (
     <View style={[styles.rightNav, pal.view]}>
