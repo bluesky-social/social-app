b990edd00e48d41429bef61c1919e6699675f051
Hailey <me@haileyok.com>
2024-04-04 19:20:36 -0700

    migrate to `expo-application`

--- src/lib/app-info.ts
+++ src/lib/app-info.ts
@@ -1,9 +1,9 @@
-import VersionNumber from 'react-native-version-number'
+import {nativeApplicationVersion, nativeBuildVersion} from 'expo-application'
 
 export const IS_DEV = process.env.EXPO_PUBLIC_ENV === 'development'
 export const IS_TESTFLIGHT = process.env.EXPO_PUBLIC_ENV === 'testflight'
 
 const UPDATES_CHANNEL = IS_TESTFLIGHT ? 'testflight' : 'production'
-export const appVersion = `${VersionNumber.appVersion} (${
-  VersionNumber.buildVersion
-}, ${IS_DEV ? 'development' : UPDATES_CHANNEL})`
+export const appVersion = `${nativeApplicationVersion} (${nativeBuildVersion}, ${
+  IS_DEV ? 'development' : UPDATES_CHANNEL
+})`
--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -4,7 +4,7 @@
  */
 
 import {Platform} from 'react-native'
-import app from 'react-native-version-number'
+import {nativeApplicationVersion, nativeBuildVersion} from 'expo-application'
 import * as info from 'expo-updates'
 import {init} from 'sentry-expo'
 
@@ -21,7 +21,7 @@ const buildChannel = (info.channel || 'development') as
  * - `dev`
  * - `1.57.0`
  */
-const release = app.appVersion ?? 'dev'
+const release = nativeApplicationVersion ?? 'dev'
 
 /**
  * Examples:
@@ -33,7 +33,7 @@ const release = app.appVersion ?? 'dev'
  * - `android.1.57.0.46`
  */
 const dist = `${Platform.OS}.${release}${
-  app.buildVersion ? `.${app.buildVersion}` : ''
+  nativeBuildVersion ? `.${nativeBuildVersion}` : ''
 }`
 
 init({
