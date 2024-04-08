ca1fa75b9599dc24a7adc1396d7e05b2c8d7bb99
Samuel Newman <mozzius@protonmail.com>
2024-04-08 19:56:40 +0100

    add bskyembed vite app

---
+++ bskyembed/src/app.tsx
@@ -0,0 +1,18 @@
+import {Fragment, h} from 'preact'
+
+export function App() {
+  return (
+    <>
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          className="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn Preact
+        </a>
+      </p>
+    </>
+  )
+}
