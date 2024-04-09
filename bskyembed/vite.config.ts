44ba12f12101c7b604c857e08ce8994b018ad916
Samuel Newman <mozzius@protonmail.com>
2024-04-09 12:17:13 +0100

    add vite plugin legacy

--- bskyembed/vite.config.ts
+++ bskyembed/vite.config.ts
@@ -1,9 +1,16 @@
 import preact from '@preact/preset-vite'
+import legacy from '@vitejs/plugin-legacy'
 import type {UserConfig} from 'vite'
 import paths from 'vite-tsconfig-paths'
 
 const config: UserConfig = {
-  plugins: [preact(), paths()],
+  plugins: [
+    preact(),
+    paths(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
   build: {
     assetsDir: 'static/embed/assets',
   },
