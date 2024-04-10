4fe29042d0aae291d36bca3463302f27b0f70799
Samuel Newman <mozzius@protonmail.com>
2024-04-10 13:22:28 +0100

    [Embeds] Add CTA and add general polish - input needed! (#3454)
    * add CTA, colourful icons, and bigger logo
    
    * make hover effect smaller + add to cta

--- bskyembed/src/post.tsx
+++ bskyembed/src/post.tsx
@@ -1,9 +1,8 @@
 import {AppBskyFeedDefs, AppBskyFeedPost, RichText} from '@atproto/api'
 import {h} from 'preact'
 
-import replyIcon from '../../assets/icons/bubble_stroke2_corner2_rounded.svg'
-import likeIcon from '../../assets/icons/heart2_stroke2_corner0_rounded.svg'
-import repostIcon from '../../assets/icons/repost_stroke2_corner2_rounded.svg'
+import replyIcon from '../assets/bubble_filled_stroke2_corner2_rounded.svg'
+import likeIcon from '../assets/heart2_filled_stroke2_corner0_rounded.svg'
 import logo from '../assets/logo.svg'
 import {Container} from './container'
 import {Embed} from './embed'
@@ -33,7 +32,7 @@ export function Post({thread}: Props) {
               className="w-10 h-10 rounded-full bg-neutral-300 shrink-0"
             />
           </Link>
-          <div>
+          <div className="flex-1">
             <Link
               href={`/profile/${post.author.did}`}
               className="font-bold text-[17px] leading-5 line-clamp-1 hover:underline underline-offset-2 decoration-2">
@@ -45,6 +44,11 @@ export function Post({thread}: Props) {
               <p>@{post.author.handle}</p>
             </Link>
           </div>
+          <Link
+            href="https://bsky.social/about"
+            className="transition-transform hover:scale-110 shrink-0 self-start">
+            <img src={logo as string} className="h-8" />
+          </Link>
         </div>
         <PostContent record={record} />
         <Embed content={post.embed} />
@@ -53,25 +57,27 @@ export function Post({thread}: Props) {
           className="text-textLight mt-1 text-sm">
           {niceDate(post.indexedAt)}
         </time>
-        <div className="border-t w-full pt-2.5 flex items-center gap-4 text-sm">
-          <div className="flex items-center gap-2">
-            <img src={replyIcon as string} className="w-4 h-4" />
-            <p>{post.replyCount}</p>
+        <div className="border-t w-full pt-2.5 flex items-center gap-5 text-sm">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <img src={likeIcon as string} className="w-5 h-5" />
+            <p className="font-bold text-neutral-500">{post.likeCount}</p>
           </div>
-          <div className="flex items-center gap-2">
-            <img src={likeIcon as string} className="w-4 h-4" />
-            <p>{post.likeCount}</p>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <img src={replyIcon as string} className="w-5 h-5" />
+            <p className="font-bold text-neutral-500">Reply</p>
           </div>
-          <div className="flex items-center gap-2">
+          {/* <div className="flex items-center gap-2">
             <img src={repostIcon as string} className="w-4 h-4" />
             <p>{post.repostCount}</p>
-          </div>
+          </div> */}
           <div className="flex-1" />
-          <Link
-            href="https://bsky.social/about"
-            className="transition-transform hover:scale-125">
-            <img src={logo as string} className="h-4" />
-          </Link>
+          <p className="cursor-pointer text-brand font-bold hover:underline">
+            {post.replyCount
+              ? `Read ${post.replyCount} ${
+                  post.replyCount > 1 ? 'replies' : 'reply'
+                } on Bluesky`
+              : `View on Bluesky`}
+          </p>
         </div>
       </div>
     </Container>
