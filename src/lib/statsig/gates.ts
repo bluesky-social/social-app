c3821fdc311fe7ddebede427715892d3a1e53716
Hailey <me@haileyok.com>
2024-04-12 15:22:09 -0700

    Remove vertical scrollbars from views on native (#3429)
    * remove vertical scrollbars
    
    * add to a few missing lists
    
    * gate this change
    
    * use `hide_vertical_scroll_indicators`
    
    * fix gate lint
    
    * fix bool

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -3,6 +3,7 @@ export type Gate =
   | 'autoexpand_suggestions_on_profile_follow'
   | 'disable_min_shell_on_foregrounding'
   | 'disable_poll_on_discover'
+  | 'hide_vertical_scroll_indicators'
   | 'new_profile_scroll_component'
   | 'new_search'
   | 'receive_updates'
