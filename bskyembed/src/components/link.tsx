8a1e7f9afb6fa2eb1d83d8bfb315fb018746a782
Samuel Newman <mozzius@protonmail.com>
2024-04-13 12:36:51 +0100

    add ref_src

--- bskyembed/src/components/link.tsx
+++ bskyembed/src/components/link.tsx
@@ -8,9 +8,14 @@ export function Link({
   href: string
   className?: string
 } & h.JSX.HTMLAttributes<HTMLAnchorElement>) {
+  const newSearchParam = new URLSearchParams()
+  newSearchParam.set('ref_src', 'embed')
+
   return (
     <a
-      href={href.startsWith('http') ? href : `https://bsky.app${href}`}
+      href={`${
+        href.startsWith('http') ? href : `https://bsky.app${href}`
+      }?${newSearchParam.toString()}`}
       target="_blank"
       rel="noopener noreferrer nofollow"
       onClick={evt => evt.stopPropagation()}
