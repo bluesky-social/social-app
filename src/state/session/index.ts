72b3cb103fb516c829bba26fc35a833abb18c7d8
Eric Bailey <git@esb.lol>
2024-04-13 14:19:30 -0500

    Add conditional exports

---
+++ src/state/session/index.ts
@@ -0,0 +1,12 @@
+import * as V1 from '#/state/session/v1'
+import * as V2 from '#/state/session/v2'
+
+export type {CurrentAccount, SessionAccount} from '#/state/session/types'
+
+const isV2 = false
+
+export const getAgent = isV2 ? V2.getAgent : V1.getAgent
+export const Provider = isV2 ? V2.Provider : V1.Provider
+export const useSession = isV2 ? V2.useSession : V1.useSession
+export const useSessionApi = isV2 ? V2.useSessionApi : V1.useSessionApi
+export const useRequireAuth = isV2 ? V2.useRequireAuth : V1.useRequireAuth
