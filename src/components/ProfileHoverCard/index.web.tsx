399bede788642ee940f6beac043360e5fe6a62dd
Minseo Lee <itoupluk427@gmail.com>
2024-04-14 18:38:43 +0900

    Update index.web.tsx

--- src/components/ProfileHoverCard/index.web.tsx
+++ src/components/ProfileHoverCard/index.web.tsx
@@ -328,7 +328,7 @@ function Inner({
             color={profileShadow.viewer?.following ? 'secondary' : 'primary'}
             variant="solid"
             label={
-              profileShadow.viewer?.following ? _('Following') : _('Follow')
+              profileShadow.viewer?.following ? _(msg`Following`) : _(msg`Follow`)
             }
             style={[a.rounded_full]}
             onPress={profileShadow.viewer?.following ? unfollow : follow}>
@@ -337,7 +337,7 @@ function Inner({
               icon={profileShadow.viewer?.following ? Check : Plus}
             />
             <ButtonText>
-              {profileShadow.viewer?.following ? _('Following') : _('Follow')}
+              {profileShadow.viewer?.following ? _(msg`Following`) : _(msg`Follow`)}
             </ButtonText>
           </Button>
         )}
