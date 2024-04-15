75c3ef90534ec344f966c65ccf629233b52e5c33
Samuel Newman <mozzius@protonmail.com>
2024-04-15 22:01:18 +0100

    remove confirmation

--- src/view/com/posts/AviFollowButton.tsx
+++ src/view/com/posts/AviFollowButton.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import {Pressable, View} from 'react-native'
 import {AppBskyActorDefs} from '@atproto/api'
-import {msg, Trans} from '@lingui/macro'
+import {msg} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 
 import {createHitslop} from '#/lib/constants'
 import {useProfileShadow} from '#/state/cache/profile-shadow'
 import {atoms as a, useTheme} from '#/alf'
+import {Button} from '#/components/Button'
 import {useFollowMethods} from '#/components/hooks/useFollowMethods'
-import {PersonPlus_Stroke2_Corner0_Rounded as PersonPlus} from '#/components/icons/Person'
 import {PlusSmall_Stroke2_Corner0_Rounded as Plus} from '#/components/icons/Plus'
-import * as Menu from '#/components/Menu'
 
 export function AviFollowButton({
   author,
@@ -37,38 +36,28 @@ export function AviFollowButton({
     <View style={a.relative}>
       {children}
 
-      <Menu.Root>
-        <Menu.Trigger label={_(msg`Follow ${name}`)}>
-          {({props, state: {pressed}}) => (
-            <Pressable
-              {...props}
-              hitSlop={createHitslop(3)}
-              style={[
-                a.rounded_full,
-                a.absolute,
-                {
-                  bottom: -4,
-                  right: -4,
-                  backgroundColor: pressed
-                    ? t.palette.primary_400
-                    : t.palette.primary_500,
-                  borderWidth: 2,
-                  borderColor: t.palette.white,
-                },
-              ]}>
-              <Plus size="md" fill={t.palette.white} />
-            </Pressable>
-          )}
-        </Menu.Trigger>
-        <Menu.Outer>
-          <Menu.Item label={_(msg`Follow ${name}`)} onPress={follow}>
-            <Menu.ItemText>
-              <Trans>Follow {name}</Trans>
-            </Menu.ItemText>
-            <Menu.ItemIcon icon={PersonPlus} />
-          </Menu.Item>
-        </Menu.Outer>
-      </Menu.Root>
+      <Button label={_(msg`Follow ${name}`)}>
+        {({pressed}) => (
+          <Pressable
+            accessibilityLabel={_(msg`Follow ${name}`)}
+            accessibilityHint=""
+            onPress={follow}
+            hitSlop={createHitslop(3)}
+            style={[
+              a.rounded_full,
+              pressed ? t.atoms.bg_contrast_800 : t.atoms.bg_contrast_975,
+              a.absolute,
+              {
+                bottom: -4,
+                right: -4,
+                borderWidth: 2,
+                borderColor: t.atoms.bg.backgroundColor,
+              },
+            ]}>
+            <Plus size="md" fill={t.atoms.bg.backgroundColor} />
+          </Pressable>
+        )}
+      </Button>
     </View>
   )
 }
