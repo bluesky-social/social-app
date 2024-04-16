69d376800619fc88f75252cf7f9b58629193850b
Hailey <me@haileyok.com>
2024-04-16 14:38:25 -0700

    Better dist strings for Sentry (#3584)
    * better version codes for sentry
    
    * use a `.` to follow the same intended format as before
    
    * ignore dist build number

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -5,16 +5,9 @@
 
 import {Platform} from 'react-native'
 import {nativeApplicationVersion, nativeBuildVersion} from 'expo-application'
-import * as info from 'expo-updates'
 import {init} from 'sentry-expo'
 
-/**
- * Matches the build profile `channel` props in `eas.json`
- */
-const buildChannel = (info.channel || 'development') as
-  | 'development'
-  | 'preview'
-  | 'production'
+import {BUILD_ENV, IS_DEV, IS_TESTFLIGHT} from 'lib/app-info'
 
 /**
  * Examples:
@@ -32,16 +25,16 @@ const release = nativeApplicationVersion ?? 'dev'
  * - `ios.1.57.0.3`
  * - `android.1.57.0.46`
  */
-const dist = `${Platform.OS}.${release}${
-  nativeBuildVersion ? `.${nativeBuildVersion}` : ''
-}`
+const dist = `${Platform.OS}.${nativeBuildVersion}.${
+  IS_TESTFLIGHT ? 'tf' : ''
+}${IS_DEV ? 'dev' : ''}`
 
 init({
   autoSessionTracking: false,
   dsn: 'https://05bc3789bf994b81bd7ce20c86ccd3ae@o4505071687041024.ingest.sentry.io/4505071690514432',
   debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
   enableInExpoDevelopment: false, // enable this to test in dev
-  environment: buildChannel,
+  environment: BUILD_ENV ?? 'development',
   dist,
   release,
 })
