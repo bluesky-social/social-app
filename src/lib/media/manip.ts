286e666173224c708e1efddcb37d79e587725f99
Hailey <me@haileyok.com>
2024-04-17 14:08:19 -0700

    replace a few other instances of `unlink()`

--- src/lib/media/manip.ts
+++ src/lib/media/manip.ts
@@ -113,7 +113,12 @@ export async function shareImageModal({uri}: {uri: string}) {
       UTI: 'image/png',
     })
   }
-  RNFS.unlink(imagePath)
+
+  try {
+    deleteAsync(imagePath)
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export async function saveImageToMediaLibrary({uri}: {uri: string}) {
@@ -131,7 +136,11 @@ export async function saveImageToMediaLibrary({uri}: {uri: string}) {
   // save
   await MediaLibrary.createAssetAsync(imagePath)
 
-  // TODO we should add a `deleteAsync()` here as well -hailey
+  try {
+    deleteAsync(imagePath)
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export function getImageDim(path: string): Promise<Dimensions> {
