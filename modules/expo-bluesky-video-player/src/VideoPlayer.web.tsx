42cdbdb06f8ffc3ab781b4d5ec8715cc21e5f57e
Hailey <me@haileyok.com>
2024-04-18 22:47:01 -0700

    basic mp4 player for web

--- modules/expo-bluesky-video-player/src/VideoPlayer.web.tsx
+++ modules/expo-bluesky-video-player/src/VideoPlayer.web.tsx
@@ -1,7 +1,30 @@
 import * as React from 'react'
+import {Pressable} from 'react-native'
 
 import {VideoPlayerViewProps} from './VideoPlayer.types'
 
 export function VideoPlayer({source, autoplay, style}: VideoPlayerViewProps) {
-  return <video src={source} autoPlay={autoplay} style={style} />
+  const videoPlayerRef = React.useRef<HTMLMediaElement>(null)
+
+  const onPress = React.useCallback(() => {
+    if (videoPlayerRef.current?.paused) {
+      videoPlayerRef.current?.play()
+    } else {
+      videoPlayerRef.current?.pause()
+    }
+  }, [])
+
+  return (
+    <Pressable accessibilityRole="button" onPress={onPress}>
+      <video
+        src={source}
+        autoPlay={autoplay ? 'autoplay' : undefined}
+        style={style}
+        preload={autoplay ? 'auto' : undefined}
+        loop="loop"
+        muted="muted"
+        ref={videoPlayerRef}
+      />
+    </Pressable>
+  )
 }
