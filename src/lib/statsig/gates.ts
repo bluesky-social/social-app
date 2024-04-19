55aea348c263f345fc4a1e7209cf3c882b94cd32
dan <dan.abramov@gmail.com>
2024-04-19 15:23:47 +0100

    [Statsig] Update experiments (#3617)

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -1,12 +1,12 @@
 export type Gate =
   // Keep this alphabetic please.
-  | 'autoexpand_suggestions_on_profile_follow'
-  | 'disable_min_shell_on_foregrounding'
-  | 'disable_poll_on_discover'
+  | 'autoexpand_suggestions_on_profile_follow_v2'
+  | 'disable_min_shell_on_foregrounding_v2'
+  | 'disable_poll_on_discover_v2'
   | 'hide_vertical_scroll_indicators'
   | 'new_profile_scroll_component'
   | 'new_search'
   | 'receive_updates'
-  | 'show_follow_back_label'
-  | 'start_session_with_following'
+  | 'show_follow_back_label_v2'
+  | 'start_session_with_following_v2'
   | 'use_new_suggestions_endpoint'
