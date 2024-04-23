76aef2d064755097903658e54607251046e88c2b
Hailey <me@haileyok.com>
2024-04-23 15:55:01 -0700

    use mp4 over webm (#3666)
    * use mp4 over webm
    
    * tinymp4
    
    * safari only

--- src/lib/strings/embed-player.ts
+++ src/lib/strings/embed-player.ts
@@ -352,8 +352,17 @@ export function parseEmbedPlayerFromUrl(
 
     if (id && filename && dimensions && id.includes('AAAAC')) {
       if (Platform.OS === 'web') {
-        id = id.replace('AAAAC', 'AAAP3')
-        filename = filename.replace('.gif', '.webm')
+        const isSafari = /^((?!chrome|android).)*safari/i.test(
+          navigator.userAgent,
+        )
+
+        if (isSafari) {
+          id = id.replace('AAAAC', 'AAAP1')
+          filename = filename.replace('.gif', '.mp4')
+        } else {
+          id = id.replace('AAAAC', 'AAAP3')
+          filename = filename.replace('.gif', '.webm')
+        }
       } else {
         id = id.replace('AAAAC', 'AAAAM')
       }
