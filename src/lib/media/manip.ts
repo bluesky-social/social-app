d6528c19d4429ac1ae94e8b06b352a58e91f1112
Hailey <me@haileyok.com>
2024-04-24 11:40:10 -0700

    assert

--- src/lib/media/manip.ts
+++ src/lib/media/manip.ts
@@ -210,7 +210,12 @@ async function moveToPermanentPath(path: string, ext = 'jpg'): Promise<string> {
   */
   const filename = uuid.v4()
 
-  const destinationPath = joinPath(cacheDirectory ?? '', `${filename}.${ext}`)
+  // cacheDirectory will not ever be null on native, but it could be on web. This function only ever gets called on
+  // native so we assert as a string.
+  const destinationPath = joinPath(
+    cacheDirectory as string,
+    `${filename}.${ext}`,
+  )
 
   await copyAsync({
     from: path,
