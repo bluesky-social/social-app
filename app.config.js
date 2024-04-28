eb83e0b921a6f236c3b4329d857445dd2b0ff56c
Hailey <me@haileyok.com>
2024-04-27 23:21:42 -0700

    enable `newArch` for ios

--- app.config.js
+++ app.config.js
@@ -159,7 +159,7 @@ module.exports = function (config) {
           {
             ios: {
               deploymentTarget: '13.4',
-              newArchEnabled: false,
+              newArchEnabled: true,
             },
             android: {
               compileSdkVersion: 34,
