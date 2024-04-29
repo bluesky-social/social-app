1dd24ca86c1763f6806191c52fb2da24d63e500c
Paul Frazee <pfrazee@gmail.com>
2024-04-29 14:12:33 -0700

    Revert: show account-level alerts on posts again

--- src/components/moderation/PostAlerts.tsx
+++ src/components/moderation/PostAlerts.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {StyleProp, View, ViewStyle} from 'react-native'
 import {ModerationCause, ModerationUI} from '@atproto/api'
 
-import {getModerationCauseKey, isAppLabeler} from '#/lib/moderation'
+import {getModerationCauseKey} from '#/lib/moderation'
 import {useModerationCauseDescription} from '#/lib/moderation/useModerationCauseDescription'
 import {atoms as a, useTheme} from '#/alf'
 import {Button} from '#/components/Button'
@@ -20,19 +20,17 @@ export function PostAlerts({
   includeMute?: boolean
   style?: StyleProp<ViewStyle>
 }) {
-  const alerts = modui.alerts.filter(shouldShow)
-  const informs = modui.informs.filter(shouldShow)
-  if (!alerts.length && !informs.length) {
+  if (!modui.alert && !modui.inform) {
     return null
   }
 
   return (
     <View style={[a.flex_col, a.gap_xs, style]}>
       <View style={[a.flex_row, a.flex_wrap, a.gap_xs]}>
-        {alerts.map(cause => (
+        {modui.alerts.map(cause => (
           <PostLabel key={getModerationCauseKey(cause)} cause={cause} />
         ))}
-        {informs.map(cause => (
+        {modui.informs.map(cause => (
           <PostLabel key={getModerationCauseKey(cause)} cause={cause} />
         ))}
       </View>
@@ -83,33 +81,4 @@ function PostLabel({cause}: {cause: ModerationCause}) {
       <ModerationDetailsDialog control={control} modcause={cause} />
     </>
   )
-}
-
-function shouldShow(cause: ModerationCause): boolean {
-  if (cause.type === 'label') {
-    // NOTE
-    //
-    // The issue we have with labels on accounts is that 'negative' labels are showing
-    // everywhere, acting as a kind of "scarlet letter" punishment, when their intent
-    // is to just enable users to hide other users that are causing issues. Labelers
-    // don't have a way to express that an account-level label shouldnt show on every
-    // post.
-    //
-    // However, there are some cases where we really do want to show the labels:
-    //
-    // 1. When the label is informational or positive (like "Verified")
-    // 2. When the label is crucial (like "Impersonation")
-    //
-    // The solution we're applying FOR NOW is to hide severity=alert labels on accounts
-    // when looking at posts unless they're from the app's baked in moderation.
-    //
-    // The labeling system will need to be expanded to improve this situation. See
-    // https://github.com/bluesky-social/atproto/issues/2444
-    //
-    // -prf
-    if (cause.labelDef.severity === 'alert' && cause.target !== 'content') {
-      return cause.source.type === 'labeler' && isAppLabeler(cause.source.did)
-    }
-  }
-  return true
 }
