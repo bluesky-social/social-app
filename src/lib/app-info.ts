8c1605d26c51f66b6d7ad3933cadac5a940b0c53
Hailey <me@haileyok.com>
2024-05-04 14:53:18 -0700

    add bundle identifiers to `app-info`

--- src/lib/app-info.ts
+++ src/lib/app-info.ts
@@ -4,6 +4,15 @@ export const BUILD_ENV = process.env.EXPO_PUBLIC_ENV
 export const IS_DEV = process.env.EXPO_PUBLIC_ENV === 'development'
 export const IS_TESTFLIGHT = process.env.EXPO_PUBLIC_ENV === 'testflight'
 
+// This is the commit hash that the current bundle was made from. The user can see the commit hash in the app's settings
+// along with the other version info. Useful for debugging/reporting.
+export const BUNDLE_IDENTIFIER = process.env.EXPO_PUBLIC_COMMIT_HASH ?? 'dev'
+
+// This will always be in the format of YYMMDD, so that it always increases for each build. This should only be used
+// for Statsig reporting and shouldn't be used to identify a specific bundle.
+export const BUNDLE_DATE =
+  IS_TESTFLIGHT || IS_DEV ? 0 : Number(process.env.EXPO_PUBLIC_BUNDLE_DATE)
+
 const UPDATES_CHANNEL = IS_TESTFLIGHT ? 'testflight' : 'production'
 export const appVersion = `${nativeApplicationVersion} (${nativeBuildVersion}, ${
   IS_DEV ? 'development' : UPDATES_CHANNEL
