cf27b50cc393da04d8e0b20c3a70ebc3d4f2b8ca
Hailey <me@haileyok.com>
2024-05-05 04:19:19 -0700

    make `onScroll` a worklet in storybook

--- src/view/screens/Storybook/ListContained.tsx
+++ src/view/screens/Storybook/ListContained.tsx
@@ -19,6 +19,7 @@ export function ListContained() {
     <View style={{width: '100%', height: 300}}>
       <ScrollProvider
         onScroll={() => {
+          'worklet'
           console.log('onScroll')
         }}>
         <List
