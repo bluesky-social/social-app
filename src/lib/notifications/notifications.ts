339ef81dfc1f0c0189889e3905d729550d6ae5af
Hailey <me@haileyok.com>
2024-05-12 12:02:52 -0700

    remove `prevDid`, move `registerPushToken` into `useEffect`

--- src/lib/notifications/notifications.ts
+++ src/lib/notifications/notifications.ts
@@ -1,9 +1,8 @@
 import React from 'react'
 import * as Notifications from 'expo-notifications'
-import {BskyAgent} from '@atproto/api'
 
 import {logger} from '#/logger'
-import {SessionAccount, useAgent, useSession} from '#/state/session'
+import {useAgent, useSession} from '#/state/session'
 import {logEvent, useGate} from 'lib/statsig/statsig'
 import {devicePlatform, isNative} from 'platform/detection'
 
@@ -12,54 +11,48 @@ const SERVICE_DID = (serviceUrl?: string) =>
     ? 'did:web:api.staging.bsky.dev'
     : 'did:web:api.bsky.app'
 
-async function registerPushToken(
-  getAgent: () => BskyAgent,
-  account: SessionAccount,
-  token: Notifications.DevicePushToken,
-) {
-  try {
-    await getAgent().api.app.bsky.notification.registerPush({
-      serviceDid: SERVICE_DID(account.service),
-      platform: devicePlatform,
-      token: token.data,
-      appId: 'xyz.blueskyweb.app',
-    })
-    logger.debug(
-      'Notifications: Sent push token (init)',
-      {
-        tokenType: token.type,
-        token: token.data,
-      },
-      logger.DebugContext.notifications,
-    )
-  } catch (error) {
-    logger.error('Notifications: Failed to set push token', {message: error})
-  }
-}
-
 export function useNotificationsRegistration() {
   const [currentPermissions] = Notifications.usePermissions()
   const {getAgent} = useAgent()
   const {currentAccount} = useSession()
 
-  const prevAccountDid = React.useRef<string | undefined>(undefined)
-
   React.useEffect(() => {
-    if (
-      !currentPermissions?.granted ||
-      !currentAccount ||
-      prevAccountDid.current === currentAccount.did
-    ) {
-      return
+    const registerPushToken = async (token: Notifications.DevicePushToken) => {
+      if (!currentAccount) {
+        return
+      }
+
+      try {
+        await getAgent().api.app.bsky.notification.registerPush({
+          serviceDid: SERVICE_DID(currentAccount.service),
+          platform: devicePlatform,
+          token: token.data,
+          appId: 'xyz.blueskyweb.app',
+        })
+        logger.debug(
+          'Notifications: Sent push token (change)',
+          {
+            tokenType: token.type,
+            token: token.data,
+          },
+          logger.DebugContext.notifications,
+        )
+      } catch (error) {
+        logger.error('Notifications: Failed to set push token', {
+          message: error,
+        })
+      }
     }
 
     // Whenever we all `getDevicePushTokenAsync()`, a change event will be fired below
-    Notifications.getDevicePushTokenAsync()
+    if (currentPermissions?.granted) {
+      Notifications.getDevicePushTokenAsync()
+    }
 
     // According to the Expo docs, there is a chance that the token will change while the app is open in some rare
     // cases. This will fire `registerPushToken` whenever that happens.
     const subscription = Notifications.addPushTokenListener(async newToken => {
-      registerPushToken(getAgent, currentAccount, newToken)
+      registerPushToken(newToken)
     })
 
     return () => {
