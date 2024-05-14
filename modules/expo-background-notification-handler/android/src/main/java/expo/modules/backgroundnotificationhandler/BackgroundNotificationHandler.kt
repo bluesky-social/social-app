d82d4eaa525213884e6fcfb1adead38176a8e859
Hailey <me@haileyok.com>
2024-05-14 14:48:15 -0700

    simplify

--- modules/expo-background-notification-handler/android/src/main/java/expo/modules/backgroundnotificationhandler/BackgroundNotificationHandler.kt
+++ modules/expo-background-notification-handler/android/src/main/java/expo/modules/backgroundnotificationhandler/BackgroundNotificationHandler.kt
@@ -1,14 +1,17 @@
 package expo.modules.backgroundnotificationhandler
 
-import android.content.Context
-import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.RemoteMessage
+import expo.modules.notifications.service.delegates.FirebaseMessagingDelegate
 
-class BackgroundNotificationHandler {
-  fun handleMessage(remoteMessage: RemoteMessage, reactContext: Context) {
+class BackgroundNotificationHandler(private val messagingDelegate: FirebaseMessagingDelegate) {
+  fun handleMessage(remoteMessage: RemoteMessage) {
     if (ExpoBackgroundNotificationHandlerModule.isForegrounded) {
       // We'll let expo-notifications handle the notification if the app is foregrounded
       return
     }
+
+    messagingDelegate.showMessage(remoteMessage)
+
   }
 }
