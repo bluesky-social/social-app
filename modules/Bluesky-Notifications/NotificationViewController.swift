afe6c2ab7d7c111ff2ec2803372bcc401bb24070
Hailey <me@haileyok.com>
2024-05-13 19:14:58 -0700

    add ios source files

---
+++ modules/Bluesky-Notifications/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  Bluesky-Notifications
+//
+//  Created by Hailey on 5/13/24.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
