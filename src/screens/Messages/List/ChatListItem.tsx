5c89087a85cb83f3387102e036281bfe3423fa39
Eric Bailey <git@esb.lol>
2024-05-14 12:17:53 -0500

    Remove labs package

--- src/screens/Messages/List/ChatListItem.tsx
+++ src/screens/Messages/List/ChatListItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {View} from 'react-native'
-import {ChatBskyConvoDefs} from '@atproto-labs/api'
+import {ChatBskyConvoDefs} from '@atproto/api'
 import {msg} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 import {useNavigation} from '@react-navigation/native'
