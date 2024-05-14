7d3fb16c862aef47c0c8286263cc27231ed23b73
Hailey <me@haileyok.com>
2024-05-13 22:55:30 -0700

    add a new module

---
+++ modules/expo-background-notification-handler/android/src/main/java/expo/modules/backgroundnotificationhandler/ExpoBackgroundNotificationHandlerView.kt
@@ -0,0 +1,7 @@
+package expo.modules.backgroundnotificationhandler
+
+import android.content.Context
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
+
+class ExpoBackgroundNotificationHandlerView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
