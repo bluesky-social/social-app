f3d290c520b0adab3b6bf7ab1d475695e9e9aefb
Hailey <me@haileyok.com>
2024-05-14 00:25:51 -0700

    add entitlements for notifications extension

--- modules/Bluesky-Notifications/NotificationService.swift
+++ modules/Bluesky-Notifications/NotificationService.swift
@@ -1,28 +1,25 @@
 import UserNotifications
 
 class NotificationService: UNNotificationServiceExtension {
+  var contentHandler: ((UNNotificationContent) -> Void)?
+  var bestAttemptContent: UNMutableNotificationContent?
 
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
-
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
-    }
+  override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    print("Hello")
+    self.contentHandler = contentHandler
+    bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
     
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
+    if let bestAttemptContent = bestAttemptContent {
+      bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+      bestAttemptContent.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "blueskydm.wav"))
+      
+      contentHandler(bestAttemptContent)
     }
-
+  }
+  
+  override func serviceExtensionTimeWillExpire() {
+    if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent {
+      contentHandler(bestAttemptContent)
+    }
+  }
 }
