3af8eb504c20191ac404ea3cd84cb388a5ab1505
Hailey <me@haileyok.com>
2024-05-15 00:06:48 -0700

    update js types

--- modules/expo-background-notification-handler/src/ExpoBackgroundNotificationHandlerModule.web.ts
+++ modules/expo-background-notification-handler/src/ExpoBackgroundNotificationHandlerModule.web.ts
@@ -1,9 +1,12 @@
-import {ExpoBackgroundNotificationHandlerModule} from './ExpoBackgroundNotificationHandler.types'
+import {
+  BackgroundNotificationHandlerPreferences,
+  ExpoBackgroundNotificationHandlerModule,
+} from './ExpoBackgroundNotificationHandler.types'
 
 // Stub for web
 export const BackgroundNotificationHandler = {
   getAllPrefsAsync: async () => {
-    return {}
+    return {} as BackgroundNotificationHandlerPreferences
   },
   getBoolAsync: async (_: string) => {
     return false
@@ -11,6 +14,14 @@ export const BackgroundNotificationHandler = {
   getStringAsync: async (_: string) => {
     return ''
   },
+  getStringArrayAsync: async (_: string) => {
+    return []
+  },
   setBoolAsync: async (_: string, __: boolean) => {},
   setStringAsync: async (_: string, __: string) => {},
+  setStringArrayAsync: async (_: string, __: string[]) => {},
+  addToStringArrayAsync: async (_: string, __: string) => {},
+  removeFromStringArrayAsync: async (_: string, __: string) => {},
+  addManyToStringArrayAsync: async (_: string, __: string[]) => {},
+  removeManyFromStringArrayAsync: async (_: string, __: string[]) => {},
 } as ExpoBackgroundNotificationHandlerModule
