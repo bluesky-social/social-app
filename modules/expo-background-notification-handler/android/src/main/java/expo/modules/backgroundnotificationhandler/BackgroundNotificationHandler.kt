666ff8d05663cd6e983304eac5abe5a67badccb9
Hailey <me@haileyok.com>
2024-05-14 17:00:45 -0700

    about finished

--- modules/expo-background-notification-handler/android/src/main/java/expo/modules/backgroundnotificationhandler/BackgroundNotificationHandler.kt
+++ modules/expo-background-notification-handler/android/src/main/java/expo/modules/backgroundnotificationhandler/BackgroundNotificationHandler.kt
@@ -1,17 +1,68 @@
 package expo.modules.backgroundnotificationhandler
 
-import androidx.core.app.NotificationCompat
+import android.content.Context
+import android.util.Log
 import com.google.firebase.messaging.RemoteMessage
-import expo.modules.notifications.service.delegates.FirebaseMessagingDelegate
 
-class BackgroundNotificationHandler(private val messagingDelegate: FirebaseMessagingDelegate) {
+class BackgroundNotificationHandler(
+  private val context: Context,
+  private val notifInterface: BackgroundNotificationHandlerInterface
+) {
   fun handleMessage(remoteMessage: RemoteMessage) {
     if (ExpoBackgroundNotificationHandlerModule.isForegrounded) {
       // We'll let expo-notifications handle the notification if the app is foregrounded
       return
     }
 
-    messagingDelegate.showMessage(remoteMessage)
+    if (remoteMessage.data.getValue("reason") == "chat-message") {
+      mutateWithChatMessage(remoteMessage)
+    } else {
+      mutateWithNoSound(remoteMessage)
+    }
 
+    notifInterface.showMessage(remoteMessage)
+  }
+
+  private fun mutateWithChatMessage(remoteMessage: RemoteMessage): RemoteMessage {
+    if (NotificationPrefs(context).getBoolean("playSoundChat")) {
+      mutateWithDmSound(remoteMessage)
+    }
+    return remoteMessage
+  }
+
+  private fun mutateWithNoSound(remoteMessage: RemoteMessage): RemoteMessage {
+    remoteMessage.data["sound"] = "false"
+    return remoteMessage
+  }
+
+  private fun mutateWithDefaultSound(remoteMessage: RemoteMessage): RemoteMessage {
+    remoteMessage.data["sound"] = "true"
+    return remoteMessage
+  }
+
+  private fun mutateWithDmSound(remoteMessage: RemoteMessage): RemoteMessage {
+    remoteMessage.data["sound"] = "blueskydm.wav"
+    return remoteMessage
+  }
+}
+
+class MutableRemoteMessage(private val remoteMessage: RemoteMessage) {
+  private val data = remoteMessage.data.toMutableMap()
+
+  init {
+    remoteMessage.data.forEach { (key, value) ->
+      data[key] = value
+    }
+  }
+
+  fun setData(key: String, value: String?) {
+    data[key] = value
+  }
+
+  fun build(): RemoteMessage {
+    return RemoteMessage.Builder(remoteMessage.to ?: "")
+      .setMessageId(remoteMessage.messageId ?: "")
+      .setData(data)
+      .build()
   }
 }
