a4264db6547c7546f8308c7b8e26f8e42974dc00
Samuel Newman <mozzius@protonmail.com>
2024-05-17 18:23:12 +0100

    style tweaks

--- src/components/dms/MessagesNUX.tsx
+++ src/components/dms/MessagesNUX.tsx
@@ -8,7 +8,7 @@ import {useUpdateActorDeclaration} from '#/state/queries/messages/actor-declarat
 import {useProfileQuery} from '#/state/queries/profile'
 import {useSession} from '#/state/session'
 import * as Toast from '#/view/com/util/Toast'
-import {atoms as a} from '#/alf'
+import {atoms as a, useTheme} from '#/alf'
 import {Button, ButtonText} from '#/components/Button'
 import * as Dialog from '#/components/Dialog'
 import * as Toggle from '#/components/forms/Toggle'
@@ -46,6 +46,7 @@ function DialogInner({
 }) {
   const control = Dialog.useDialogContext()
   const {_} = useLingui()
+  const t = useTheme()
   const {mutate: updateDeclaration} = useUpdateActorDeclaration({
     onError: () => {
       Toast.show(_(msg`Failed to update settings`))
@@ -69,61 +70,72 @@ function DialogInner({
 
   return (
     <Dialog.ScrollableInner label={_(msg`Introducing Direct Messages`)}>
-      <View style={a.gap_md}>
-        <View style={[a.gap_xs, a.align_center, a.my_lg]}>
+      <View style={a.gap_xl}>
+        <View style={[a.align_center, a.my_lg]}>
           <Message_Stroke2_Corner0_Rounded width={64} />
           <Text style={[a.text_2xl, a.font_bold, a.text_center, a.mt_2xl]}>
             <Trans>Direct Messages are here!</Trans>
           </Text>
-          <Text style={[a.text_md, a.text_center]}>
-            <Trans>Chat with other users, privately.</Trans>
+          <Text style={[a.text_md, a.text_center, a.mt_sm]}>
+            <Trans>Privately chat with other users.</Trans>
           </Text>
         </View>
-        <Text style={a.text_sm}>
+        <Text style={[a.text_md, a.leading_snug]}>
           <Trans>
             You are in control of who can start chats with you. You can change
-            these settings at any time.
+            this setting any time.
           </Trans>
         </Text>
-        <View style={a.gap_xs}>
-          <Text style={a.text_sm}>
-            <Trans>Allow messages from:</Trans>
-          </Text>
-          <Toggle.Group
-            label={_(msg`Allow messages from`)}
-            type="radio"
-            values={[chatDeclation?.allowIncoming ?? 'following']}
-            onChange={onSelectItem}>
-            <View>
-              <Toggle.Item
-                name="all"
-                label={_(msg`Everyone`)}
-                style={[a.justify_between, a.py_sm]}>
-                <Toggle.LabelText>
-                  <Trans>Everyone</Trans>
-                </Toggle.LabelText>
-                <Toggle.Radio />
-              </Toggle.Item>
-              <Toggle.Item
-                name="following"
-                label={_(msg`Users I follow`)}
-                style={[a.justify_between, a.py_sm]}>
-                <Toggle.LabelText>
-                  <Trans>Users I follow</Trans>
-                </Toggle.LabelText>
-                <Toggle.Radio />
-              </Toggle.Item>
-              <Toggle.Item
-                name="none"
-                label={_(msg`No one`)}
-                style={[a.justify_between, a.py_sm]}>
-                <Toggle.LabelText>
-                  <Trans>No one</Trans>
-                </Toggle.LabelText>
-                <Toggle.Radio />
-              </Toggle.Item>
-            </View>
-          </Toggle.Group>
+        <View
+          style={[
+            a.gap_xs,
+            a.border,
+            a.overflow_hidden,
+            a.rounded_sm,
+            t.atoms.border_contrast_low,
+          ]}>
+          <View style={[a.p_sm, a.border_b, t.atoms.border_contrast_low]}>
+            <Text style={[a.text_sm, a.text_center]}>
+              <Trans>Allow messages from</Trans>
+            </Text>
+          </View>
+          <View style={[a.pb_sm, a.pt_xs, a.px_xs]}>
+            <Toggle.Group
+              label={_(msg`Allow messages from`)}
+              type="radio"
+              values={[chatDeclation?.allowIncoming ?? 'following']}
+              onChange={onSelectItem}>
+              <View>
+                <Toggle.Item
+                  name="all"
+                  label={_(msg`Everyone`)}
+                  style={[a.justify_between, a.p_sm, a.rounded_sm]}>
+                  <Toggle.LabelText>
+                    <Trans>Everyone</Trans>
+                  </Toggle.LabelText>
+                  <Toggle.Radio />
+                </Toggle.Item>
+                <Toggle.Item
+                  name="following"
+                  label={_(msg`Users I follow`)}
+                  style={[a.justify_between, a.p_sm, a.rounded_sm]}>
+                  <Toggle.LabelText>
+                    <Trans>Users I follow</Trans>
+                  </Toggle.LabelText>
+                  <Toggle.Radio />
+                </Toggle.Item>
+                <Toggle.Item
+                  name="none"
+                  label={_(msg`No one`)}
+                  style={[a.justify_between, a.p_sm, a.rounded_sm]}>
+                  <Toggle.LabelText>
+                    <Trans>No one</Trans>
+                  </Toggle.LabelText>
+                  <Toggle.Radio />
+                </Toggle.Item>
+              </View>
+            </Toggle.Group>
+          </View>
         </View>
         <Button
           label={_(msg`Start chatting`)}
@@ -131,8 +143,7 @@ function DialogInner({
           size="medium"
           color="primary"
           variant="solid"
-          onPress={() => control.close()}
-          style={a.mt_md}>
+          onPress={() => control.close()}>
           <ButtonText>Start chatting!</ButtonText>
         </Button>
       </View>
