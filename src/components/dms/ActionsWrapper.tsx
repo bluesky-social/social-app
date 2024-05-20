61d6080cdd09d9656c2b6b29f27f965ca6bf7939
Hailey <me@haileyok.com>
2024-05-19 19:16:52 -0700

    better gestures for press and hold

--- src/components/dms/ActionsWrapper.tsx
+++ src/components/dms/ActionsWrapper.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Keyboard, Pressable, View} from 'react-native'
+import {Keyboard} from 'react-native'
 import {Gesture, GestureDetector} from 'react-native-gesture-handler'
 import Animated, {
   cancelAnimation,
@@ -16,8 +16,6 @@ import {atoms as a} from '#/alf'
 import {MessageMenu} from '#/components/dms/MessageMenu'
 import {useMenuControl} from '#/components/Menu'
 
-const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
-
 export function ActionsWrapper({
   message,
   isFromSelf,
@@ -31,66 +29,59 @@ export function ActionsWrapper({
   const menuControl = useMenuControl()
 
   const scale = useSharedValue(1)
-  const animationDidComplete = useSharedValue(false)
 
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [{scale: scale.value}],
   }))
 
-  // Reanimated's `runOnJS` doesn't like refs, so we can't use `runOnJS(menuControl.open)()`. Instead, we'll use this
-  // function
   const open = React.useCallback(() => {
+    playHaptic()
     Keyboard.dismiss()
     menuControl.open()
-  }, [menuControl])
+  }, [menuControl, playHaptic])
 
   const shrink = React.useCallback(() => {
     'worklet'
     cancelAnimation(scale)
-    scale.value = withTiming(1, {duration: 200}, () => {
-      animationDidComplete.value = false
-    })
-  }, [animationDidComplete, scale])
+    scale.value = withTiming(1, {duration: 200})
+  }, [scale])
 
-  const grow = React.useCallback(() => {
-    'worklet'
-    scale.value = withTiming(1.05, {duration: 300}, finished => {
-      if (!finished) return
-      animationDidComplete.value = true
-      runOnJS(playHaptic)()
-      runOnJS(open)()
-      shrink()
-    })
-  }, [scale, animationDidComplete, playHaptic, shrink, open])
-
-  const gesture = Gesture.Tap()
+  const doubleTapGesture = Gesture.Tap()
     .numberOfTaps(2)
     .hitSlop(HITSLOP_10)
-    .onEnd(() => {
-      playHaptic()
-      open()
+    .onEnd(open)
+
+  const pressAndHoldGesture = Gesture.LongPress()
+    .onStart(() => {
+      scale.value = withTiming(1.05, {duration: 200}, finished => {
+        if (!finished) return
+        runOnJS(open)()
+        shrink()
+      })
     })
+    .onTouchesUp(shrink)
+    .onTouchesMove(shrink)
+    .cancelsTouchesInView(false)
     .runOnJS(true)
 
+  const composedGestures = Gesture.Exclusive(
+    doubleTapGesture,
+    pressAndHoldGesture,
+  )
+
   return (
-    <GestureDetector gesture={gesture}>
-      <View
+    <GestureDetector gesture={composedGestures}>
+      <Animated.View
         style={[
           {
             maxWidth: '80%',
           },
           isFromSelf ? a.self_end : a.self_start,
+          animatedStyle,
         ]}>
-        <AnimatedPressable
-          style={animatedStyle}
-          unstable_pressDelay={200}
-          onPressIn={grow}
-          onTouchEnd={shrink}
-          hitSlop={HITSLOP_10}>
-          {children}
-        </AnimatedPressable>
+        {children}
         <MessageMenu message={message} control={menuControl} />
-      </View>
+      </Animated.View>
     </GestureDetector>
   )
 }
