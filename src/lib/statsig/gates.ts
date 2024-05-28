adbbded003206f8005b680203c45bc1810be6537
dan <dan.abramov@gmail.com>
2024-05-28 16:56:06 +0100

    Remove old onboarding (#4224)
    * Hardcode onboarding_v2 to true, rm dead code
    
    * Rm initialState, use initialStateReduced
    
    * Rm dead code
    
    * Drop *reduced prefix in code
    
    * Prettier

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -1,5 +1,4 @@
 export type Gate =
   // Keep this alphabetic please.
-  | 'reduced_onboarding_and_home_algo_v2'
   | 'request_notifications_permission_after_onboarding'
   | 'show_follow_back_label_v2'
