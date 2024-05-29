87ce60cf8bc97295b1fa1143fd8341e676a3247a
Nick Manos <nick@manos.tech>
2024-05-29 13:58:33 -0400

    Change app name and package name/bundle id for dev builds

--- app.config.js
+++ app.config.js
@@ -33,7 +33,7 @@ module.exports = function (_config) {
   return {
     expo: {
       version: VERSION,
-      name: 'Bluesky',
+      name: IS_DEV ? 'Bluesky Dev' : 'Bluesky',
       slug: 'bluesky',
       scheme: 'bluesky',
       owner: 'blueskysocial',
@@ -46,7 +46,9 @@ module.exports = function (_config) {
       newArchEnabled: false,
       ios: {
         supportsTablet: false,
-        bundleIdentifier: 'xyz.blueskyweb.app',
+        bundleIdentifier: IS_DEV
+          ? 'xyz.blueskyweb.app.dev'
+          : 'xyz.blueskyweb.app',
         config: {
           usesNonExemptEncryption: false,
         },
@@ -181,8 +183,10 @@ module.exports = function (_config) {
           monochromeImage: './assets/icon-android-monochrome.png',
           backgroundColor: '#006AFF',
         },
-        googleServicesFile: './google-services.json',
-        package: 'xyz.blueskyweb.app',
+        googleServicesFile: IS_DEV
+          ? './google-services-dev.json'
+          : './google-services.json',
+        package: IS_DEV ? 'xyz.blueskyweb.app.dev' : 'xyz.blueskyweb.app',
         intentFilters: [
           {
             action: 'VIEW',
