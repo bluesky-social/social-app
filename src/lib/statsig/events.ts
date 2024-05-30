0bcc08f920306761e527db50dddce7af89cd13e7
Eric Bailey <git@esb.lol>
2024-05-30 15:15:22 -0500

    Change log context

--- src/lib/statsig/events.ts
+++ src/lib/statsig/events.ts
@@ -13,7 +13,7 @@ export type LogEvents = {
     withPassword: boolean
   }
   'account:loggedOut': {
-    logContext: 'SwitchAccount' | 'Settings' | 'Deactivated'
+    logContext: 'SwitchAccount' | 'Settings' | 'SignupQueued'
   }
   'notifications:openApp': {}
   'notifications:request': {
