d92036f2c576d33964b1141ac63888bdc2fb1ca4
Samuel Newman <mozzius@protonmail.com>
2024-05-30 09:44:49 +0300

    Post controls update followup (#4276)
    * rm legacy repost modal
    
    * make repost button transparent
    
    * reduce gap between post and ctrls
    
    * remove old repost modal on web

--- src/view/com/modals/Modal.tsx
+++ src/view/com/modals/Modal.tsx
@@ -22,7 +22,6 @@ import * as ContentLanguagesSettingsModal from './lang-settings/ContentLanguages
 import * as PostLanguagesSettingsModal from './lang-settings/PostLanguagesSettings'
 import * as LinkWarningModal from './LinkWarning'
 import * as ListAddUserModal from './ListAddRemoveUsers'
-import * as RepostModal from './Repost'
 import * as SelfLabelModal from './SelfLabel'
 import * as ThreadgateModal from './Threadgate'
 import * as UserAddRemoveListsModal from './UserAddRemoveLists'
@@ -74,9 +73,6 @@ export function ModalsContainer() {
   } else if (activeModal?.name === 'delete-account') {
     snapPoints = DeleteAccountModal.snapPoints
     element = <DeleteAccountModal.Component />
-  } else if (activeModal?.name === 'repost') {
-    snapPoints = RepostModal.snapPoints
-    element = <RepostModal.Component {...activeModal} />
   } else if (activeModal?.name === 'self-label') {
     snapPoints = SelfLabelModal.snapPoints
     element = <SelfLabelModal.Component {...activeModal} />
