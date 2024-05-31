f868821cfcc87b62a320e5a1e11375fdb973adc1
Eric Bailey <git@esb.lol>
2024-05-31 12:56:06 -0500

    [🐴] Embed backwards compat (#4302)

--- src/components/dms/MessageItemEmbed.tsx
+++ src/components/dms/MessageItemEmbed.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {View} from 'react-native'
 import {AppBskyEmbedRecord} from '@atproto/api'
 
+import {isNative} from '#/platform/detection'
 import {PostEmbeds} from '#/view/com/util/post-embeds'
 import {atoms as a, useTheme} from '#/alf'
 
@@ -13,7 +14,8 @@ let MessageItemEmbed = ({
   const t = useTheme()
 
   return (
-    <View style={[a.my_xs, t.atoms.bg, a.rounded_md, {flexBasis: 0}]}>
+    <View
+      style={[a.my_xs, t.atoms.bg, a.rounded_md, isNative && {flexBasis: 0}]}>
       <PostEmbeds embed={embed} />
     </View>
   )
