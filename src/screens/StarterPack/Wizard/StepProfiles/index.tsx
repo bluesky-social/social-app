8bc48ecc599987d4d657d24a83c7c43928346795
Hailey <me@haileyok.com>
2024-05-31 14:48:50 -0700

    scoping

--- src/screens/StarterPack/Wizard/StepProfiles/index.tsx
+++ src/screens/StarterPack/Wizard/StepProfiles/index.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import {ListRenderItemInfo} from 'react-native'
+import {AppBskyActorDefs} from '@atproto/api'
 
 import {List} from 'view/com/util/List'
+import {useWizardState} from '#/screens/StarterPack/Wizard/State'
 import {WizardProfileCard} from '#/screens/StarterPack/Wizard/StepProfiles/WizardProfileCard'
 import {atoms as a} from '#/alf'
 
-function renderItem() {
-  return <WizardProfileCard />
+function renderItem({
+  item,
+}: ListRenderItemInfo<AppBskyActorDefs.ProfileViewBasic>) {
+  return <WizardProfileCard profile={item} />
+}
+
+function keyExtractor(item: AppBskyActorDefs.ProfileViewBasic) {
+  return item.did
 }
 
 export function StepProfiles() {
+  const [state] = useWizardState()
+
   return (
-    <List data={[1, 2, 3, 4, 5]} renderItem={renderItem} style={[a.flex_1]} />
+    <List
+      data={state.profiles}
+      renderItem={renderItem}
+      style={[a.flex_1]}
+      keyExtractor={keyExtractor}
+    />
   )
 }
