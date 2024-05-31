d782fc57eab162faeef5e3653d3ead8d81820a97
Jan Aagaard <jan@aagaard.net>
2024-06-01 00:08:04 +0200

    Create dateDiff function
    This is a copy of ago(), but with the ability to specify the second date instead of using Date.now().

--- src/lib/strings/time.ts
+++ src/lib/strings/time.ts
@@ -40,6 +40,53 @@ export function ago(date: number | string | Date): string {
   }
 }
 
+export function dateDiff(
+  date1: number | string | Date,
+  date2: number | string | Date,
+): string {
+  let ts1: number
+  if (typeof date1 === 'string') {
+    ts1 = Number(new Date(date1))
+  } else if (date1 instanceof Date) {
+    ts1 = Number(date1)
+  } else {
+    ts1 = date1
+  }
+  let ts2: number
+  if (typeof date2 === 'string') {
+    ts2 = Number(new Date(date2))
+  } else if (date2 instanceof Date) {
+    ts2 = Number(date2)
+  } else {
+    ts2 = date2
+  }
+  const diffSeconds = Math.floor((ts2 - ts1) / 1e3)
+  if (diffSeconds < NOW) {
+    return `now`
+  } else if (diffSeconds < MINUTE) {
+    return `${diffSeconds}s`
+  } else if (diffSeconds < HOUR) {
+    return `${Math.floor(diffSeconds / MINUTE)}m`
+  } else if (diffSeconds < DAY) {
+    return `${Math.floor(diffSeconds / HOUR)}h`
+  } else if (diffSeconds < MONTH_30) {
+    return `${Math.round(diffSeconds / DAY)}d`
+  } else {
+    let months = diffSeconds / MONTH
+    if (months % 1 >= 0.9) {
+      months = Math.ceil(months)
+    } else {
+      months = Math.floor(months)
+    }
+
+    if (months < 12) {
+      return `${months}mo`
+    } else {
+      return new Date(ts1).toLocaleDateString()
+    }
+  }
+}
+
 export function niceDate(date: number | string | Date) {
   const d = new Date(date)
   return `${d.toLocaleDateString('en-us', {
