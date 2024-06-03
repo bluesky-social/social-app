de257a11869292953144da956b05b8e7cc276991
Eric Bailey <git@esb.lol>
2024-06-03 17:05:14 -0500

    Revert "[🐴] Embed backwards compat (#4302)" (#4338)
    This reverts commit f868821cfcc87b62a320e5a1e11375fdb973adc1.

--- src/components/dms/MessageItemEmbed.tsx
+++ src/components/dms/MessageItemEmbed.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {View} from 'react-native'
 import {AppBskyEmbedRecord} from '@atproto/api'
 
-import {isNative} from '#/platform/detection'
 import {PostEmbeds} from '#/view/com/util/post-embeds'
 import {atoms as a, useTheme} from '#/alf'
 
@@ -14,8 +13,7 @@ let MessageItemEmbed = ({
   const t = useTheme()
 
   return (
-    <View
-      style={[a.my_xs, t.atoms.bg, a.rounded_md, isNative && {flexBasis: 0}]}>
+    <View style={[a.my_xs, t.atoms.bg, a.rounded_md, {flexBasis: 0}]}>
       <PostEmbeds embed={embed} />
     </View>
   )
