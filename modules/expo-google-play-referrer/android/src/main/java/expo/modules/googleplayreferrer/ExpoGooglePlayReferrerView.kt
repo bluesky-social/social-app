042b88df75ae06bc956069cc214ea6cae250e2e6
Hailey <me@haileyok.com>
2024-06-04 12:37:43 -0700

    rm boiler

--- modules/expo-google-play-referrer/android/src/main/java/expo/modules/googleplayreferrer/ExpoGooglePlayReferrerView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.googleplayreferrer
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class ExpoGooglePlayReferrerView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
