5690f5235f887c3363f2c31736046cc682cc9b34
Eric Bailey <git@esb.lol>
2024-06-05 14:14:39 -0500

    Swap icon, sizing

--- src/view/com/home/HomeHeaderLayout.web.tsx
+++ src/view/com/home/HomeHeaderLayout.web.tsx
@@ -10,7 +10,7 @@ import {useMinimalShellMode} from 'lib/hooks/useMinimalShellMode'
 import {useWebMediaQueries} from 'lib/hooks/useWebMediaQueries'
 import {Logo} from '#/view/icons/Logo'
 import {atoms as a, useTheme} from '#/alf'
-import {ListPlus_Stroke2_Corner0_Rounded as FeedsIcon} from '#/components/icons/ListPlus'
+import {SettingsSliderVertical_Stroke2_Corner0_Rounded as FeedsIcon} from '#/components/icons/SettingsSlider'
 import {Link} from '#/components/Link'
 import {useKawaiiMode} from '../../../state/preferences/kawaii'
 import {HomeHeaderLayoutMobile} from './HomeHeaderLayoutMobile'
@@ -52,8 +52,8 @@ function HomeHeaderLayoutDesktopAndTablet({
             a.justify_end,
             a.align_center,
             a.pt_lg,
-            a.px_lg,
-            a.pb_sm,
+            a.px_md,
+            a.pb_2xs,
             t.atoms.bg,
             t.atoms.border_contrast_low,
             styles.bar,
@@ -63,7 +63,6 @@ function HomeHeaderLayoutDesktopAndTablet({
               a.absolute,
               a.inset_0,
               a.pt_lg,
-              a.pb_sm,
               a.m_auto,
               kawaii && {paddingTop: 4, paddingBottom: 0},
               {
@@ -76,8 +75,18 @@ function HomeHeaderLayoutDesktopAndTablet({
           <Link
             to="/feeds"
             hitSlop={10}
-            label={_(msg`View your feeds and explore more`)}>
-            <FeedsIcon size="lg" fill={t.atoms.text_contrast_medium.color} />
+            label={_(msg`View your feeds and explore more`)}
+            size="small"
+            variant="ghost"
+            color="secondary"
+            shape="square"
+            style={[
+              a.justify_center,
+              {
+                marginTop: -4,
+              },
+            ]}>
+            <FeedsIcon size="md" fill={t.atoms.text_contrast_medium.color} />
           </Link>
         </View>
       )}
