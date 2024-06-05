4bd0ff8ba208c3e2d82ed6958e149de2f7de739a
Samuel Newman <mozzius@protonmail.com>
2024-06-05 09:29:23 +0300

    add ffmpeg-kit-react-native

--- app.config.js
+++ app.config.js
@@ -202,6 +202,7 @@ module.exports = function (config) {
             sounds: PLATFORM === 'ios' ? ['assets/dm.aiff'] : ['assets/dm.mp3'],
           },
         ],
+        ['@config-plugins/ffmpeg-kit-react-native', {package: 'video'}],
         './plugins/withAndroidManifestPlugin.js',
         './plugins/withAndroidManifestFCMIconPlugin.js',
         './plugins/withAndroidStylesWindowBackgroundPlugin.js',
