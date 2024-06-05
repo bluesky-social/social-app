1518fa941f82c78266a192b15f0e8ddd65020eae
Hailey <me@haileyok.com>
2024-06-04 17:05:02 -0700

    better empty states

--- src/screens/StarterPack/Wizard/StepFeeds.tsx
+++ src/screens/StarterPack/Wizard/StepFeeds.tsx
@@ -5,6 +5,7 @@ import {GeneratorView} from '@atproto/api/dist/client/types/app/bsky/feed/defs'
 import {List} from 'view/com/util/List'
 import {useWizardState} from '#/screens/StarterPack/Wizard/State'
 import {WizardFeedCard} from '#/screens/StarterPack/Wizard/WizardFeedCard'
+import {WizardListEmpty} from '#/screens/StarterPack/Wizard/WizardListEmpty'
 import {atoms as a} from '#/alf'
 
 function keyExtractor(item: GeneratorView) {
@@ -26,6 +27,7 @@ export function StepFeeds() {
       renderItem={renderItem}
       keyExtractor={keyExtractor}
       style={[a.flex_1]}
+      ListEmptyComponent={<WizardListEmpty type="feeds" />}
     />
   )
 }
