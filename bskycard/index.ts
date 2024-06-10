9ae29e8aa8c1806bd22665d63b591a10d95813b5
Devin Ivy <devinivy@gmail.com>
2024-06-09 23:47:18 -0400

    quick proof of concept for png card generation

---
+++ bskycard/index.ts
@@ -0,0 +1,106 @@
+import {once} from 'node:events'
+import {readFileSync} from 'node:fs'
+import {createServer} from 'node:http'
+
+import {AtpAgent} from '@atproto/api'
+import resvg from '@resvg/resvg-js'
+import satori from 'satori'
+
+const PORT = 3000
+async function main() {
+  const server = createServer(async (req, res) => {
+    try {
+      const url = req.url
+        ? new URL(req.url, 'https://domain.invalid')
+        : undefined
+      if (!url || url.pathname !== '/') {
+        res.statusCode = 404
+        res.setHeader('content-type', 'text/plain')
+        return res.end('not found')
+      }
+      const output = await render()
+      res.statusCode = 200
+      res.setHeader('content-type', 'image/png')
+      return res.end(output.asPng())
+    } catch (err) {
+      console.error(err)
+      if (!res.headersSent) {
+        res.statusCode = 500
+        res.setHeader('content-type', 'text/plain')
+        return res.end('server error')
+      }
+    }
+  })
+  await once(server.listen(PORT), 'listening')
+  console.log('listening')
+  process.once('SIGINT', () => server.close())
+  await once(server, 'close')
+  console.log('closed')
+}
+
+async function render() {
+  const inter = readFileSync('./assets/Inter-Regular.ttf')
+  const appview = new AtpAgent({service: 'https://api.bsky.app'})
+  const {
+    data: {followers},
+  } = await appview.api.app.bsky.graph.getFollowers({
+    actor: 'divy.zone',
+  })
+  const images = await Promise.all(
+    followers
+      .filter(p => p.avatar)
+      .slice(0, 8)
+      .map(async p => {
+        if (!p.avatar) return
+        const res = await fetch(p.avatar)
+        if (res.status !== 200) return
+        const arrayBuf = await res.arrayBuffer()
+        return Buffer.from(arrayBuf)
+      }),
+  )
+  const svg = await satori(
+    {
+      type: 'div',
+      props: {
+        style: {
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignItems: 'stretch',
+          width: 1200,
+          height: 630,
+          backgroundColor: 'black',
+          color: 'white',
+          fontFamily: 'Inter',
+        },
+        children: [...Array(8)].map((bg, i) => {
+          const image = images.at(i)
+          return {
+            type: 'div',
+            props: {
+              style: {
+                flex: '1 0 25%',
+                height: 315,
+                display: 'flex',
+              },
+              children: image
+                ? {
+                    type: 'img',
+                    props: {
+                      src: `data:image/jpeg;base64,${image.toString('base64')}`,
+                      width: '100%',
+                      height: '100%',
+                    },
+                  }
+                : '',
+            },
+          }
+        }),
+      },
+      key: 'none',
+    },
+    {fonts: [{data: inter, name: 'Inter'}], height: 630, width: 1200},
+  )
+  return resvg.renderAsync(svg)
+}
+
+main()
