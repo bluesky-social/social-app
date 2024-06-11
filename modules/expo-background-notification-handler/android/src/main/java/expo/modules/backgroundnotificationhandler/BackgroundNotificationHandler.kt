8511fa74a249371e93dfb5878552da9e8b5f79c2
Hailey <me@haileyok.com>
2024-06-11 09:57:04 -0700

    Revert "use a `0` string for badge count"
    This reverts commit efac39861817f4237c58211f68ef266d919b4d40.

--- modules/expo-background-notification-handler/android/src/main/java/expo/modules/backgroundnotificationhandler/BackgroundNotificationHandler.kt
+++ modules/expo-background-notification-handler/android/src/main/java/expo/modules/backgroundnotificationhandler/BackgroundNotificationHandler.kt
@@ -37,6 +37,6 @@ class BackgroundNotificationHandler(
     }
 
     // TODO - Remove this once we have more backend capability
-    remoteMessage.data["badge"] = "0"
+    remoteMessage.data["badge"] = null
   }
 }
