e45180a078710f871f111430a1b863deb2176636
Hailey <me@haileyok.com>
2024-06-12 12:01:08 -0700

    entry for web

--- src/components/hooks/useStarterPackEntry.ts
+++ src/components/hooks/useStarterPackEntry.ts
@@ -1,3 +1,23 @@
+import React from 'react'
+
+import {useSetUsedStarterPack} from 'state/preferences/starter-pack'
+
 export function useStarterPackEntry() {
+  const setUsedStarterPack = useSetUsedStarterPack()
+
+  React.useEffect(() => {
+    const url = new URL(window.href)
+
+    if (url.href.startsWith('https://bsky.app/start/')) {
+      const [_, _start, name, rkey] = url.pathname.split('/')
+
+      if (name && rkey) {
+        setUsedStarterPack({
+          uri: window.href,
+        })
+      }
+    }
+  }, [setUsedStarterPack])
+
   return true
 }
