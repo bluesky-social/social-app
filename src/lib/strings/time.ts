5544e804e198d9bbd9836998208ebbb14c59b41f
Eric Bailey <git@esb.lol>
2024-06-17 18:35:48 -0500

    Split up tests, use date-fns as helpers

--- src/lib/strings/time.ts
+++ src/lib/strings/time.ts
@@ -1,3 +1,5 @@
+import {differenceInSeconds} from 'date-fns'
+
 export function ago(date: number | string | Date): string {
   return dateDiff(date, Date.now())
 }
@@ -7,28 +9,17 @@ const MINUTE = 60
 const HOUR = MINUTE * 60
 const DAY = HOUR * 24
 const MONTH_30 = DAY * 30
-/** Returns the difference between date1 and date2 in full seconds, minutes, hours etc. All month are considered exactly 30 days. Assuming that date1 <= date2. Differences >= 360 days are returned as the "M/D/YYYY" string. */
+
+/**
+ * Returns the difference between date1 and date2 in full seconds, minutes,
+ * hours etc. All month are considered exactly 30 days. Assuming that date1 <=
+ * date2. Differences >= 360 days are returned as the "M/D/YYYY" string
+ */
 export function dateDiff(
-  date1: number | string | Date,
-  date2: number | string | Date,
+  earlier: number | string | Date,
+  later: number | string | Date,
 ): string {
-  let ts1: number
-  if (typeof date1 === 'string') {
-    ts1 = Number(new Date(date1))
-  } else if (date1 instanceof Date) {
-    ts1 = Number(date1)
-  } else {
-    ts1 = date1
-  }
-  let ts2: number
-  if (typeof date2 === 'string') {
-    ts2 = Number(new Date(date2))
-  } else if (date2 instanceof Date) {
-    ts2 = Number(date2)
-  } else {
-    ts2 = date2
-  }
-  const diffSeconds = Math.floor((ts2 - ts1) / 1e3)
+  const diffSeconds = differenceInSeconds(new Date(later), new Date(earlier))
   if (diffSeconds < NOW) {
     return `now`
   } else if (diffSeconds < MINUTE) {
@@ -44,7 +35,7 @@ export function dateDiff(
     if (diffMonths30 < 12) {
       return `${diffMonths30}mo`
     } else {
-      return new Date(ts1).toLocaleDateString()
+      return new Date(earlier).toLocaleDateString()
     }
   }
 }
