40e87b98d0d42806e2a634b002bb9c58fa177a3e
Dan Abramov <dan.abramov@gmail.com>
2024-06-18 13:58:15 +0300

    Add a lint rule

--- eslint/index.js
+++ eslint/index.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     'keep-i18n-patch-in-sync': require('./keep-i18n-patch-in-sync'),
     'avoid-unwrapped-text': require('./avoid-unwrapped-text'),
+    'use-exact-imports': require('./use-exact-imports'),
     'use-typed-gates': require('./use-typed-gates'),
   },
 }
