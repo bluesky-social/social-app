ffdc3dca696e59f7e0ae98c545f97fbe8f257884
Hailey <me@haileyok.com>
2024-06-17 20:57:50 -0700

    change to bool

--- src/state/preferences/used-starter-packs.tsx
+++ src/state/preferences/used-starter-packs.tsx
@@ -2,25 +2,25 @@ import React from 'react'
 
 import * as persisted from '#/state/persisted'
 
-type StateContext = string[] | undefined
-type SetContext = (v: string) => void
+type StateContext = boolean | undefined
+type SetContext = (v: boolean) => void
 
-const stateContext = React.createContext<StateContext>([])
-const setContext = React.createContext<SetContext>((_: string) => {})
+const stateContext = React.createContext<StateContext>(false)
+const setContext = React.createContext<SetContext>((_: boolean) => {})
 
 export function Provider({children}: {children: React.ReactNode}) {
   const [state, setState] = React.useState<StateContext>(() =>
-    persisted.get('usedStarterPacks'),
+    persisted.get('hasCheckedForStarterPack'),
   )
 
-  const setStateWrapped = (v: string) => {
-    persisted.write('usedStarterPacks', [...(state ? state : []), v])
-    setState(prev => [...(prev ? prev : []), v])
+  const setStateWrapped = (v: boolean) => {
+    setState(v)
+    persisted.write('hasCheckedForStarterPack', v)
   }
 
   React.useEffect(() => {
     return persisted.onUpdate(() => {
-      setState(persisted.get('usedStarterPacks'))
+      setState(persisted.get('hasCheckedForStarterPack'))
     })
   }, [])
 
