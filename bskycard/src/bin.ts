8ce6bbb627db1395fbffa01d04fa5f78fec05360
Devin Ivy <devinivy@gmail.com>
2024-06-17 23:57:55 -0400

    bskycard: support cluster mode

--- bskycard/src/bin.ts
+++ bskycard/src/bin.ts
@@ -1,3 +1,7 @@
+import cluster, {Worker} from 'node:cluster'
+
+import {envInt} from '@atproto/common'
+
 import {CardService, envToCfg, httpLogger, readEnv} from './index.js'
 
 async function main() {
@@ -10,7 +14,35 @@ async function main() {
     httpLogger.info('card service is stopping')
     await card.destroy()
     httpLogger.info('card service is stopped')
+    if (cluster.isWorker) process.exit(0)
   })
 }
 
-main()
+const workerCount = envInt('CARD_CLUSTER_WORKER_COUNT')
+
+if (workerCount) {
+  if (cluster.isPrimary) {
+    httpLogger.info(`primary ${process.pid} is running`)
+    const workers = new Set<Worker>()
+    for (let i = 0; i < workerCount; ++i) {
+      workers.add(cluster.fork())
+    }
+    let teardown = false
+    cluster.on('exit', worker => {
+      workers.delete(worker)
+      if (!teardown) {
+        workers.add(cluster.fork()) // restart on crash
+      }
+    })
+    process.on('SIGTERM', () => {
+      teardown = true
+      httpLogger.info('disconnecting workers')
+      workers.forEach(w => w.kill('SIGTERM'))
+    })
+  } else {
+    httpLogger.info(`worker ${process.pid} is running`)
+    main()
+  }
+} else {
+  main() // non-clustering
+}
