1677625c5cd29ea47131687fc049bafddb74cff2
Samuel Newman <mozzius@protonmail.com>
2024-06-27 21:55:32 +0100

    start trying to hoist up video player instance

---
+++ src/components/icons/Play.tsx
@@ -0,0 +1,9 @@
+import {createSinglePathSVG} from './TEMPLATE'
+
+export const Play_Stroke2_Corner2_Rounded = createSinglePathSVG({
+  path: 'M5 5.086C5 2.736 7.578 1.3 9.576 2.534L20.77 9.448c1.899 1.172 1.899 3.932 0 5.104L9.576 21.466C7.578 22.701 5 21.263 5 18.914V5.086Zm3.525-.85A1 1 0 0 0 7 5.085v13.828a1 1 0 0 0 1.525.85l11.194-6.913a1 1 0 0 0 0-1.702L8.525 4.235Z',
+})
+
+export const Play_Filled_Corner2_Rounded = createSinglePathSVG({
+  path: 'M9.576 2.534C7.578 1.299 5 2.737 5 5.086v13.828c0 2.35 2.578 3.787 4.576 2.552l11.194-6.914c1.899-1.172 1.899-3.932 0-5.104L9.576 2.534Z',
+})
--- src/view/com/util/post-embeds/VideoEmbed.tsx
+++ src/view/com/util/post-embeds/VideoEmbed.tsx
@@ -1,28 +1,41 @@
 import React, {useCallback, useId, useMemo, useState} from 'react'
 import {View} from 'react-native'
-import {msg, Trans} from '@lingui/macro'
+import {msg} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 
 import {atoms as a, useTheme} from '#/alf'
-import {Button, ButtonText} from '#/components/Button'
+import {Button, ButtonIcon} from '#/components/Button'
+import {Play_Filled_Corner2_Rounded as PlayIcon} from '#/components/icons/Play'
 import {VideoEmbedInner} from './VideoEmbedInner'
+import {VideoPlayerProvider} from './VideoPlayerContext'
 
 const ActiveVideoContext = React.createContext<{
   activeVideo: string | null
-  setActiveVideo: (video: string) => void
+  setActiveVideo: (video: string, src: string) => void
 } | null>(null)
 
 export function ActiveVideoProvider({children}: {children: React.ReactNode}) {
   const [activeVideo, setActiveVideo] = useState<string | null>(null)
+  const [source, setSource] = useState<string | null>(null)
 
   const value = useMemo(
-    () => ({activeVideo, setActiveVideo}),
+    () => ({
+      activeVideo,
+      setActiveVideo: (video: string, src: string) => {
+        setActiveVideo(video)
+        setSource(src)
+      },
+    }),
     [activeVideo, setActiveVideo],
   )
 
   return (
     <ActiveVideoContext.Provider value={value}>
-      {children}
+      {source ? (
+        <VideoPlayerProvider source={source}>{children}</VideoPlayerProvider>
+      ) : (
+        children
+      )}
     </ActiveVideoContext.Provider>
   )
 }
@@ -36,7 +49,10 @@ function useActiveVideo() {
 
   return {
     active: context.activeVideo === id,
-    setActive: useCallback(() => context.setActiveVideo(id), [context, id]),
+    setActive: useCallback(
+      (source: string) => context.setActiveVideo(id, source),
+      [context, id],
+    ),
   }
 }
 
@@ -44,6 +60,8 @@ export function VideoEmbed({source}: {source: string}) {
   const t = useTheme()
   const {active, setActive} = useActiveVideo()
   const {_} = useLingui()
+
+  const onPress = useCallback(() => setActive(source), [setActive, source])
 
   return (
     <View
@@ -60,13 +78,12 @@ export function VideoEmbed({source}: {source: string}) {
       ) : (
         <Button
           style={[a.flex_1, t.atoms.bg_contrast_25]}
-          onPress={setActive}
+          onPress={onPress}
           label={_(msg`Play video`)}
           variant="ghost"
-          color="secondary">
-          <ButtonText>
-            <Trans>Play video</Trans>
-          </ButtonText>
+          color="secondary"
+          size="large">
+          <ButtonIcon icon={PlayIcon} />
         </Button>
       )}
     </View>
