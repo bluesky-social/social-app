fa254d38d19f6dd2c3695f7f4cd4df396496013c
Hailey <me@haileyok.com>
2024-06-27 09:21:19 -0700

    offer a json response for links

--- bskylink/src/routes/redirect.ts
+++ bskylink/src/routes/redirect.ts
@@ -11,6 +11,7 @@ export default function (ctx: AppContext, app: Express) {
     '/:linkId',
     handler(async (req, res) => {
       const linkId = req.params.linkId
+      const acceptsJson = req.accepts('application/json')
       assert(
         typeof linkId === 'string',
         'express guarantees id parameter is a string',
@@ -21,9 +22,16 @@ export default function (ctx: AppContext, app: Express) {
         .where('id', '=', linkId)
         .executeTakeFirst()
       if (!found) {
-        // potentially broken or mistyped link— send user to the app
-        res.setHeader('Location', `https://${ctx.cfg.service.appHostname}`)
+        // potentially broken or mistyped link
         res.setHeader('Cache-Control', 'no-store')
+        if (acceptsJson) {
+          return res.status(404).json({
+            error: 'NotFound',
+            message: 'Link not found',
+          })
+        }
+        // send the user to the app
+        res.setHeader('Location', `https://${ctx.cfg.service.appHostname}`)
         return res.status(302).end()
       }
       // build url from original url in order to preserve query params
@@ -32,8 +40,11 @@ export default function (ctx: AppContext, app: Express) {
         `https://${ctx.cfg.service.appHostname}`,
       )
       url.pathname = found.path
-      res.setHeader('Location', url.href)
       res.setHeader('Cache-Control', `max-age=${(7 * DAY) / SECOND}`)
+      if (acceptsJson) {
+        return res.json({url: url.href})
+      }
+      res.setHeader('Location', url.href)
       return res.status(301).end()
     }),
   )
