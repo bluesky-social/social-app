b2c375b5ba525fdd9b80f609b4d9de6031f8b218
Hailey <me@haileyok.com>
2024-06-27 11:37:16 -0700

    get JSON

--- src/lib/link-meta/resolve-short-link.ts
+++ src/lib/link-meta/resolve-short-link.ts
@@ -1,19 +1,23 @@
 import {logger} from '#/logger'
-import {startUriToStarterPackUri} from 'lib/strings/starter-pack'
 
 export async function resolveShortLink(shortLink: string) {
   const controller = new AbortController()
   const to = setTimeout(() => controller.abort(), 2e3)
 
   try {
-    const res = await fetch(shortLink, {
+    const res = await fetch(`${shortLink}`, {
       method: 'GET',
+      headers: {
+        contentType: 'application/json',
+      },
       signal: controller.signal,
     })
     if (res.status !== 200) {
+      logger.error('Failed to resolve short link', {status: res.status})
       return shortLink
     }
-    return startUriToStarterPackUri(res.url)
+    const json = (await res.json()) as {url: string}
+    return json.url
   } catch (e: unknown) {
     logger.error('Failed to resolve short link', {safeMessage: e})
     return null
