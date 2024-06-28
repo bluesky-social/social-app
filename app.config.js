99653f5efa032c334a754fc9282cc7241a0c8fc7
Hailey <me@haileyok.com>
2024-06-27 17:59:16 -0700

    Revert "temp config testing"
    This reverts commit 80736070d717fd8b11b1768261139ac63ef8bf66.

--- app.config.js
+++ app.config.js
@@ -134,7 +134,6 @@ module.exports = function (config) {
         backgroundColor: DARK_SPLASH_CONFIG_ANDROID.backgroundColor,
       },
       android: {
-        versionCode: 300,
         icon: './assets/icon.png',
         adaptiveIcon: {
           foregroundImage: './assets/icon-android-foreground.png',
