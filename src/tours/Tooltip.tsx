39027004a15de013da6d6201a6e789565762c73b
Eric Bailey <git@esb.lol>
2024-07-02 11:16:35 -0500

    Native a11y

--- src/tours/Tooltip.tsx
+++ src/tours/Tooltip.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react'
-import {View} from 'react-native'
+import {
+  AccessibilityInfo,
+  findNodeHandle,
+  Pressable,
+  Text as RNText,
+  View,
+} from 'react-native'
 import {msg, Trans} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 import {IStep, Labels} from 'rn-tourguide'
 
 import {isWeb} from '#/platform/detection'
+import {useA11y} from '#/state/a11y'
 import {Logo} from '#/view/icons/Logo'
 import {atoms as a, useTheme} from '#/alf'
 import {Button, ButtonText} from '#/components/Button'
-import {Text} from '#/components/Typography'
+import {leading, Text} from '#/components/Typography'
 
 const stopPropagation = (e: any) => e.stopPropagation()
 
@@ -31,7 +38,9 @@ export function TooltipComponent({
 }: TooltipComponentProps) {
   const t = useTheme()
   const {_} = useLingui()
-  const btnRef = React.useRef<HTMLButtonElement>()
+  const btnRef = React.useRef<View>(null)
+  const textRef = React.useRef<RNText>(null)
+  const {screenReaderEnabled} = useA11y()
 
   React.useEffect(() => {
     if (!isWeb) {
@@ -58,11 +67,34 @@ export function TooltipComponent({
     }
   }, [])
 
+  const focusTextNode = () => {
+    const node = textRef.current ? findNodeHandle(textRef.current) : undefined
+    if (node) {
+      AccessibilityInfo.setAccessibilityFocus(node)
+    }
+  }
+
+  // handle initial focus immediately on mount
+  React.useLayoutEffect(() => {
+    focusTextNode()
+  }, [])
+
+  // handle focus between steps
+  const innerHandleNext = () => {
+    handleNext?.()
+    setTimeout(() => focusTextNode(), 200)
+  }
+
   return (
     <View
-      role="dialog"
-      aria-role="dialog"
+      role="alert"
+      aria-role="alert"
       aria-label={_(msg`A help tooltip`)}
+      accessibilityLiveRegion="polite"
+      // iOS
+      accessibilityViewIsModal
+      // Android
+      importantForAccessibility="yes"
       // @ts-ignore web only
       onClick={stopPropagation}
       onStartShouldSetResponder={_ => true}
@@ -77,27 +109,50 @@ export function TooltipComponent({
         a.shadow_md,
         {maxWidth: 300},
       ]}>
+      {screenReaderEnabled && (
+        <Pressable
+          style={[a.absolute, a.inset_0, a.z_10, {height: 10, bottom: 'auto'}]}
+          accessibilityLabel={_(
+            msg`Start of onboarding tour window. Do not move backward. Instead, go forward for more options, or press to skip.`,
+          )}
+          accessibilityHint={undefined}
+          onPress={handleStop}
+        />
+      )}
+
       <View style={[a.flex_row, a.align_center, a.gap_sm]}>
         <Logo width={16} style={{position: 'relative', top: 0}} />
         <Text
+          accessible={false}
           style={[a.text_sm, a.font_semibold, t.atoms.text_contrast_medium]}>
           <Trans>Quick tip</Trans>
         </Text>
       </View>
-      <Text
+      <RNText
+        ref={textRef}
         testID="stepDescription"
-        style={[a.text_md, a.leading_snug, a.pb_sm]}>
-        {currentStep && currentStep.text}
-      </Text>
+        accessibilityLabel={_(
+          msg`Onboarding tour step ${currentStep.name}: ${currentStep.text}`,
+        )}
+        accessibilityHint={undefined}
+        style={[
+          a.text_md,
+          t.atoms.text,
+          a.pb_sm,
+          {
+            lineHeight: leading(a.text_md, a.leading_snug),
+          },
+        ]}>
+        {currentStep.text}
+      </RNText>
       {!isLastStep ? (
         <Button
-          // @ts-ignore the type is correct for Web, which is the only place the ref is used
           ref={btnRef}
           variant="gradient"
           color="gradient_sky"
           size="medium"
-          onPress={handleNext}
-          label={labels?.next || _(msg`Next`)}>
+          onPress={innerHandleNext}
+          label={labels?.next || _(msg`Go to the next step of the tour`)}>
           <ButtonText>{labels?.next || _(msg`Next`)}</ButtonText>
         </Button>
       ) : (
@@ -106,9 +161,23 @@ export function TooltipComponent({
           color="gradient_sky"
           size="medium"
           onPress={handleStop}
-          label={labels?.finish || _(msg`Let's go!`)}>
+          label={
+            labels?.finish ||
+            _(msg`Finish tour and begin using the application`)
+          }>
           <ButtonText>{labels?.finish || _(msg`Let's go!`)}</ButtonText>
         </Button>
+      )}
+
+      {screenReaderEnabled && (
+        <Pressable
+          style={[a.absolute, a.inset_0, a.z_10, {height: 10, top: 'auto'}]}
+          accessibilityLabel={_(
+            msg`End of onboarding tour window. Do not move forward. Instead, go backward for more options, or press to skip.`,
+          )}
+          accessibilityHint={undefined}
+          onPress={handleStop}
+        />
       )}
     </View>
   )
