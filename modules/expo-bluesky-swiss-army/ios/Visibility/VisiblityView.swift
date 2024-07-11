fe3d3b8f3b7bd031814df3cbd17165d59a1617cb
Hailey <me@haileyok.com>
2024-07-10 17:30:52 -0700

    rm comment

--- modules/expo-bluesky-swiss-army/ios/Visibility/VisiblityView.swift
+++ modules/expo-bluesky-swiss-army/ios/Visibility/VisiblityView.swift
@@ -1,24 +1,5 @@
 import ExpoModulesCore
 
-/*
- We want to keep track of different views that are actually present on the screen. Of
- those views, we want to tell a _single_ view "hey, start playing that video!".
- 
- Things that we know right now:
- - FlatList will remove views that are no longer visible for us - so if we scroll past
-   a view or if we switch tabs, the `newWindow` in `willMove` will be nil
- - Because of that, the views that we currently have at our disposal will be the
-   "available" views.
- - Whenever a single video is present, that video can play at any time that it is
-   visible on-screen.
-   However, whenever there is more than one video, we want to give prescedence to the
-   video that is "most" on-screen
- 
- First, we should probably keep a manager with the available views. This will let us
- run a callback for determining the location of that video on the screen at any time -
- and let us run that same measurment on the other visible views *at the same time*.
- */
-
 class VisibilityView: ExpoView {
   // Props
   var enabled = false {
