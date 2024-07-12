8b121af2e438ca77cc5f5b1715516107c18aff6f
Hailey <me@haileyok.com>
2024-07-11 18:43:10 -0700

    referrers for all platforms (#4514)

--- modules/expo-bluesky-swiss-army/android/src/main/java/expo/modules/blueskyswissarmy/referrer/ExpoBlueskyReferrerModule.kt
+++ modules/expo-bluesky-swiss-army/android/src/main/java/expo/modules/blueskyswissarmy/referrer/ExpoBlueskyReferrerModule.kt
@@ -1,5 +1,8 @@
 package expo.modules.blueskyswissarmy.referrer
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.util.Log
 import com.android.installreferrer.api.InstallReferrerClient
 import com.android.installreferrer.api.InstallReferrerStateListener
@@ -8,9 +11,52 @@ import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class ExpoBlueskyReferrerModule : Module() {
+  private var intent: Intent? = null
+  private var activityReferrer: Uri? = null
+
   override fun definition() =
     ModuleDefinition {
       Name("ExpoBlueskyReferrer")
+
+      OnNewIntent {
+        intent = it
+        activityReferrer = appContext.currentActivity?.referrer
+      }
+
+      AsyncFunction("getReferrerInfoAsync") {
+        val intentReferrer =
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent?.getParcelableExtra(Intent.EXTRA_REFERRER, Uri::class.java)
+          } else {
+            intent?.getParcelableExtra(Intent.EXTRA_REFERRER)
+          }
+
+        // Some apps explicitly set a referrer, like Chrome. In these cases, we prefer this since
+        // it's the actual website that the user came from rather than the app.
+        if (intentReferrer is Uri) {
+          val res =
+            mapOf(
+              "referrer" to intentReferrer.toString(),
+              "hostname" to intentReferrer.host,
+            )
+          intent = null
+          return@AsyncFunction res
+        }
+
+        // In all other cases, we'll just record the app that sent the intent.
+        if (activityReferrer != null) {
+          // referrer could become null here. `.toString()` though can be called on null
+          val res =
+            mapOf(
+              "referrer" to activityReferrer.toString(),
+              "hostname" to (activityReferrer?.host ?: ""),
+            )
+          activityReferrer = null
+          return@AsyncFunction res
+        }
+
+        return@AsyncFunction null
+      }
 
       AsyncFunction("getGooglePlayReferrerInfoAsync") { promise: Promise ->
         val referrerClient = InstallReferrerClient.newBuilder(appContext.reactContext).build()
