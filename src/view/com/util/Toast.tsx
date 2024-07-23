2189763b408b70d0da851983170170589f3e8ee6
Samuel Newman <10959775+mozzius@users.noreply.github.com>
2024-07-23 19:19:44 +0100

    fix animation on iOS

--- src/view/com/util/Toast.tsx
+++ src/view/com/util/Toast.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {View} from 'react-native'
 import Animated, {FadeInUp, FadeOutUp} from 'react-native-reanimated'
 import RootSiblings from 'react-native-root-siblings'
@@ -12,7 +12,7 @@ import {atoms as a, useTheme} from '#/alf'
 import {Text} from '#/components/Typography'
 import {IS_TEST} from '#/env'
 
-const TIMEOUT = 4e3
+const TIMEOUT = 3.7e3
 
 export function show(
   message: string,
@@ -20,9 +20,10 @@ export function show(
 ) {
   if (IS_TEST) return
   const item = new RootSiblings(<Toast message={message} icon={icon} />)
+  // timeout has some leeway to account for the animation
   setTimeout(() => {
     item.destroy()
-  }, TIMEOUT)
+  }, TIMEOUT + 1e3)
 }
 
 function Toast({
@@ -35,52 +36,58 @@ function Toast({
   const t = useTheme()
   const {top} = useSafeAreaInsets()
 
+  // for the exit animation to work on iOS the animated component
+  // must not be the root component
+  // so we need to wrap it in a view and unmount the toast ahead of time
+  const [alive, setAlive] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlive(false)
+    }, TIMEOUT)
+  }, [])
+
   return (
-    <Animated.View
-      entering={FadeInUp}
-      exiting={FadeOutUp}
-      style={[
-        a.flex_1,
-        a.absolute,
-        {top: top + 15, left: 16, right: 16},
-        a.align_center,
-        // shadow must be here, otherwise it isn't animated and looks janky -sfn
-        a.shadow_lg,
-        a.rounded_sm,
-      ]}
+    <View
+      style={[a.absolute, {top: top + 15, left: 16, right: 16}]}
       pointerEvents="none">
-      <View
-        style={[
-          a.flex_1,
-          t.atoms.bg,
-          t.atoms.border_contrast_medium,
-          a.w_full,
-          a.rounded_sm,
-          a.px_md,
-          a.py_lg,
-          a.border,
-          a.flex_row,
-          a.gap_md,
-        ]}>
-        <View
+      {alive && (
+        <Animated.View
+          entering={FadeInUp}
+          exiting={FadeOutUp}
           style={[
-            a.flex_shrink_0,
-            a.rounded_full,
-            {width: 32, height: 32},
-            t.atoms.bg_contrast_25,
-            a.align_center,
-            a.justify_center,
+            a.flex_1,
+            t.atoms.bg,
+            a.shadow_lg,
+            a.rounded_sm,
+            t.atoms.border_contrast_medium,
+            a.rounded_sm,
+            a.px_md,
+            a.py_lg,
+            a.border,
+            a.flex_row,
+            a.gap_md,
           ]}>
-          <FontAwesomeIcon
-            icon={icon}
-            size={16}
-            style={t.atoms.text_contrast_low}
-          />
-        </View>
-        <View style={[a.h_full, a.justify_center, a.flex_1]}>
-          <Text style={[a.text_md]}>{message}</Text>
-        </View>
-      </View>
-    </Animated.View>
+          <View
+            style={[
+              a.flex_shrink_0,
+              a.rounded_full,
+              {width: 32, height: 32},
+              t.atoms.bg_contrast_25,
+              a.align_center,
+              a.justify_center,
+            ]}>
+            <FontAwesomeIcon
+              icon={icon}
+              size={16}
+              style={t.atoms.text_contrast_low}
+            />
+          </View>
+          <View style={[a.h_full, a.justify_center, a.flex_1]}>
+            <Text style={a.text_md}>{message}</Text>
+          </View>
+        </Animated.View>
+      )}
+    </View>
   )
 }
