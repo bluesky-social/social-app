31031e471446e96d29f144e1bb9632fd2c148eae
Dmitrii Kartashev <dmitrii@gotrellis.com>
2024-07-25 17:44:55 -0400

    upgrade typescript and use new feature

--- src/state/queries/post-feed.ts
+++ src/state/queries/post-feed.ts
@@ -378,11 +378,11 @@ export function usePostFeedQuery(
                         }
                         return undefined
                       })
-                      .filter(<T>(n?: T): n is T => Boolean(n)),
+                      .filter(n => !!n),
                   }
                   return feedPostSlice
                 })
-                .filter(<T>(n?: T): n is T => Boolean(n)),
+                .filter(n => !!n),
             })),
           ],
         }
