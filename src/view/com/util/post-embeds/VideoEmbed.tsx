7d6195764025ddd4067c975c8f724d5afa0c11a0
Hailey <me@haileyok.com>
2024-07-25 13:18:59 -0700

    tweak

--- src/view/com/util/post-embeds/VideoEmbed.tsx
+++ src/view/com/util/post-embeds/VideoEmbed.tsx
@@ -1,8 +1,11 @@
-import React, {useCallback} from 'react'
-import {View} from 'react-native'
+import React, {useCallback, useState} from 'react'
+import {View, ViewToken} from 'react-native'
 import {msg} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
+import {useFocusEffect} from '@react-navigation/native'
 
+import {useNonReactiveCallback} from 'lib/hooks/useNonReactiveCallback'
+import {VisibilityEmitter} from 'view/com/util/List'
 import {atoms as a, useTheme} from '#/alf'
 import {Button, ButtonIcon} from '#/components/Button'
 import {Play_Filled_Corner2_Rounded as PlayIcon} from '#/components/icons/Play'
@@ -13,6 +16,31 @@ export function VideoEmbed({source}: {source: string}) {
   const t = useTheme()
   const {active, setActive} = useActiveVideoView()
   const {_} = useLingui()
+
+  const [isVisibleInList, setIsVisibleInList] = useState(false)
+  const [isVisibleOnScreen, setIsVisibleOnScreen] = useState(false)
+
+  useFocusEffect(
+    useNonReactiveCallback(() => {
+      const onViewableItemsChanged = (viewableItems: ViewToken[]) => {
+        console.log('test')
+        console.log(viewableItems)
+      }
+
+      VisibilityEmitter.addListener(
+        'viewableItemsChanged',
+        onViewableItemsChanged,
+      )
+      console.log('hello')
+      return () => {
+        console.log('bye')
+        VisibilityEmitter.removeListener(
+          'viewableItemsChanged',
+          onViewableItemsChanged,
+        )
+      }
+    }),
+  )
 
   const onPress = useCallback(() => setActive(source), [setActive, source])
 
