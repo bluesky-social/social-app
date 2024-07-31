ddfa0939a242effad501dfbca5edeb56d4dffc5c
Hailey <me@haileyok.com>
2024-07-30 23:22:11 -0700

    tweak

--- src/view/com/notifications/FeedItem.tsx
+++ src/view/com/notifications/FeedItem.tsx
@@ -187,19 +187,24 @@ let FeedItem = ({
     icon = <RepostIcon size="xl" style={{color: t.palette.positive_600}} />
   } else if (item.type === 'follow') {
     let isFollowBack = false
+
     if (
       item.notification.author.viewer?.following &&
       AppBskyGraphFollow.isRecord(item.notification.record)
     ) {
+      let followingTimestamp
       try {
         const rkey = new AtUri(item.notification.author.viewer.following).rkey
-        const followingTimestamp = TID.fromStr(rkey).timestamp()
+        followingTimestamp = TID.fromStr(rkey).timestamp()
       } catch (e) {
         // For some reason the following URI was invalid. Default to it not being a follow back.
         console.error('Invalid following URI')
       }
-      const followedTimestamp = new Date(item.notification.record.createdAt).getTime() * 1000
-      isFollowBack = followedTimestamp > followingTimestamp
+      if (followingTimestamp) {
+        const followedTimestamp =
+          new Date(item.notification.record.createdAt).getTime() * 1000
+        isFollowBack = followedTimestamp > followingTimestamp
+      }
     }
 
     if (isFollowBack && gate('ungroup_follow_backs')) {
