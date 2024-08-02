8d8ce689f6ebaf4875c73040c4827aee41866f1c
Dan Abramov <dan.abramov@gmail.com>
2024-08-02 23:44:36 +0100

    Apply Following settings to all lists

--- src/state/preferences/feed-tuners.tsx
+++ src/state/preferences/feed-tuners.tsx
@@ -24,31 +24,7 @@ export function useFeedTuners(feedDesc: FeedDescriptor) {
         FeedTuner.preferredLangOnly(langPrefs.contentLanguages),
       ]
     }
-    if (feedDesc.startsWith('list')) {
-      const feedTuners = []
-
-      if (feedDesc.endsWith('|as_following')) {
-        // Same as Following tuners below, copypaste for now.
-        if (preferences?.feedViewPrefs.hideReposts) {
-          feedTuners.push(FeedTuner.removeReposts)
-        }
-        if (preferences?.feedViewPrefs.hideReplies) {
-          feedTuners.push(FeedTuner.removeReplies)
-        } else {
-          feedTuners.push(
-            FeedTuner.followedRepliesOnly({
-              userDid: currentAccount?.did || '',
-            }),
-          )
-        }
-        if (preferences?.feedViewPrefs.hideQuotePosts) {
-          feedTuners.push(FeedTuner.removeQuotePosts)
-        }
-      }
-      feedTuners.push(FeedTuner.dedupThreads)
-      return feedTuners
-    }
-    if (feedDesc === 'following') {
+    if (feedDesc.startsWith('list') || feedDesc === 'following') {
       const feedTuners = []
 
       if (preferences?.feedViewPrefs.hideReposts) {
