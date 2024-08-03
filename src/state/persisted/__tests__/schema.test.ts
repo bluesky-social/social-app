63c97bace0f848193433dc39fd18dc761aae6c63
Dan Abramov <dan.abramov@gmail.com>
2024-08-03 21:33:45 +0100

    Delete superfluous tests
    At this point these tests aren't testing anything useful, let's just get rid of them.

--- src/state/persisted/__tests__/schema.test.ts
+++
@@ -1,7 +0,0 @@
-import {expect, test} from '@jest/globals'
-
-import {defaults, schema} from '#/state/persisted/schema'
-
-test('defaults', () => {
-  expect(() => schema.parse(defaults)).not.toThrow()
-})
