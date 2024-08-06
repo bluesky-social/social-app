74823a8812e7a9f4af3003fee5a2a8817cc0bdc9
Hailey <me@haileyok.com>
2024-08-06 16:58:34 -0700

    jest mock

--- modules/expo-bluesky-swiss-army/src/VisibilityView/index.tsx
+++ modules/expo-bluesky-swiss-army/src/VisibilityView/index.tsx
@@ -1,4 +1,9 @@
+import {NotImplementedError} from '../NotImplemented'
 import {VisibilityViewProps} from './types'
+
+export async function updateActiveViewAsync() {
+  throw new NotImplementedError()
+}
 
 export default function VisibilityView({
   children,
