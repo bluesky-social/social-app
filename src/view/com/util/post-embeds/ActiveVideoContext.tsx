d7c8567ab554eb423161e92a032e23ec65b7196a
Samuel Newman <10959775+mozzius@users.noreply.github.com>
2024-08-07 17:47:22 +0100

    minimise rerenders in `ActiveViewContext`

--- src/view/com/util/post-embeds/ActiveVideoContext.tsx
+++ src/view/com/util/post-embeds/ActiveVideoContext.tsx
@@ -1,4 +1,11 @@
-import React, {useCallback, useId, useMemo, useRef, useState} from 'react'
+import React, {
+  useCallback,
+  useEffect,
+  useId,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 import {useWindowDimensions} from 'react-native'
 
 import {isNative} from '#/platform/detection'
@@ -14,55 +21,75 @@ export function ActiveVideoProvider({children}: {children: React.ReactNode}) {
   const [activeViewId, setActiveViewId] = useState<string | null>(null)
   const activeViewLocationRef = useRef(Infinity)
   const [source, setSource] = useState<string | null>(null)
-  const [manuallySet, setManuallySet] = useState(false)
   const {height: windowHeight} = useWindowDimensions()
+
+  // minimising re-renders by using refs
+  const manuallySetRef = useRef(false)
+  const activeViewIdRef = useRef(activeViewId)
+  useEffect(() => {
+    activeViewIdRef.current = activeViewId
+  }, [activeViewId])
+
+  const setActiveView = useCallback(
+    (viewId: string, src: string) => {
+      setActiveViewId(viewId)
+      setSource(src)
+      manuallySetRef.current = true
+      // we don't know the exact position, but it's definitely on screen
+      // so just guess that it's in the middle. Any value is fine
+      // so long as it's not offscreen
+      activeViewLocationRef.current = windowHeight / 2
+    },
+    [windowHeight],
+  )
+
+  const sendViewPosition = useCallback(
+    (viewId: string, y: number) => {
+      if (isNative) return
+
+      if (viewId === activeViewIdRef.current) {
+        activeViewLocationRef.current = y
+      } else {
+        if (
+          distanceToIdealPosition(y) <
+          distanceToIdealPosition(activeViewLocationRef.current)
+        ) {
+          // if the old view was manually set, only usurp if the old view is offscreen
+          if (
+            manuallySetRef.current &&
+            withinViewport(activeViewLocationRef.current)
+          )
+            return
+
+          setActiveViewId(viewId)
+          activeViewLocationRef.current = y
+          manuallySetRef.current = false
+        }
+      }
+
+      function distanceToIdealPosition(yPos: number) {
+        return Math.abs(yPos - windowHeight / 2.5)
+      }
+
+      function withinViewport(yPos: number) {
+        return yPos > 0 && yPos < windowHeight
+      }
+    },
+    [windowHeight],
+  )
 
   const value = useMemo(
     () => ({
       activeViewId,
-      setActiveView: (viewId: string, src: string) => {
-        setActiveViewId(viewId)
-        setSource(src)
-        setManuallySet(true)
-        // we don't know the exact position, but it's definitely on screen
-        // so just guess that it's in the middle. Any value is fine
-        // so long as it's not offscreen
-        activeViewLocationRef.current = windowHeight / 2
-      },
-      sendViewPosition: (viewId: string, y: number) => {
-        if (isNative) return
-
-        if (viewId === activeViewId) {
-          activeViewLocationRef.current = y
-        } else {
-          if (
-            distanceToIdealPosition(y) <
-            distanceToIdealPosition(activeViewLocationRef.current)
-          ) {
-            // if the old view was manually set, only usurp if the old view is offscreen
-            if (manuallySet && withinViewport(activeViewLocationRef.current))
-              return
-
-            setActiveViewId(viewId)
-            activeViewLocationRef.current = y
-          }
-        }
-
-        function distanceToIdealPosition(yPos: number) {
-          return Math.abs(yPos - windowHeight / 2.5)
-        }
-
-        function withinViewport(yPos: number) {
-          return yPos > 0 && yPos < windowHeight
-        }
-      },
+      setActiveView,
+      sendViewPosition,
     }),
-    [activeViewId, windowHeight, manuallySet],
+    [activeViewId, setActiveView, sendViewPosition],
   )
 
   return (
     <ActiveVideoContext.Provider value={value}>
-      <VideoPlayerProvider source={source ?? ''} viewId={activeViewId}>
+      <VideoPlayerProvider source={source ?? ''}>
         {children}
       </VideoPlayerProvider>
     </ActiveVideoContext.Provider>
