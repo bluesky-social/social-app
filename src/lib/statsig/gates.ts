a0756c53be7991de1b2bd0c536c25e0d5995c992
Eric Bailey <git@esb.lol>
2024-08-08 12:14:15 -0500

    Remove show_avi_follow_button (#4900)

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -4,7 +4,6 @@ export type Gate =
   | 'new_user_guided_tour'
   | 'onboarding_minimum_interests'
   | 'session_withproxy_fix'
-  | 'show_avi_follow_button'
   | 'show_follow_back_label_v2'
   | 'suggested_feeds_interstitial'
   | 'video_debug'
