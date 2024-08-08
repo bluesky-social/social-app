5ddec8ed2b4fb3115a3b6bf4ec1455e4e5a260c4
Eric Bailey <git@esb.lol>
2024-08-08 10:20:54 -0500

    Remove new_user_progress_guide

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -4,7 +4,6 @@ export type Gate =
   | 'explore_page_profile_card_social_proof'
   | 'native_pwi_disabled'
   | 'new_user_guided_tour'
-  | 'new_user_progress_guide'
   | 'onboarding_minimum_interests'
   | 'session_withproxy_fix'
   | 'show_avi_follow_button'
