cb78e842326a35e18d51b5f41f78efb2bf4d8543
Hailey <me@haileyok.com>
2024-08-09 13:04:01 -0700

    lint

--- modules/expo-bluesky-swiss-army/ios/PlatformInfo/ExpoPlatformInfoModule.swift
+++ modules/expo-bluesky-swiss-army/ios/PlatformInfo/ExpoPlatformInfoModule.swift
@@ -3,16 +3,16 @@ import ExpoModulesCore
 public class ExpoPlatformInfoModule: Module {
   public func definition() -> ModuleDefinition {
     Name("ExpoPlatformInfo")
-    
+
     Function("getIsReducedMotionEnabled") {
       return UIAccessibility.isReduceMotionEnabled
     }
-    
+
     Function("setAudioCategory") { (audioCategoryString: String) in
       let audioCategory = AVAudioSession.Category(rawValue: audioCategoryString)
       try? AVAudioSession.sharedInstance().setCategory(audioCategory)
     }
-    
+
     Function("setAudioMixWithOthers") { (mixWithOthers: Bool) in
       var options: AVAudioSession.CategoryOptions
       let currentCategory = AVAudioSession.sharedInstance().category
