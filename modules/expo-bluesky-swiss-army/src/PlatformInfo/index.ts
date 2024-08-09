cb574b7be3757c25063957ff561bae6f1be7577f
Hailey <me@haileyok.com>
2024-08-09 03:05:12 -0700

    [Videos] Add iOS function to set `AVAudioSession.CategoryOptions` to `.mixWithOthers` (#4905)
    * audio mixing pref
    
    * lint

--- modules/expo-bluesky-swiss-army/src/PlatformInfo/index.ts
+++ modules/expo-bluesky-swiss-army/src/PlatformInfo/index.ts
@@ -3,3 +3,7 @@ import {NotImplementedError} from '../NotImplemented'
 export function getIsReducedMotionEnabled(): boolean {
   throw new NotImplementedError()
 }
+
+export function setAudioMixWithOthers(mixWithOthers: boolean): void {
+  throw new NotImplementedError({mixWithOthers})
+}
