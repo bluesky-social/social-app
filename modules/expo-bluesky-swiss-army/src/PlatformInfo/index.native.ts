003e4e2f489fb1c3ec83f84352ea9e797dfd0841
Hailey <me@haileyok.com>
2024-08-08 18:28:45 -0700

    audio mixing pref

--- modules/expo-bluesky-swiss-army/src/PlatformInfo/index.native.ts
+++ modules/expo-bluesky-swiss-army/src/PlatformInfo/index.native.ts
@@ -1,7 +1,13 @@
+import {Platform} from 'react-native'
 import {requireNativeModule} from 'expo-modules-core'
 
 const NativeModule = requireNativeModule('ExpoPlatformInfo')
 
 export function getIsReducedMotionEnabled(): boolean {
   return NativeModule.getIsReducedMotionEnabled()
+}
+
+export function setAudioMixWithOthers(mixWithOthers: boolean): void {
+  if (Platform.OS !== 'ios') return
+  NativeModule.setAudioMixWithOthers(mixWithOthers)
 }
