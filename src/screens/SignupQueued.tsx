b6e515c664d51ffe357c3562fd514301805ade8c
dan <dan.abramov@gmail.com>
2024-08-15 20:58:13 +0100

    Move global "Sign out" out of the current account row (#4941)
    * Rename logout to logoutEveryAccount
    
    * Add logoutCurrentAccount()
    
    * Make all "Log out" buttons refer to current account
    
    Each of these usages is completely contextual and refers to a specific account.
    
    * Add Sign out of all accounts to Settings
    
    * Move single account Sign Out below as well
    
    * Prompt on account removal
    
    * Add Other Accounts header to reduce ambiguity
    
    * Spacing fix
    
    ---------
    
    Co-authored-by: Paul Frazee <pfrazee@gmail.com>

--- src/screens/SignupQueued.tsx
+++ src/screens/SignupQueued.tsx
@@ -23,7 +23,7 @@ export function SignupQueued() {
   const insets = useSafeAreaInsets()
   const {gtMobile} = useBreakpoints()
   const onboardingDispatch = useOnboardingDispatch()
-  const {logout} = useSessionApi()
+  const {logoutCurrentAccount} = useSessionApi()
   const agent = useAgent()
 
   const [isProcessing, setProcessing] = React.useState(false)
@@ -153,7 +153,7 @@ export function SignupQueued() {
                   variant="ghost"
                   size="large"
                   label={_(msg`Log out`)}
-                  onPress={() => logout('SignupQueued')}>
+                  onPress={() => logoutCurrentAccount('SignupQueued')}>
                   <ButtonText style={[{color: t.palette.primary_500}]}>
                     <Trans>Log out</Trans>
                   </ButtonText>
@@ -182,7 +182,7 @@ export function SignupQueued() {
               variant="ghost"
               size="large"
               label={_(msg`Log out`)}
-              onPress={() => logout('SignupQueued')}>
+              onPress={() => logoutCurrentAccount('SignupQueued')}>
               <ButtonText style={[{color: t.palette.primary_500}]}>
                 <Trans>Log out</Trans>
               </ButtonText>
