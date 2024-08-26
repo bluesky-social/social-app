c8438c6243767b4b461510f964a5d208ba1cbdf1
Hailey <me@haileyok.com>
2024-08-26 15:05:21 -0700

    cleanup

--- src/lib/hooks/useDedupe.ts
+++ src/lib/hooks/useDedupe.ts
@@ -1,12 +1,21 @@
 import React from 'react'
 
 export const useDedupe = (timeout = 250) => {
+  const timeoutRef = React.useRef<NodeJS.Timeout | null>(null)
   const canDo = React.useRef(true)
+
+  React.useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+    }
+  }, [])
 
   return (cb: () => unknown) => {
     if (canDo.current) {
       canDo.current = false
-      setTimeout(() => {
+      timeoutRef.current = setTimeout(() => {
         canDo.current = true
       }, timeout)
       cb()
