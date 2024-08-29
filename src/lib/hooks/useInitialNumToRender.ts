3ec5f5433bca6c7ac772b9d87b87ea9c5bf66498
Hailey <me@haileyok.com>
2024-08-29 08:41:20 -0700

    tweak

--- src/lib/hooks/useInitialNumToRender.ts
+++ src/lib/hooks/useInitialNumToRender.ts
@@ -1,5 +1,7 @@
 import {useWindowDimensions} from 'react-native'
-import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs'
+import {useSafeAreaInsets} from 'react-native-safe-area-context'
+
+import {useBottomBarOffset} from 'lib/hooks/useBottomBarOffset'
 
 const MIN_POST_HEIGHT = 100
 
@@ -8,7 +10,10 @@ export function useInitialNumToRender({
   screenHeightOffset = 0,
 }: {minItemHeight?: number; screenHeightOffset?: number} = {}) {
   const {height: screenHeight} = useWindowDimensions()
-  const bottomBarHeight = useBottomTabBarHeight()
-  const finalHeight = screenHeight - screenHeightOffset - bottomBarHeight
+  const {top: topInset} = useSafeAreaInsets()
+  const bottomBarHeight = useBottomBarOffset()
+
+  const finalHeight =
+    screenHeight - screenHeightOffset - topInset - bottomBarHeight
   return Math.floor(finalHeight / minItemHeight) + 1
 }
