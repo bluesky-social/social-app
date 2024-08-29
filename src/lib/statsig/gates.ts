e51eb391df754d02f006220eef177ef8c0cd0795
dan <dan.abramov@gmail.com>
2024-08-29 20:09:11 +0100

    Enable show_follow_back_label_v2 (#5022)

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -3,7 +3,6 @@ export type Gate =
   | 'debug_show_feedcontext'
   | 'fixed_bottom_bar'
   | 'onboarding_minimum_interests'
-  | 'show_follow_back_label_v2'
   | 'suggested_feeds_interstitial'
   | 'video_debug'
   | 'videos'
