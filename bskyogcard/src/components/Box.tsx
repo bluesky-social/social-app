95aebf572490ca74871c4c5bb60ef574a21fbfc6
Eric Bailey <git@esb.lol>
2024-08-29 12:55:59 -0500

    Simplify box

--- bskyogcard/src/components/Box.tsx
+++ bskyogcard/src/components/Box.tsx
@@ -4,10 +4,10 @@ import {atoms as a, style as s} from '../theme/index.js'
 
 export function Box({
   children,
-  cx,
+  cx = [],
 }: {
   children?: React.ReactNode
   cx?: Record<string, any>[]
 }) {
-  return <div style={s([a.flex, a.flex_col, ...(cx ?? [])])}>{children}</div>
+  return <div style={s([a.flex, a.flex_col, ...cx])}>{children}</div>
 }
