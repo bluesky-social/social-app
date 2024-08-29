faf66f3178fbddcfaab9c851b9315f20cd285c38
dan <dan.abramov@gmail.com>
2024-08-29 18:57:36 +0100

    Remove new_user_guided_tour and tour code (#5023)

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -2,7 +2,6 @@ export type Gate =
   // Keep this alphabetic please.
   | 'debug_show_feedcontext'
   | 'fixed_bottom_bar'
-  | 'new_user_guided_tour'
   | 'onboarding_minimum_interests'
   | 'show_follow_back_label_v2'
   | 'suggested_feeds_interstitial'
