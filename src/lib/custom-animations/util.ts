d60e0530249f4acd81dcff8f7608c7510de1ea0f
Hailey <me@haileyok.com>
2024-09-04 12:08:25 -0700

    dont roll first like

--- src/lib/custom-animations/util.ts
+++ src/lib/custom-animations/util.ts
@@ -7,7 +7,7 @@ export function decideShouldRoll(isSet: boolean, count: number) {
   let shouldRoll = false
   if (!isSet && count === 0) {
     shouldRoll = true
-  } else if (count > 0 && count < 1000) {
+  } else if (count > 1 && count < 1000) {
     shouldRoll = true
   } else if (count > 0) {
     const mod = count % 100
