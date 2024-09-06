c5faa6034472f241778276a234021fb4eb12f804
Eric Bailey <git@esb.lol>
2024-09-06 15:55:23 -0500

    Redesign play button (#5192)

--- src/components/video/PlayButtonIcon.tsx
+++ src/components/video/PlayButtonIcon.tsx
@@ -2,24 +2,47 @@ import React from 'react'
 import {View} from 'react-native'
 
 import {atoms as a, useTheme} from '#/alf'
-import {Play_Filled_Corner2_Rounded as PlayIcon} from '#/components/icons/Play'
+import {Play_Filled_Corner0_Rounded as PlayIcon} from '#/components/icons/Play'
 
-export function PlayButtonIcon({size = 44}: {size?: number}) {
+export function PlayButtonIcon({size = 36}: {size?: number}) {
   const t = useTheme()
+  const bg = t.name === 'light' ? t.palette.contrast_25 : t.palette.contrast_975
+  const fg = t.name === 'light' ? t.palette.contrast_975 : t.palette.contrast_25
 
   return (
     <View
       style={[
         a.rounded_full,
+        a.overflow_hidden,
         a.align_center,
         a.justify_center,
+        t.atoms.shadow_lg,
         {
-          backgroundColor: t.palette.primary_500,
-          width: size + 16,
-          height: size + 16,
+          width: size + size / 1.5,
+          height: size + size / 1.5,
         },
       ]}>
-      <PlayIcon height={size} width={size} style={{color: 'white'}} />
+      <View
+        style={[
+          a.absolute,
+          a.inset_0,
+          {
+            backgroundColor: bg,
+            opacity: 0.7,
+          },
+        ]}
+      />
+      <PlayIcon
+        width={size}
+        fill={fg}
+        style={[
+          a.relative,
+          a.z_10,
+          {
+            left: size / 50,
+          },
+        ]}
+      />
     </View>
   )
 }
