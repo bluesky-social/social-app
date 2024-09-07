7d7431d14e52d94cce51941013c8a5fe5665adb1
dan <dan.abramov@gmail.com>
2024-09-07 17:07:30 +0200

    Set fixed_bottom_bar to true (#5203)

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -1,7 +1,6 @@
 export type Gate =
   // Keep this alphabetic please.
   | 'debug_show_feedcontext'
-  | 'fixed_bottom_bar'
   | 'onboarding_minimum_interests'
   | 'suggested_feeds_interstitial'
   | 'show_follow_suggestions_in_profile'
