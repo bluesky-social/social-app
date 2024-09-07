b55ef5a02cfbb9eb6564089854a68d0d66cabcbf
Dan Abramov <dan.abramov@gmail.com>
2024-09-07 14:46:02 +0200

    Set show_follow_suggestions_in_profile to true

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -4,7 +4,6 @@ export type Gate =
   | 'fixed_bottom_bar'
   | 'onboarding_minimum_interests'
   | 'suggested_feeds_interstitial'
-  | 'show_follow_suggestions_in_profile'
   | 'video_debug' // not recommended
   | 'video_upload' // upload videos
   | 'video_view_on_posts' // see posted videos
