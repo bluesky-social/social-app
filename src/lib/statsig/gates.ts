3471c46e6662b3ac711219e199ced527a75fdfb6
Dan Abramov <dan.abramov@gmail.com>
2024-09-07 14:08:22 +0200

    Set onboarding_minimum_interests to false

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -2,7 +2,6 @@ export type Gate =
   // Keep this alphabetic please.
   | 'debug_show_feedcontext'
   | 'fixed_bottom_bar'
-  | 'onboarding_minimum_interests'
   | 'suggested_feeds_interstitial'
   | 'show_follow_suggestions_in_profile'
   | 'video_debug' // not recommended
