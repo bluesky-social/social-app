0509c6997cfca98a36557eaa2e81e84b9928e9c1
Samuel Newman <mozzius@protonmail.com>
2024-09-07 19:56:54 +0100

    rm shitty debug mode

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -2,6 +2,5 @@ export type Gate =
   // Keep this alphabetic please.
   | 'debug_show_feedcontext'
   | 'suggested_feeds_interstitial'
-  | 'video_debug' // not recommended
   | 'video_upload' // upload videos
   | 'video_view_on_posts' // see posted videos
