42fb92064acc383a2d264f3cae0b1234c1822beb
dan <dan.abramov@gmail.com>
2024-09-07 20:22:34 +0200

    Set onboarding_minimum_interests to false (#5204)
    Co-authored-by: Hailey <me@haileyok.com>

--- src/lib/statsig/gates.ts
+++ src/lib/statsig/gates.ts
@@ -1,7 +1,6 @@
 export type Gate =
   // Keep this alphabetic please.
   | 'debug_show_feedcontext'
-  | 'onboarding_minimum_interests'
   | 'suggested_feeds_interstitial'
   | 'video_debug' // not recommended
   | 'video_upload' // upload videos
