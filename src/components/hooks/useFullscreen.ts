2a11e8780050a5e261ef932ac3cc14d6cfebb5a3
Samuel Newman <mozzius@protonmail.com>
2024-09-10 01:27:33 +0100

    chrome scroll restoration

--- src/components/hooks/useFullscreen.ts
+++ src/components/hooks/useFullscreen.ts
@@ -1,5 +1,6 @@
-import {useCallback, useSyncExternalStore} from 'react'
+import {useCallback, useRef, useSyncExternalStore} from 'react'
 
+import {isFirefox, isSafari} from '#/lib/browser'
 import {isWeb} from '#/platform/detection'
 
 function fullscreenSubscribe(onChange: () => void) {
@@ -12,13 +13,22 @@ export function useFullscreen(ref?: React.RefObject<HTMLElement>) {
   const isFullscreen = useSyncExternalStore(fullscreenSubscribe, () =>
     Boolean(document.fullscreenElement),
   )
+  const scrollYRef = useRef(0)
 
   const toggleFullscreen = useCallback(() => {
     if (isFullscreen) {
       document.exitFullscreen()
+      // Chrome has an issue where it doesn't scroll back to the top after exiting fullscreen
+      // Let's play it safe and do it if not FF or Safari, since anything else will probably be chromium
+      if (!isFirefox && !isSafari) {
+        setTimeout(() => {
+          window.scrollTo(0, scrollYRef.current)
+        }, 100)
+      }
     } else {
       if (!ref) throw new Error('No ref provided')
       if (!ref.current) return
+      scrollYRef.current = window.scrollY
       ref.current.requestFullscreen()
     }
   }, [isFullscreen, ref])
