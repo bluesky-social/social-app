9030f58238192181b2d48dd2e8f62f5f9e06ecd9
Eric Bailey <git@esb.lol>
2024-09-13 11:17:06 -0500

    Ignore interactive elements

--- src/state/shell/composer/useComposerKeyboardShortcut.tsx
+++ src/state/shell/composer/useComposerKeyboardShortcut.tsx
@@ -1,17 +1,49 @@
 import React from 'react'
 
-import {useComposerControls, useComposerState} from './'
+import {useComposerControls} from './'
+
+/**
+ * Based on {@link https://github.com/jaywcjlove/hotkeys-js/blob/b0038773f3b902574f22af747f3bb003a850f1da/src/index.js#L51C1-L64C2}
+ */
+function shouldIgnore(event: KeyboardEvent) {
+  const target: any = event.target || event.srcElement
+  if (!target) return false
+  const {tagName} = target
+  if (!tagName) return false
+  const isInput =
+    tagName === 'INPUT' &&
+    ![
+      'checkbox',
+      'radio',
+      'range',
+      'button',
+      'file',
+      'reset',
+      'submit',
+      'color',
+    ].includes(target.type)
+  // ignore: isContentEditable === 'true', <input> and <textarea> when readOnly state is false, <select>
+  if (
+    target.isContentEditable ||
+    ((isInput || tagName === 'TEXTAREA' || tagName === 'SELECT') &&
+      !target.readOnly)
+  ) {
+    return true
+  }
+  return false
+}
 
 export function useComposerKeyboardShortcut() {
-  const isOpen = !!useComposerState()
   const {openComposer} = useComposerControls()
 
   React.useEffect(() => {
     function handler(event: KeyboardEvent) {
-      if (isOpen) return
-      if (event.key === 'n' || event.key === 'N') openComposer({})
+      if (shouldIgnore(event)) return
+      if (event.key === 'n' || event.key === 'N') {
+        openComposer({})
+      }
     }
     document.addEventListener('keydown', handler)
     return () => document.removeEventListener('keydown', handler)
-  }, [isOpen, openComposer])
+  }, [openComposer])
 }
