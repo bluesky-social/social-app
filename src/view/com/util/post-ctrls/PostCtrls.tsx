2ef4e4df0e8671bf8bebb7835318ab42b0fa23c5
Hichem Fantar <hichemfantar2049@gmail.com>
2024-09-18 11:52:43 +0100

    speed up transition

--- src/view/com/util/post-ctrls/PostCtrls.tsx
+++ src/view/com/util/post-ctrls/PostCtrls.tsx
@@ -250,7 +250,7 @@ let PostCtrls = ({
       {
         transitionProperty: 'all',
         transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
-        transitionDuration: '150ms',
+        transitionDuration: '100ms',
       },
     ],
     [t.atoms.bg_contrast_25],
