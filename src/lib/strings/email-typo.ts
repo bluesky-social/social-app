f2b4945c98017146c061c400098b9a984913e83b
Eric Bailey <git@esb.lol>
2024-09-20 15:57:32 -0500

    Move test file, adjust regex, add test

--- src/lib/strings/email-typo.ts
+++ src/lib/strings/email-typo.ts
@@ -1,7 +1,7 @@
 import type tldts from 'tldts'
 
 const COMMON_ERROR_PATTERN =
-  /\b([a-zA-Z0-9._%+-]+)@(gnail\.(co|com)|gmaill\.(co|com)|gmai\.(co|com)|gmail\.co|gmal\.(co|com)|iclod\.(co|com)|icloud\.co|outllok\.(co|com)|outlok\.(co|com)|outlook\.co|yaoo\.(co|com)|yaho\.(co|com)|yahoo\.co|yahooo\.(co|com))\b/
+  /([a-zA-Z0-9._%+-]+)@(gnail\.(co|com)|gmaill\.(co|com)|gmai\.(co|com)|gmail\.co|gmal\.(co|com)|iclod\.(co|com)|icloud\.co|outllok\.(co|com)|outlok\.(co|com)|outlook\.co|yaoo\.(co|com)|yaho\.(co|com)|yahoo\.co|yahooo\.(co|com))$/
 
 export function emailTypoCheck(email: string, dynamicTldts: typeof tldts) {
   const isIcann = dynamicTldts.parse(email).isIcann
