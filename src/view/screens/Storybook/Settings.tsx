685ab8cda462d86e04a1d7d339ae1fce8b2cc312
Samuel Newman <mozzius@protonmail.com>
2024-09-24 15:57:48 +0100

    add title

--- src/view/screens/Storybook/Settings.tsx
+++ src/view/screens/Storybook/Settings.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import * as SettingsList from '#/screens/Settings/components/SettingsList'
 import {Person_Stroke2_Corner0_Rounded as PersonIcon} from '#/components/icons/Person'
+import {Text} from '#/components/Typography'
 
 export function Settings() {
   return (
     <>
+      <Text>Settings</Text>
       <SettingsList.LinkItem to="/settings" label="Account">
         <SettingsList.ItemIcon icon={PersonIcon} />
         <SettingsList.ItemText>Account</SettingsList.ItemText>
