47479da7e8ab06201685227ba785c51d03f6f800
Samuel Newman <mozzius@protonmail.com>
2024-09-24 16:23:44 +0100

    add some styles

--- src/screens/Settings/components/SettingsList.tsx
+++ src/screens/Settings/components/SettingsList.tsx
@@ -1,16 +1,60 @@
 import React from 'react'
+import {View} from 'react-native'
 
+import {atoms as a, useTheme} from '#/alf'
 import * as Button from '#/components/Button'
+import {ChevronRight_Stroke2_Corner0_Rounded as ChevronRightIcon} from '#/components/icons/Chevron'
 import {Link, LinkProps} from '#/components/Link'
 
-export function LinkItem({style, ...props}: LinkProps) {
-  return <Link style={[style]} {...props} />
+export function LinkItem({style, children, ...props}: LinkProps) {
+  const t = useTheme()
+  return (
+    <Link color="secondary" {...props}>
+      {args => (
+        <View
+          style={[
+            a.px_xl,
+            a.py_sm,
+            a.align_center,
+            a.gap_sm,
+            a.flex_row,
+            {minHeight: 48},
+            (args.hovered || args.pressed) && [t.atoms.bg_contrast_25],
+            style,
+          ]}>
+          {typeof children === 'function' ? children(args) : children}
+          <View style={[a.flex_1]} />
+          <ItemIcon icon={ChevronRightIcon} size="lg" />
+        </View>
+      )}
+    </Link>
+  )
 }
 
-export function ItemIcon(
-  props: React.ComponentProps<typeof Button.ButtonIcon>,
-) {
-  return <Button.ButtonIcon {...props} />
+export function ItemIcon({
+  icon: Comp,
+  size = 'xl',
+}: Omit<React.ComponentProps<typeof Button.ButtonIcon>, 'position'>) {
+  const t = useTheme()
+
+  /*
+   * Copied here from icons/common.tsx so we can tweak if we need to, but
+   * also so that we can calculate transforms.
+   */
+  const iconSize = {
+    xs: 12,
+    sm: 16,
+    md: 20,
+    lg: 24,
+    xl: 28,
+    '2xl': 32,
+  }[size]
+
+  return (
+    <View style={[a.z_20, {width: iconSize, height: iconSize}]}>
+      <Comp width={iconSize} style={[t.atoms.text]} />
+    </View>
+  )
 }
 
 export function ItemText({
@@ -18,5 +62,7 @@ export function ItemText({
   style,
   ...props
 }: React.ComponentProps<typeof Button.ButtonText>) {
-  return <Button.ButtonText style={[style]} {...props} />
+  return (
+    <Button.ButtonText style={[a.text_lg, a.font_normal, style]} {...props} />
+  )
 }
--- src/view/screens/Storybook/Settings.tsx
+++ src/view/screens/Storybook/Settings.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {View} from 'react-native'
 
 import * as SettingsList from '#/screens/Settings/components/SettingsList'
 import {Person_Stroke2_Corner0_Rounded as PersonIcon} from '#/components/icons/Person'
@@ -6,8 +7,8 @@ import {Text} from '#/components/Typography'
 
 export function Settings() {
   return (
-    <>
-      <Text>Settings</Text>
+    <View style={{marginLeft: -20, marginRight: -20}}>
+      <Text style={{marginLeft: 20, paddingBottom: 12}}>Settings</Text>
       <SettingsList.LinkItem to="/settings" label="Account">
         <SettingsList.ItemIcon icon={PersonIcon} />
         <SettingsList.ItemText>Account</SettingsList.ItemText>
@@ -18,6 +19,6 @@ export function Settings() {
         <SettingsList.ItemIcon icon={PersonIcon} />
         <SettingsList.ItemText>Accessibilty & appearance</SettingsList.ItemText>
       </SettingsList.LinkItem>
-    </>
+    </View>
   )
 }
