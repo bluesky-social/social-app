b38d4697b7a42a5e4c48d86a6528a20ace9c034e
Eric Bailey <git@esb.lol>
2024-09-24 20:10:13 -0500

    [Neue] Post avi, `PostMeta` cleanup (#5450)
    * Support emoji in text with custom font
    
    * Add emoji support to elements that need it
    
    * Remove unused file causing lint failure
    
    * Add web only link variant
    
    * Refactor PostMeta
    
    * Reduce avi size in feeds
    
    * Fix alignment, emoji, in PostMeta
    
    * Smaller avis in notifications
    
    * Shrink post placeholder avi
    
    * Handle the handle again
    
    * Link cleanup
    
    * Cleanup unused props
    
    * Fix text wrapping in timestamp
    
    * Fix underline color
    
    * Tighten up spacing
    
    * Web only whiteSpace

--- src/view/com/post/Post.tsx
+++ src/view/com/post/Post.tsx
@@ -163,7 +163,7 @@ function PostInner({
         <View style={styles.layoutAvi}>
           <AviFollowButton author={post.author} moderation={moderation}>
             <PreviewableUserAvatar
-              size={52}
+              size={42}
               profile={post.author}
               moderation={moderation.ui('avatar')}
               type={post.author.associated?.labeler ? 'labeler' : 'user'}
@@ -174,7 +174,6 @@ function PostInner({
           <PostMeta
             author={post.author}
             moderation={moderation}
-            authorHasWarning={!!post.author.labels?.length}
             timestamp={post.indexedAt}
             postHref={itemHref}
           />
