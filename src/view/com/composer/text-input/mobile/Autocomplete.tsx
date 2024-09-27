f4352344b89731cf42a8871f8e2b8d3a106eb1d1
Samuel Newman <mozzius@protonmail.com>
2024-09-28 00:32:11 +0300

    use Reanimated layout transitions

--- src/view/com/composer/text-input/mobile/Autocomplete.tsx
+++ src/view/com/composer/text-input/mobile/Autocomplete.tsx
@@ -1,11 +1,10 @@
-import React, {useEffect, useRef} from 'react'
-import {Animated, View} from 'react-native'
+import React, {useRef} from 'react'
+import {View} from 'react-native'
+import Animated, {FadeInDown, FadeOut} from 'react-native-reanimated'
 import {AppBskyActorDefs} from '@atproto/api'
 import {Trans} from '@lingui/macro'
 
 import {PressableScale} from '#/lib/custom-animations/PressableScale'
-import {useAnimatedValue} from '#/lib/hooks/useAnimatedValue'
-import {usePalette} from '#/lib/hooks/usePalette'
 import {sanitizeDisplayName} from '#/lib/strings/display-names'
 import {sanitizeHandle} from '#/lib/strings/handles'
 import {useActorAutocompleteQuery} from '#/state/queries/actor-autocomplete'
@@ -22,8 +21,7 @@ export function Autocomplete({
   onSelect: (item: string) => void
 }) {
   const t = useTheme()
-  const pal = usePalette('default')
-  const positionInterp = useAnimatedValue(0)
+
   const {getGraphemeString} = useGrapheme()
   const isActive = !!prefix
   const {data: suggestions, isFetching} = useActorAutocompleteQuery(prefix)
@@ -34,108 +32,85 @@ export function Autocomplete({
     suggestionsRef.current = suggestions
   }
 
-  useEffect(() => {
-    Animated.timing(positionInterp, {
-      toValue: isActive ? 1 : 0,
-      duration: 200,
-      useNativeDriver: true,
-    }).start()
-  }, [positionInterp, isActive])
-
-  const topAnimStyle = {
-    transform: [
-      {
-        translateY: positionInterp.interpolate({
-          inputRange: [0, 1],
-          outputRange: [200, 0],
-        }),
-      },
-    ],
-  }
+  if (!isActive) return null
 
   return (
-    <Animated.View style={topAnimStyle}>
-      {isActive ? (
-        <View
-          style={[
-            pal.view,
-            a.mt_sm,
-            a.border,
-            a.rounded_sm,
-            t.atoms.border_contrast_high,
-            {marginLeft: -62},
-          ]}>
-          {suggestionsRef.current?.length ? (
-            suggestionsRef.current.slice(0, 5).map((item, index, arr) => {
-              // Eventually use an average length
-              const MAX_CHARS = 40
-              const MAX_HANDLE_CHARS = 20
+    <Animated.View
+      entering={FadeInDown.duration(200)}
+      exiting={FadeOut.duration(100)}
+      style={[
+        t.atoms.bg,
+        a.mt_sm,
+        a.border,
+        a.rounded_sm,
+        t.atoms.border_contrast_high,
+        {marginLeft: -62},
+      ]}>
+      {suggestionsRef.current?.length ? (
+        suggestionsRef.current.slice(0, 5).map((item, index, arr) => {
+          // Eventually use an average length
+          const MAX_CHARS = 40
+          const MAX_HANDLE_CHARS = 20
 
-              // Using this approach because styling is not respecting
-              // bounding box wrapping (before converting to ellipsis)
-              const {name: displayHandle, remainingCharacters} =
-                getGraphemeString(item.handle, MAX_HANDLE_CHARS)
+          // Using this approach because styling is not respecting
+          // bounding box wrapping (before converting to ellipsis)
+          const {name: displayHandle, remainingCharacters} = getGraphemeString(
+            item.handle,
+            MAX_HANDLE_CHARS,
+          )
 
-              const {name: displayName} = getGraphemeString(
-                item.displayName || item.handle,
-                MAX_CHARS -
-                  MAX_HANDLE_CHARS +
-                  (remainingCharacters > 0 ? remainingCharacters : 0),
-              )
+          const {name: displayName} = getGraphemeString(
+            item.displayName || item.handle,
+            MAX_CHARS -
+              MAX_HANDLE_CHARS +
+              (remainingCharacters > 0 ? remainingCharacters : 0),
+          )
 
-              return (
-                <View
-                  style={[
-                    index !== arr.length - 1 && a.border_b,
-                    t.atoms.border_contrast_high,
-                    a.px_sm,
-                    a.py_md,
-                  ]}
-                  key={item.handle}>
-                  <PressableScale
-                    testID="autocompleteButton"
-                    style={[
-                      a.flex_row,
-                      a.gap_sm,
-                      a.justify_between,
-                      a.align_center,
-                    ]}
-                    onPress={() => onSelect(item.handle)}
-                    accessibilityLabel={`Select ${item.handle}`}
-                    accessibilityHint="">
-                    <View style={[a.flex_row, a.gap_sm, a.align_center]}>
-                      <UserAvatar
-                        avatar={item.avatar ?? null}
-                        size={24}
-                        type={item.associated?.labeler ? 'labeler' : 'user'}
-                      />
-                      <Text
-                        style={[a.text_md, a.font_bold]}
-                        emoji={true}
-                        numberOfLines={1}>
-                        {sanitizeDisplayName(displayName)}
-                      </Text>
-                    </View>
-                    <Text
-                      style={[t.atoms.text_contrast_medium]}
-                      numberOfLines={1}>
-                      {sanitizeHandle(displayHandle, '@')}
-                    </Text>
-                  </PressableScale>
+          return (
+            <View
+              style={[
+                index !== arr.length - 1 && a.border_b,
+                t.atoms.border_contrast_high,
+                a.px_sm,
+                a.py_md,
+              ]}
+              key={item.handle}>
+              <PressableScale
+                testID="autocompleteButton"
+                style={[
+                  a.flex_row,
+                  a.gap_sm,
+                  a.justify_between,
+                  a.align_center,
+                ]}
+                onPress={() => onSelect(item.handle)}
+                accessibilityLabel={`Select ${item.handle}`}
+                accessibilityHint="">
+                <View style={[a.flex_row, a.gap_sm, a.align_center]}>
+                  <UserAvatar
+                    avatar={item.avatar ?? null}
+                    size={24}
+                    type={item.associated?.labeler ? 'labeler' : 'user'}
+                  />
+                  <Text
+                    style={[a.text_md, a.font_bold]}
+                    emoji={true}
+                    numberOfLines={1}>
+                    {sanitizeDisplayName(displayName)}
+                  </Text>
                 </View>
-              )
-            })
-          ) : (
-            <Text style={[a.text_md, a.px_sm, a.py_md]}>
-              {isFetching ? (
-                <Trans>Loading...</Trans>
-              ) : (
-                <Trans>No result</Trans>
-              )}
-            </Text>
-          )}
-        </View>
-      ) : null}
+                <Text style={[t.atoms.text_contrast_medium]} numberOfLines={1}>
+                  {sanitizeHandle(displayHandle, '@')}
+                </Text>
+              </PressableScale>
+            </View>
+          )
+        })
+      ) : (
+        <Text style={[a.text_md, a.px_sm, a.py_md]}>
+          {isFetching ? <Trans>Loading...</Trans> : <Trans>No result</Trans>}
+        </Text>
+      )}
     </Animated.View>
   )
 }
