32bf38940de5a179ca1b659eaa557f93cadc9458
Paul Frazee <pfrazee@gmail.com>
2024-10-10 19:07:55 -0700

    Tweak threadgate button sizing in the composer on Web (#5692)
    * Adjust threadgate btn sizing in composer
    
    * Tweak threadgate button size on web

--- src/view/com/composer/threadgate/ThreadgateBtn.tsx
+++ src/view/com/composer/threadgate/ThreadgateBtn.tsx
@@ -63,7 +63,10 @@ export function ThreadgateBtn({
           label={label}
           accessibilityHint={_(
             msg`Opens a dialog to choose who can reply to this thread`,
-          )}>
+          )}
+          style={
+            !isNative ? {paddingVertical: 6, paddingHorizontal: 8} : undefined
+          }>
           <ButtonIcon icon={anyoneCanInteract ? Earth : Group} />
           <ButtonText>{label}</ButtonText>
         </Button>
