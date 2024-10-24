cc1cda8bf60ac1f791dcdac4a80dbca918fd3cc1
Eric Bailey <git@esb.lol>
2024-10-24 11:37:08 -0500

    Add Android billing perms

--- plugins/withAndroidManifestPlugin.js
+++ plugins/withAndroidManifestPlugin.js
@@ -6,6 +6,16 @@ module.exports = function withAndroidManifestPlugin(appConfig) {
       decoratedAppConfig.modResults.manifest.application[0].$[
         'android:largeHeap'
       ] = 'true'
+      decoratedAppConfig.modResults.manifest['uses-permission'] =
+        decoratedAppConfig.modResults.manifest['uses-permission'] || []
+      /**
+       * @see https://www.revenuecat.com/docs/getting-started/installation/reactnative#import-purchases
+       */
+      decoratedAppConfig.modResults.manifest['uses-permission'].push({
+        $: {
+          'android:name': 'com.android.vending.BILLING',
+        },
+      })
     } catch (e) {
       console.error(`withAndroidManifestPlugin failed`, e)
     }
