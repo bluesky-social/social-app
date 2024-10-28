1c992beff6f9a30b9ecd25f6b9b7ad4f00459222
Samuel Newman <mozzius@protonmail.com>
2024-10-28 18:28:54 +0200

    flex_1 text container (#5891)

--- src/components/StarterPack/StarterPackCard.tsx
+++ src/components/StarterPack/StarterPackCard.tsx
@@ -65,7 +65,7 @@ export function Card({
     <View style={[a.w_full, a.gap_md]}>
       <View style={[a.flex_row, a.gap_sm]}>
         {!noIcon ? <StarterPack width={40} gradient="sky" /> : null}
-        <View>
+        <View style={[a.flex_1]}>
           <Text emoji style={[a.text_md, a.font_bold, a.leading_snug]}>
             {record.name}
           </Text>
