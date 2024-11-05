b806d150dea79d1f7b823877bc7ce0683e4c90e4
Samuel Newman <mozzius@protonmail.com>
2024-11-05 16:43:32 +0000

    remove ts-expect-error

--- src/view/com/util/numeric/format.ts
+++ src/view/com/util/numeric/format.ts
@@ -5,8 +5,6 @@ export const formatCount = (i18n: I18n, num: number) => {
     notation: 'compact',
     maximumFractionDigits: 1,
     // `1,953` shouldn't be rounded up to 2k, it should be truncated.
-    // @ts-expect-error: `roundingMode` doesn't seem to be in the typings yet
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#roundingmode
     roundingMode: 'trunc',
   })
 }
