4e36fe093ac64af1fd1d90971594058211938b11
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-11-06 11:19:23 -0800

    Use Type Assertion for clearTimeout

--- src/view/com/util/List.web.tsx
+++ src/view/com/util/List.web.tsx
@@ -466,7 +466,7 @@ let Row = function RowImpl<ItemT>({
             }
           } else {
             if (intersectionTimeout.current) {
-              clearTimeout(intersectionTimeout.current)
+              clearTimeout(intersectionTimeout.current as NodeJS.Timeout)
               intersectionTimeout.current = undefined
             }
           }
