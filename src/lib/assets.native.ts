dc01b3839183cf246e434c2ccd2b151b3b3f75d4
Samuel Newman <mozzius@protonmail.com>
2024-11-07 14:22:16 +0000

    delete some apparently unused code

--- src/lib/assets.native.ts
+++
@@ -1,4 +0,0 @@
-import {type ImageRequireSource} from 'react-native'
-
-export const DEF_AVATAR: ImageRequireSource = require('../../assets/default-avatar.png')
-export const CLOUD_SPLASH: ImageRequireSource = require('../../assets/splash.png')
