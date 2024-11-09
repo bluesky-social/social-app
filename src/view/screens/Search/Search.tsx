33c8307f4b274dcc3f129a103636a58396635e41
Andrew Lisowski <lisowski54@gmail.com>
2024-11-09 01:37:49 -0800

    sort mobile search results
    (cherry picked from commit bd8f8f1090564d1ce0c6de848ccc6e46c5c86adc)

--- src/view/screens/Search/Search.tsx
+++ src/view/screens/Search/Search.tsx
@@ -66,6 +66,7 @@ import {SearchInput} from '#/components/forms/SearchInput'
 import {ChevronBottom_Stroke2_Corner0_Rounded as ChevronDown} from '#/components/icons/Chevron'
 import {Menu_Stroke2_Corner0_Rounded as Menu} from '#/components/icons/Menu'
 import * as Layout from '#/components/Layout'
+import {sortProfiles} from '../../../lib/functions'
 
 function Loader() {
   const pal = usePalette('default')
@@ -1006,7 +1007,7 @@ let AutocompleteResults = ({
             }
             style={{borderBottomWidth: 1}}
           />
-          {autocompleteData?.map(item => (
+          {autocompleteData?.sort(sortProfiles).map(item => (
             <SearchProfileCard
               key={item.did}
               profile={item}
