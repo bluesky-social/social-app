6b91567dc618f5f7096badf5a7fa2629ef4b96d7
khuddite <biliie811028@hotmail.com>
2024-11-10 15:15:54 -0500

    Fix 'Show badge' button label being cut off

--- src/components/forms/ToggleButton.tsx
+++ src/components/forms/ToggleButton.tsx
@@ -95,6 +95,9 @@ function ButtonInner({children}: React.PropsWithChildren<{}>) {
         native({
           paddingBottom: 10,
         }),
+        a.w_full,
+        a.h_full,
+        a.justify_center,
         a.px_md,
         t.atoms.bg,
         t.atoms.border_contrast_low,
