0071f8fa94ea815940d03a2a640d7f22233836ab
Hailey <me@haileyok.com>
2024-11-10 12:12:26 -0800

    rm kotlin override

--- app.config.js
+++ app.config.js
@@ -209,7 +209,6 @@ module.exports = function (config) {
               compileSdkVersion: 34,
               targetSdkVersion: 34,
               buildToolsVersion: '34.0.0',
-              // kotlinVersion: '1.8.0',
               newArchEnabled: false,
             },
           },
