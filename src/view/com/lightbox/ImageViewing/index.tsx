3a603b34103def9c7b5746f334cdd4f4cf69edf9
Dan Abramov <dan.abramov@gmail.com>
2024-11-11 19:27:54 +0000

    Tweak lightbox springs

--- src/view/com/lightbox/ImageViewing/index.tsx
+++ src/view/com/lightbox/ImageViewing/index.tsx
@@ -62,7 +62,7 @@ const EDGES =
     ? (['top', 'bottom', 'left', 'right'] satisfies Edge[])
     : (['left', 'right'] satisfies Edge[]) // iOS, so no top/bottom safe area
 
-const SLOW_SPRING = {stiffness: 120}
+const SLOW_SPRING = {stiffness: 180}
 const FAST_SPRING = {stiffness: 700}
 
 export default function ImageViewRoot({
@@ -433,7 +433,7 @@ function LightboxImage({
       if (openProgress.value !== 1 || isFlyingAway.value) {
         return
       }
-      if (Math.abs(e.velocityY) > 1000) {
+      if (Math.abs(e.velocityY) > 200) {
         isFlyingAway.value = true
         if (dismissSwipeTranslateY.value === 0) {
           // HACK: If the initial value is 0, withDecay() animation doesn't start.
