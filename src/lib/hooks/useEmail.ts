461d2a724c021bb1fe95eb5298ebe1f36864f748
Hailey <me@haileyok.com>
2024-11-11 15:54:32 -0800

    check service config

--- src/lib/hooks/useEmail.ts
+++ src/lib/hooks/useEmail.ts
@@ -1,10 +1,15 @@
+import {useServiceConfigQuery} from '#/state/queries/email-verification-required'
 import {useSession} from '#/state/session'
 import {BSKY_SERVICE} from '../constants'
 import {getHostnameFromUrl} from '../strings/url-helpers'
 
 export function useEmail() {
   const {currentAccount} = useSession()
+
+  const {data: serviceConfig} = useServiceConfigQuery()
+
   const isSelfHost =
+    serviceConfig?.checkEmailConfirmed &&
     currentAccount &&
     getHostnameFromUrl(currentAccount.service) !==
       getHostnameFromUrl(BSKY_SERVICE)
