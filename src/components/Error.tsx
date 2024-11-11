5ee809e4213b57693fc6767c86d66d73382064db
Khuddite <62555977+khuddite@users.noreply.github.com>
2024-11-11 18:21:07 -0500

    Update error component of lists to take up the full height instead of its content height (#6236)
    Co-authored-by: Samuel Newman <mozzius@protonmail.com>

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -32,7 +32,7 @@ export function Error({
   return (
     <CenteredView
       style={[
-        a.w_full,
+        a.h_full_vh,
         a.align_center,
         a.gap_5xl,
         !gtMobile && a.justify_between,
