37e0b1c15d33ae5a9541fed5dd86769219f79096
khuddite <biliie811028@hotmail.com>
2024-11-12 07:20:58 -0500

    Update the way remaining character limit is displayed on new post dialog

--- src/view/com/composer/char-progress/CharProgress.tsx
+++ src/view/com/composer/char-progress/CharProgress.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import {StyleProp, TextStyle, View, ViewStyle} from 'react-native'
 // @ts-ignore no type definition -prf
 import ProgressCircle from 'react-native-progress/Circle'
-// @ts-ignore no type definition -prf
-import ProgressPie from 'react-native-progress/Pie'
 
+// @ts-ignore no type definition -prf
+// import ProgressPie from 'react-native-progress/Pie'
 import {MAX_GRAPHEME_LENGTH} from '#/lib/constants'
 import {usePalette} from '#/lib/hooks/usePalette'
 import {atoms as a} from '#/alf'
-import {Text} from '../../util/text/Text'
 
 export function CharProgress({
   count,
@@ -27,35 +26,33 @@ export function CharProgress({
   const pal = usePalette('default')
   const textColor = count > maxLength ? '#e60000' : pal.colors.text
   const circleColor = count > maxLength ? '#e60000' : pal.colors.link
+  const remainingCount = maxLength - count
+  const shouldShowText = remainingCount <= 20
+  const shouldShowCircle = remainingCount > -10
+
   return (
     <View
       style={[a.flex_row, a.align_center, a.justify_between, a.gap_sm, style]}>
-      <Text
-        style={[
-          {color: textColor, fontVariant: ['tabular-nums']},
-          a.flex_grow,
+      <ProgressCircle
+        animated={false}
+        size={size ?? 30}
+        borderWidth={shouldShowCircle ? 1 : 0}
+        borderColor={pal.colors.border}
+        color={circleColor}
+        progress={shouldShowCircle ? Math.min(count / maxLength, 1) : 0}
+        showsText={shouldShowText}
+        formatText={() => remainingCount}
+        textStyle={[
+          {
+            color: textColor,
+            fontVariant: ['tabular-nums'],
+            fontSize: 14,
+            fontWeight: 400,
+          },
           a.text_right,
           textStyle,
-        ]}>
-        {maxLength - count}
-      </Text>
-      {count > maxLength ? (
-        <ProgressPie
-          size={size ?? 30}
-          borderWidth={4}
-          borderColor={circleColor}
-          color={circleColor}
-          progress={Math.min((count - maxLength) / maxLength, 1)}
-        />
-      ) : (
-        <ProgressCircle
-          size={size ?? 30}
-          borderWidth={1}
-          borderColor={pal.colors.border}
-          color={circleColor}
-          progress={count / maxLength}
-        />
-      )}
+        ]}
+      />
     </View>
   )
 }
