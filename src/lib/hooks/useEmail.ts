7cc6df16a1369f818baaa6a692fcb8c2ef24a39f
Hailey <me@haileyok.com>
2024-11-15 18:23:28 -0800

    fix

--- src/lib/hooks/useEmail.ts
+++ src/lib/hooks/useEmail.ts
@@ -24,7 +24,7 @@ export function useEmail() {
   const needsEmailVerification =
     !isSelfHost &&
     checkEmailConfirmed &&
-    !!currentAccount?.emailConfirmed &&
+    !currentAccount?.emailConfirmed &&
     isNewEnough
 
   return {needsEmailVerification}
