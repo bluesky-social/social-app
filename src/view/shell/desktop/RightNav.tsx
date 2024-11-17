4cc26903654dcb21abfa9d0986796115792ee7c4
Eric Bailey <git@esb.lol>
2024-11-17 13:56:59 -0600

    Clean up right nav

--- src/view/shell/desktop/RightNav.tsx
+++ src/view/shell/desktop/RightNav.tsx
@@ -1,22 +1,21 @@
-import {StyleSheet, View} from 'react-native'
+import React from 'react'
+import {View} from 'react-native'
 import {msg, Trans} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 
 import {FEEDBACK_FORM_URL, HELP_DESK_URL} from '#/lib/constants'
-import {usePalette} from '#/lib/hooks/usePalette'
 import {useWebMediaQueries} from '#/lib/hooks/useWebMediaQueries'
-import {s} from '#/lib/styles'
 import {useKawaiiMode} from '#/state/preferences/kawaii'
 import {useSession} from '#/state/session'
-import {TextLink} from '#/view/com/util/Link'
-import {Text} from '#/view/com/util/text/Text'
-import {atoms as a} from '#/alf'
+import {DesktopFeeds} from '#/view/shell/desktop/Feeds'
+import {DesktopSearch} from '#/view/shell/desktop/Search'
+import {atoms as a, useTheme, web} from '#/alf'
+import {InlineLinkText} from '#/components/Link'
 import {ProgressGuideList} from '#/components/ProgressGuide/List'
-import {DesktopFeeds} from './Feeds'
-import {DesktopSearch} from './Search'
+import {Text} from '#/components/Typography'
 
 export function DesktopRightNav({routeName}: {routeName: string}) {
-  const pal = usePalette('default')
+  const t = useTheme()
   const {_} = useLingui()
   const {hasSession, currentAccount} = useSession()
 
@@ -28,117 +27,81 @@ export function DesktopRightNav({routeName}: {routeName: string}) {
   }
 
   return (
-    <View style={[styles.rightNav, pal.view]}>
-      <View style={{paddingVertical: 20}}>
-        {routeName === 'Search' ? (
-          <View style={{marginBottom: 18}}>
-            <DesktopFeeds />
-          </View>
-        ) : (
-          <>
+    <View
+      style={[
+        web({
+          position: 'fixed',
+          left: 'calc(50vw + 300px + 20px)',
+          width: 300,
+          maxHeight: '100%',
+          overflowY: 'auto',
+        }),
+      ]}>
+      <View style={[a.py_xl]}>
+        {routeName !== 'Search' && (
+          <View style={[a.pb_lg]}>
             <DesktopSearch />
-
-            {hasSession && (
-              <>
-                <ProgressGuideList style={[{marginTop: 22, marginBottom: 8}]} />
-                <View style={[pal.border, styles.desktopFeedsContainer]}>
-                  <DesktopFeeds />
-                </View>
-              </>
-            )}
+          </View>
+        )}
+        {hasSession && (
+          <>
+            <ProgressGuideList style={[a.mt_xl, a.mb_sm]} />
+            <View
+              style={[
+                a.pb_lg,
+                a.mb_lg,
+                a.border_b,
+                t.atoms.border_contrast_low,
+              ]}>
+              <DesktopFeeds />
+            </View>
           </>
         )}
 
-        <View
-          style={[
-            styles.message,
-            {
-              paddingTop: hasSession ? 0 : 18,
-            },
-          ]}>
-          <View style={[{flexWrap: 'wrap'}, s.flexRow, a.gap_xs]}>
-            {hasSession && (
-              <>
-                <TextLink
-                  type="md"
-                  style={pal.link}
-                  href={FEEDBACK_FORM_URL({
-                    email: currentAccount?.email,
-                    handle: currentAccount?.handle,
-                  })}
-                  text={_(msg`Feedback`)}
-                />
-                <Text type="md" style={pal.textLight}>
-                  &middot;
-                </Text>
-              </>
-            )}
-            <TextLink
-              type="md"
-              style={pal.link}
-              href="https://bsky.social/about/support/privacy-policy"
-              text={_(msg`Privacy`)}
-            />
-            <Text type="md" style={pal.textLight}>
-              &middot;
-            </Text>
-            <TextLink
-              type="md"
-              style={pal.link}
-              href="https://bsky.social/about/support/tos"
-              text={_(msg`Terms`)}
-            />
-            <Text type="md" style={pal.textLight}>
-              &middot;
-            </Text>
-            <TextLink
-              type="md"
-              style={pal.link}
-              href={HELP_DESK_URL}
-              text={_(msg`Help`)}
-            />
-          </View>
-          {kawaii && (
-            <Text type="md" style={[pal.textLight, {marginTop: 12}]}>
-              <Trans>
-                Logo by{' '}
-                <TextLink
-                  type="md"
-                  href="/profile/sawaratsuki.bsky.social"
-                  text="@sawaratsuki.bsky.social"
-                  style={pal.link}
-                />
-              </Trans>
-            </Text>
+        <Text style={[a.leading_snug, t.atoms.text_contrast_low]}>
+          {hasSession && (
+            <>
+              <InlineLinkText
+                to={FEEDBACK_FORM_URL({
+                  email: currentAccount?.email,
+                  handle: currentAccount?.handle,
+                })}
+                label={_(msg`Feedback`)}>
+                {_(msg`Feedback`)}
+              </InlineLinkText>
+              {' • '}
+            </>
           )}
-        </View>
+          <InlineLinkText
+            to="https://bsky.social/about/support/privacy-policy"
+            label={_(msg`Privacy`)}>
+            {_(msg`Privacy`)}
+          </InlineLinkText>
+          {' • '}
+          <InlineLinkText
+            to="https://bsky.social/about/support/tos"
+            label={_(msg`Terms`)}>
+            {_(msg`Terms`)}
+          </InlineLinkText>
+          {' • '}
+          <InlineLinkText label={_(msg`Help`)} to={HELP_DESK_URL}>
+            {_(msg`Help`)}
+          </InlineLinkText>
+        </Text>
+
+        {kawaii && (
+          <Text style={[t.atoms.text_contrast_medium, {marginTop: 12}]}>
+            <Trans>
+              Logo by{' '}
+              <InlineLinkText
+                label={_(msg`Logo by @sawaratsuki.bsky.social`)}
+                to="/profile/sawaratsuki.bsky.social">
+                @sawaratsuki.bsky.social
+              </InlineLinkText>
+            </Trans>
+          </Text>
+        )}
       </View>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  rightNav: {
-    // @ts-ignore web only
-    position: 'fixed',
-    // @ts-ignore web only
-    left: 'calc(50vw + 300px + 20px)',
-    width: 300,
-    maxHeight: '100%',
-    overflowY: 'auto',
-  },
-
-  message: {
-    paddingVertical: 18,
-    paddingHorizontal: 12,
-  },
-  messageLine: {
-    marginBottom: 10,
-  },
-  desktopFeedsContainer: {
-    borderTopWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    marginTop: 18,
-    marginBottom: 18,
-  },
-})
