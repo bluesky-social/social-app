a4a7a84a141c9c6761078e8fd361f949cd8a26a9
Paul Coroneos <paul.coroneos@icloud.com>
2024-11-18 06:58:16 -0600

    allow unused vars to be prefixed with _ and not fail lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -99,6 +99,7 @@ export default tseslint.config(
           ],
         },
       ],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
     settings: {
       react: {
