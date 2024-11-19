8f3b2bcc71ce73fd2edd6d5e45fa91743beae644
khuddite <biliie811028@hotmail.com>
2024-11-19 16:23:16 -0500

    Manual truncation & identify factor points for each lang

--- src/view/com/util/numeric/format.ts
+++ src/view/com/util/numeric/format.ts
@@ -1,12 +1,54 @@
-import type {I18n} from '@lingui/core'
+import {I18n} from '@lingui/core'
+
+import {AppLanguage} from '#/locale/languages'
+
+const truncateRounding = (num: number, factors: Array<number>): number => {
+  for (const factor of factors.reverse()) {
+    if (num >= 10 ** factor) {
+      if (factor === 10) {
+        // Exception for ES and CA langs
+        return Math.floor(num / 10 ** (factor - 2)) * 10 ** (factor - 2)
+      }
+      return Math.floor(num / 10 ** (factor - 1)) * 10 ** (factor - 1)
+    }
+  }
+  return num
+}
 
 export const formatCount = (i18n: I18n, num: number) => {
-  return i18n.number(num, {
+  const koFactors = [3, 4, 8]
+  const hiFactors = [3, 5, 7, 9]
+  const esCaFactors = [3, 6, 10]
+  const itDeFactors = [6, 9]
+  const jaZhFactors = [4, 8]
+  const normalFactors = [3, 6, 9]
+
+  let truncatedNum: number
+
+  if (i18n.locale === AppLanguage.hi) {
+    truncatedNum = truncateRounding(num, hiFactors)
+  } else if (i18n.locale === AppLanguage.ko) {
+    truncatedNum = truncateRounding(num, koFactors)
+  } else if (
+    [AppLanguage.es, AppLanguage.ca].includes(i18n.locale as AppLanguage)
+  ) {
+    truncatedNum = truncateRounding(num, esCaFactors)
+  } else if (
+    [AppLanguage.ja, AppLanguage.zh_CN, AppLanguage.zh_TW].includes(
+      i18n.locale as AppLanguage,
+    )
+  ) {
+    truncatedNum = truncateRounding(num, jaZhFactors)
+  } else if (
+    [AppLanguage.it, AppLanguage.de].includes(i18n.locale as AppLanguage)
+  ) {
+    truncatedNum = truncateRounding(num, itDeFactors)
+  } else {
+    truncatedNum = truncateRounding(num, normalFactors)
+  }
+
+  return i18n.number(truncatedNum, {
     notation: 'compact',
     maximumFractionDigits: 1,
-    // `1,953` shouldn't be rounded up to 2k, it should be truncated.
-    // @ts-expect-error: `roundingMode` doesn't seem to be in the typings yet
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#roundingmode
-    roundingMode: 'trunc',
   })
 }
