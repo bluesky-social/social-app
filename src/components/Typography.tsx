7ca57365797c88a1a4ec1f94191c20d859c81409
Dan Abramov <dan.abramov@gmail.com>
2024-11-21 20:01:53 +0000

    Separate non-components from components

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {StyleProp, TextProps as RNTextProps, TextStyle} from 'react-native'
+import {TextProps as RNTextProps} from 'react-native'
 import {UITextView} from 'react-native-uitextview'
 import createEmojiRegex from 'emoji-regex'
 
 import {logger} from '#/logger'
-import {isIOS, isNative} from '#/platform/detection'
-import {Alf, applyFonts, atoms, flatten, useAlf, useTheme, web} from '#/alf'
+import {isIOS} from '#/platform/detection'
+import {atoms, flatten, useAlf, useTheme, web} from '#/alf'
+import {normalizeTextStyles} from '#/alf/typography'
 import {IS_DEV} from '#/env'
 
 export type StringChild = string | (string | null)[]
@@ -88,55 +89,6 @@ export function renderChildrenWithEmoji(
 }
 
 /**
- * Util to calculate lineHeight from a text size atom and a leading atom
- *
- * Example:
- *   `leading(atoms.text_md, atoms.leading_normal)` // => 24
- */
-export function leading<
-  Size extends {fontSize?: number},
-  Leading extends {lineHeight?: number},
->(textSize: Size, leading: Leading) {
-  const size = textSize?.fontSize || atoms.text_md.fontSize
-  const lineHeight = leading?.lineHeight || atoms.leading_normal.lineHeight
-  return Math.round(size * lineHeight)
-}
-
-/**
- * Ensures that `lineHeight` defaults to a relative value of `1`, or applies
- * other relative leading atoms.
- *
- * If the `lineHeight` value is > 2, we assume it's an absolute value and
- * returns it as-is.
- */
-export function normalizeTextStyles(
-  styles: StyleProp<TextStyle>,
-  {
-    fontScale,
-    fontFamily,
-  }: {
-    fontScale: number
-    fontFamily: Alf['fonts']['family']
-  } & Pick<Alf, 'flags'>,
-) {
-  const s = flatten(styles)
-  // should always be defined on these components
-  s.fontSize = (s.fontSize || atoms.text_md.fontSize) * fontScale
-
-  if (s?.lineHeight) {
-    if (s.lineHeight !== 0 && s.lineHeight <= 2) {
-      s.lineHeight = Math.round(s.fontSize * s.lineHeight)
-    }
-  } else if (!isNative) {
-    s.lineHeight = s.fontSize
-  }
-
-  applyFonts(s, fontFamily)
-
-  return s
-}
-
-/**
  * Our main text component. Use this most of the time.
  */
 export function Text({
@@ -183,7 +135,7 @@ export function Text({
   )
 }
 
-export function createHeadingElement({level}: {level: number}) {
+function createHeadingElement({level}: {level: number}) {
   return function HeadingElement({style, ...rest}: TextProps) {
     const attr =
       web({
