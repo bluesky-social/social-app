91e71460731b41f5e66be2790e10d4dbdde36352
Samuel Newman <mozzius@protonmail.com>
2024-11-26 14:20:18 +0000

    add comment

--- src/view/com/util/Views.tsx
+++ src/view/com/util/Views.tsx
@@ -5,6 +5,11 @@ import Animated from 'react-native-reanimated'
 import {FlatListPropsWithLayout} from 'react-native-reanimated'
 
 // If you explode these into functions, don't forget to forwardRef!
+
+/**
+ * Avoid using `FlatList_INTERNAL` and use `List` where possible.
+ * The types are a bit wrong on `FlatList_INTERNAL`
+ */
 export const FlatList_INTERNAL = Animated.FlatList
 export type FlatList_INTERNAL<ItemT = any> = Omit<
   FlatListComponent<ItemT, FlatListPropsWithLayout<ItemT>>,
