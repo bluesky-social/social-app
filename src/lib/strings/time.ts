c451b7be07f183dd32c0a749cda8e1817364f980
Frudrax Cheng <i@cynosura.one>
2024-11-26 13:15:15 +0800

    Fix timezone

--- src/lib/strings/time.ts
+++ src/lib/strings/time.ts
@@ -1,8 +1,16 @@
 import {DateTimeFormat} from '@formatjs/intl-datetimeformat'
 import {I18n} from '@lingui/core'
 
-import {isNative,isWeb} from '#/platform/detection'
+import {isNative, isWeb} from '#/platform/detection'
 
+/**
+ * Formats the given date into a localized string.
+ *
+ * This function chooses different formatting approaches based on the platform:
+ * - On the Web, it uses the `i18n.date` method to format the date.
+ * - On Native, it adjusts the time zone offset and uses `DateTimeFormat` for formatting.
+ *
+ */
 export function niceDate(i18n: I18n, date: number | string | Date) {
   const d = new Date(date)
 
@@ -14,6 +22,9 @@ export function niceDate(i18n: I18n, date: number | string | Date) {
   }
 
   if (isNative) {
+    const timeZoneOffsetInMinutes = new Date().getTimezoneOffset()
+    d.setMinutes(d.getMinutes() - timeZoneOffsetInMinutes)
+
     const dateFormatter = new DateTimeFormat(i18n.locale, {
       dateStyle: 'long',
       timeStyle: 'short',
