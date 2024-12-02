8ee1e6a17c824ea6af5a14081bba62cbd0869680
Samuel Newman <mozzius@protonmail.com>
2024-12-02 20:10:33 +0000

    wip exploration

--- src/screens/Settings/AppIconSettings.tsx
+++ src/screens/Settings/AppIconSettings.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import {Alert, View} from 'react-native'
+import {Alert, ImageSourcePropType, View} from 'react-native'
 import {Image} from 'expo-image'
-import {msg} from '@lingui/macro'
+import {msg, Trans} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 import * as AppIcon from '@mozzius/expo-dynamic-app-icon'
 import {NativeStackScreenProps} from '@react-navigation/native-stack'
@@ -9,9 +9,9 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack'
 import {PressableScale} from '#/lib/custom-animations/PressableScale'
 import {CommonNavigatorParams} from '#/lib/routes/types'
 import {isAndroid} from '#/platform/detection'
-import {atoms as a, platform} from '#/alf'
+import {atoms as a, platform, useTheme} from '#/alf'
 import * as Layout from '#/components/Layout'
-import {Text} from '#/components/Typography'
+import * as SettingsList from './components/SettingsList'
 
 type Props = NativeStackScreenProps<CommonNavigatorParams, 'AppIconSettings'>
 export function AppIconSettingsScreen({}: Props) {
@@ -21,104 +21,93 @@ export function AppIconSettingsScreen({}: Props) {
   return (
     <Layout.Screen>
       <Layout.Header title={_('App Icon')} />
-      <Layout.Content
-        contentContainerStyle={[a.py_2xl, a.px_xl, {paddingBottom: 100}]}>
-        <Text style={[a.text_lg, a.font_heavy]}>Defaults</Text>
-        <View style={[a.flex_row, a.flex_wrap]}>
-          {sets.defaults.map(icon => (
-            <View
-              style={[{width: '50%'}, a.py_lg, a.px_xs, a.align_center]}
-              key={icon.id}>
-              <PressableScale
-                accessibilityLabel={icon.name}
-                accessibilityHint={_(msg`Tap to change app icon`)}
-                targetScale={0.95}
-                onPress={() => AppIcon.setAppIcon(icon.id)}>
-                <Image
-                  source={platform({
-                    ios: icon.iosImage(),
-                    android: icon.androidImage(),
-                  })}
-                  style={[
-                    {width: 100, height: 100},
-                    platform({
-                      ios: {borderRadius: 20},
-                      android: a.rounded_full,
-                    }),
-                    a.curve_continuous,
-                  ]}
-                  accessibilityIgnoresInvertColors
-                />
-              </PressableScale>
-              <Text style={[a.text_center, a.font_bold, a.text_md, a.mt_md]}>
-                {icon.name}
-              </Text>
+      <Layout.Content>
+        <SettingsList.Container>
+          <SettingsList.Group iconInset={false}>
+            <SettingsList.ItemText style={[a.font_heavy]}>
+              <Trans>Default</Trans>
+            </SettingsList.ItemText>
+            <View style={[a.flex_row, a.flex_wrap, a.gap_lg]}>
+              {sets.defaults.map(icon => (
+                <Icon icon={icon} key={icon.id} />
+              ))}
             </View>
-          ))}
-        </View>
-
-        <Text style={[a.text_lg, a.font_heavy]}>Bluesky+</Text>
-        <View style={[a.flex_row, a.flex_wrap]}>
-          {sets.core.map(icon => (
-            <View
-              style={[{width: '50%'}, a.py_lg, a.px_xs, a.align_center]}
-              key={icon.id}>
-              <PressableScale
-                accessibilityLabel={icon.name}
-                accessibilityHint={_(msg`Tap to change app icon`)}
-                targetScale={0.95}
-                onPress={() => {
-                  if (isAndroid) {
-                    Alert.alert(
-                      _(msg`Change app icon to "${icon.name}"`),
-                      _(msg`The app will be restarted`),
-                      [
-                        {
-                          text: _(msg`Cancel`),
-                          style: 'cancel',
-                        },
-                        {
-                          text: _(msg`OK`),
-                          onPress: () => {
-                            AppIcon.setAppIcon(icon.id)
-                          },
-                          style: 'default',
-                        },
-                      ],
-                    )
-                  } else {
-                    AppIcon.setAppIcon(icon.id)
-                  }
-                }}>
-                <Image
-                  source={platform({
-                    ios: icon.iosImage(),
-                    android: icon.androidImage(),
-                  })}
-                  style={[
-                    {width: 100, height: 100},
-                    platform({
-                      ios: {borderRadius: 20},
-                      android: a.rounded_full,
-                    }),
-                    a.curve_continuous,
-                    a.shadow_lg,
-                  ]}
-                  accessibilityIgnoresInvertColors
-                />
-              </PressableScale>
-              <Text
-                style={[a.text_center, a.font_bold, a.text_md, a.mt_md]}
-                // for Classic™
-                emoji>
-                {icon.name}
-              </Text>
+          </SettingsList.Group>
+          <SettingsList.Group iconInset={false}>
+            <SettingsList.ItemText style={[a.font_heavy]}>
+              <Trans>Bluesky+</Trans>
+            </SettingsList.ItemText>
+            <View style={[a.flex_row, a.flex_wrap, a.gap_lg]}>
+              {sets.core.map(icon => (
+                <Icon icon={icon} key={icon.id} />
+              ))}
             </View>
-          ))}
-        </View>
+          </SettingsList.Group>
+        </SettingsList.Container>
       </Layout.Content>
     </Layout.Screen>
   )
+}
+
+function Icon({icon}: {icon: AppIconSet}) {
+  const {_} = useLingui()
+  const t = useTheme()
+  return (
+    <View style={[{minWidth: '20%'}, a.align_center]}>
+      <PressableScale
+        accessibilityLabel={icon.name}
+        accessibilityHint={_(msg`Tap to change app icon`)}
+        targetScale={0.95}
+        onPress={() => {
+          if (isAndroid) {
+            Alert.alert(
+              _(msg`Change app icon to "${icon.name}"`),
+              _(msg`The app will be restarted`),
+              [
+                {
+                  text: _(msg`Cancel`),
+                  style: 'cancel',
+                },
+                {
+                  text: _(msg`OK`),
+                  onPress: () => {
+                    AppIcon.setAppIcon(icon.id)
+                  },
+                  style: 'default',
+                },
+              ],
+            )
+          } else {
+            AppIcon.setAppIcon(icon.id)
+          }
+        }}>
+        <Image
+          source={platform({
+            ios: icon.iosImage(),
+            android: icon.androidImage(),
+          })}
+          style={[
+            {width: 50, height: 50},
+            platform({
+              ios: {borderRadius: 10},
+              android: a.rounded_full,
+            }),
+            a.curve_continuous,
+            t.atoms.border_contrast_medium,
+            a.border,
+          ]}
+          accessibilityIgnoresInvertColors
+        />
+      </PressableScale>
+    </View>
+  )
+}
+
+type AppIconSet = {
+  id: string
+  name: string
+  iosImage: () => ImageSourcePropType
+  androidImage: () => ImageSourcePropType
 }
 
 function useAppIconSets() {
@@ -146,7 +135,7 @@ function useAppIconSets() {
           return require(`../../../assets/app-icons/android_icon_default_dark.png`)
         },
       },
-    ]
+    ] satisfies AppIconSet[]
 
     /**
      * Bluesky+
@@ -242,7 +231,7 @@ function useAppIconSets() {
           return require(`../../../assets/app-icons/android_icon_core_classic.png`)
         },
       },
-    ]
+    ] satisfies AppIconSet[]
 
     return {
       defaults,
