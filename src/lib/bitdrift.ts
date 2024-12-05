a26f97218f38a734124f84b6666c137e0d0d8ba4
Snow Pettersen <snowp@lyft.com>
2024-12-05 14:23:37 -0800

    use the new url

--- src/lib/bitdrift.ts
+++ src/lib/bitdrift.ts
@@ -3,5 +3,5 @@ import {init} from '@bitdrift/react-native'
 const BITDRIFT_API_KEY = process.env.BITDRIFT_API_KEY
 
 if (BITDRIFT_API_KEY) {
-  init(BITDRIFT_API_KEY, {url: 'https://bsky.api.bitdrift.io'})
+  init(BITDRIFT_API_KEY, {url: 'https://api-bsky.bitdrift.io'})
 }
