691f9aa594f9e8e90ad575a9ff7ce5c4054a16d4
Dan Abramov <dan.abramov@gmail.com>
2024-12-06 03:22:55 +0000

    Make sizing work

--- app.config.js
+++ app.config.js
@@ -264,11 +264,11 @@ module.exports = function (config) {
             android: {
               backgroundColor: '#0c7cff',
               image: './assets/splash-android-icon.png',
-              imageWidth: 180,
+              imageWidth: 100,
               dark: {
                 backgroundColor: '#0c2a49',
                 image: './assets/splash-android-icon-dark.png',
-                imageWidth: 180,
+                imageWidth: 100,
               },
             },
           },
