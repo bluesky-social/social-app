b798d4fb50276bdc705d54071717b203e87f3ced
Samuel Newman <mozzius@protonmail.com>
2024-12-11 20:31:08 +0000

    exact count for following

--- src/view/screens/ProfileFollows.tsx
+++ src/view/screens/ProfileFollows.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
-import {msg} from '@lingui/macro'
+import {Trans} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 import {useFocusEffect} from '@react-navigation/native'
 
 import {CommonNavigatorParams, NativeStackScreenProps} from '#/lib/routes/types'
-import {isWeb} from '#/platform/detection'
+import {sanitizeDisplayName} from '#/lib/strings/display-names'
+import {useProfileQuery} from '#/state/queries/profile'
+import {useResolveDidQuery} from '#/state/queries/resolve-uri'
 import {useSetMinimalShellMode} from '#/state/shell'
 import {ProfileFollows as ProfileFollowsComponent} from '#/view/com/profile/ProfileFollows'
-import {ViewHeader} from '#/view/com/util/ViewHeader'
-import {CenteredView} from '#/view/com/util/Views'
 import * as Layout from '#/components/Layout'
 
 type Props = NativeStackScreenProps<CommonNavigatorParams, 'ProfileFollows'>
 export const ProfileFollowsScreen = ({route}: Props) => {
   const {name} = route.params
   const setMinimalShellMode = useSetMinimalShellMode()
-  const {_} = useLingui()
+  const {i18n} = useLingui()
+
+  const {data: resolvedDid} = useResolveDidQuery(name)
+  const {data: profile} = useProfileQuery({
+    did: resolvedDid,
+  })
 
   useFocusEffect(
     React.useCallback(() => {
@@ -25,10 +30,27 @@ export const ProfileFollowsScreen = ({route}: Props) => {
 
   return (
     <Layout.Screen testID="profileFollowsScreen">
-      <CenteredView sideBorders={true}>
-        <ViewHeader title={_(msg`Following`)} showBorder={!isWeb} />
+      <Layout.Header.Outer>
+        <Layout.Header.BackButton />
+        <Layout.Header.Content>
+          {profile && (
+            <>
+              <Layout.Header.TitleText>
+                {sanitizeDisplayName(profile.displayName || profile.handle)}
+              </Layout.Header.TitleText>
+              <Layout.Header.SubtitleText>
+                <Trans>
+                  {i18n.number(profile.followsCount ?? 0)} following
+                </Trans>
+              </Layout.Header.SubtitleText>
+            </>
+          )}
+        </Layout.Header.Content>
+        <Layout.Header.Slot />
+      </Layout.Header.Outer>
+      <Layout.Center>
         <ProfileFollowsComponent name={name} />
-      </CenteredView>
+      </Layout.Center>
     </Layout.Screen>
   )
 }
