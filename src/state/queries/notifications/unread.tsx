22f2502c8cd845f8a277386491aeaa8b3b11015b
Dan Abramov <dan.abramov@gmail.com>
2024-12-11 23:46:24 +0000

    Fix types

--- src/state/queries/notifications/unread.tsx
+++ src/state/queries/notifications/unread.tsx
@@ -145,6 +145,7 @@ export function Provider({children}: React.PropsWithChildren<{}>) {
             limit: 40,
             queryClient,
             moderationOpts,
+            reasons: [],
 
             // only fetch subjects when the page is going to be used
             // in the notifications query, otherwise skip it
