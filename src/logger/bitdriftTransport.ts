ca1f5bebba7d71a3f9ad8c37c14f38b6185949e6
Dan Abramov <dan.abramov@gmail.com>
2024-12-12 04:55:59 +0000

    Hoist log functions

--- src/logger/bitdriftTransport.ts
+++ src/logger/bitdriftTransport.ts
@@ -8,17 +8,16 @@ import {
 import {LogLevel, Transport} from './types'
 
 export function createBitdriftTransport(): Transport {
-  return (level, message) => {
-    const log = (
-      {
-        [LogLevel.Debug]: bdDebug,
-        [LogLevel.Info]: bdInfo,
-        [LogLevel.Log]: bdInfo,
-        [LogLevel.Warn]: bdWarn,
-        [LogLevel.Error]: bdError,
-      } as const
-    )[level]
+  const logFunctions = {
+    [LogLevel.Debug]: bdDebug,
+    [LogLevel.Info]: bdInfo,
+    [LogLevel.Log]: bdInfo,
+    [LogLevel.Warn]: bdWarn,
+    [LogLevel.Error]: bdError,
+  } as const
 
+  return (level, message) => {
+    const log = logFunctions[level]
     log(message.toString())
   }
 }
