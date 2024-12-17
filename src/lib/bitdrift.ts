88c49eee4052f442f69625b1c6f61eb64b40cd7b
Dan Abramov <dan.abramov@gmail.com>
2024-12-17 12:03:11 +0000

    Fix tests

--- src/lib/bitdrift.ts
+++ src/lib/bitdrift.ts
@@ -6,16 +6,18 @@ import {initPromise} from './statsig/statsig'
 
 const BITDRIFT_API_KEY = process.env.BITDRIFT_API_KEY
 
-initPromise.then(() => {
-  let isEnabled = false
-  try {
-    if (Statsig.checkGate('enable_bitdrift')) {
-      isEnabled = true
+if (process.env.NODE_ENV !== 'test') {
+  initPromise.then(() => {
+    let isEnabled = false
+    try {
+      if (Statsig.checkGate('enable_bitdrift')) {
+        isEnabled = true
+      }
+    } catch (e) {
+      // Statsig may complain about it being called too early.
     }
-  } catch (e) {
-    // Statsig may complain about it being called too early.
-  }
-  if (isEnabled && BITDRIFT_API_KEY) {
-    init(BITDRIFT_API_KEY, {url: 'https://api-bsky.bitdrift.io'})
-  }
-})
+    if (isEnabled && BITDRIFT_API_KEY) {
+      init(BITDRIFT_API_KEY, {url: 'https://api-bsky.bitdrift.io'})
+    }
+  })
+}
