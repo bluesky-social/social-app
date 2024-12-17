0a549b82a78a04c04d122f4af20ff0ec4e48b74e
Dan Abramov <dan.abramov@gmail.com>
2024-12-17 12:11:06 +0000

    Consolidate mocks, fix tests

--- src/lib/bitdrift.ts
+++ src/lib/bitdrift.ts
@@ -6,18 +6,16 @@ import {initPromise} from './statsig/statsig'
 
 const BITDRIFT_API_KEY = process.env.BITDRIFT_API_KEY
 
-if (process.env.NODE_ENV !== 'test') {
-  initPromise.then(() => {
-    let isEnabled = false
-    try {
-      if (Statsig.checkGate('enable_bitdrift')) {
-        isEnabled = true
-      }
-    } catch (e) {
-      // Statsig may complain about it being called too early.
+initPromise.then(() => {
+  let isEnabled = false
+  try {
+    if (Statsig.checkGate('enable_bitdrift')) {
+      isEnabled = true
     }
-    if (isEnabled && BITDRIFT_API_KEY) {
-      init(BITDRIFT_API_KEY, {url: 'https://api-bsky.bitdrift.io'})
-    }
-  })
-}
+  } catch (e) {
+    // Statsig may complain about it being called too early.
+  }
+  if (isEnabled && BITDRIFT_API_KEY) {
+    init(BITDRIFT_API_KEY, {url: 'https://api-bsky.bitdrift.io'})
+  }
+})
