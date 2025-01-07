e2c360044718086699e2b6251541e575f5ad60a5
Eric Bailey <git@esb.lol>
2025-01-07 10:42:10 -0600

    Use dangerous util, we can trust post records

--- src/view/com/post/Post.tsx
+++ src/view/com/post/Post.tsx
@@ -28,6 +28,7 @@ import {atoms as a} from '#/alf'
 import {ProfileHoverCard} from '#/components/ProfileHoverCard'
 import {RichText} from '#/components/RichText'
 import {SubtleWebHover} from '#/components/SubtleWebHover'
+import * as atp from '#/types/atproto'
 import {ContentHider} from '../../../components/moderation/ContentHider'
 import {LabelsOnMyPost} from '../../../components/moderation/LabelsOnMe'
 import {PostAlerts} from '../../../components/moderation/PostAlerts'
@@ -53,7 +54,12 @@ export function Post({
   const moderationOpts = useModerationOpts()
   const record = useMemo<AppBskyFeedPost.Record | undefined>(
     () =>
-      AppBskyFeedPost.isValidRecord(post.record) ? post.record : undefined,
+      atp.dangerousIsType<AppBskyFeedPost.Record>(
+        post.record,
+        AppBskyFeedPost.isRecord,
+      )
+        ? post.record
+        : undefined,
     [post],
   )
   const postShadowed = usePostShadow(post)
