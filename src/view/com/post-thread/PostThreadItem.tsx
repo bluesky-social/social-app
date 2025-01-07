44cb25d3a18f9614395e092d9683d04956f18d4a
Eric Bailey <git@esb.lol>
2025-01-07 10:40:40 -0600

    Use dangerous util

--- src/view/com/post-thread/PostThreadItem.tsx
+++ src/view/com/post-thread/PostThreadItem.tsx
@@ -58,6 +58,7 @@ import {RichText} from '#/components/RichText'
 import {SubtleWebHover} from '#/components/SubtleWebHover'
 import {Text} from '#/components/Typography'
 import {WhoCanReply} from '#/components/WhoCanReply'
+import * as atp from '#/types/atproto'
 
 export function PostThreadItem({
   post,
@@ -785,7 +786,10 @@ function BackdatedPostIndicator({post}: {post: AppBskyFeedDefs.PostView}) {
   const control = Prompt.usePromptControl()
 
   const indexedAt = new Date(post.indexedAt)
-  const createdAt = AppBskyFeedPost.isValidRecord(post.record)
+  const createdAt = atp.dangerousIsType<AppBskyFeedPost.Record>(
+    post.record,
+    AppBskyFeedPost.isRecord,
+  )
     ? new Date(post.record.createdAt)
     : new Date(post.indexedAt)
 
