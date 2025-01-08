0d28fd5834745689c5e0e368cd490a7d5bb0eb66
Eric Bailey <git@esb.lol>
2025-01-08 09:29:27 -0600

    Use package exports
    Co-authored-by: Matthieu Sieben <matthieusieben@users.noreply.github.com>

--- src/types/atproto/starterPack.ts
+++ src/types/atproto/starterPack.ts
@@ -1,17 +1,7 @@
 import {AppBskyGraphDefs} from '@atproto/api'
 
-export {
-  /**
-   * Renamed to clarify source of this util, but directly aliases the original.
-   * {@link AppBskyGraphDefs.isStarterPackViewBasic}
-   */
-  isStarterPackViewBasic as isBasicView,
-  /**
-   * Renamed to clarify source of this util, but directly aliases the original.
-   * {@link AppBskyGraphDefs.isStarterPackView}
-   */
-  isStarterPackView as isView,
-} from '@atproto/api/dist/client/types/app/bsky/graph/defs'
+export const isBasicView = AppBskyGraphDefs.isStarterPackViewBasic
+export const isView = AppBskyGraphDefs.isStarterPackView
 
 /**
  * Matches any starter pack view exported by our SDK
