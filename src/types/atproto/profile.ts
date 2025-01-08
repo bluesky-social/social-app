2f0dd92466506dd9d782b2d9492a0eb8cf1b6d98
Eric Bailey <git@esb.lol>
2025-01-08 09:31:22 -0600

    Use package exports

--- src/types/atproto/profile.ts
+++ src/types/atproto/profile.ts
@@ -1,22 +1,8 @@
 import {AppBskyActorDefs, ChatBskyActorDefs} from '@atproto/api'
 
-export {
-  /**
-   * Renamed to clarify source of this util, but directly aliases the original.
-   * {@link AppBskyActorDefs.isProfileViewBasic}
-   */
-  isProfileViewBasic as isBasicView,
-  /**
-   * Renamed to clarify source of this util, but directly aliases the original.
-   * {@link AppBskyActorDefs.isProfileViewDetailed}
-   */
-  isProfileViewDetailed as isDetailedView,
-  /**
-   * Renamed to clarify source of this util, but directly aliases the original.
-   * {@link AppBskyActorDefs.isProfileView}
-   */
-  isProfileView as isView,
-} from '@atproto/api/dist/client/types/app/bsky/actor/defs'
+export const isBasicView = AppBskyActorDefs.isProfileViewBasic
+export const isDetailedView = AppBskyActorDefs.isProfileViewDetailed
+export const isView = AppBskyActorDefs.isProfileView
 
 /**
  * Matches any profile view exported by our SDK
