e01fc2fbaf5a381c273bf8dbe762882604301c0b
Eric Bailey <git@esb.lol>
2025-01-15 13:35:00 -0600

    Only apply CDN fix to webpack in prod (#7461)

--- webpack.config.js
+++ webpack.config.js
@@ -28,10 +28,10 @@ module.exports = async function (env, argv) {
   ]
   if (env.mode === 'development') {
     config.plugins.push(new ReactRefreshWebpackPlugin())
+  } else {
+    // Support static CDN for chunks
+    config.output.publicPath = 'auto'
   }
-
-  // Support static CDN for chunks
-  config.output.publicPath = 'auto'
 
   if (GENERATE_STATS || OPEN_ANALYZER) {
     config.plugins.push(
