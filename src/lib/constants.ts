3dd306c8b8903f3568df533b44237b21de1266cd
Hailey <me@haileyok.com>
2025-01-17 12:19:16 -0800

    tweaks to constants (#7478)
    * add did
    
    * use correct did
    
    * typo
    
    * tweak

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -63,6 +63,21 @@ export function IS_PROD_SERVICE(url?: string) {
 export const PROD_DEFAULT_FEED = (rkey: string) =>
   `at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/${rkey}`
 
+export const STAGING_DEFAULT_FEED = (rkey: string) =>
+  `at://did:plc:yofh3kx63drvfljkibw5zuxo/app.bsky.feed.generator/${rkey}`
+
+export const PROD_FEEDS = [
+  `feedgen|${PROD_DEFAULT_FEED('whats-hot')}`,
+  `feedgen|${PROD_DEFAULT_FEED('thevids')}`,
+]
+
+export const STAGING_FEEDS = [
+  `feedgen|${STAGING_DEFAULT_FEED('whats-hot')}`,
+  `feedgen|${STAGING_DEFAULT_FEED('thevids')}`,
+]
+
+export const FEEDBACK_FEEDS = [...PROD_FEEDS, ...STAGING_FEEDS]
+
 export const POST_IMG_MAX = {
   width: 2000,
   height: 2000,
