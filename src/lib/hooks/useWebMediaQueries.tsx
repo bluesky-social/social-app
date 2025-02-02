fbca5e49f3baed3872339e76974856528e878a6e
Christian Schmidt <github@chsc.dk>
2025-02-02 14:28:04 +0100

    Separate files for web and native

--- src/lib/hooks/useWebMediaQueries.tsx
+++ src/lib/hooks/useWebMediaQueries.tsx
@@ -1,21 +1,9 @@
-import {useMediaQuery} from 'react-responsive'
-
-import {isNative} from '#/platform/detection'
-
 export function useWebMediaQueries() {
-  const isDesktop = useMediaQuery({query: '(width >= 1300px)'})
-  const isTablet = useMediaQuery({query: '(800px <= width < 1300px)'})
-  const isMobile = useMediaQuery({query: '(width < 800px)'})
-  const isTabletOrMobile = isMobile || isTablet
-  const isTabletOrDesktop = isDesktop || isTablet
-  if (isNative) {
-    return {
-      isMobile: true,
-      isTablet: false,
-      isTabletOrMobile: true,
-      isTabletOrDesktop: false,
-      isDesktop: false,
-    }
+  return {
+    isMobile: true,
+    isTablet: false,
+    isTabletOrMobile: true,
+    isTabletOrDesktop: false,
+    isDesktop: false,
   }
-  return {isMobile, isTablet, isTabletOrMobile, isTabletOrDesktop, isDesktop}
 }
