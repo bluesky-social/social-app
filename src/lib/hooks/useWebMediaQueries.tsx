9abaa4de0df8bae258b1c7cb235e34ad937d053a
Christian Schmidt <github@chsc.dk>
2025-02-02 13:02:18 +0100

    Use contiguous width ranges

--- src/lib/hooks/useWebMediaQueries.tsx
+++ src/lib/hooks/useWebMediaQueries.tsx
@@ -3,9 +3,9 @@ import {useMediaQuery} from 'react-responsive'
 import {isNative} from '#/platform/detection'
 
 export function useWebMediaQueries() {
-  const isDesktop = useMediaQuery({minWidth: 1300})
-  const isTablet = useMediaQuery({minWidth: 800, maxWidth: 1300 - 1})
-  const isMobile = useMediaQuery({maxWidth: 800 - 1})
+  const isDesktop = useMediaQuery({query: '(width >= 1300px)'})
+  const isTablet = useMediaQuery({query: '(800px <= width < 1300px)'})
+  const isMobile = useMediaQuery({query: '(width < 800px)'})
   const isTabletOrMobile = isMobile || isTablet
   const isTabletOrDesktop = isDesktop || isTablet
   if (isNative) {
