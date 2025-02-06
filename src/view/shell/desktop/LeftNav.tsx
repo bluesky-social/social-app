500a3b0cf4b29bc976145eae9678dfc5367c8dda
Samuel Newman <mozzius@protonmail.com>
2025-02-06 21:27:22 +0000

    adjust left nav spacing

--- src/view/shell/desktop/LeftNav.tsx
+++ src/view/shell/desktop/LeftNav.tsx
@@ -557,7 +557,6 @@ export function DesktopLeftNav() {
         a.px_xl,
         styles.leftNav,
         leftNavMinimal && styles.leftNavMinimal,
-        leftNavMinimal && !centerColumnOffset && {width: 76},
         {
           transform: [
             {translateX: centerColumnOffset ? -450 : -300},
