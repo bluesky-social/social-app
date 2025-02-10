c55d795f804264f5ae1c71728f7921f57fe2d764
Christian Schmidt <github@chsc.dk>
2025-02-10 22:17:49 +0100

    Exclude pins when polling author feed for updates

--- src/lib/api/feed/author.ts
+++ src/lib/api/feed/author.ts
@@ -33,6 +33,7 @@ export class AuthorFeedAPI implements FeedAPI {
     const res = await this.agent.getAuthorFeed({
       ...this.params,
       limit: 1,
+      includePins: false,
     })
     return res.data.feed[0]
   }
