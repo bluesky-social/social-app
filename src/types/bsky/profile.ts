be40a0322927da37eff46e0c34fa14de2e543a92
Eric Bailey <git@esb.lol>
2025-02-11 10:32:08 -0600

    Remove unused code

--- src/types/bsky/profile.ts
+++ src/types/bsky/profile.ts
@@ -1,9 +1,5 @@
 import {AppBskyActorDefs, ChatBskyActorDefs} from '@atproto/api'
 
-export const isBasicView = AppBskyActorDefs.isProfileViewBasic
-export const isDetailedView = AppBskyActorDefs.isProfileViewDetailed
-export const isView = AppBskyActorDefs.isProfileView
-
 /**
  * Matches any profile view exported by our SDK
  */
