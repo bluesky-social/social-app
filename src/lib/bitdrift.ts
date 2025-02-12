09d933469b69f370244e0f766c1b1524af219d8a
Eric Bailey <git@esb.lol>
2025-02-12 16:10:37 -0600

    Temp debug

--- src/lib/bitdrift.ts
+++ src/lib/bitdrift.ts
@@ -2,6 +2,7 @@ import {init, SessionStrategy} from '@bitdrift/react-native'
 import {Statsig} from 'statsig-react-native-expo'
 
 import {initPromise} from '#/lib/statsig/statsig'
+import {logger} from '#/logger'
 
 export {debug, error, info, warn} from '@bitdrift/react-native'
 
@@ -18,10 +19,13 @@ initPromise.then(() => {
     // Statsig may complain about it being called too early.
   }
   if (isEnabled && BITDRIFT_API_KEY) {
+    logger.info('Bitdrift is enabled')
     init(BITDRIFT_API_KEY, SessionStrategy.Activity, {
       url: 'https://api-bsky.bitdrift.io',
       // TODO gate
       enableNetworkInstrumentation: true, // Only effects iOS, Android instrumentation is set via Gradle Plugin
     })
+  } else {
+    logger.info('Bitdrift is disabled')
   }
 })
