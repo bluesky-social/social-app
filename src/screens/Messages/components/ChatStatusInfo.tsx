afb7e3f906f8f96272cbb35c25b7e965952c9ac0
Samuel Newman <mozzius@protonmail.com>
2025-02-24 15:58:26 +0000

    add prompt to delete

--- src/screens/Messages/components/ChatStatusInfo.tsx
+++ src/screens/Messages/components/ChatStatusInfo.tsx
@@ -6,7 +6,9 @@ import {ActiveConvoStates} from '#/state/messages/convo'
 import {useModerationOpts} from '#/state/preferences/moderation-opts'
 import {useSession} from '#/state/session'
 import {atoms as a, useTheme} from '#/alf'
+import {LeaveConvoPrompt} from '#/components/dms/LeaveConvoPrompt'
 import {KnownFollowers} from '#/components/KnownFollowers'
+import {usePromptControl} from '#/components/Prompt'
 import {DeleteChatButton, RejectMenu} from './RequestButtons'
 
 export function ChatStatusInfo({convoState}: {convoState: ActiveConvoStates}) {
@@ -14,6 +16,7 @@ export function ChatStatusInfo({convoState}: {convoState: ActiveConvoStates}) {
   const {_} = useLingui()
   const moderationOpts = useModerationOpts()
   const {currentAccount} = useSession()
+  const leaveConvoControl = usePromptControl()
 
   const otherUser = convoState.recipients.find(
     user => user.did !== currentAccount?.did,
@@ -62,6 +65,13 @@ export function ChatStatusInfo({convoState}: {convoState: ActiveConvoStates}) {
           color="secondary"
           size="small"
           currentScreen="conversation"
+          onPress={leaveConvoControl.open}
+        />
+        <LeaveConvoPrompt
+          convoId={convoState.convo.id}
+          control={leaveConvoControl}
+          currentScreen="conversation"
+          hasMessages={false}
         />
       </View>
     </View>
