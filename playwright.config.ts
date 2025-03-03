3a1b98af2c8e395c0830afd7f4319a46839fcbc3
alistaircc <160368040+alistaircc@users.noreply.github.com>
2025-03-02 22:18:40 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,14 +50,14 @@ export default defineConfig({
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+       name: 'Mobile Chrome',
+       use: { ...devices['Pixel 7'] },
+    },
+    {
+       name: 'Mobile Safari',
+       use: { ...devices['iPhone 15'] },
+    },
 
     /* Test against branded browsers. */
     // {
