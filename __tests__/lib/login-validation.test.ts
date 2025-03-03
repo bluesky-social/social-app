f340c73cbf0f065f36c494b32dc60ee2d91ddf1d
alistaircc <160368040+alistaircc@users.noreply.github.com>
2025-03-02 21:40:08 -0500

    Update login-validation.test.ts

--- __tests__/lib/login-validation.test.ts
+++ __tests__/lib/login-validation.test.ts
@@ -10,7 +10,7 @@ test.beforeEach(async ({ page }) => {
     await page.getByRole('button', {name: 'Sign in'}).click(); // Click Sign in button
 });
 
-test('Login Only Email Filled', async ({ page }) => {
+test('Login Only Email Filled', {tag: '@login'}, async ({ page }) => {
     await page.getByTestId('loginUsernameInput').click(); // Click the username input field
     await page.getByTestId('loginUsernameInput').fill('test@gmail.com'); // Fill the username field
     await page.getByRole('button', {name: 'Next'}).click() // Click the next button to trigger a login attempt
@@ -18,7 +18,7 @@ test('Login Only Email Filled', async ({ page }) => {
     await expect(page.getByText('Please enter your password')).toHaveText('Please enter your password'); // Find the error message and then assert on it
 });
 
-test('Login Only Password Filled', async ({ page }) => {
+test('Login Only Password Filled', {tag: '@login'}, async ({ page }) => {
     await page.getByTestId('loginPasswordInput').click(); // Click the password input field
     await page.getByTestId('loginPasswordInput').fill('password'); // Find the error message and then assert on it
 
@@ -27,7 +27,7 @@ test('Login Only Password Filled', async ({ page }) => {
     await expect(page.getByText('Please enter your username')).toHaveText('Please enter your username'); // Find the error message and then assert on it
 });
 
-test('Login Bad Email Bad Password', async ({ page }) => {
+test('Login Bad Email Bad Password', {tag: '@login'}, async ({ page }) => {
     await page.getByTestId('loginUsernameInput').click(); // Click the username input field
     await page.getByTestId('loginUsernameInput').fill('test'); // Fill the username field
 
@@ -39,13 +39,13 @@ test('Login Bad Email Bad Password', async ({ page }) => {
     await expect(page.getByText('Incorrect username or password')).toHaveText('Incorrect username or password'); // Find the error message and then assert on it
 });
 
-test('Forgot Password Login', async ({ page }) => {
+test('Forgot Password Login', {tag: '@login'}, async ({ page }) => {
     await page.getByText('Forgot?').click(); // Click Forgot? button
 
     await expect(page.getByText('Enter the email you used to create your account. We\'ll send you a "reset code" so you can set a new password.')).toHaveText('Enter the email you used to create your account. We\'ll send you a "reset code" so you can set a new password.'); // Find the descriptor and assert on it
 });
 
-test('Successful Login', async ({ page }) => {
+test('Successful Login', {tag: '@login'}, async ({ page }) => {
     await page.getByTestId('loginUsernameInput').click(); // Click the username input field
     await page.getByTestId('loginUsernameInput').fill(BSKY_EMAIL); // Fill the username field
 
