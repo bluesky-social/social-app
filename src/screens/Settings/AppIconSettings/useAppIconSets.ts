6ce9f47d01fdf842710713209fc378d9fc2b2a62
Samuel Newman <mozzius@protonmail.com>
2025-03-05 11:12:59 -0800

    Apply suggestions from code review
    Co-authored-by: surfdude29 <149612116+surfdude29@users.noreply.github.com>

--- src/screens/Settings/AppIconSettings/useAppIconSets.ts
+++ src/screens/Settings/AppIconSettings/useAppIconSets.ts
@@ -21,7 +21,7 @@ export function useAppIconSets() {
       },
       {
         id: 'default_dark',
-        name: _(msg`Dark`),
+        name: _(msg({ context: 'Name of app icon variant', message: 'Dark' })),
         iosImage: () => {
           return require(`../../../../assets/app-icons/ios_icon_default_dark.png`)
         },
@@ -37,7 +37,7 @@ export function useAppIconSets() {
     const core = [
       {
         id: 'core_aurora',
-        name: _(msg`Aurora`),
+        name: _(msg({ context: 'Name of app icon variant', message: 'Aurora' })),
         iosImage: () => {
           return require(`../../../../assets/app-icons/ios_icon_core_aurora.png`)
         },
@@ -47,7 +47,7 @@ export function useAppIconSets() {
       },
       // {
       //   id: 'core_bonfire',
-      //   name: _(msg`Bonfire`),
+      //   name: _(msg({ context: 'Name of app icon variant', message: 'Bonfire' })),
       //   iosImage: () => {
       //     return require(`../../../../assets/app-icons/ios_icon_core_bonfire.png`)
       //   },
@@ -57,7 +57,7 @@ export function useAppIconSets() {
       // },
       {
         id: 'core_sunrise',
-        name: _(msg`Sunrise`),
+        name: _(msg({ context: 'Name of app icon variant', message: 'Sunrise' })),
         iosImage: () => {
           return require(`../../../../assets/app-icons/ios_icon_core_sunrise.png`)
         },
@@ -67,7 +67,7 @@ export function useAppIconSets() {
       },
       {
         id: 'core_sunset',
-        name: _(msg`Sunset`),
+        name: _(msg({ context: 'Name of app icon variant', message: 'Sunset' })),
         iosImage: () => {
           return require(`../../../../assets/app-icons/ios_icon_core_sunset.png`)
         },
@@ -77,7 +77,7 @@ export function useAppIconSets() {
       },
       {
         id: 'core_midnight',
-        name: _(msg`Midnight`),
+        name: _(msg({ context: 'Name of app icon variant', message: 'Midnight' })),
         iosImage: () => {
           return require(`../../../../assets/app-icons/ios_icon_core_midnight.png`)
         },
@@ -87,7 +87,7 @@ export function useAppIconSets() {
       },
       {
         id: 'core_flat_blue',
-        name: _(msg`Flat Blue`),
+        name: _(msg({ context: 'Name of app icon variant', message: 'Flat Blue' })),
         iosImage: () => {
           return require(`../../../../assets/app-icons/ios_icon_core_flat_blue.png`)
         },
@@ -97,7 +97,7 @@ export function useAppIconSets() {
       },
       {
         id: 'core_flat_white',
-        name: _(msg`Flat White`),
+        name: _(msg({ context: 'Name of app icon variant', message: 'Flat White' })),
         iosImage: () => {
           return require(`../../../../assets/app-icons/ios_icon_core_flat_white.png`)
         },
@@ -107,7 +107,7 @@ export function useAppIconSets() {
       },
       {
         id: 'core_flat_black',
-        name: _(msg`Flat Black`),
+        name: _(msg({ context: 'Name of app icon variant', message: 'Flat Black' })),
         iosImage: () => {
           return require(`../../../../assets/app-icons/ios_icon_core_flat_black.png`)
         },
@@ -117,7 +117,7 @@ export function useAppIconSets() {
       },
       {
         id: 'core_classic',
-        name: _(msg`Bluesky Classic™`),
+        name: _(msg({ context: 'Name of app icon variant', message: 'Bluesky Classic™' })),
         iosImage: () => {
           return require(`../../../../assets/app-icons/ios_icon_core_classic.png`)
         },
