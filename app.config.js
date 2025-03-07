102beafc38e26dae1eebb25fb3849260b37bb337
Eric Bailey <git@esb.lol>
2025-03-07 15:29:38 -0600

    Bump sentry, update script

--- app.config.js
+++ app.config.js
@@ -202,7 +202,7 @@ module.exports = function (config) {
           {
             organization: 'blueskyweb',
             project: 'app',
-            url: 'https://sentry.io/',
+            url: 'https://sentry.io',
           },
         ],
         [
