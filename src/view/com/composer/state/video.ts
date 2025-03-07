6a4acb5e39a8e1e59a8ddc2407aaae7bc27c6f4d
surfdude29 <149612116+surfdude29@users.noreply.github.com>
2025-03-07 03:28:13 +0000

    tweak

--- src/view/com/composer/state/video.ts
+++ src/view/com/composer/state/video.ts
@@ -392,7 +392,7 @@ function getCompressErrorMessage(e: unknown, _: I18n['_']): string | null {
     return null
   }
   if (e instanceof VideoTooLargeError) {
-    return _(msg`The selected video is larger than 100MB.`)
+    return _(msg`The selected video is larger than 100 MB.`)
   }
   logger.error('Error compressing video', {safeMessage: e})
   return _(msg`An error occurred while compressing the video.`)
