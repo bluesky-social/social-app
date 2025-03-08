f34e0bdceb4f0ecf2bd23be447b64176b629ebf0
Samuel Newman <mozzius@protonmail.com>
2025-03-08 12:16:21 +0000

    fix types

--- src/view/com/util/Toast.tsx
+++ src/view/com/util/Toast.tsx
@@ -1,5 +1,5 @@
 import {Icon, toast} from '#/components/Toast'
 
-export function show(message: string, legacyIcon: Icon.LegacyIconName) {
+export function show(message: string, legacyIcon?: Icon.LegacyIconName) {
   toast({message, icon: Icon.compat_convertLegacyIcon(legacyIcon)})
 }
