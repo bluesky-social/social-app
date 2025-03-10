c6cdc2eba590099fa0b7d1a0a4bc245b65cdf101
Samuel Newman <mozzius@protonmail.com>
2025-03-10 22:00:24 +0200

    fix accidental type breakage

--- src/view/com/util/Toast.tsx
+++ src/view/com/util/Toast.tsx
@@ -3,6 +3,6 @@ import {Icon, toast} from '#/components/Toast'
 /**
  * @deprecated Use `toast` from '#/components/Toast'
  */
-export function show(message: string, legacyIcon: Icon.LegacyIconName) {
+export function show(message: string, legacyIcon?: Icon.LegacyIconName) {
   toast({message, icon: Icon.compat_convertLegacyIcon(legacyIcon)})
 }
