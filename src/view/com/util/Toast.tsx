78b560cffa9828650a72d556c5d45b6dd2e276ef
Samuel Newman <mozzius@protonmail.com>
2025-03-10 17:58:53 +0200

    deprecated legacy toast

--- src/view/com/util/Toast.tsx
+++ src/view/com/util/Toast.tsx
@@ -1,5 +1,8 @@
 import {Icon, toast} from '#/components/Toast'
 
-export function show(message: string, legacyIcon?: Icon.LegacyIconName) {
+/**
+ * @deprecated Use `toast` from '#/components/Toast'
+ */
+export function show(message: string, legacyIcon: Icon.LegacyIconName) {
   toast({message, icon: Icon.compat_convertLegacyIcon(legacyIcon)})
 }
