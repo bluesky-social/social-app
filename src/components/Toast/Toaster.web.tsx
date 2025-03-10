fb0de5873b6b4385b0140658c02266ff76a13e6c
Samuel Newman <mozzius@protonmail.com>
2025-03-10 17:26:43 +0200

    fix web background

--- src/components/Toast/Toaster.web.tsx
+++ src/components/Toast/Toaster.web.tsx
@@ -1,13 +1,15 @@
 import {Toaster as BurntToaster} from 'burnt/web'
 
-import {useBreakpoints, useTheme} from '#/alf'
+import {flatten, useBreakpoints, useTheme} from '#/alf'
 
 export function Toaster() {
   const t = useTheme()
   const {gtMobile} = useBreakpoints()
+
   return (
     <BurntToaster
       theme={t.scheme}
+      toastOptions={{style: flatten([t.atoms.bg])}}
       position={gtMobile ? 'bottom-left' : 'top-center'}
     />
   )
