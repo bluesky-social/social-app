13f75bd004279bccbc63790630bc94967777c4db
Hailey <me@haileyok.com>
2025-03-20 14:37:08 -0700

    use config

--- bskylink/src/routes/root.ts
+++ bskylink/src/routes/root.ts
@@ -3,11 +3,12 @@ import {Express} from 'express'
 import {AppContext} from '../context.js'
 import {handler} from './util.js'
 
-export default function (_ctx: AppContext, app: Express) {
+export default function (ctx: AppContext, app: Express) {
   return app.get(
     '/',
     handler(async (_req, res) => {
-      return res.status(301).location('https://bsky.app')
+      res.setHeader('Location', `https://${ctx.cfg.service.appHostname}`)
+      return res.status(301)
     }),
   )
 }
