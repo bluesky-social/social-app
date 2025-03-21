eeb9f7e96dc500fd8008a0d765887940daed1cad
Eric Bailey <git@esb.lol>
2025-03-21 09:53:10 -0500

    Message embed width tweaks

--- src/components/dms/MessageItemEmbed.tsx
+++ src/components/dms/MessageItemEmbed.tsx
@@ -3,7 +3,7 @@ import {useWindowDimensions, View} from 'react-native'
 import {AppBskyEmbedRecord} from '@atproto/api'
 
 import {PostEmbeds, PostEmbedViewContext} from '#/view/com/util/post-embeds'
-import {atoms as a, native, tokens, useTheme} from '#/alf'
+import {atoms as a, native, tokens, useTheme,web} from '#/alf'
 import {MessageContextProvider} from './MessageContext'
 
 let MessageItemEmbed = ({
@@ -21,8 +21,15 @@ let MessageItemEmbed = ({
           a.my_xs,
           t.atoms.bg,
           a.rounded_md,
-          native({flexBasis: 0}),
-          {width: Math.min(screen.width, 600) / 1.7},
+          native({
+            flexBasis: 0,
+            width: Math.min(screen.width, 600) / 1.4,
+          }),
+          web({
+            width: '100%',
+            minWidth: 280,
+            maxWidth: 360,
+          }),
         ]}>
         <View style={{marginTop: tokens.space.sm * -1}}>
           <PostEmbeds
