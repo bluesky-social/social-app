dbbc99c647ad0d53630f145c9da3253b4ad3a2d4
Samuel Newman <mozzius@protonmail.com>
2025-04-21 18:10:45 +0300

    delete waitlist modal, which doesn't exist

--- src/state/modals/index.tsx
+++ src/state/modals/index.tsx
@@ -15,10 +15,6 @@ export interface DeleteAccountModal {
   name: 'delete-account'
 }
 
-export interface WaitlistModal {
-  name: 'waitlist'
-}
-
 export interface InviteCodesModal {
   name: 'invite-codes'
 }
@@ -41,7 +37,6 @@ export type Modal =
   | UserAddRemoveListsModal
 
   // Bluesky access
-  | WaitlistModal
   | InviteCodesModal
 
 const ModalContext = React.createContext<{
