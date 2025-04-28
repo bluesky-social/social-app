ab1afe1133654d6bf1194925fa0771e1ad2d7d30
Samuel Newman <mozzius@protonmail.com>
2025-04-28 13:15:50 +0300

    memoize emoji picker styles

--- modules/expo-emoji-picker/src/EmojiPicker.android.tsx
+++ modules/expo-emoji-picker/src/EmojiPicker.android.tsx
@@ -1,3 +1,4 @@
+import {useMemo} from 'react'
 import {useColorScheme} from 'react-native'
 
 import {type EmojiPickerViewProps} from './EmojiPickerModule.types'
@@ -5,19 +6,23 @@ import EmojiPickerNativeView from './EmojiPickerView'
 
 const EmojiPicker = ({onEmojiSelected}: EmojiPickerViewProps) => {
   const scheme = useColorScheme()
+  const styles = useMemo(
+    () =>
+      ({
+        flex: 1,
+        width: '100%',
+        backgroundColor: scheme === 'dark' ? '#000' : '#fff',
+      } as const),
+    [scheme],
+  )
+
   return (
     <EmojiPickerNativeView
       onEmojiSelected={emoji => {
         onEmojiSelected(emoji)
       }}
-      style={[styles, {backgroundColor: scheme === 'dark' ? '#000' : '#fff'}]}
+      style={styles}
     />
   )
 }
-
-const styles = {
-  flex: 1,
-  width: '100%',
-} as const
-
 export default EmojiPicker
