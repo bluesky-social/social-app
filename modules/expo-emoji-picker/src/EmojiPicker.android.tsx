0877b6dcfe91a2754c2c12e8766828a2a8f94173
Samuel Newman <mozzius@protonmail.com>
2025-04-28 10:36:36 +0300

    fix emoji picker dark mode

--- modules/expo-emoji-picker/src/EmojiPicker.android.tsx
+++ modules/expo-emoji-picker/src/EmojiPicker.android.tsx
@@ -1,13 +1,16 @@
+import {useColorScheme} from 'react-native'
+
 import {type EmojiPickerViewProps} from './EmojiPickerModule.types'
 import EmojiPickerNativeView from './EmojiPickerView'
 
 const EmojiPicker = ({onEmojiSelected}: EmojiPickerViewProps) => {
+  const scheme = useColorScheme()
   return (
     <EmojiPickerNativeView
       onEmojiSelected={emoji => {
         onEmojiSelected(emoji)
       }}
-      style={styles}
+      style={[styles, {backgroundColor: scheme === 'dark' ? '#000' : '#fff'}]}
     />
   )
 }
@@ -15,7 +18,6 @@ const EmojiPicker = ({onEmojiSelected}: EmojiPickerViewProps) => {
 const styles = {
   flex: 1,
   width: '100%',
-  backgroundColor: 'white',
 } as const
 
 export default EmojiPicker
