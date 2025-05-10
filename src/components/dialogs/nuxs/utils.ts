85b336856b27296cb58d9a214c8c20624fad1fa3
Eric Bailey <git@esb.lol>
2025-05-10 10:58:23 -0500

    Update comment

--- src/components/dialogs/nuxs/utils.ts
+++ src/components/dialogs/nuxs/utils.ts
@@ -2,9 +2,12 @@ const ONE_DAY = 1000 * 60 * 60 * 24
 
 export function isDaysOld(days: number, createdAt?: string) {
   /*
-   * Should never happen, but if it does, the account is likely quite old
+   * Should never happen because we gate NUXs to only accounts with a valid
+   * profile and a `createdAt` (see `nuxs/index.tsx`). But if it ever did, the
+   * account is either old enough to be pre-onboarding, or some failure happened
+   * during account creation. Fail closed. - esb
    */
-  if (!createdAt) return true
+  if (!createdAt) return false
 
   const now = Date.now()
   const then = new Date(createdAt).getTime()
