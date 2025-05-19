d54b3f8894c7e442cb7472eda12b900ff2d7a790
Samuel Newman <mozzius@protonmail.com>
2025-05-19 19:34:10 +0300

    move warning into card

--- src/components/live/LinkPreview.tsx
+++ src/components/live/LinkPreview.tsx
@@ -7,11 +7,9 @@ import {type LinkMeta} from '#/lib/link-meta/link-meta'
 import {toNiceDomain} from '#/lib/strings/url-helpers'
 import {LoadingPlaceholder} from '#/view/com/util/LoadingPlaceholder'
 import {atoms as a, useTheme} from '#/alf'
-import {CircleX_Stroke2_Corner0_Rounded as CircleXIcon} from '#/components/icons/CircleX'
 import {Globe_Stroke2_Corner0_Rounded as GlobeIcon} from '#/components/icons/Globe'
-import {Warning_Stroke2_Corner0_Rounded as WarningIcon} from '#/components/icons/Warning'
+import {Image_Stroke2_Corner0_Rounded as ImageIcon} from '#/components/icons/Image'
 import {Text} from '#/components/Typography'
-import {colors} from '../Admonition'
 
 export function LinkPreview({
   linkMeta,
@@ -28,90 +26,73 @@ export function LinkPreview({
   }
 
   return (
-    <>
+    <View
+      style={[
+        a.w_full,
+        a.border,
+        t.atoms.border_contrast_low,
+        t.atoms.bg,
+        a.flex_row,
+        a.rounded_sm,
+        a.overflow_hidden,
+        a.align_stretch,
+      ]}>
       <View
         style={[
-          a.w_full,
-          a.border,
-          t.atoms.border_contrast_low,
-          t.atoms.bg,
-          a.flex_row,
-          a.rounded_sm,
-          a.overflow_hidden,
-          a.align_stretch,
+          t.atoms.bg_contrast_25,
+          {minHeight: 64, width: 114},
+          a.justify_center,
+          a.align_center,
+          a.gap_xs,
         ]}>
-        {(!linkMeta || linkMeta.image) && (
-          <View
-            style={[
-              t.atoms.bg_contrast_25,
-              {minHeight: 64, width: 114},
-              a.justify_center,
-              a.align_center,
-            ]}>
-            {linkMeta?.image && (
-              <Image
-                source={linkMeta.image}
-                accessibilityIgnoresInvertColors
-                transition={200}
-                style={[a.absolute, a.inset_0]}
-                contentFit="cover"
-                onLoad={() => setImageLoadError(false)}
-                onError={() => setImageLoadError(true)}
-              />
-            )}
-            {linkMeta && imageLoadError && (
-              <CircleXIcon style={[t.atoms.text_contrast_low]} size="xl" />
-            )}
-          </View>
+        {linkMeta?.image && (
+          <Image
+            source={linkMeta.image}
+            accessibilityIgnoresInvertColors
+            transition={200}
+            style={[a.absolute, a.inset_0]}
+            contentFit="cover"
+            onLoad={() => setImageLoadError(false)}
+            onError={() => setImageLoadError(true)}
+          />
         )}
-        <View style={[a.flex_1, a.justify_center, a.py_sm, a.gap_xs, a.px_md]}>
-          {linkMeta ? (
-            <>
-              <Text
-                numberOfLines={2}
-                style={[a.leading_snug, a.font_bold, a.text_md]}>
-                {linkMeta.title || linkMeta.url}
-              </Text>
-              <View style={[a.flex_row, a.align_center, a.gap_2xs]}>
-                <GlobeIcon size="xs" style={[t.atoms.text_contrast_low]} />
-                <Text
-                  numberOfLines={1}
-                  style={[
-                    a.text_xs,
-                    a.leading_snug,
-                    t.atoms.text_contrast_medium,
-                  ]}>
-                  {toNiceDomain(linkMeta.url)}
-                </Text>
-              </View>
-            </>
-          ) : (
-            <>
-              <LoadingPlaceholder height={16} width={128} />
-              <LoadingPlaceholder height={12} width={72} />
-            </>
-          )}
-        </View>
+        {linkMeta && (!linkMeta.image || imageLoadError) && (
+          <>
+            <ImageIcon style={[t.atoms.text_contrast_low]} />
+            <Text style={[t.atoms.text_contrast_low, a.text_xs, a.text_center]}>
+              <Trans>No image</Trans>
+            </Text>
+          </>
+        )}
       </View>
-      {linkMeta && (!linkMeta.image || imageLoadError) && (
-        <View style={[a.flex_row, a.gap_xs, a.align_center, a.px_sm]}>
-          <WarningIcon style={[{color: colors.warning[t.scheme]}]} size="sm" />
-          <Text
-            style={[
-              a.text_sm,
-              a.leading_snug,
-              a.flex_1,
-              a.font_bold,
-              t.atoms.text_contrast_medium,
-            ]}>
-            {imageLoadError ? (
-              <Trans>Failed to load image</Trans>
-            ) : (
-              <Trans>This link does not have an image</Trans>
-            )}
-          </Text>
-        </View>
-      )}
-    </>
+      <View style={[a.flex_1, a.justify_center, a.py_sm, a.gap_xs, a.px_md]}>
+        {linkMeta ? (
+          <>
+            <Text
+              numberOfLines={2}
+              style={[a.leading_snug, a.font_bold, a.text_md]}>
+              {linkMeta.title || linkMeta.url}
+            </Text>
+            <View style={[a.flex_row, a.align_center, a.gap_2xs]}>
+              <GlobeIcon size="xs" style={[t.atoms.text_contrast_low]} />
+              <Text
+                numberOfLines={1}
+                style={[
+                  a.text_xs,
+                  a.leading_snug,
+                  t.atoms.text_contrast_medium,
+                ]}>
+                {toNiceDomain(linkMeta.url)}
+              </Text>
+            </View>
+          </>
+        ) : (
+          <>
+            <LoadingPlaceholder height={16} width={128} />
+            <LoadingPlaceholder height={12} width={72} />
+          </>
+        )}
+      </View>
+    </View>
   )
 }
