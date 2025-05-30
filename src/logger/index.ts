08741ee203628aa8f279bb9dccb7ba6fae799b96
Hailey <me@haileyok.com>
2025-05-30 11:46:09 -0700

    prettier

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -6,7 +6,12 @@ import {type MetricEvents} from '#/logger/metrics'
 import {bitdriftTransport} from '#/logger/transports/bitdrift'
 import {consoleTransport} from '#/logger/transports/console'
 import {sentryTransport} from '#/logger/transports/sentry'
-import {LogContext, LogLevel, type Metadata, type Transport} from '#/logger/types'
+import {
+  LogContext,
+  LogLevel,
+  type Metadata,
+  type Transport,
+} from '#/logger/types'
 import {enabledLogLevels} from '#/logger/util'
 import {isNative} from '#/platform/detection'
 
