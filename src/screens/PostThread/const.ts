14c6076c33764a6ef861e9654e4baa36ec566bbb
Eric Bailey <git@esb.lol>
2025-06-09 13:44:48 -0500

    Relate consts to theme values

--- src/screens/PostThread/const.ts
+++ src/screens/PostThread/const.ts
@@ -1,5 +1,7 @@
-export const TREE_INDENT = 16
+import {tokens} from '#/alf'
+
+export const TREE_INDENT = tokens.space.lg
 export const TREE_AVI_WIDTH = 24
 export const LINEAR_AVI_WIDTH = 42
 export const REPLY_LINE_WIDTH = 2
-export const OUTER_SPACE = 16
+export const OUTER_SPACE = tokens.space.lg
