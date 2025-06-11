7341294df6156afdf24ab43e1e27ebba94f265ad
Samuel Newman <mozzius@protonmail.com>
2025-06-11 20:12:05 +0300

    Fix using screen names in `Link` (#8473)
    * use our router in favour of useLinkBuilder
    
    * test feature using Home header feeds button
    
    * handle non-string params properly

--- src/lib/routes/types.ts
+++ src/lib/routes/types.ts
@@ -143,5 +143,5 @@ export type RouteParams = Record<string, string>
 export type MatchResult = {params: RouteParams}
 export type Route = {
   match: (path: string) => MatchResult | undefined
-  build: (params: RouteParams) => string
+  build: (params?: Record<string, any>) => string
 }
