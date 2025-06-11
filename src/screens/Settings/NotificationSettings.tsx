2ad758f320a1874453b146e45ac50793844e521d
Samuel Newman <mozzius@protonmail.com>
2025-06-11 13:54:23 +0300

    new icons

--- src/screens/Settings/NotificationSettings.tsx
+++ src/screens/Settings/NotificationSettings.tsx
@@ -1,3 +1,4 @@
+import {View} from 'react-native'
 import {msg, Trans} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 
@@ -5,18 +6,27 @@ import {
   type AllNavigatorParams,
   type NativeStackScreenProps,
 } from '#/lib/routes/types'
-import {Bell_Stroke2_Corner0_Rounded as BellIcon} from '#/components/icons/Bell'
+import {atoms as a, useTheme} from '#/alf'
+import {BellRinging_Stroke2_Corner0_Rounded as BellRingingIcon} from '#/components/icons/BellRinging'
 import {Bubble_Stroke2_Corner2_Rounded as BubbleIcon} from '#/components/icons/Bubble'
-import {Heart2_Stroke2_Corner0_Rounded as HeartIcon} from '#/components/icons/Heart2'
+import {
+  Heart2_Stroke2_Corner0_Rounded as HeartIcon,
+  LikeRepost_Stroke2_Corner2_Rounded as LikeRepostIcon,
+} from '#/components/icons/Heart2'
 import {PersonPlus_Stroke2_Corner2_Rounded as PersonPlusIcon} from '#/components/icons/Person'
-import {Repost_Stroke2_Corner2_Rounded as RepostIcon} from '#/components/icons/Repost'
+import {
+  Repost_Stroke2_Corner2_Rounded as RepostIcon,
+  RepostRepost_Stroke2_Corner2_Rounded as RepostRepostIcon,
+} from '#/components/icons/Repost'
 import {Shapes_Stroke2_Corner0_Rounded as ShapesIcon} from '#/components/icons/Shapes'
 import * as Layout from '#/components/Layout'
+import {Text} from '#/components/Typography'
 import * as SettingsList from './components/SettingsList'
 
 type Props = NativeStackScreenProps<AllNavigatorParams, 'NotificationSettings'>
 export function NotificationSettingsScreen({}: Props) {
   const {_} = useLingui()
+  const t = useTheme()
   return (
     <Layout.Screen>
       <Layout.Header.Outer>
@@ -32,72 +42,113 @@ export function NotificationSettingsScreen({}: Props) {
         <SettingsList.Container>
           <SettingsList.LinkItem
             label={_(msg`Settings for reply, mention, and quote notifications`)}
-            to="/settings/notifications/replies">
+            to="/settings/notifications/replies"
+            contentContainerStyle={[a.align_start]}>
             <SettingsList.ItemIcon icon={BubbleIcon} />
-            <SettingsList.ItemText>
-              <Trans>Replies, mentions, and quotes</Trans>
-            </SettingsList.ItemText>
+            <ItemTextWithSubtitle
+              titleText={<Trans>Replies, Mentions, and Quotes</Trans>}
+              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+            />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
             label={_(msg`Settings for like notifications`)}
-            to="/settings/notifications/likes">
+            to="/settings/notifications/likes"
+            contentContainerStyle={[a.align_start]}>
             <SettingsList.ItemIcon icon={HeartIcon} />
-            <SettingsList.ItemText>
-              <Trans>Likes</Trans>
-            </SettingsList.ItemText>
+            <ItemTextWithSubtitle
+              titleText={<Trans>Likes</Trans>}
+              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+            />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
             label={_(msg`Settings for repost notifications`)}
-            to="/settings/notifications/reposts">
+            to="/settings/notifications/reposts"
+            contentContainerStyle={[a.align_start]}>
             <SettingsList.ItemIcon icon={RepostIcon} />
-            <SettingsList.ItemText>
-              <Trans>Reposts</Trans>
-            </SettingsList.ItemText>
+            <ItemTextWithSubtitle
+              titleText={<Trans>Reposts</Trans>}
+              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+            />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
             label={_(msg`Settings for new follower notifications`)}
-            to="/settings/notifications/new-followers">
+            to="/settings/notifications/new-followers"
+            contentContainerStyle={[a.align_start]}>
             <SettingsList.ItemIcon icon={PersonPlusIcon} />
-            <SettingsList.ItemText>
-              <Trans>New followers</Trans>
-            </SettingsList.ItemText>
+            <ItemTextWithSubtitle
+              titleText={<Trans>New followers</Trans>}
+              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+            />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
             label={_(msg`Settings for notifications for likes on your reposts`)}
-            to="/settings/notifications/likes-on-reposts">
-            <SettingsList.ItemIcon icon={HeartIcon} />
-            <SettingsList.ItemText>
-              <Trans>Likes on your reposts</Trans>
-            </SettingsList.ItemText>
+            to="/settings/notifications/likes-on-reposts"
+            contentContainerStyle={[a.align_start]}>
+            <SettingsList.ItemIcon icon={LikeRepostIcon} />
+            <ItemTextWithSubtitle
+              titleText={<Trans>Likes on your reposts</Trans>}
+              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+            />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
             label={_(
               msg`Settings for notifications for reposts on your reposts`,
             )}
-            to="/settings/notifications/reposts-on-reposts">
-            <SettingsList.ItemIcon icon={RepostIcon} />
-            <SettingsList.ItemText>
-              <Trans>Reposts on your reposts</Trans>
-            </SettingsList.ItemText>
+            to="/settings/notifications/reposts-on-reposts"
+            contentContainerStyle={[a.align_start]}>
+            <SettingsList.ItemIcon icon={RepostRepostIcon} />
+            <ItemTextWithSubtitle
+              titleText={<Trans>Reposts on your reposts</Trans>}
+              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+            />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
-            label={_(msg`Settings for account subscription notifications`)}
-            to="/settings/notifications/account-subscriptions">
-            <SettingsList.ItemIcon icon={BellIcon} />
-            <SettingsList.ItemText>
-              <Trans>Account subscriptions</Trans>
-            </SettingsList.ItemText>
+            label={_(msg`Settings for other's activity`)}
+            to="/settings/notifications/account-subscriptions"
+            contentContainerStyle={[a.align_start]}>
+            <SettingsList.ItemIcon icon={BellRingingIcon} />
+
+            <ItemTextWithSubtitle
+              titleText={<Trans>Other's activity</Trans>}
+              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+            />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
             label={_(msg`Settings for notifications for everything else`)}
-            to="/settings/notifications/miscellaneous">
+            to="/settings/notifications/miscellaneous"
+            contentContainerStyle={[a.align_start]}>
             <SettingsList.ItemIcon icon={ShapesIcon} />
-            <SettingsList.ItemText>
-              <Trans>Everything else</Trans>
-            </SettingsList.ItemText>
+            <View style={[a.flex_1]}>
+              <SettingsList.ItemText>
+                <Trans>Everything else</Trans>
+              </SettingsList.ItemText>
+              <Text style={[a.text_sm, t.atoms.text_contrast_medium]}>
+                In-app, Push, Everyone
+              </Text>
+            </View>
           </SettingsList.LinkItem>
         </SettingsList.Container>
       </Layout.Content>
     </Layout.Screen>
+  )
+}
+
+function ItemTextWithSubtitle({
+  titleText,
+  subtitleText,
+}: {
+  titleText: React.ReactNode
+  subtitleText: React.ReactNode
+}) {
+  const t = useTheme()
+  return (
+    <View style={[a.flex_1, a.gap_2xs]}>
+      <SettingsList.ItemText>
+        <Trans>{titleText}</Trans>
+      </SettingsList.ItemText>
+      <Text style={[a.text_sm, t.atoms.text_contrast_medium]}>
+        <Trans>{subtitleText}</Trans>
+      </Text>
+    </View>
   )
 }
