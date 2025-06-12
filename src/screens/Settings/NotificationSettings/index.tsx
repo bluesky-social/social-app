f4373b26a5f256dc3a1e989cb6190480d7bc74cc
Samuel Newman <mozzius@protonmail.com>
2025-06-12 23:01:25 +0300

    load settings

--- src/screens/Settings/NotificationSettings/index.tsx
+++ src/screens/Settings/NotificationSettings/index.tsx
@@ -1,4 +1,5 @@
 import {View} from 'react-native'
+import {type AppBskyNotificationDefs} from '@atproto/api'
 import {msg, Trans} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 
@@ -6,7 +7,9 @@ import {
   type AllNavigatorParams,
   type NativeStackScreenProps,
 } from '#/lib/routes/types'
-import {atoms as a, useTheme} from '#/alf'
+import {useNotificationSettingsQuery} from '#/state/queries/notification-settings'
+import {atoms as a} from '#/alf'
+import {Admonition} from '#/components/Admonition'
 import {At_Stroke2_Corner2_Rounded as AtIcon} from '#/components/icons/At'
 // import {BellRinging_Stroke2_Corner0_Rounded as BellRingingIcon} from '#/components/icons/BellRinging'
 import {Bubble_Stroke2_Corner2_Rounded as BubbleIcon} from '#/components/icons/Bubble'
@@ -22,14 +25,14 @@ import {
 } from '#/components/icons/Repost'
 import {Shapes_Stroke2_Corner0_Rounded as ShapesIcon} from '#/components/icons/Shapes'
 import * as Layout from '#/components/Layout'
-import {Text} from '#/components/Typography'
 import * as SettingsList from '../components/SettingsList'
 import {ItemTextWithSubtitle} from './components/ItemTextWithSubtitle'
 
 type Props = NativeStackScreenProps<AllNavigatorParams, 'NotificationSettings'>
 export function NotificationSettingsScreen({}: Props) {
   const {_} = useLingui()
-  const t = useTheme()
+
+  const {data: settings, isError} = useNotificationSettingsQuery()
 
   return (
     <Layout.Screen>
@@ -44,6 +47,13 @@ export function NotificationSettingsScreen({}: Props) {
       </Layout.Header.Outer>
       <Layout.Content>
         <SettingsList.Container>
+          <View style={[a.px_lg, a.pb_md]}>
+            {isError && (
+              <Admonition type="error">
+                <Trans>Failed to load notification settings.</Trans>
+              </Admonition>
+            )}
+          </View>
           <SettingsList.LinkItem
             label={_(msg`Settings for reply notifications`)}
             to={{screen: 'ReplyNotificationSettings'}}
@@ -51,7 +61,8 @@ export function NotificationSettingsScreen({}: Props) {
             <SettingsList.ItemIcon icon={BubbleIcon} />
             <ItemTextWithSubtitle
               titleText={<Trans>Replies</Trans>}
-              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+              subtitleText={<SettingPreview preference={settings?.reply} />}
+              showSkeleton={!settings}
             />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
@@ -61,7 +72,8 @@ export function NotificationSettingsScreen({}: Props) {
             <SettingsList.ItemIcon icon={AtIcon} />
             <ItemTextWithSubtitle
               titleText={<Trans>Mentions</Trans>}
-              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+              subtitleText={<SettingPreview preference={settings?.mention} />}
+              showSkeleton={!settings}
             />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
@@ -71,7 +83,8 @@ export function NotificationSettingsScreen({}: Props) {
             <SettingsList.ItemIcon icon={CloseQuoteIcon} />
             <ItemTextWithSubtitle
               titleText={<Trans>Quotes</Trans>}
-              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+              subtitleText={<SettingPreview preference={settings?.quote} />}
+              showSkeleton={!settings}
             />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
@@ -81,7 +94,8 @@ export function NotificationSettingsScreen({}: Props) {
             <SettingsList.ItemIcon icon={HeartIcon} />
             <ItemTextWithSubtitle
               titleText={<Trans>Likes</Trans>}
-              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+              subtitleText={<SettingPreview preference={settings?.like} />}
+              showSkeleton={!settings}
             />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
@@ -91,7 +105,8 @@ export function NotificationSettingsScreen({}: Props) {
             <SettingsList.ItemIcon icon={RepostIcon} />
             <ItemTextWithSubtitle
               titleText={<Trans>Reposts</Trans>}
-              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+              subtitleText={<SettingPreview preference={settings?.repost} />}
+              showSkeleton={!settings}
             />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
@@ -101,7 +116,8 @@ export function NotificationSettingsScreen({}: Props) {
             <SettingsList.ItemIcon icon={PersonPlusIcon} />
             <ItemTextWithSubtitle
               titleText={<Trans>New followers</Trans>}
-              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+              subtitleText={<SettingPreview preference={settings?.follow} />}
+              showSkeleton={!settings}
             />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
@@ -111,7 +127,10 @@ export function NotificationSettingsScreen({}: Props) {
             <SettingsList.ItemIcon icon={LikeRepostIcon} />
             <ItemTextWithSubtitle
               titleText={<Trans>Likes on your reposts</Trans>}
-              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+              subtitleText={
+                <SettingPreview preference={settings?.likeViaRepost} />
+              }
+              showSkeleton={!settings}
             />
           </SettingsList.LinkItem>
           <SettingsList.LinkItem
@@ -123,7 +142,10 @@ export function NotificationSettingsScreen({}: Props) {
             <SettingsList.ItemIcon icon={RepostRepostIcon} />
             <ItemTextWithSubtitle
               titleText={<Trans>Reposts on your reposts</Trans>}
-              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+              subtitleText={
+                <SettingPreview preference={settings?.repostViaRepost} />
+              }
+              showSkeleton={!settings}
             />
           </SettingsList.LinkItem>
           {/* <SettingsList.LinkItem
@@ -134,7 +156,8 @@ export function NotificationSettingsScreen({}: Props) {
 
             <ItemTextWithSubtitle
               titleText={<Trans>Other's activity</Trans>}
-              subtitleText={<Trans>In-app, Push, Everyone</Trans>}
+              subtitleText={<SettingPreview preference={settings?.subscribedPost} />}
+              showSkeleton={!settings}
             />
           </SettingsList.LinkItem> */}
           <SettingsList.LinkItem
@@ -142,17 +165,65 @@ export function NotificationSettingsScreen({}: Props) {
             to={{screen: 'MiscellaneousNotificationSettings'}}
             contentContainerStyle={[a.align_start]}>
             <SettingsList.ItemIcon icon={ShapesIcon} />
-            <View style={[a.flex_1]}>
-              <SettingsList.ItemText>
-                <Trans>Everything else</Trans>
-              </SettingsList.ItemText>
-              <Text style={[a.text_sm, t.atoms.text_contrast_medium]}>
-                In-app, Push, Everyone
-              </Text>
-            </View>
+            <ItemTextWithSubtitle
+              titleText={<Trans>Reposts on your reposts</Trans>}
+              // technically a bundle of several settings, but since they're set together
+              // and are most likely in sync we'll just show the state of one of them
+              subtitleText={
+                <SettingPreview preference={settings?.starterpackJoined} />
+              }
+              showSkeleton={!settings}
+            />
           </SettingsList.LinkItem>
         </SettingsList.Container>
       </Layout.Content>
     </Layout.Screen>
   )
+}
+
+function SettingPreview({
+  preference,
+}: {
+  preference?:
+    | AppBskyNotificationDefs.Preference
+    | AppBskyNotificationDefs.FilterablePreference
+}) {
+  const {_} = useLingui()
+  if (!preference) {
+    return null
+  } else {
+    if (!preference.$type) throw new Error('Preference lacks $type')
+
+    if (preference.$type === 'app.bsky.notification.defs#preference') {
+      if (preference.list && preference.push) {
+        return _(msg`In-app, Push`)
+      } else if (preference.list) {
+        return _(msg`In-app`)
+      } else if (preference.push) {
+        return _(msg`Push`)
+      }
+    } else if (
+      preference.$type === 'app.bsky.notification.defs#filterablePreference'
+    ) {
+      if (preference.filter === 'all') {
+        if (preference.list && preference.push) {
+          return _(msg`In-app, Push, Everyone`)
+        } else if (preference.list) {
+          return _(msg`In-app, Everyone`)
+        } else if (preference.push) {
+          return _(msg`Push, Everyone`)
+        }
+      } else if (preference.filter === 'follows') {
+        if (preference.list && preference.push) {
+          return _(msg`In-app, Push, People you follow`)
+        } else if (preference.list) {
+          return _(msg`In-app, People you follow`)
+        } else if (preference.push) {
+          return _(msg`Push, People you follow`)
+        }
+      }
+    }
+  }
+
+  return _(msg`Disabled`)
 }
