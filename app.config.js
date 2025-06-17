005a2a60e06079e697a08f03e6a987dd0bf3949e
Thorne 🌸 <ExistentialEnso@users.noreply.github.com>
2025-06-17 19:42:13 -0400

    Change primary color

--- app.config.js
+++ app.config.js
@@ -47,7 +47,7 @@ module.exports = function (_config) {
       },
       icon: './assets/app-icons/ios_icon_default_light.png',
       userInterfaceStyle: 'automatic',
-      primaryColor: '#1083fe',
+      primaryColor: '#61389d',
       ios: {
         supportsTablet: false,
         bundleIdentifier: 'xyz.blueskyweb.app',
