deb13a5a26926d0b597d4880565723a301584bf3
Eric Bailey <git@esb.lol>
2025-06-18 13:41:46 -0500

    Fix anchor text overflow in threads v2

--- src/components/ProfileHoverCard/types.ts
+++ src/components/ProfileHoverCard/types.ts
@@ -1,7 +1,9 @@
 import type React from 'react'
 
-export type ProfileHoverCardProps = {
-  children: React.ReactElement
+import {type ViewStyleProp} from '#/alf'
+
+export type ProfileHoverCardProps = ViewStyleProp & {
+  children: React.ReactNode
   did: string
   disable?: boolean
 }
