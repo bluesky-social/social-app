ede4cd33058cdbe47d5852c269850e29f6120920
Samuel Newman <mozzius@protonmail.com>
2025-06-20 16:14:09 +0300

    use system bars api on ios too

--- src/view/shell/Composer.ios.tsx
+++ src/view/shell/Composer.ios.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
+import {useEffect} from 'react'
 import {Modal, View} from 'react-native'
+import {SystemBars} from 'react-native-edge-to-edge'
 
-import {useDialogStateControlContext} from '#/state/dialogs'
 import {useComposerState} from '#/state/shell/composer'
 import {ComposePost, useComposerCancelRef} from '#/view/com/composer/Composer'
 import {atoms as a, useTheme} from '#/alf'
 import {SheetCompatProvider as TooltipSheetCompatProvider} from '#/components/Tooltip'
 
-export function Composer({}: {winHeight: number}) {
-  const {setFullyExpandedCount} = useDialogStateControlContext()
+export function Composer() {
   const t = useTheme()
   const state = useComposerState()
   const ref = useComposerCancelRef()
 
   const open = !!state
-  const prevOpen = React.useRef(open)
 
-  React.useEffect(() => {
-    if (open && !prevOpen.current) {
-      setFullyExpandedCount(c => c + 1)
-    } else if (!open && prevOpen.current) {
-      setFullyExpandedCount(c => c - 1)
+  useEffect(() => {
+    if (open) {
+      const entry = SystemBars.pushStackEntry({
+        style: {
+          statusBar: 'light',
+        },
+      })
+      return () => SystemBars.popStackEntry(entry)
     }
-    prevOpen.current = open
-  }, [open, setFullyExpandedCount])
+  }, [open])
 
   return (
     <Modal
