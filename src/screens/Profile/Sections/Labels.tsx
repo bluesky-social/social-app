c570103eeb7e45771fb9c09c965e5be4c67a2fdd
Samuel Newman <mozzius@protonmail.com>
2025-06-26 09:29:59 +0300

    dedupe `labelValues`

--- src/screens/Profile/Sections/Labels.tsx
+++ src/screens/Profile/Sections/Labels.tsx
@@ -74,6 +74,7 @@ export function ProfileLabelsSection({
     if (isLabelerLoading || !labelerInfo || labelerError) return []
     const customDefs = interpretLabelValueDefinitions(labelerInfo)
     return labelerInfo.policies.labelValues
+      .filter((val, i, arr) => arr.indexOf(val) === i)
       .map(val => lookupLabelValueDefinition(val, customDefs))
       .filter(
         def => def && def?.configurable,
