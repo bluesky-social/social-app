5c141d750ebcdc3aed72328dd3d77239cde5728a
Eric Bailey <git@esb.lol>
2025-07-09 15:08:14 -0500

    Do service auth for init

--- src/components/ageAssurance/useInitAgeAssurance.ts
+++ src/components/ageAssurance/useInitAgeAssurance.ts
@@ -1,27 +1,48 @@
 import {
   type AppBskyUnspeccedDefs,
   type AppBskyUnspeccedInitAgeAssurance,
+  AtpAgent,
 } from '@atproto/api'
 import {useMutation, useQueryClient} from '@tanstack/react-query'
 
 import {wait} from '#/lib/async/wait'
+import {
+  DEV_ENV_APPVIEW,
+  DEV_ENV_APPVIEW_DID,
+  PUBLIC_APPVIEW,
+  PUBLIC_APPVIEW_DID,
+} from '#/lib/constants'
 import {isNetworkError} from '#/lib/hooks/useCleanError'
 import {logger} from '#/logger'
 import {createAgeAssuranceQueryKey} from '#/state/age-assurance'
 import {useAgent} from '#/state/session'
+
+const APPVIEW = __DEV__ ? DEV_ENV_APPVIEW : PUBLIC_APPVIEW
+const APPVIEW_DID = __DEV__ ? DEV_ENV_APPVIEW_DID : PUBLIC_APPVIEW_DID
 
 export function useInitAgeAssurance() {
   const qc = useQueryClient()
   const agent = useAgent()
   return useMutation({
     async mutationFn(props: AppBskyUnspeccedInitAgeAssurance.InputSchema) {
+      const {
+        data: {token},
+      } = await agent.com.atproto.server.getServiceAuth({
+        aud: APPVIEW_DID,
+        lxm: `app.bsky.unspecced.initAgeAssurance`,
+      })
+
+      const appView = new AtpAgent({service: APPVIEW})
+      appView.sessionManager.session = agent.session!
+      appView.sessionManager.session.accessJwt = token
+
       /*
        * 2s wait is good actually. Email sending takes a hot sec and this helps
        * ensure the email is ready for the user once they open their inbox.
        */
       const {data} = await wait(
         2e3,
-        agent.app.bsky.unspecced.initAgeAssurance(props),
+        appView.app.bsky.unspecced.initAgeAssurance(props),
       )
 
       qc.setQueryData<AppBskyUnspeccedDefs.AgeAssuranceState>(
