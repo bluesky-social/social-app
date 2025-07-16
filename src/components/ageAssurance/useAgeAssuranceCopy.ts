75df5829447ae3b0f7a4cf1469be129ccc2d4b39
Eric Bailey <git@esb.lol>
2025-07-16 16:46:28 -0500

    Add feed banner

--- src/components/ageAssurance/useAgeAssuranceCopy.ts
+++ src/components/ageAssurance/useAgeAssuranceCopy.ts
@@ -10,6 +10,9 @@ export function useAgeAssuranceCopy() {
       notice: _(
         msg`The laws in your location require that you verify your age before accessing certain features on Bluesky like adult content and direct messaging.`,
       ),
+      banner: _(
+        msg`The laws in your location require you verify your age. Tap to learn more.`,
+      ),
       chatsInfoText: _(
         msg`Don't worry! All existing messages and settings are saved and will be available after you've been verified to be 18 or older.`,
       ),
