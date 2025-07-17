ef80de5e7916fc171e028b20c5e2db1d9aabc211
Eric Bailey <git@esb.lol>
2025-07-17 18:24:54 -0500

    Update copy

--- src/components/ageAssurance/useAgeAssuranceCopy.ts
+++ src/components/ageAssurance/useAgeAssuranceCopy.ts
@@ -11,7 +11,7 @@ export function useAgeAssuranceCopy() {
         msg`The laws in your location require you to verify you're an adult before accessing certain features on Bluesky, like adult content and direct messaging.`,
       ),
       banner: _(
-        msg`The laws in your location require you to verify you're an adult. Tap to learn more.`,
+        msg`The laws in your location require you to verify you're an adult to access certain features. Tap to learn more.`,
       ),
       chatsInfoText: _(
         msg`Don't worry! All existing messages and settings are saved and will be available after you verify you're an adult.`,
