09dcc384f479d7ea621ffff2288784c528bb4010
Eric Bailey <git@esb.lol>
2025-07-23 17:16:13 -0500

    [APP-1331] Migrate `app-info` to new env (#8703)
    * Move env files into directory with platform specific files
    
    * Migrate usages of app-info to new env
    
    * Fix bad import

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import {nativeBuildVersion} from 'expo-application'
+
+import packageJson from '#/../package.json'
+import {BUNDLE_IDENTIFIER, IS_TESTFLIGHT} from '#/env/common'
+
+export * from '#/env/common'
+
+/**
+ * The semver version of the app, specified in our `package.json`.file. On
+ * iOs/Android, the native build version is appended to the semver version, so
+ * that it can be used to identify a specific build.
+ */
+export const APP_VERSION = `${packageJson.version}.${nativeBuildVersion}`
+
+/**
+ * The short commit hash and environment of the current bundle.
+ */
+export const APP_METADATA = `${BUNDLE_IDENTIFIER} (${
+  __DEV__ ? 'dev' : IS_TESTFLIGHT ? 'tf' : 'prod'
+})`
