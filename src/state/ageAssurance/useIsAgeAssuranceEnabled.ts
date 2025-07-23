3ae3ed245bad12c2222ea04a9bf13ba4589e90ad
Eric Bailey <git@esb.lol>
2025-07-23 14:32:19 -0500

    Clean up AA gate (#8707)

--- src/state/ageAssurance/useIsAgeAssuranceEnabled.ts
+++ src/state/ageAssurance/useIsAgeAssuranceEnabled.ts
@@ -1,14 +1,11 @@
 import {useMemo} from 'react'
 
-import {useGate} from '#/lib/statsig/statsig'
 import {useGeolocation} from '#/state/geolocation'
 
 export function useIsAgeAssuranceEnabled() {
-  const gate = useGate()
   const {geolocation} = useGeolocation()
 
   return useMemo(() => {
-    const enabled = gate('age_assurance')
-    return enabled && !!geolocation?.isAgeRestrictedGeo
-  }, [geolocation, gate])
+    return !!geolocation?.isAgeRestrictedGeo
+  }, [geolocation])
 }
