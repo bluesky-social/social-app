3e988c030e1c72dc5a64e40cbde6112734386eb1
Hailey <me@haileyok.com>
2025-07-24 08:16:21 -0700

    rm exported const

--- src/state/ageAssurance/const.ts
+++ src/state/ageAssurance/const.ts
@@ -2,14 +2,10 @@ import {type ModerationPrefs} from '@atproto/api'
 
 import {DEFAULT_LOGGED_OUT_LABEL_PREFERENCES} from '#/state/queries/preferences/moderation'
 
-export const AGE_RESTRICTED_MODERATION_PREFS: Partial<ModerationPrefs> = {
-  adultContentEnabled: false,
-  labels: DEFAULT_LOGGED_OUT_LABEL_PREFERENCES,
-}
-
 export const makeAgeRestrictedModerationPrefs = (
   prefs: ModerationPrefs,
 ): ModerationPrefs => ({
   ...prefs,
-  ...AGE_RESTRICTED_MODERATION_PREFS,
+  adultContentEnabled: false,
+  labels: DEFAULT_LOGGED_OUT_LABEL_PREFERENCES,
 })
