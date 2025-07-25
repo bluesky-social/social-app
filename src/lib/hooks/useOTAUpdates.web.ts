8484eb239d69e0fb37e674c2802e31dd82603093
Hailey <me@haileyok.com>
2025-07-25 12:30:39 -0700

    add web hook

--- src/lib/hooks/useOTAUpdates.web.ts
+++ src/lib/hooks/useOTAUpdates.web.ts
@@ -1,1 +1,9 @@
 export function useOTAUpdates() {}
+export function useApplyPullRequestOTAUpdate() {
+  return {
+    tryApplyUpdate: () => {},
+    revertToEmbedded: () => {},
+    isCurrentlyRunningPullRequestDeployment: false,
+    currentChannel: 'web-build',
+  }
+}
