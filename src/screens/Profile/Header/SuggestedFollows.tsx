c91ed12e3b0f69ce875cf844d9cd516400dcfdfb
Caidan Williams <caidan@internet.dev>
2025-08-05 00:08:59 -0700

    fix: replace AnimatedSuggestedFollows with the AccordionAnimation component

--- src/screens/Profile/Header/SuggestedFollows.tsx
+++ src/screens/Profile/Header/SuggestedFollows.tsx
@@ -1,12 +1,3 @@
-import {useEffect, useState} from 'react'
-import {View} from 'react-native'
-import Animated, {
-  Easing,
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated'
-
 import {useSuggestedFollowsByActorQuery} from '#/state/queries/suggested-follows'
 import {ProfileGrid} from '#/components/FeedInterstitials'
 
@@ -23,32 +14,5 @@ export function ProfileHeaderSuggestedFollows({actorDid}: {actorDid: string}) {
       error={error}
       viewContext="profileHeader"
     />
-  )
-}
-
-export const AnimatedSuggestedFollows = ({actorDid}: {actorDid: string}) => {
-  const [contentHeight, setContentHeight] = useState(0)
-  const animatedHeight = useSharedValue(0)
-
-  useEffect(() => {
-    if (contentHeight > 0) {
-      animatedHeight.value = withTiming(contentHeight, {
-        duration: 300,
-        easing: Easing.inOut(Easing.cubic),
-      })
-    }
-  }, [contentHeight, animatedHeight])
-
-  const animatedStyle = useAnimatedStyle(() => ({
-    height: animatedHeight.value,
-    overflow: 'hidden',
-  }))
-
-  return (
-    <Animated.View style={animatedStyle}>
-      <View onLayout={e => setContentHeight(e.nativeEvent.layout.height)}>
-        <ProfileHeaderSuggestedFollows actorDid={actorDid} />
-      </View>
-    </Animated.View>
   )
 }
