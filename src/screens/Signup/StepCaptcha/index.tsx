f54e05e959134693e72c4fa926708e251475e4ff
Hailey <me@haileyok.com>
2025-08-07 09:11:34 -0700

    fallback if no env is set

--- src/screens/Signup/StepCaptcha/index.tsx
+++ src/screens/Signup/StepCaptcha/index.tsx
@@ -13,9 +13,11 @@ import {useSignupContext} from '#/screens/Signup/state'
 import {CaptchaWebView} from '#/screens/Signup/StepCaptcha/CaptchaWebView'
 import {atoms as a, useTheme} from '#/alf'
 import {FormError} from '#/components/forms/FormError'
+import {GCP_PROJECT_ID} from '#/env'
 import {BackNextButtons} from '../BackNextButtons'
 
-const CAPTCHA_PATH = isWeb ? '/gate/signup' : '/gate/signup/attempt-attest'
+const CAPTCHA_PATH =
+  isWeb || GCP_PROJECT_ID === 0 ? '/gate/signup' : '/gate/signup/attempt-attest'
 
 export function StepCaptcha() {
   if (isWeb) {
