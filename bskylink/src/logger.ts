6f8c23339739a1b504607b4f5947ab1079758caa
Hailey <me@haileyok.com>
2025-08-17 18:51:12 -0700

    tidy

--- bskylink/src/logger.ts
+++ bskylink/src/logger.ts
@@ -5,14 +5,11 @@ export const httpLogger: Logger = subsystemLogger('bskylink')
 export const dbLogger: Logger = subsystemLogger('bskylink:db')
 export const redirectLogger: Logger = subsystemLogger('bskylink:redirect')
 
-// Also log to stdout
 redirectLogger.info = (
   orig =>
   (...args: any[]) => {
     const [msg, ...rest] = args
     orig.apply(redirectLogger, [String(msg), ...rest])
-    // Print to stdout as well
-    // You can format this as needed
     console.log('[bskylink:redirect]', ...args)
   }
 )(redirectLogger.info) as typeof redirectLogger.info
