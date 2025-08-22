2f22e230c7b6c595e4d0c3eff8235646893c05f0
Samuel Newman <mozzius@protonmail.com>
2025-08-22 14:41:57 +0100

    add focus styles, remove focus when dragging

--- src/view/com/pager/DraggableScrollView.native.tsx
+++
@@ -1,8 +0,0 @@
-import {type ComponentPropsWithRef} from 'react'
-import {ScrollView} from 'react-native'
-
-export function DraggableScrollView(
-  props: ComponentPropsWithRef<typeof ScrollView>,
-) {
-  return <ScrollView horizontal {...props} />
-}
