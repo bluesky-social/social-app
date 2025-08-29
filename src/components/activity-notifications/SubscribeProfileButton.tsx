10a7d36ceca6093d27c6e0e663d2f205031a7e78
Samuel Newman <mozzius@protonmail.com>
2025-08-29 13:55:52 +0100

    add minimal header to profiles

--- src/components/activity-notifications/SubscribeProfileButton.tsx
+++ src/components/activity-notifications/SubscribeProfileButton.tsx
@@ -18,9 +18,11 @@ import {SubscribeProfileDialog} from './SubscribeProfileDialog'
 export function SubscribeProfileButton({
   profile,
   moderationOpts,
+  disableHint,
 }: {
   profile: bsky.profile.AnyProfileView
   moderationOpts: ModerationOpts
+  disableHint?: boolean
 }) {
   const {_} = useLingui()
   const requireEmailVerification = useRequireEmailVerification()
@@ -69,7 +71,7 @@ export function SubscribeProfileButton({
   return (
     <>
       <Tooltip.Outer
-        visible={showTooltip}
+        visible={showTooltip && !disableHint}
         onVisibleChange={onDismissTooltip}
         position="bottom">
         <Tooltip.Target>
