5ea3161f043791d21cd54384f534add425b49ba9
Hailey <me@haileyok.com>
2025-09-03 07:57:43 -0700

    add cursor constraint to safelink

--- bskylink/src/db/migrations/index.ts
+++ bskylink/src/db/migrations/index.ts
@@ -1,7 +1,9 @@
 import * as init from './001-init.js'
 import * as safelink from './002-safelink.js'
+import * as safelinkCursorConstraint from './003-safelink-cursor-constraint.js'
 
 export default {
   '001': init,
   '002': safelink,
+  '003': safelinkCursorConstraint,
 }
