01ba0d0ed61083a6d38f76733f57736c51659338
Samuel Newman <mozzius@protonmail.com>
2025-09-05 18:11:56 +0300

    Revert "use comment instead of context (#8987)" (#8990)
    This reverts commit c7f8da8f9768515be7d3c79e9128c0411390c48e.

--- src/view/shell/desktop/LeftNav.tsx
+++ src/view/shell/desktop/LeftNav.tsx
@@ -763,7 +763,7 @@ export function DesktopLeftNav() {
             label={_(
               msg({
                 message: 'Saved',
-                comment: 'link to bookmarks screen',
+                context: 'link to bookmarks screen',
               }),
             )}
           />
