5be753ecdfaf486c5d01d90c34008a1f5f275f00
Samuel Newman <mozzius@protonmail.com>
2025-09-10 17:44:19 +0300

    add prevent default to thread anchor checkmark (#9020)

--- src/components/verification/VerificationCheckButton.tsx
+++ src/components/verification/VerificationCheckButton.tsx
@@ -99,15 +99,15 @@ export function Badge({
             : _(msg`View this user's verifications`)
         }
         hitSlop={20}
-        onPress={() => {
+        onPress={evt => {
+          evt.preventDefault()
           logger.metric('verification:badge:click', {}, {statsig: true})
           if (state.profile.role === 'verifier') {
             verifierDialogControl.open()
           } else {
             verificationsDialogControl.open()
           }
-        }}
-        style={[]}>
+        }}>
         {({hovered}) => (
           <View
             style={[
