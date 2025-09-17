7a97ba8aa9294601c4113dddb2aa93c020f9a258
Caidan Williams <caidan@internet.dev>
2025-09-16 17:24:41 -0700

    debug(bskyweb): add logging to WebPost to diagnose .bsky.social handle issues
    - Add warning logs for parsing failures and authentication requirements
    - Will help identify where WebPost handler fails for .bsky.social handles
    - Temporary debugging commit to investigate OpenGraph tag issues

--- bskyweb/cmd/bskyweb/server.go
+++ bskyweb/cmd/bskyweb/server.go
@@ -465,11 +465,13 @@ func (srv *Server) WebPost(c echo.Context) error {
 	rkeyParam := c.Param("rkey")
 	rkey, err := syntax.ParseRecordKey(rkeyParam)
 	if err != nil {
+		log.Warnf("WebPost: failed to parse rkey %s: %v", rkeyParam, err)
 		return c.Render(http.StatusOK, "post.html", data)
 	}
 	handleOrDIDParam := c.Param("handleOrDID")
 	handleOrDID, err := syntax.ParseAtIdentifier(handleOrDIDParam)
 	if err != nil {
+		log.Warnf("WebPost: failed to parse identifier %s: %v", handleOrDIDParam, err)
 		return c.Render(http.StatusOK, "post.html", data)
 	}
 
@@ -489,6 +491,7 @@ func (srv *Server) WebPost(c echo.Context) error {
 	}
 
 	if !unauthedViewingOkay {
+		log.Warnf("WebPost: profile %s requires authentication", identifier)
 		return c.Render(http.StatusOK, "post.html", data)
 	}
 	did := pv.Did
