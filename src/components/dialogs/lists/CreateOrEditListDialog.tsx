1209c2f140110052558f5337d4c5e2fafa64c22d
Samuel Newman <mozzius@protonmail.com>
2025-09-24 10:41:54 +0300

    add corner radius to new dialog

--- src/components/dialogs/lists/CreateOrEditListDialog.tsx
+++ src/components/dialogs/lists/CreateOrEditListDialog.tsx
@@ -83,6 +83,7 @@ export function CreateOrEditListDialog({
       nativeOptions={{
         preventDismiss: dirty,
         minHeight: height,
+        cornerRadius: 20,
       }}
       testID="createOrEditListDialog">
       <DialogInner
