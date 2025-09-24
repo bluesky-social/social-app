d5a1c53d46bc620729aa1fed9c90f6d3a5f59dcb
Eric Hamilton <ericjean873@gmail.com>
2025-09-24 14:03:45 -0700

    Changed threaded view change operation to a save - testing this.

--- src/state/queries/usePostThread/index.ts
+++ src/state/queries/usePostThread/index.ts
@@ -47,7 +47,7 @@ export function usePostThread({anchor}: {anchor?: string}) {
     view,
     setView: baseSetView,
     prioritizeFollowedUsers,
-  } = useThreadPreferences()
+  } = useThreadPreferences({save: true})
   const below = useMemo(() => {
     return view === 'linear'
       ? LINEAR_VIEW_BELOW
