4197e0b316a8946eab9a4b7d4b5713cc93f675ad
Chenyu Huang <itschenyu@gmail.com>
2025-09-25 16:50:48 -0700

    design tweak

--- src/components/Admonition.tsx
+++ src/components/Admonition.tsx
@@ -1,7 +1,7 @@
-import {createContext, useContext, useMemo} from 'react'
+import {createContext, useContext} from 'react'
 import {type StyleProp, View, type ViewStyle} from 'react-native'
 
-import {atoms as a, useAlf, useBreakpoints, useTheme} from '#/alf'
+import {atoms as a, useBreakpoints, useTheme} from '#/alf'
 import {Button as BaseButton, type ButtonProps} from '#/components/Button'
 import {CircleInfo_Stroke2_Corner0_Rounded as CircleInfoIcon} from '#/components/icons/CircleInfo'
 import {CircleX_Stroke2_Corner0_Rounded as CircleXIcon} from '#/components/icons/CircleX'
@@ -44,22 +44,23 @@ export function Text({
   style,
   ...rest
 }: Pick<TextProps, 'children' | 'style'>) {
-  const {fontScaleCompensation} = useAdmonitionFontScaleCompensation()
-
   return (
-    <View
+    <BaseText
+      {...rest}
       style={[
         a.flex_1,
+        a.text_sm,
+        a.leading_snug,
+        a.pr_md,
+        style,
         {
-          top: fontScaleCompensation,
+          minHeight: 20,
+          display: 'flex',
+          alignItems: 'center',
         },
       ]}>
-      <BaseText
-        {...rest}
-        style={[a.flex_1, a.text_sm, a.leading_snug, a.pr_md, style]}>
-        {children}
-      </BaseText>
-    </View>
+      {children}
+    </BaseText>
   )
 }
 
@@ -128,21 +129,5 @@ export function Admonition({
         <Text>{children}</Text>
       </Row>
     </Outer>
-  )
-}
-
-// This is very vibe based, but it seems to work well enough across all 3 font scales.
-function useAdmonitionFontScaleCompensation() {
-  const {fonts} = useAlf()
-  const fontScaleCompensation = useMemo(() => {
-    const scale = parseInt(fonts.scale, 10)
-    return 1.0 - 0.57 * scale
-  }, [fonts.scale])
-
-  return useMemo(
-    () => ({
-      fontScaleCompensation,
-    }),
-    [fontScaleCompensation],
   )
 }
