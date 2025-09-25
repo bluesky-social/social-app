b40e5ec8152f3e4be4c9b5905126f5fdc9570bc6
Samuel Newman <mozzius@protonmail.com>
2025-09-25 18:26:25 +0300

    Fix `extToMime` function (#9084)
    (cherry picked from commit 66526a8e107e07303b4de039525b446c8d02c2df)

--- src/lib/media/video/util.ts
+++ src/lib/media/video/util.ts
@@ -40,7 +40,7 @@ export function mimeToExt(mimeType: SupportedMimeTypes | (string & {})) {
 }
 
 export function extToMime(ext: string) {
-  switch (ext) {
+  switch (ext.toLowerCase()) {
     case 'mp4':
       return 'video/mp4'
     case 'webm':
