ceef065b0280b79fd6aa6c2d51a8de6aee26d1a0
Samuel Newman <mozzius@protonmail.com>
2025-09-29 11:45:33 +0300

    don't send some "activity no longer available" errors

--- src/alf/util/systemUI.ts
+++ src/alf/util/systemUI.ts
@@ -1,14 +1,20 @@
 import * as SystemUI from 'expo-system-ui'
 import {type Theme} from '@bsky.app/alf'
 
+import {logger} from '#/logger'
 import {isAndroid} from '#/platform/detection'
 
 export function setSystemUITheme(themeType: 'theme' | 'lightbox', t: Theme) {
   if (isAndroid) {
-    if (themeType === 'theme') {
-      SystemUI.setBackgroundColorAsync(t.atoms.bg.backgroundColor)
-    } else {
-      SystemUI.setBackgroundColorAsync('black')
+    try {
+      if (themeType === 'theme') {
+        SystemUI.setBackgroundColorAsync(t.atoms.bg.backgroundColor)
+      } else {
+        SystemUI.setBackgroundColorAsync('black')
+      }
+    } catch (error) {
+      // Can reject with 'The current activity is no longer available' - no big deal
+      logger.debug('Could not set system UI theme', {safeMessage: error})
     }
   }
 }
