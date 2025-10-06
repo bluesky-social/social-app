163941e584c2e906c56fff752718af52908e433a
Samuel Newman <mozzius@protonmail.com>
2025-10-06 20:39:51 +0300

    Revert "specify ndk version"
    This reverts commit 577393518e67ee59028b0c775536f53605dba6d4.

--- app.config.js
+++ app.config.js
@@ -215,7 +215,6 @@ module.exports = function (_config) {
               compileSdkVersion: 35,
               targetSdkVersion: 35,
               buildToolsVersion: '35.0.0',
-              ndkVersion: '27.3.13750724',
             },
           },
         ],
