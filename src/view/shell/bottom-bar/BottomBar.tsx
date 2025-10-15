fc944841ce3ad31f0b601eed62001ff3614fbbac
Samuel Newman <mozzius@protonmail.com>
2025-10-15 19:02:47 +0300

    add large content viewer to tabs (#8394)

--- src/view/shell/bottom-bar/BottomBar.tsx
+++ src/view/shell/bottom-bar/BottomBar.tsx
@@ -405,7 +405,9 @@ function Btn({
       accessible={accessible}
       accessibilityLabel={accessibilityLabel}
       accessibilityHint={accessibilityHint}
-      targetScale={0.8}>
+      targetScale={0.8}
+      accessibilityLargeContentTitle={accessibilityLabel}
+      accessibilityShowsLargeContentViewer>
       {icon}
       {notificationCount ? (
         <View style={[styles.notificationCount, a.rounded_full]}>
