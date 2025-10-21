1838f4a6dcba4e196d0f9226b4f0f52a5e4f0220
Eric Bailey <git@esb.lol>
2025-10-21 09:52:40 -0500

    Add nativeCrash tester

--- src/view/screens/Storybook/index.tsx
+++ src/view/screens/Storybook/index.tsx
@@ -3,6 +3,7 @@ import {View} from 'react-native'
 import {useNavigation} from '@react-navigation/native'
 
 import {type NavigationProp} from '#/lib/routes/types'
+import {Sentry} from '#/logger/sentry/lib'
 import {useSetThemePrefs} from '#/state/shell'
 import {ListContained} from '#/view/screens/Storybook/ListContained'
 import {atoms as a, ThemeProvider} from '#/alf'
@@ -94,6 +95,13 @@ function StorybookInner() {
               label="two"
               testID="sharedPrefsTestOpenBtn">
               <ButtonText>Open Shared Prefs Tester</ButtonText>
+            </Button>
+            <Button
+              color="negative"
+              size="large"
+              onPress={() => Sentry.nativeCrash()}
+              label="crash">
+              <ButtonText>Sentry Crash</ButtonText>
             </Button>
 
             <ThemeProvider theme="light">
