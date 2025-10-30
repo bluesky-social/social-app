74048945e62108c78ef98fd4ae926cd004377dbb
Samuel Newman <mozzius@protonmail.com>
2025-10-30 13:37:42 +0200

    give up on expo-glass-effect for now

--- src/platform/detection.ts
+++ src/platform/detection.ts
@@ -1,5 +1,4 @@
 import {Platform} from 'react-native'
-import {isLiquidGlassAvailable} from 'expo-glass-effect'
 
 export const isIOS = Platform.OS === 'ios'
 export const isAndroid = Platform.OS === 'android'
@@ -12,4 +11,10 @@ export const isMobileWeb =
   global.window.matchMedia(isMobileWebMediaQuery)?.matches
 export const isIPhoneWeb = isWeb && /iPhone/.test(navigator.userAgent)
 
-export const isLiquid = isIOS && isLiquidGlassAvailable()
+// ideally we'd use isLiquidGlassAvailable() from expo-glass-effect but checking iOS version is good enough for now
+// for some reason Platform.OS === 'ios' AND Platform.Version is undefined in our CI unit tests -sfn
+const iOSMajorVersion =
+  Platform.OS === 'ios' && typeof Platform.Version === 'string'
+    ? parseInt(Platform.Version.split('.')[0], 10)
+    : 0
+export const isLiquid = iOSMajorVersion >= 26
