01896ff8b9f0236d70df04a86752e687ab8c45cb
Samuel Newman <mozzius@protonmail.com>
2025-11-13 20:20:57 +0200

    [Threadgate] Add hint (#9350)
    * get tooltip working on web
    
    * add compatibility layer for working in iOS sheets
    
    * add timeout to profile tooltip now that it appears instantly
    
    * rm debug code
    
    * Update ThreadgateBtn.tsx
    
    Co-authored-by: surfdude29 <149612116+surfdude29@users.noreply.github.com>
    
    * remeasure when keyboard changes
    
    ---------
    
    Co-authored-by: surfdude29 <149612116+surfdude29@users.noreply.github.com>

--- src/view/com/composer/threadgate/ThreadgateBtn.tsx
+++ src/view/com/composer/threadgate/ThreadgateBtn.tsx
@@ -1,7 +1,8 @@
+import {useEffect, useState} from 'react'
 import {Keyboard, type StyleProp, type ViewStyle} from 'react-native'
 import {type AnimatedStyle} from 'react-native-reanimated'
 import {type AppBskyFeedPostgate} from '@atproto/api'
-import {msg} from '@lingui/macro'
+import {msg, Trans} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 
 import {isNative} from '#/platform/detection'
@@ -12,6 +13,9 @@ import {PostInteractionSettingsControlledDialog} from '#/components/dialogs/Post
 import {TinyChevronBottom_Stroke2_Corner0_Rounded as TinyChevronIcon} from '#/components/icons/Chevron'
 import {Earth_Stroke2_Corner0_Rounded as EarthIcon} from '#/components/icons/Globe'
 import {Group3_Stroke2_Corner0_Rounded as GroupIcon} from '#/components/icons/Group'
+import * as Tooltip from '#/components/Tooltip'
+import {Text} from '#/components/Typography'
+import {useThreadgateNudged} from '#/storage/hooks/threadgate-nudged'
 
 export function ThreadgateBtn({
   postgate,
@@ -29,11 +33,31 @@ export function ThreadgateBtn({
 }) {
   const {_} = useLingui()
   const control = Dialog.useDialogControl()
+  const [threadgateNudged, setThreadgateNudged] = useThreadgateNudged()
+  const [showTooltip, setShowTooltip] = useState(false)
+
+  useEffect(() => {
+    if (!threadgateNudged) {
+      const timeout = setTimeout(() => {
+        setShowTooltip(true)
+      }, 1000)
+      return () => clearTimeout(timeout)
+    }
+  }, [threadgateNudged])
+
+  const onDismissTooltip = (visible: boolean) => {
+    if (visible) return
+    setThreadgateNudged(true)
+    setShowTooltip(false)
+  }
 
   const onPress = () => {
     if (isNative && Keyboard.isVisible()) {
       Keyboard.dismiss()
     }
+
+    setShowTooltip(false)
+    setThreadgateNudged(true)
 
     control.open()
   }
@@ -50,19 +74,32 @@ export function ThreadgateBtn({
 
   return (
     <>
-      <Button
-        color="secondary"
-        size="small"
-        testID="openReplyGateButton"
-        onPress={onPress}
-        label={label}
-        accessibilityHint={_(
-          msg`Opens a dialog to choose who can reply to this thread`,
-        )}>
-        <ButtonIcon icon={anyoneCanInteract ? EarthIcon : GroupIcon} />
-        <ButtonText numberOfLines={1}>{label}</ButtonText>
-        <ButtonIcon icon={TinyChevronIcon} size="2xs" />
-      </Button>
+      <Tooltip.Outer
+        visible={showTooltip}
+        onVisibleChange={onDismissTooltip}
+        position="top">
+        <Tooltip.Target>
+          <Button
+            color={showTooltip ? 'primary_subtle' : 'secondary'}
+            size="small"
+            testID="openReplyGateButton"
+            onPress={onPress}
+            label={label}
+            accessibilityHint={_(
+              msg`Opens a dialog to choose who can interact with this post`,
+            )}>
+            <ButtonIcon icon={anyoneCanInteract ? EarthIcon : GroupIcon} />
+            <ButtonText numberOfLines={1}>{label}</ButtonText>
+            <ButtonIcon icon={TinyChevronIcon} size="2xs" />
+          </Button>
+        </Tooltip.Target>
+        <Tooltip.TextBubble>
+          <Text>
+            <Trans>Psst! You can edit who can interact with this post.</Trans>
+          </Text>
+        </Tooltip.TextBubble>
+      </Tooltip.Outer>
+
       <PostInteractionSettingsControlledDialog
         control={control}
         onSave={() => {
