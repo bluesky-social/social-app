19ab11bb6948d590262752319985271f3a98ccce
Samuel Newman <mozzius@protonmail.com>
2025-11-17 16:37:36 +0200

    remove tooltip in e2e

---
+++ src/components/Tooltip/index.e2e.tsx
@@ -0,0 +1,23 @@
+export function SheetCompatProvider({children}: {children: React.ReactNode}) {
+  return children
+}
+
+export function Outer({children}: {children: React.ReactNode}) {
+  return children
+}
+
+export function Target({children}: {children: React.ReactNode}) {
+  return children
+}
+
+export function Content() {
+  return null
+}
+
+function Bubble() {
+  return null
+}
+
+export function TextBubble({children}: {children: React.ReactNode}) {
+  return null
+}
