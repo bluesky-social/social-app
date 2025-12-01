b9944db7f2cb60388c7b16c629d5e0d7c93a98c2
Samuel Newman <mozzius@protonmail.com>
2025-12-01 13:49:43 +0200

    car dialog add close button

--- src/screens/Settings/components/ExportCarDialog.tsx
+++ src/screens/Settings/components/ExportCarDialog.tsx
@@ -7,7 +7,7 @@ import {saveBytesToDisk} from '#/lib/media/manip'
 import {logger} from '#/logger'
 import {useAgent} from '#/state/session'
 import * as Toast from '#/view/com/util/Toast'
-import {atoms as a, useTheme} from '#/alf'
+import {atoms as a, useTheme, web} from '#/alf'
 import {Button, ButtonIcon, ButtonText} from '#/components/Button'
 import * as Dialog from '#/components/Dialog'
 import {Download_Stroke2_Corner0_Rounded as DownloadIcon} from '#/components/icons/Download'
@@ -56,7 +56,8 @@ export function ExportCarDialog({
       <Dialog.Handle />
       <Dialog.ScrollableInner
         accessibilityDescribedBy="dialog-description"
-        accessibilityLabelledBy="dialog-title">
+        accessibilityLabelledBy="dialog-title"
+        style={web({maxWidth: 500})}>
         <View style={[a.relative, a.gap_lg, a.w_full]}>
           <Text nativeID="dialog-title" style={[a.text_2xl, a.font_bold]}>
             <Trans>Export My Data</Trans>
@@ -105,6 +106,7 @@ export function ExportCarDialog({
             </Trans>
           </Text>
         </View>
+        <Dialog.Close />
       </Dialog.ScrollableInner>
     </Dialog.Outer>
   )
