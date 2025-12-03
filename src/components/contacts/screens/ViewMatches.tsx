80fc3a221dd189098833b6fa4defa7aa00808385
Samuel Newman <mozzius@protonmail.com>
2025-12-03 15:41:44 +0200

    api integration for flow

--- src/components/contacts/screens/ViewMatches.tsx
+++ src/components/contacts/screens/ViewMatches.tsx
@@ -90,7 +90,7 @@ export function ViewMatches({
   const {data: profiles} = useProfilesQuery({
     handles: ['pfrazee.com', 'internet.bsky.social', 'darrin.bsky.team'],
   })
-  state.matches = profiles?.profiles?.map(profile => ({profile})) ?? []
+  state.matches = profiles?.profiles ?? []
   // state.contacts = []
 
   const [search, setSearch] = useState('')
@@ -101,7 +101,7 @@ export function ViewMatches({
   } = useInteractionState()
 
   const followableDids = state.matches
-    .map(match => match.profile.did)
+    .map(match => match.did)
     .filter(did => !state.dismissedMatches.includes(did))
   const [didFollowAll, setDidFollowAll] = useState(followableDids.length === 0)
 
@@ -144,20 +144,16 @@ export function ViewMatches({
 
     if (searchFocused || search.length > 0) {
       for (const match of state.matches) {
-        const profile = match.profile
-
-        if (state.dismissedMatches.includes(profile.did)) continue
+        if (state.dismissedMatches.includes(match.did)) continue
 
         if (
           search.length === 0 ||
-          (profile.displayName ?? '')
+          (match.displayName ?? '')
             .toLocaleLowerCase()
             .includes(search.toLocaleLowerCase()) ||
-          profile.handle
-            .toLocaleLowerCase()
-            .includes(search.toLocaleLowerCase())
+          match.handle.toLocaleLowerCase().includes(search.toLocaleLowerCase())
         ) {
-          all.push({type: 'match', profile})
+          all.push({type: 'match', profile: match})
         }
       }
 
@@ -179,14 +175,13 @@ export function ViewMatches({
       }
     } else {
       const matches = state.matches.filter(
-        match => !state.dismissedMatches.includes(match.profile.did),
+        match => !state.dismissedMatches.includes(match.did),
       )
 
       if (matches.length > 0) {
         all.push({type: 'matches header', count: matches.length})
         for (const match of matches) {
-          const profile = match.profile
-          all.push({type: 'match', profile})
+          all.push({type: 'match', profile: match})
         }
 
         if (state.contacts.length > 0) {
