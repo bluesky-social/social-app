6056c6978d19d3b2149c566c698e42c460c93ab3
surfdude29 <149612116+surfdude29@users.noreply.github.com>
2025-12-15 15:15:52 +0000

    Restore notification count in BottomBar (#9554)
    (cherry picked from commit 348e7fa379859ff4a9b33871e1c8dc0e8a613697)

--- src/view/shell/bottom-bar/BottomBar.tsx
+++ src/view/shell/bottom-bar/BottomBar.tsx
@@ -418,7 +418,7 @@ function Btn({
             a.rounded_full,
             {backgroundColor: t.palette.primary_500},
           ]}>
-          <Text style={styles.notificationCountLabel}>1</Text>
+          <Text style={styles.notificationCountLabel}>{notificationCount}</Text>
         </View>
       ) : hasNew ? (
         <View style={[styles.hasNewBadge, a.rounded_full]} />
