9893403165e351493be24ac87a27093f75793423
Samuel Newman <mozzius@protonmail.com>
2025-12-26 16:35:09 +0000

    rm deep imports

--- modules/react-native-scroll-forwarder/src/ScrollForwarderViewNativeComponent.ts
+++ modules/react-native-scroll-forwarder/src/ScrollForwarderViewNativeComponent.ts
@@ -1,16 +1,15 @@
-import {type ViewProps} from 'react-native'
 import {
-  type BubblingEventHandler,
-  type Int32,
-} from 'react-native/Libraries/Types/CodegenTypes'
-import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent'
+  codegenNativeComponent,
+  type CodegenTypes,
+  type ViewProps,
+} from 'react-native'
 
 type OnRefreshEvent = {}
 
 export interface NativeProps extends ViewProps {
-  scrollViewTag: Int32 | null
+  scrollViewTag: CodegenTypes.Int32 | null
   refreshing?: boolean
-  onRefresh?: BubblingEventHandler<OnRefreshEvent>
+  onRefresh?: CodegenTypes.BubblingEventHandler<OnRefreshEvent>
 }
 
 export default codegenNativeComponent<NativeProps>('ScrollForwarderView')
