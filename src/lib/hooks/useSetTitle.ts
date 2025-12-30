8b1031a2eebb34a8481b5a4f9c9f6c5bf52f01c6
Samuel Newman <mozzius@protonmail.com>
2025-12-30 20:28:29 +0200

    Update useSetTitle.ts

--- src/lib/hooks/useSetTitle.ts
+++ src/lib/hooks/useSetTitle.ts
@@ -2,14 +2,12 @@ import {useEffect} from 'react'
 import {useNavigation} from '@react-navigation/native'
 
 import {type NavigationProp} from '#/lib/routes/types'
-import {useUnreadNotifications} from '#/state/queries/notifications/unread'
 
 export function useSetTitle(title?: string) {
   const navigation = useNavigation<NavigationProp>()
-  const numUnread = useUnreadNotifications()
   useEffect(() => {
     if (title) {
       navigation.setOptions({title})
     }
-  }, [title, navigation, numUnread])
+  }, [title, navigation])
 }
