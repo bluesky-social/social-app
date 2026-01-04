be2d4120c3915e07582fe6c78bc864f89a9a5865
Alex Benzer <abenzer@gmail.com>
2026-01-03 20:18:24 -0800

    Update feed drag-and-drop handle icon

---
+++ src/components/icons/Grip.tsx
@@ -0,0 +1,5 @@
+import {createSinglePathSVG} from './TEMPLATE'
+
+export const Grip_Stroke2_Corner0_Rounded = createSinglePathSVG({
+  path: 'M 7.5 6 a 1.5 1.5 0 1 0 3 0 a 1.5 1.5 0 1 0 -3 0 M 13.5 6 a 1.5 1.5 0 1 0 3 0 a 1.5 1.5 0 1 0 -3 0 M 7.5 12 a 1.5 1.5 0 1 0 3 0 a 1.5 1.5 0 1 0 -3 0 M 13.5 12 a 1.5 1.5 0 1 0 3 0 a 1.5 1.5 0 1 0 -3 0 M 7.5 18 a 1.5 1.5 0 1 0 3 0 a 1.5 1.5 0 1 0 -3 0 M 13.5 18 a 1.5 1.5 0 1 0 3 0 a 1.5 1.5 0 1 0 -3 0',
+})
