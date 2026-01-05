b7511282fcd1c88a663a18fbb6bc0d40ef0f03cc
Fran Aguilera <fran@bitdrift.io>
2026-01-05 13:10:25 +0100

    Adding flag for js errors

--- src/logger/bitdrift/setup/index.ts
+++ src/logger/bitdrift/setup/index.ts
@@ -7,12 +7,16 @@ import {BITDRIFT_API_KEY} from '#/env'
 initPromise.then(() => {
   let isEnabled = false
   let isNetworkEnabled = false
+  let isJsErrorsEnabled = false
   try {
     if (Statsig.checkGate('enable_bitdrift_v2')) {
       isEnabled = true
     }
     if (Statsig.checkGate('enable_bitdrift_v2_networking')) {
       isNetworkEnabled = true
+    }
+    if (Statsig.checkGate('enable_bitdrift_v2_js_errors')) {
+      isJsErrorsEnabled = true
     }
   } catch (e) {
     // Statsig may complain about it being called too early.
@@ -22,6 +26,10 @@ initPromise.then(() => {
       url: 'https://api-bsky.bitdrift.io',
       // Only effects iOS, Android instrumentation is set via Gradle Plugin
       enableNetworkInstrumentation: isNetworkEnabled,
+      crashReporting: {
+        enableNativeFatalIssues: true,
+        UNSTABLE_enableJsErrors: isJsErrorsEnabled,
+      },
     })
   }
 })
