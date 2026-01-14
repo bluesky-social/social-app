f3beff7c8478c6461885ea578752939d6ace7636
Eric Bailey <git@esb.lol>
2026-01-14 11:30:41 -0600

    Show multiple feeds on Explore for bsky team

--- src/features/liveEvents/components/ExploreScreenLiveEventFeedsBanner.tsx
+++ src/features/liveEvents/components/ExploreScreenLiveEventFeedsBanner.tsx
@@ -7,11 +7,11 @@ import {useLiveEvents} from '#/features/liveEvents/context'
 export function ExploreScreenLiveEventFeedsBanner() {
   const t = useTheme()
   const events = useLiveEvents()
-  const feed = events.feeds.at(0)
-  if (!feed) return null
-  return (
-    <View style={[a.p_lg, a.border_b, t.atoms.border_contrast_low]}>
+  return events.feeds.map(feed => (
+    <View
+      key={feed.id}
+      style={[a.p_lg, a.border_b, t.atoms.border_contrast_low]}>
       <LiveEventFeedCardWide feed={feed} metricContext="explore" />
     </View>
-  )
+  ))
 }
