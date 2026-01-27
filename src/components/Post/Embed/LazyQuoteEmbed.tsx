f70db59f37033699249ee68077806f68f8cf63d1
0rig0-del <257399774+0rig0-del@users.noreply.github.com>
2026-01-27 16:06:47 +1100

    Add compact quote post display for media-on-media quotes

--- src/components/Post/Embed/LazyQuoteEmbed.tsx
+++ src/components/Post/Embed/LazyQuoteEmbed.tsx
@@ -4,9 +4,17 @@ import {View} from 'react-native'
 import {createEmbedViewRecordFromPost} from '#/state/queries/postgate/util'
 import {useResolveLinkQuery} from '#/state/queries/resolve-link'
 import {atoms as a, useTheme} from '#/alf'
-import {QuoteEmbed} from '#/components/Post/Embed'
+import {QuoteEmbed, QuoteEmbedViewContext} from '#/components/Post/Embed'
+import {embedHasMedia} from '#/components/Post/Embed/util'
+import {parseEmbed} from '#/types/bsky/post'
 
-export function LazyQuoteEmbed({uri}: {uri: string}) {
+export function LazyQuoteEmbed({
+  uri,
+  parentHasMedia,
+}: {
+  uri: string
+  parentHasMedia?: boolean
+}) {
   const t = useTheme()
   const {data} = useResolveLinkQuery(uri)
 
@@ -15,12 +23,25 @@ export function LazyQuoteEmbed({uri}: {uri: string}) {
     return createEmbedViewRecordFromPost(data.view)
   }, [data])
 
+  // Determine if we should use compact layout
+  const viewContext = useMemo(() => {
+    if (!parentHasMedia || !view) return undefined
+    const quotedEmbed = view.embeds?.[0]
+      ? parseEmbed(view.embeds[0])
+      : undefined
+    if (embedHasMedia(quotedEmbed)) {
+      return QuoteEmbedViewContext.CompactWithMedia
+    }
+    return undefined
+  }, [parentHasMedia, view])
+
   return view ? (
     <QuoteEmbed
       embed={{
         type: 'post',
         view,
       }}
+      viewContext={viewContext}
     />
   ) : (
     <View
