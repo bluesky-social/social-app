62c5186762f1afe5cd1b348564542330375a9c9c
Samuel Newman <mozzius@protonmail.com>
2026-01-28 11:08:09 +0200

    update dynamic app icon lib

--- app.config.js
+++ app.config.js
@@ -335,7 +335,7 @@ module.exports = function (_config) {
           },
         ],
         [
-          '@mozzius/expo-dynamic-app-icon',
+          '@bsky.app/expo-dynamic-app-icon',
           {
             /**
              * Default set
