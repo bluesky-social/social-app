4e89ffc7905e539445d3644bf3a7d76141e91bc7
Eric Bailey <git@esb.lol>
2026-02-13 14:39:12 -0600

    Add network retries and ensure only one attempt

--- src/state/appConfig.tsx
+++ src/state/appConfig.tsx
@@ -1,6 +1,7 @@
 import {createContext, useContext} from 'react'
 import {QueryClient, useQuery} from '@tanstack/react-query'
 
+import {networkRetry} from '#/lib/async/retry'
 import {APP_CONFIG_URL} from '#/env'
 
 const qc = new QueryClient()
@@ -26,9 +27,22 @@ export const DEFAULT_APP_CONFIG_RESPONSE: AppConfigResponse = {
   },
 }
 
+let fetchAppConfigPromise: Promise<Response>
+
 async function fetchAppConfig(): Promise<AppConfigResponse | null> {
   try {
-    const res = await fetch(`${APP_CONFIG_URL}/config`)
+    if (!fetchAppConfigPromise) {
+      fetchAppConfigPromise = networkRetry(
+        3,
+        async () => {
+          const r = await fetch(`${APP_CONFIG_URL}/config`)
+          if (!r.ok) throw new Error(await r.text())
+          return r
+        },
+        1e3,
+      )
+    }
+    const res = await fetchAppConfigPromise
     if (!res.ok) return null
     const data = await res.json()
     return data
