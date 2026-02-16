10e4fe70ff7d0aa1543efc0cd4820c27c30080a5
Eric Bailey <git@esb.lol>
2026-02-16 10:15:15 -0600

    Region
    Co-authored-by: surfdude29 <149612116+surfdude29@users.noreply.github.com>

--- src/components/ageAssurance/useAgeAssuranceCopy.ts
+++ src/components/ageAssurance/useAgeAssuranceCopy.ts
@@ -20,7 +20,7 @@ export function useAgeAssuranceCopy() {
             msg`Due to laws in your region, certain features on Bluesky are currently restricted until you're able to verify you're an adult.`,
           ),
       banner: _(
-        msg`The laws in your location require you to verify you're an adult to access certain features. Tap to learn more.`,
+        msg`The laws in your region require you to verify you're an adult to access certain features. Tap to learn more.`,
       ),
       chatsInfoText: _(
         msg`Don't worry! All existing messages and settings are saved and will be available after you verify you're an adult.`,
