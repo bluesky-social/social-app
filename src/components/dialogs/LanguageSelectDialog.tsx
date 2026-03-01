0279fcadf3c875ca3533ebc28545bd69ac4b8e45
Samuel Newman <mozzius@protonmail.com>
2026-03-01 22:53:35 +0200

    style nit

--- src/components/dialogs/LanguageSelectDialog.tsx
+++ src/components/dialogs/LanguageSelectDialog.tsx
@@ -56,11 +56,7 @@ export function LanguageSelectDialog({
   )
 
   return (
-    <Dialog.Outer
-      control={control}
-      nativeOptions={{
-        fullHeight: true,
-      }}>
+    <Dialog.Outer control={control} nativeOptions={{fullHeight: true}}>
       <Dialog.Handle />
       <ErrorBoundary renderError={renderErrorBoundary}>
         <DialogInner
