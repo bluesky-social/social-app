267fbee474f2076d1f374b4368a83c9ec7338989
DS Boyce <260543580+ds-boyce@users.noreply.github.com>
2026-03-04 16:29:45 -0800

    Add TODO for guessLanguage utility

--- src/lib/translation/utils.ts
+++ src/lib/translation/utils.ts
@@ -2,6 +2,7 @@ import lande from 'lande'
 
 import {code3ToCode2Strict} from '#/locale/helpers'
 
+// TODO: Replace with expo-guess-language
 export function guessLanguage(text: string): string | null {
   const results = lande(text)
   // only return high-confidence results
