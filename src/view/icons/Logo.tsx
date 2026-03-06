e62a1e3b5eff8fe017551df335bd8c2f94eabca1
Ark <discord.ark@outlook.com>
2026-03-06 11:06:05 +0000

    Replaced logos

--- src/view/icons/Logo.tsx
+++ src/view/icons/Logo.tsx
@@ -12,6 +12,7 @@ import {Image} from 'expo-image'
 
 import {useKawaiiMode} from '#/state/preferences/kawaii'
 import {flatten, useTheme} from '#/alf'
+import LogoSvg from '../../../assets/icons/logomark.svg'
 
 const ratio = 57 / 64
 
@@ -28,7 +29,7 @@ export const Logo = React.forwardRef(function LogoImpl(props: Props, ref) {
   const _fill = gradient
     ? 'url(#sky)'
     : fill || styles?.color || t.palette.primary_500
-  // @ts-ignore it's fiiiiine
+  // @ts-ignore
   const size = parseInt(rest.width || 32, 10)
 
   const isKawaii = useKawaiiMode()
@@ -50,26 +51,14 @@ export const Logo = React.forwardRef(function LogoImpl(props: Props, ref) {
   }
 
   return (
-    <Svg
-      fill="none"
-      // @ts-ignore it's fiiiiine
-      ref={ref}
-      viewBox="0 0 64 57"
+    <LogoSvg
+      width={size}
+      height={size * ratio}
+      fill={_fill}
+      style={styles}
       {...rest}
-      style={[{width: size, height: size * ratio}, styles]}>
-      {gradient && (
-        <Defs>
-          <LinearGradient id="sky" x1="0" y1="0" x2="0" y2="1">
-            <Stop offset="0" stopColor="#0A7AFF" stopOpacity="1" />
-            <Stop offset="1" stopColor="#59B9FF" stopOpacity="1" />
-          </LinearGradient>
-        </Defs>
-      )}
-
-      <Path
-        fill={_fill}
-        d="M13.873 3.805C21.21 9.332 29.103 20.537 32 26.55v15.882c0-.338-.13.044-.41.867-1.512 4.456-7.418 21.847-20.923 7.944-7.111-7.32-3.819-14.64 9.125-16.85-7.405 1.264-15.73-.825-18.014-9.015C1.12 23.022 0 8.51 0 6.55 0-3.268 8.579-.182 13.873 3.805ZM50.127 3.805C42.79 9.332 34.897 20.537 32 26.55v15.882c0-.338.13.044.41.867 1.512 4.456 7.418 21.847 20.923 7.944 7.111-7.32 3.819-14.64-9.125-16.85 7.405 1.264 15.73-.825 18.014-9.015C62.88 23.022 64 8.51 64 6.55c0-9.818-8.578-6.732-13.873-2.745Z"
-      />
-    </Svg>
+      // @ts-ignore
+      ref={ref}
+    />
   )
 })
