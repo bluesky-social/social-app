cc06669c0e87f007a81437daa6d8d636a78933ce
DS Boyce <260543580+ds-boyce@users.noreply.github.com>
2026-03-19 14:53:37 -0700

    Fix issue with site loading on older versions of Safari

--- src/lib/media/util.ts
+++ src/lib/media/util.ts
@@ -36,12 +36,14 @@ export type ImgproxyPreset =
   | 'feed_thumbnail'
   | 'download'
 
+// Using capturing groups here instead of lookbehinds in order to support older versions of Safari.
+// https://bugs.webkit.org/show_bug.cgi?id=174931
 const IMGPROXY_PRESET_RE =
-  /(?<=\/img\/)(default|avatar_thumbnail|avatar|banner|feed_fullsize|feed_thumbnail|download)(?=\/)/
+  /(\/img\/)(default|avatar_thumbnail|avatar|banner|feed_fullsize|feed_thumbnail|download)(\/)/
 
 /**
  * Replaces any imgproxy preset in a CDN URI with the given preset.
  */
 export function convertCdnPreset(uri: string, preset: ImgproxyPreset): string {
-  return uri.replace(IMGPROXY_PRESET_RE, preset)
+  return uri.replace(IMGPROXY_PRESET_RE, `$1${preset}$3`)
 }
