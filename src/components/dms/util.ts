b2c99544b7ad4008154c434e212819f1d9efacaa
Samuel Newman <mozzius@protonmail.com>
2026-04-16 11:11:10 +0300

    add utility for making convo view easier to work with

--- src/components/dms/util.ts
+++ src/components/dms/util.ts
@@ -1,7 +1,7 @@
-import {type ChatBskyConvoDefs} from '@atproto/api'
+import {type $Typed, ChatBskyActorDefs, ChatBskyConvoDefs} from '@atproto/api'
 
 import {EMOJI_REACTION_LIMIT} from '#/lib/constants'
-import type * as bsky from '#/types/bsky'
+import * as bsky from '#/types/bsky'
 
 export function canBeMessaged(profile: bsky.profile.AnyProfileView) {
   switch (profile.associated?.chat?.allowIncoming) {
@@ -53,4 +53,101 @@ export function hasReachedReactionLimit(
     reaction => reaction.sender.did === myDid,
   )
   return myReactions.length >= EMOJI_REACTION_LIMIT
+}
+
+type GroupConvoMember = ChatBskyActorDefs.ProfileViewBasic & {
+  kind: $Typed<ChatBskyActorDefs.GroupConvoMember>
+}
+
+type DirectConvoMember = ChatBskyActorDefs.ProfileViewBasic & {
+  kind: $Typed<ChatBskyActorDefs.DirectConvoMember>
+}
+
+export type ConvoWithDetails = {view: ChatBskyConvoDefs.ConvoView} & (
+  | {
+      kind: 'group'
+      details: ChatBskyConvoDefs.GroupConvo
+      primaryMember: GroupConvoMember // the owner
+      members: Array<GroupConvoMember>
+    }
+  | {
+      kind: 'direct'
+      details: ChatBskyConvoDefs.DirectConvo
+      primaryMember: DirectConvoMember // the other user
+      members: Array<DirectConvoMember>
+    }
+)
+
+/**
+ * Converts a raw convoView into something easier to use (i.e. extracts chat owner)
+ */
+export function parseConvoView(
+  convo: ChatBskyConvoDefs.ConvoView,
+  ownDid?: string,
+): ConvoWithDetails {
+  if (
+    bsky.dangerousIsType<ChatBskyConvoDefs.GroupConvo>(
+      convo.kind,
+      ChatBskyConvoDefs.isGroupConvo,
+    )
+  ) {
+    let owner: GroupConvoMember | undefined = undefined
+
+    for (const member of convo.members) {
+      if (
+        bsky.dangerousIsType<ChatBskyActorDefs.GroupConvoMember>(
+          member.kind,
+          ChatBskyActorDefs.isGroupConvoMember,
+        )
+      ) {
+        if (member.kind.role === 'owner') {
+          // have to do a type assertion here
+          // this works: {...member, kind: member.kind}
+          // however that's creating a new object for no good reason
+          owner = member as GroupConvoMember
+        }
+      } else {
+        throw new Error(
+          'Expected a GroupConvoMember, got an unknown kind of member',
+        )
+      }
+    }
+
+    if (!owner) {
+      throw new Error('No owner found in group convo')
+    }
+
+    return {
+      convo,
+      kind: 'group',
+      details: convo.kind,
+      primaryMember: owner,
+      members: convo.members as Array<GroupConvoMember>,
+    }
+  } else if (
+    bsky.dangerousIsType<ChatBskyConvoDefs.DirectConvo>(
+      convo.kind,
+      ChatBskyConvoDefs.isDirectConvo,
+    )
+  ) {
+    const otherUser = convo.members.find(m => m.did !== ownDid)
+
+    if (
+      !bsky.dangerousIsType<ChatBskyActorDefs.DirectConvoMember>(
+        otherUser?.kind,
+        ChatBskyActorDefs.isDirectConvoMember,
+      )
+    ) {
+      throw new Error('No other user found in direct convo')
+    }
+
+    return {
+      convo,
+      kind: 'direct',
+      details: convo.kind,
+      primaryMember: otherUser as DirectConvoMember,
+      members: convo.members as Array<DirectConvoMember>,
+    }
+  }
+  throw new Error(`Unknown convo kind: ${JSON.stringify(convo.kind)}`)
 }
