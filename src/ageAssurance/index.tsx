b9c3e0ff9ff1c4eb018d86a16a187733be399389
Eric Bailey <git@esb.lol>
2026-04-20 15:31:40 -0500

    Remove unecessary callback dependency

--- src/ageAssurance/index.tsx
+++ src/ageAssurance/index.tsx
@@ -94,7 +94,7 @@ function InnerProvider({children}: {children: React.ReactNode}) {
         maybeRestrictChatSettings({agent})
       }
     },
-    [agent, data, getAndRegisterPushToken],
+    [agent, getAndRegisterPushToken],
   )
   useOnAgeAssuranceAccessUpdate(handleAccessUpdate)
 
