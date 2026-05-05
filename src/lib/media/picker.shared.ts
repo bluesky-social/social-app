05629edcc9457461ac6b86ba8acdadf6212ccd60
Samuel Newman <mozzius@protonmail.com>
2026-05-06 01:25:45 +0300

    add comment

--- src/lib/media/picker.shared.ts
+++ src/lib/media/picker.shared.ts
@@ -57,6 +57,9 @@ export async function openUnifiedPicker({
     legacy: true,
     base64: IS_WEB,
     selectionLimit: IS_IOS ? selectionCountRemaining : undefined,
+    // important: anything other than `Current` can cause the image picker to appear
+    // to hang after selecting a video as it might attempt to silently transcode it before
+    // returning anything, which can take ages. -sfn
     preferredAssetRepresentationMode:
       UIImagePickerPreferredAssetRepresentationMode.Current,
     videoMaxDuration: VIDEO_MAX_DURATION_MS / 1000,
