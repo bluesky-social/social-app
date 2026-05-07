41cde2c91c8280650f0e69d185435056a28f4cac
Rudy Fraser <him@rudyfraser.com>
2026-05-07 17:40:24 -0400

    Add 'Powered by GIPHY' attribution mark to picker header
    GIPHY's terms require apps using their API to display the attribution.
    Added small horizontal logos (light + dark variants) under the search
    input.

---
+++ src/features/gifPicker/components/PoweredByGiphy.tsx
@@ -0,0 +1,32 @@
+import {Image} from 'expo-image'
+import {useLingui} from '@lingui/react/macro'
+
+import {atoms as a, useTheme} from '#/alf'
+
+const ASSET_DARK = require('../../../../assets/images/giphy_attribution_dark.png')
+const ASSET_LIGHT = require('../../../../assets/images/giphy_attribution_light.png')
+
+const HEIGHT = 16
+const ASPECT_RATIO = 200 / 28
+
+export function PoweredByGiphy() {
+  const t = useTheme()
+  const {t: l} = useLingui()
+  const source = t.name === 'light' ? ASSET_LIGHT : ASSET_DARK
+  return (
+    <Image
+      source={source}
+      contentFit="contain"
+      style={[
+        a.self_end,
+        {
+          height: HEIGHT,
+          width: HEIGHT * ASPECT_RATIO,
+        },
+      ]}
+      accessibilityLabel={l`Powered by GIPHY`}
+      accessibilityHint=""
+      accessibilityIgnoresInvertColors
+    />
+  )
+}
