17b8797b0850d0d93cf0123dc0720558914bf9ae
Samuel Newman <mozzius@protonmail.com>
2026-05-19 10:04:22 +0300

    migrate to ESM atproto packages, remove jank in configs

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,7 @@
 const {getSentryExpoConfig} = require('@sentry/react-native/metro')
 const cfg = getSentryExpoConfig(__dirname)
 
+// inject `.e2e.ts` and `.e2e.tsx` into the sourceExts when running tests
 cfg.resolver.sourceExts = process.env.RN_SRC_EXT
   ? process.env.RN_SRC_EXT.split(',').concat(cfg.resolver.sourceExts)
   : cfg.resolver.sourceExts
@@ -16,25 +17,7 @@ if (process.env.BSKY_PROFILE) {
 
 cfg.resolver.assetExts = [...cfg.resolver.assetExts, 'woff2']
 
-// Enabled by default in RN 0.79+, but this breaks Lingui + others
-cfg.resolver.unstable_enablePackageExports = false
-
 cfg.resolver.resolveRequest = (context, moduleName, platform) => {
-  // HACK: manually resolve a few packages that use `exports` in `package.json`.
-  // A proper solution is to enable `unstable_enablePackageExports` but this needs careful testing.
-  if (moduleName.startsWith('multiformats/hashes/hasher')) {
-    return context.resolveRequest(
-      context,
-      'multiformats/cjs/src/hashes/hasher',
-      platform,
-    )
-  }
-  if (moduleName.startsWith('multiformats/cid')) {
-    return context.resolveRequest(context, 'multiformats/cjs/src/cid', platform)
-  }
-  if (moduleName === '@ipld/dag-cbor') {
-    return context.resolveRequest(context, '@ipld/dag-cbor/src', platform)
-  }
   if (process.env.BSKY_PROFILE) {
     if (moduleName.endsWith('ReactNativeRenderer-prod')) {
       return context.resolveRequest(
@@ -51,16 +34,6 @@ cfg.transformer.getTransformOptions = async () => ({
   transform: {
     experimentalImportSupport: true,
     inlineRequires: true,
-    nonInlinedRequires: [
-      // We can remove this option and rely on the default after
-      // https://github.com/facebook/metro/pull/1390 is released.
-      'React',
-      'react',
-      'react-compiler-runtime',
-      'react/jsx-dev-runtime',
-      'react/jsx-runtime',
-      'react-native',
-    ],
   },
 })
 
