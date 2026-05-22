e164c6aa9a2ce829151f7f1172bbc9e30e598988
DS Boyce <260543580+ds-boyce@users.noreply.github.com>
2026-05-22 10:16:34 -0700

    Add labels

--- src/screens/Messages/components/MessagesListInfoPanel.tsx
+++ src/screens/Messages/components/MessagesListInfoPanel.tsx
@@ -1,10 +1,12 @@
 import {View} from 'react-native'
+import {moderateProfile} from '@atproto/api'
 import {Trans, useLingui} from '@lingui/react/macro'
 import {useNavigation} from '@react-navigation/native'
 
 import {createSanitizedDisplayName} from '#/lib/moderation/create-sanitized-display-name'
 import {type NavigationProp} from '#/lib/routes/types'
 import {isInvalidHandle, sanitizeHandle} from '#/lib/strings/handles'
+import {useModerationOpts} from '#/state/preferences/moderation-opts'
 import {useSession} from '#/state/session'
 import {UserAvatar} from '#/view/com/util/UserAvatar'
 import {atoms as a, useTheme} from '#/alf'
@@ -12,6 +14,7 @@ import {Button, ButtonIcon, ButtonText} from '#/components/Button'
 import {type ConvoWithDetails} from '#/components/dms/util'
 import {Person_Stroke2_Corner2_Rounded as PersonIcon} from '#/components/icons/Person'
 import {ProfileBadges} from '#/components/ProfileBadges'
+import * as ProfileCard from '#/components/ProfileCard'
 import {Text} from '#/components/Typography'
 
 export function MessagesListInfoPanel({
@@ -24,12 +27,19 @@ export function MessagesListInfoPanel({
   const {t: l} = useLingui()
 
   const {currentAccount} = useSession()
+  const moderationOpts = useModerationOpts()
 
   const profile = convo.members.filter(
     profile => profile.did !== currentAccount?.did,
   )[0]
-  const displayName = createSanitizedDisplayName(profile, true)
   const handle = sanitizeHandle(profile.handle, '@')
+  const displayName = moderationOpts
+    ? createSanitizedDisplayName(
+        profile,
+        true,
+        moderateProfile(profile, moderationOpts).ui('displayName'),
+      )
+    : handle
   const profileLink =
     profile.handle && !isInvalidHandle(profile.handle)
       ? profile.handle
@@ -54,6 +64,14 @@ export function MessagesListInfoPanel({
       <Text style={[a.text_sm, a.mt_xs, t.atoms.text_contrast_high]}>
         {handle}
       </Text>
+      {moderationOpts ? (
+        <View style={[a.mt_xs]}>
+          <ProfileCard.Labels
+            profile={profile}
+            moderationOpts={moderationOpts}
+          />
+        </View>
+      ) : null}
       <View
         style={[
           a.flex_row,
