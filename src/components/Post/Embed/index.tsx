d72081da3b2a15f835b6497e62151a27cda25d38
Eric Bailey <git@esb.lol>
2026-06-04 21:12:49 -0500

    Just don't use the component yet

--- src/components/Post/Embed/index.tsx
+++ src/components/Post/Embed/index.tsx
@@ -37,7 +37,6 @@ import {
 import {ChatInviteEmbed} from './ChatInviteEmbed'
 import {ExternalEmbed} from './ExternalEmbed'
 import {ModeratedFeedEmbed} from './FeedEmbed'
-import {GalleryFallbackEmbed} from './GalleryFallbackEmbed'
 import {ImageEmbed} from './ImageEmbed'
 import {ModeratedListEmbed} from './ListEmbed'
 import {PostPlaceholder as PostPlaceholderText} from './PostPlaceholder'
@@ -91,7 +90,8 @@ function MediaEmbed({
   embed: TEmbed
 }) {
   switch (embed.type) {
-    case 'images': {
+    case 'images':
+    case 'gallery': {
       return (
         <ContentHider
           modui={rest.moderation?.ui('contentMedia')}
@@ -147,15 +147,6 @@ function MediaEmbed({
           modui={rest.moderation?.ui('contentMedia')}
           activeStyle={[a.mt_sm]}>
           <VideoEmbed embed={embed.view} />
-        </ContentHider>
-      )
-    }
-    case 'gallery': {
-      return (
-        <ContentHider
-          modui={rest.moderation?.ui('contentMedia')}
-          activeStyle={[a.mt_sm]}>
-          <GalleryFallbackEmbed count={embed.view.items.length} />
         </ContentHider>
       )
     }
