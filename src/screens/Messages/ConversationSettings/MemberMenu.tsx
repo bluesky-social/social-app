0c89198ded5e57ec5f71ec9e5dcc0d7ce957674b
DS Boyce <260543580+ds-boyce@users.noreply.github.com>
2026-06-08 12:14:51 -0700

    Remove TODO for uninviting group chat members (#10789)

--- src/screens/Messages/ConversationSettings/MemberMenu.tsx
+++ src/screens/Messages/ConversationSettings/MemberMenu.tsx
@@ -38,7 +38,7 @@ export function MemberMenu({
 }: {
   convo: ConvoWithDetails
   profile: Shadow<bsky.profile.AnyProfileView>
-  type: 'owner' | 'standard' | 'invited'
+  type: 'owner' | 'standard'
   displayName: string
   isOwner: boolean
 }) {
@@ -128,10 +128,7 @@ export function MemberMenu({
   }
 
   const canBlockMember = type === 'owner' || type === 'standard'
-  const canRemoveMember = isOwner && type !== 'invited'
-  // TODO Need to integrate this. -dsb
-  const canUninviteMember = false
-  // const canUninviteMember = isOwner && type === 'invited'
+  const canRemoveMember = isOwner
 
   return (
     <>
@@ -147,7 +144,7 @@ export function MemberMenu({
                 props.onPress()
               },
             }
-            return type === 'owner' || type === 'invited' ? (
+            return type === 'owner' ? (
               <StatusBadge
                 label={type === 'owner' ? l`Admin` : l`Invited`}
                 pressableProps={triggerProps}
@@ -231,18 +228,6 @@ export function MemberMenu({
                 <Menu.ItemIcon icon={ArrowBoxLeftIcon} />
                 <Menu.ItemText>
                   <Trans>Remove from chat</Trans>
-                </Menu.ItemText>
-              </Menu.Item>
-            ) : null}
-            {canUninviteMember ? (
-              <Menu.Item
-                destructive
-                label={l`Uninvite ${displayName} from this group chat`}
-                // TODO Need to wire up the uninvite flow. -dsb
-                onPress={() => {}}>
-                <Menu.ItemIcon icon={ArrowBoxLeftIcon} />
-                <Menu.ItemText>
-                  <Trans>Uninvite</Trans>
                 </Menu.ItemText>
               </Menu.Item>
             ) : null}
