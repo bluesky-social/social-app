cd47b854aae56949857b00cd9c38665daa33e50b
DS Boyce <260543580+ds-boyce@users.noreply.github.com>
2026-06-24 15:27:05 -0700

    Use correct unread request cap

--- src/screens/Messages/components/InboxRequests.tsx
+++ src/screens/Messages/components/InboxRequests.tsx
@@ -1,13 +1,11 @@
 import {plural} from '@lingui/core/macro'
 import {useLingui} from '@lingui/react/macro'
 
+import {UNREAD_REQUEST_CAP} from '#/state/queries/messages/get-unread-counts'
 import {atoms as a} from '#/alf'
 import {ButtonIcon, ButtonText} from '#/components/Button'
 import {Inbox_Stroke2_Corner2_Rounded as InboxIcon} from '#/components/icons/Inbox'
 import {Link} from '#/components/Link'
-
-// The server caps unreadRequestConvos at 11, where 11 means "any more than 10".
-const REQUEST_COUNT_CAP = 11
 
 export function InboxRequests({
   count,
@@ -21,7 +19,7 @@ export function InboxRequests({
   const {t: l} = useLingui()
 
   const unread = count > 0
-  const overflow = count >= REQUEST_COUNT_CAP
+  const overflow = count >= UNREAD_REQUEST_CAP
 
   const label = !unread
     ? l({
