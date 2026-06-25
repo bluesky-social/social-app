f0f5c6de6c1b7a6982f4d1edf6b0fadc83ac6172
Oleksii Bulenok <oleksii.bulenok@swmansion.com>
2026-06-25 13:20:42 +0200

    temp changes for testflight testing

--- app.config.js
+++ app.config.js
@@ -31,7 +31,7 @@ module.exports = function (_config) {
     ...(IS_DEV || IS_TESTFLIGHT ? [] : []),
   ]
 
-  const UPDATES_ENABLED = IS_TESTFLIGHT || IS_PRODUCTION
+  const UPDATES_ENABLED = false // TODO: revert before merging // IS_TESTFLIGHT || IS_PRODUCTION
 
   const USE_SENTRY = Boolean(process.env.SENTRY_AUTH_TOKEN)
 
