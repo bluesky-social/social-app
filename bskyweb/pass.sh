00b4ef749fecb80698d3f46bc00b3d2d9fb393e1
vineyardbovines <spencerfpope@gmail.com>
2026-06-26 10:43:58 -0400

    chore: untrack local pass.sh test script

--- bskyweb/pass.sh
+++
@@ -1,9 +0,0 @@
-HANDLE="thepope.dev"
-read -s -p "App password: " PW; echo
-
-JWT=$(curl -s -X POST https://bsky.social/xrpc/com.atproto.server.createSession \
-  -H "Content-Type: application/json" \
-  -d "{\"identifier\":\"$HANDLE\",\"password\":\"$PW\"}" \
-  | jq -r .accessJwt)
-
-echo "Got JWT (length: ${#JWT})"
