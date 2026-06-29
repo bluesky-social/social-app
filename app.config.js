9ed618c68a7b101b3316c8a29003631d697b43e2
Samuel Newman <mozzius@protonmail.com>
2026-06-29 11:47:58 +0300

    set apple team id via env var

--- app.config.js
+++ app.config.js
@@ -59,6 +59,7 @@ module.exports = function (_config) {
       ios: {
         supportsTablet: false,
         bundleIdentifier: 'xyz.blueskyweb.app',
+        appleTeamId: process.env.EXPO_APPLE_TEAM_ID,
         config: {
           usesNonExemptEncryption: false,
         },
