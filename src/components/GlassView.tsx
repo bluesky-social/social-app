385c6153ddcec292881868c6abce71de03d0c0d2
Oleksii Bulenok <oleksii.bulenok@swmansion.com>
2026-07-03 12:47:54 +0200

    Add comment

--- src/components/GlassView.tsx
+++ src/components/GlassView.tsx
@@ -15,6 +15,8 @@ export const IS_GLASS_AVAILABLE =
  * Liquid Glass View that uses `expo-glass-effect`
  *
  * If unavailable, falls back to a regular `View`. Use `fallbackStyle` to customize the fallback appearance.
+ * Note: Setting opacity to 0 on Expo GlassView or any of its parent views causes the glass effect to not render at all. https://docs.expo.dev/versions/v56.0.0/sdk/glass-effect/#known-issues
+ * If animating the opacity of a parent view, start from a non-zero opacity to avoid this issue.
  */
 export const GlassView = IS_GLASS_AVAILABLE ? InnerGlassView : FallbackView
 
