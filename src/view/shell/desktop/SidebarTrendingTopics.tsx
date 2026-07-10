0ab87e708af65ff3ff0249115b56154cb0b4f483
DS Boyce <260543580+ds-boyce@users.noreply.github.com>
2026-07-10 13:47:36 -0700

    Fix duplicate import

--- src/view/shell/desktop/SidebarTrendingTopics.tsx
+++ src/view/shell/desktop/SidebarTrendingTopics.tsx
@@ -1,6 +1,5 @@
 import {View} from 'react-native'
-import {useLingui} from '@lingui/react/macro'
-import {Trans} from '@lingui/react/macro'
+import {Trans, useLingui} from '@lingui/react/macro'
 
 import {
   useTrendingSettings,
