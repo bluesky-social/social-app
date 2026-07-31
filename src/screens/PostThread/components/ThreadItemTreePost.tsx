236013b5fc0c7c6401c448c1e706102c8a33c6f2
DS Boyce <260543580+ds-boyce@users.noreply.github.com>
2026-07-30 17:03:53 -0700

    Revert "Tweak padding for threaded posts"
    This reverts commit e1345f240289777cbd6780f9613f10cc221a3ba8.

--- src/screens/PostThread/components/ThreadItemTreePost.tsx
+++ src/screens/PostThread/components/ThreadItemTreePost.tsx
@@ -100,7 +100,7 @@ function ThreadItemTreePostDeleted({
             t.atoms.bg_contrast_25,
             {
               gap: 6,
-              paddingHorizontal: OUTER_SPACE,
+              paddingHorizontal: OUTER_SPACE / 2,
               height: TREE_AVI_WIDTH,
             },
           ]}>
@@ -180,13 +180,13 @@ const ThreadItemTreePostInnerWrapper = memo(
             paddingTop: OUTER_SPACE / 2,
           },
           item.ui.indent === 1 && [
-            !item.ui.showParentReplyLine && {paddingTop: OUTER_SPACE},
+            !item.ui.showParentReplyLine && {paddingTop: OUTER_SPACE / 1.5},
             !item.ui.showChildReplyLine && a.pb_sm,
           ],
           item.ui.isLastChild &&
             !item.ui.precedesChildReadMore && [
               {
-                paddingBottom: OUTER_SPACE,
+                paddingBottom: OUTER_SPACE / 2,
               },
             ],
         ]}>
@@ -417,7 +417,7 @@ export function ThreadItemTreePostSkeleton({index}: {index: number}) {
   return (
     <View
       style={[
-        {paddingHorizontal: OUTER_SPACE, paddingVertical: OUTER_SPACE},
+        {paddingHorizontal: OUTER_SPACE, paddingVertical: OUTER_SPACE / 1.5},
         a.border_t,
         t.atoms.border_contrast_low,
       ]}>
