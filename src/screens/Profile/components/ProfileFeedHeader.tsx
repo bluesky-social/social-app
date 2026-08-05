399c4fb1b3f982b9c20f36d27f16fd67f6ac7349
Samuel Newman <mozzius@protonmail.com>
2026-08-05 20:46:16 +0300

    make profilefeedheader preventExpansion

--- src/screens/Profile/components/ProfileFeedHeader.tsx
+++ src/screens/Profile/components/ProfileFeedHeader.tsx
@@ -395,7 +395,9 @@ export function ProfileFeedHeader({
           ) : null}
         </Layout.Header.Outer>
       </Layout.Center>
-      <Dialog.Outer control={infoControl}>
+      <Dialog.Outer
+        control={infoControl}
+        nativeOptions={{preventExpansion: true}}>
         <Dialog.Handle />
         <Dialog.ScrollableInner
           label={l`Feed menu`}
