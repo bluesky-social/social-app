f31c1c6c2a4cd60a5b9a81a6adc4531caf454b93
Samuel Newman <mozzius@protonmail.com>
2026-08-10 17:05:14 +0300

    fix import order

--- src/components/Pills.tsx
+++ src/components/Pills.tsx
@@ -1,8 +1,8 @@
 import {useMemo} from 'react'
 import {View} from 'react-native'
-import {Trans, useLingui} from '@lingui/react/macro'
 import {BSKY_LABELER_DID} from '@atproto/api'
 import {type ModerationCause} from '@bsky.app/sdk/moderation'
+import {Trans, useLingui} from '@lingui/react/macro'
 
 import {useModerationCauseDescription} from '#/lib/moderation/useModerationCauseDescription'
 import {UserAvatar} from '#/view/com/util/UserAvatar'
