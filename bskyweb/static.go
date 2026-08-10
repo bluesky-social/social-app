8b7b5d866818a5643c55c7bc8ef4f43dc726a8cb
Oleksii Bulenok <oleksii.bulenok@swmansion.com>
2026-08-11 00:34:57 +0200

    dockerfile builds

--- bskyweb/static.go
+++ bskyweb/static.go
@@ -2,7 +2,10 @@ package bskyweb
 
 import "embed"
 
-//go:embed static/*
+// `all:` disables the default exclusion of files/dirs beginning with `_` or `.`,
+// which is needed because Metro emits chunks like `__common-...js` and
+// `__expo-metro-runtime-...js`.
+//go:embed all:static
 var StaticFS embed.FS
 
 //go:embed embedr-static/*
