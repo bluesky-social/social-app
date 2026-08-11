19be24aded372863b8890f3055a44c8a34f21f5a
vineyardbovines <spencerfpope@gmail.com>
2026-08-11 09:12:46 -0400

    Update algorithmic visibility copy

--- src/screens/Settings/components/AlgoVisibilityOptOut.tsx
+++ src/screens/Settings/components/AlgoVisibilityOptOut.tsx
@@ -96,9 +96,9 @@ export function AlgoVisibilityOptOut() {
 
       <Text style={[a.leading_snug, t.atoms.text_contrast_high]}>
         <Trans>
-          Bluesky will not show your posts in algorithmic recommendations and
-          will ask other apps to do the same. Your posts may still appear in the
-          Discover feed, but only to your followers.
+          Bluesky will not show your posts in the Discover feed (except to your
+          followers) and will ask other apps not to show your posts in their own
+          algorithmic recommendations.
         </Trans>
       </Text>
     </View>
