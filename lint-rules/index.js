3898aeaf277d592b55aff45ec8f101315af325ea
Samuel Newman <mozzius@protonmail.com>
2026-08-13 16:42:20 +0300

    drop eslint dependency

--- lint-rules/index.js
+++ lint-rules/index.js
@@ -7,7 +7,6 @@ const plugin = {
   },
   rules: {
     'avoid-unwrapped-text': require('./avoid-unwrapped-text'),
-    'use-exact-imports': require('./use-exact-imports'),
     'use-prefixed-imports': require('./use-prefixed-imports'),
     'lingui-msg-rule': require('./lingui-msg-rule'),
   },
