dbe2d15cb5643f2e3adabd23d584e7e9a7da6ff2
Oleksii Bulenok <oleksii.bulenok@swmansion.com>
2026-08-18 16:41:49 +0200

    lint

--- src/view/com/home/HomeHeaderLayoutMobile.tsx
+++ src/view/com/home/HomeHeaderLayoutMobile.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native'
+import {Text, View} from 'react-native'
 import Animated from 'react-native-reanimated'
 import {useSafeAreaInsets} from 'react-native-safe-area-context'
 import {msg} from '@lingui/core/macro'
