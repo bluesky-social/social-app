361569bcd884717e407f9c44499f42d928c6168e
Eric Bailey <git@esb.lol>
2026-08-26 18:31:34 -0500

    Remove feature gate

--- src/analytics/features/types.ts
+++ src/analytics/features/types.ts
@@ -22,7 +22,6 @@ export enum Features {
   FollowSortEnable = 'follow_sort:enable',
   OnboardingInterestsRequiredEnable = 'onboarding:interests:required:enable',
   CanonicalPostNumberingEnable = 'canonical_post_numbering:enable',
-  ContentVisibilitySettingsEnable = 'content_visibility_settings:enable',
   ModerationInboxEnable = 'moderation_inbox:enable',
 
   // values
