50bf97d0698f2041cd19acd274d9f86dac7c1201
Samuel Newman <mozzius@protonmail.com>
2026-08-31 15:50:24 +0300

    handle expo fetch errors

--- src/lib/strings/errors.ts
+++ src/lib/strings/errors.ts
@@ -83,6 +83,7 @@ const NETWORK_ERRORS = [
   'Abort',
   'Network request failed',
   'Failed to fetch',
+  'fetch failed',
   'Load failed',
   'Upstream service unreachable',
   'NetworkError when attempting to fetch resource',
