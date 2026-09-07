a4f3efa1bc4a885f98c198d747097a3373d67a0c
Samuel Newman <mozzius@protonmail.com>
2026-09-07 10:11:08 +0300

    surface growthbook flags in sentry

--- src/analytics/index.tsx
+++ src/analytics/index.tsx
@@ -9,6 +9,7 @@ import {Platform} from 'react-native'
 import {type Result, type WidenPrimitives} from '@growthbook/growthbook-react'
 
 import {Logger} from '#/logger'
+import {recordFeatureFlagEvaluation} from '#/logger/sentry/featureFlags'
 import {
   Features,
   features as feats,
@@ -313,6 +314,7 @@ export function AnalyticsFeaturesContext({
         ? sessionMetadataForResult(parentContext, result.experimentResult)
         : undefined,
     )
+    recordFeatureFlagEvaluation(feature, result.value)
   })
   setAttributes(parentContext.metadata)
 
