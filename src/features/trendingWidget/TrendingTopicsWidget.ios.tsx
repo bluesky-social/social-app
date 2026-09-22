9a11220e00aa2c14c2db6f6be14f6938d16c39b9
Samuel Newman <mozzius@protonmail.com>
2026-09-22 21:43:27 +0300

    adjust widget padding

--- src/features/trendingWidget/TrendingTopicsWidget.ios.tsx
+++ src/features/trendingWidget/TrendingTopicsWidget.ios.tsx
@@ -130,7 +130,7 @@ function TrendingTopicsWidget(
       modifiers={[
         containerBackground(backgroundColor, 'widget'),
         frame({maxWidth: 1000, maxHeight: 1000, alignment: 'topLeading'}),
-        padding({all: isSmall ? 12 : isMedium ? 14 : 18}),
+        padding({all: isSmall ? 14 : isMedium ? 16 : 18}),
         widgetURL(rootDestination),
       ]}>
       {!isSmall ? (
