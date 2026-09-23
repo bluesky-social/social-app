c0df4eacbfc727bdc9dc86d6e9d69335399b6c81
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2026-09-23 23:39:07 +0200

    Enable `ios.enableSceneSupport` in app.config.js

--- app.config.js
+++ app.config.js
@@ -294,6 +294,7 @@ module.exports = function (_config) {
                   branch: 'main',
                 },
               ],
+              enableSceneSupport: true,
             },
             android: {
               compileSdkVersion: 36,
